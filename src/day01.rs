1ed6e342f7b3cd241ef13db3926ab79e4c0ca8e7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-12-03 08:49:10 +0900

    day 2
    	modified:   Cargo.toml
    	new file:   input-day2.txt
    	new file:   src/day01.rs
    	new file:   src/day02.rs
    	modified:   src/main.rs

---
+++ src/day01.rs
@@ -0,0 +1,27 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    let mut v: Vec<usize> = Vec::new();
+    io::stdin().read_to_string(&mut buffer).expect("something wrong");
+    for s in buffer.lines() {
+        v.push(s.parse::<usize>().expect("bad int"));
+    }
+    for i in &v {
+        for j in &v {
+            if i + j == 2020 {
+                println!("{} * {} = {}", i, j, i * j)
+            }
+        }
+    }
+    for (i, x) in v.iter().enumerate() {
+        for (j, y) in v.iter().enumerate().skip(i) {
+            for z in v.iter().skip(j) {
+                if x + y + z == 2020 {
+                    println!("{} * {} * {} = {}", x, y, z, x * y * z);
+                }
+            }
+        }
+    }
+    
+}
