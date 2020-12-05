3ad81a939e1b66ec90e8f314398b6d7429a1f13e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-12-05 14:27:47 +0900

    day 05
    	new file:   input-day5-test.txt
    	new file:   input-day5.txt
    	new file:   src/day05.rs

---
+++ src/day05.rs
@@ -0,0 +1,41 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use {
+    lazy_static::lazy_static,
+    regex::Regex,
+    std::{
+        collections::HashMap,
+        io::{self, Read},
+    },
+};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("something wrong");
+
+    let mut max_sid = 0;
+    let mut seats = [0; 128 * 8 + 1];
+
+    for code in buffer.split('\n') {
+        if code.is_empty() { break; }
+        let (row_code, col_code) = code.split_at(7);
+        let row = row_code.chars().map(|c| (c == 'B') as usize).collect::<Vec<_>>();
+        let col = col_code.chars().map(|c| (c == 'R') as usize).collect::<Vec<_>>();
+        let r = row.iter().fold(0, |t, n| t * 2 + n);
+        let c = col.iter().fold(0, |t, n| t * 2 + n);
+        let sid = r * 8 + c;
+        dbg!((&code, r, c, sid));
+        seats[sid] = 1;
+        if max_sid < sid {
+            max_sid = sid;
+        }
+    }
+    dbg!(max_sid);
+    for (i, e) in seats.iter().enumerate() {
+        if *e == 0 && 7 < i && i < 126 * 8 {
+            dbg!(i);
+        }
+    }
+}
