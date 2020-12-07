0cd4c5a3b295d859d4bcc2aa819a3cabf968d12a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-12-08 01:23:20 +0900

    day 07
    	renamed:    data/input-day1.txt -> data/input-day01.txt
    	renamed:    data/input-day2.txt -> data/input-day02.txt
    	renamed:    data/input-day3-test.txt -> data/input-day03-test.txt
    	renamed:    data/input-day3.txt -> data/input-day03.txt
    	renamed:    data/input-day4-test.txt -> data/input-day04-test.txt
    	renamed:    data/input-day4-test1.txt -> data/input-day04-test1.txt
    	renamed:    data/input-day4-test2.txt -> data/input-day04-test2.txt
    	renamed:    data/input-day4.txt -> data/input-day04.txt
    	renamed:    data/input-day5-test.txt -> data/input-day05-test.txt
    	renamed:    data/input-day5.txt -> data/input-day05.txt
    	renamed:    data/input-day6-test.txt -> data/input-day06-test.txt
    	renamed:    data/input-day6.txt -> data/input-day06.txt
    	new file:   data/input-day07-test.txt
    	new file:   data/input-day07-test2.txt
    	new file:   data/input-day07.txt
    	new file:   src/day07.rs

---
+++ src/day07.rs
@@ -0,0 +1,68 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use {
+    lazy_static::lazy_static,
+    regex::Regex,
+    std::{
+        collections::HashSet,
+        io::{self, Read},
+    },
+};
+
+fn most_outer_of(links: &HashSet<(String, String, usize)>, origin: &str, outers: &mut HashSet<String>) {
+    for (from, to, _) in links {
+        if to == origin {
+            outers.insert(from.to_string());
+            most_outer_of(links, &from, outers);
+        }
+    }
+}
+
+fn collects(links: &HashSet<(String, String, usize)>, origin: &str) -> usize {
+    let mut count = 0;
+    for (from, to, n) in links {
+        if from == origin {
+            count += n * collects(links, &to);
+        }
+    }
+    count + 1
+}
+
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("something wrong");
+
+    let mut links: HashSet<(String, String, usize)> = HashSet::new();
+
+    for c in buffer.split("\n") {
+        parse(&mut links, c);
+    }
+    let mut outers: HashSet<String> = HashSet::new();
+    most_outer_of(&links, "shiny gold", &mut outers);
+    dbg!(outers.len());
+    dbg!(collects(&links, "shiny gold") - 1);
+}
+
+fn parse(dic: &mut HashSet<(String, String, usize)>, line: &str) {
+    lazy_static! {
+        static ref HEAD: Regex = Regex::new(r"^([a-z]+ [a-z]+) bags? contain (.*)").expect("wrong");
+        static ref PREP: Regex = Regex::new(r"(\d+) ([a-z]+ [a-z]+) bags?(, (.*))?").expect("wrong");
+    }
+    if let Some(head) = HEAD.captures(line) {
+        let mut b: String = head[2].to_string();
+        while let Some(prep) = PREP.captures(&b) {
+            dic.insert((head[1].to_string(), prep[2].to_string(), prep[1].parse::<usize>().unwrap()));
+            if let Some(rest) = prep.get(4) {
+                b = rest.as_str().to_string();
+                if b == "." {
+                    break;
+                }
+            } else {
+                break;
+            } 
+        }
+    }
+}
