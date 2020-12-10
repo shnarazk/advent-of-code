fa92713e4824b3fa55d39d0af78dfabe05a37e0b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-12-10 14:28:37 +0900

    day 10
    	new file:   data/input-day10-test1.txt
    	new file:   data/input-day10-test2.txt
    	new file:   data/input-day10.txt
    	new file:   src/day10.rs

---
+++ src/day10.rs
@@ -0,0 +1,54 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use std::io::{self, Read};
+
+fn main() {
+    let mut buffer = String::new();
+    io::stdin()
+        .read_to_string(&mut buffer)
+        .expect("something wrong");
+
+    let mut vec: Vec<usize> = Vec::new();
+    vec.push(0);
+
+    for c in buffer.split("\n") {
+        if c.is_empty() {
+            break;
+        }
+        if let Ok(n) = c.parse::<usize>() {
+            vec.push(n);
+        } else {
+            panic!("wrong number");
+        }
+    }
+    vec.sort_unstable();
+    // dbg!(&vec);
+    let mut diff1 = 0;
+    let mut diff3 = 0;
+    for i in 1..vec.len() {
+        match vec[i] - vec[i - 1] {
+            1 => diff1 += 1,
+            3 => diff3 += 1,
+            _ => panic!("wrong"),
+        }
+    }
+    diff3 += 1;
+    assert_eq!(vec.len(), diff1 + diff3);
+    dbg!((diff1, diff3, diff1 * diff3));
+    let mx = *vec.last().unwrap();
+    let mut count: Vec<usize> = Vec::new();
+    count.push(1);
+    for _ in 0..mx {
+        count.push(0);
+    }
+    for n in &vec[1..] {
+        count[*n] += count[*n - 1];
+        if 2 <= *n {
+            count[*n] += count[*n - 2];
+        }
+        if 3 <= *n {
+            count[*n] += count[*n - 3];
+        }
+    }
+    dbg!(count.last());
+}
