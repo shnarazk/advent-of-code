4210d2d2eaa39a60868285ebd84b9738c197f599
Narazaki, Shuji <shujinarazaki@protonmail.com>
2020-12-31 10:36:29 +0900

    update day 13
    	modified:   src/bin/adventofcode2020.rs
    	modified:   src/day13.rs

--- src/day13.rs
+++ src/day13.rs
@@ -1,46 +1,79 @@
-#![allow(dead_code)]
-#![allow(unused_variables)]
-use std::io::{stdin, Read};
+use crate::{Description, ProblemObject, ProblemSolver};
 
-pub fn day13() {
-    let mut buf = String::new();
-    stdin().read_to_string(&mut buf).expect("wrong");
+pub fn day13(part: usize, desc: Description) {
+    dbg!(Setting::parse(desc).run(part));
+}
 
-    let mut vec: Vec<(usize, usize)> = Vec::new();
-    let mut iter = buf.split('\n');
-    let time = iter.next().unwrap().parse::<usize>().unwrap();
-    dbg!(time);
-    let mut wait = usize::MAX;
-    // let mut best = 0;
-    for (i, b) in iter.next().unwrap().split(',').enumerate() {
-        if let Ok(n) = b.parse::<usize>() {
-            vec.push((n, i));
-            let before = time % n;
-            // if before == 0 {
-            //    panic!("found: {}", 0);
-            // }
-            let w = n - before;
-            if w < wait {
-                wait = w;
-                // best = n;
+#[derive(Debug, PartialEq)]
+struct Object {
+    time: usize,
+    bus: Vec<(usize, usize)>,
+}
+
+impl ProblemObject for Object {
+    fn parse(s: &str) -> Option<Self> {
+        let mut bus: Vec<(usize, usize)> = Vec::new();
+        let mut iter = s.split('\n');
+        let time = iter.next().unwrap().parse::<usize>().unwrap();
+        for (i, b) in iter.next().unwrap().split(',').enumerate() {
+            if let Ok(n) = b.parse::<usize>() {
+                bus.push((n, i));
             }
         }
+        Some(Object { time, bus })
     }
-    // dbg!(&vec);
-    let mut x = vec[0];
-    for tuple in vec.iter().skip(1) {
-        let offset = if tuple.1 <= tuple.0 {
-            tuple.0 - tuple.1
-        } else {
-            println!("逆転{}, {}", tuple.0, tuple.1);
-            (tuple.0 * tuple.1 - tuple.1) % tuple.0
-        };
-        println!("周期{}で{}余る", tuple.0, offset);
-        x = chinese(x, (tuple.0, offset));
-        dbg!(x);
-        //assert_eq!(tuple.0 - x.1 % tuple.0, tuple.1);
+}
+
+#[derive(Debug, PartialEq)]
+struct Setting {
+    time: usize,
+    bus: Vec<(usize, usize)>,
+}
+
+impl ProblemSolver<Object, usize, usize> for Setting {
+    const DAY: usize = 13;
+    const DELIMITER: &'static str = "\n\n";
+    fn default() -> Self {
+        Setting {
+            time: 0,
+            bus: Vec::new(),
+        }
     }
-    panic!("found {}", x.1);
+    fn insert(&mut self, mut object: Object) {
+        std::mem::swap(&mut self.time, &mut object.time);
+        std::mem::swap(&mut self.bus, &mut object.bus);
+    }
+    fn part1(&mut self) -> usize {
+        let mut bus: usize = self.bus[0].0;
+        let mut wait = usize::MAX;
+        for (cycle, _) in self.bus.iter() {
+            let before = self.time % cycle;
+            if before == 0 {
+                return 0;
+            }
+            let w = cycle - before;
+            if w < wait {
+                wait = w;
+                bus = *cycle;
+            }
+        }
+        wait * bus
+    }
+    fn part2(&mut self) -> usize {
+        let mut x = self.bus[0];
+        for tuple in self.bus.iter().skip(1) {
+            let offset = if tuple.1 <= tuple.0 {
+                tuple.0 - tuple.1
+            } else {
+                println!("逆転{}, {}", tuple.0, tuple.1);
+                (tuple.0 * tuple.1 - tuple.1) % tuple.0
+            };
+            println!("周期{}で{}余る", tuple.0, offset);
+            x = chinese(x, (tuple.0, offset));
+            //assert_eq!(tuple.0 - x.1 % tuple.0, tuple.1);
+        }
+        x.1
+    }
 }
 
 /// Return `x` such that:
@@ -78,34 +111,33 @@ fn solve1(a: usize, m: usize) -> usize {
     panic!("can't");
 }
 
-fn lcm(a: usize, b: usize) -> usize {
-    a * b / gcd(a, b)
-}
-
-fn gcd(a: usize, b: usize) -> usize {
-    if a % b == 0 {
-        b
-    } else {
-        gcd(b, a % b)
-    }
-}
-
 #[cfg(test)]
 mod test {
-    use super::*;
-    #[test]
-    fn test_lcm() {
-        assert_eq!(lcm(3, 10), 30);
-        assert_eq!(lcm(3, 8), 24);
-        assert_eq!(lcm(3, 9), 9);
-        assert_eq!(lcm(1, 11), 11);
-        assert_eq!(lcm(280, 5928), 207480);
-    }
+    use {
+        super::*,
+        crate::{Answer, Description},
+    };
+
     #[test]
     fn test_chinese() {
         let a = chinese((3, 2), (5, 3));
         assert_eq!(a.1, 8);
         let c = chinese(a, (2, 7));
         assert_eq!(c.1, 23);
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(
+            Setting::parse(Description::FileTag("test".to_string())).run(1),
+            Answer::Part1(295)
+        );
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            Setting::parse(Description::FileTag("test".to_string())).run(2),
+            Answer::Part2(1068781)
+        );
     }
 }
