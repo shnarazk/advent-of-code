84ced5bafb673e3dbe1c39f0602ff18b44b91525
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-05 15:22:20 +0900

    05 done

--- src/y2021/day05.rs
+++ src/y2021/day05.rs
@@ -7,21 +7,33 @@ use regex::Regex;
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)]
-struct DataSegment {}
+struct DataSegment {
+    beg: (usize, usize),
+    end: (usize, usize),
+}
 
 impl TryParse for DataSegment {
-    /// make a `Object` from a string block
     fn parse(s: &str) -> Result<Self, ParseError> {
+        let mut i = DataSegment { beg: (0,0), end: (0, 0) };
         lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+            static ref PARSER: Regex = Regex::new(r"^([0-9]+),([0-9]+) -> ([0-9]+),([0-9]+)$").expect("wrong");
         }
         let segment = PARSER.captures(s).ok_or(ParseError)?;
-        Err(ParseError)
+        i.beg.0 = segment[1].parse::<usize>()?;
+        i.beg.1 = segment[2].parse::<usize>()?;
+        i.end.0 = segment[3].parse::<usize>()?;
+        i.end.1 = segment[4].parse::<usize>()?;
+        Ok(i)
     }
 }
 
 #[derive(Debug, PartialEq)]
-struct Puzzle {}
+struct Puzzle {
+    line: Vec<DataSegment>,
+    max_x: usize,
+    max_y: usize,
+    count: Vec<Vec<usize>>,
+}
 
 impl AdventOfCode for Puzzle {
     type Segment = DataSegment;
@@ -31,9 +43,13 @@ impl AdventOfCode for Puzzle {
     const DAY: usize = 5;
     const DELIMITER: &'static str = "\n";
     fn default() -> Self {
-        Self {}
+        Self {
+            line: Vec::new(),
+            max_x: 0,
+            max_y: 0,
+            count: Vec::new(),
+        }
     }
-    // handle header
     // fn header(&mut self, input: &str) -> Maybe<Option<String>> {
     //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
     //     let segment = parser.captures(input).ok_or(ParseError)?;
@@ -42,48 +58,78 @@ impl AdventOfCode for Puzzle {
     //     }
     //     Ok(Some(segment[2].to_string()))
     // }
-    /// add a data block
+
     fn insert(&mut self, object: Self::Segment) {
-        // self = object;
+        self.line.push(object);
     }
-    // finalize
-    // fn after_insert(&mut self) {}
-    /// solver for part1
+    fn after_insert(&mut self) {
+        self.max_x = self.line.iter().map(|ds| ds.beg.0.max(ds.end.0)).max().unwrap_or(0);
+        self.max_y = self.line.iter().map(|ds| ds.beg.1.max(ds.end.1)).max().unwrap_or(0);
+        for _ in 0..=self.max_y {
+            let mut v = Vec::new();
+            v.resize(self.max_x + 1, 0);
+            self.count.push(v);
+        }
+    }
     fn part1(&mut self) -> usize {
-        0
+        for ds in self.line.iter() {
+            if ds.beg.0 == ds.end.0 {
+                let beg = ds.beg.1.min(ds.end.1);
+                let end = ds.beg.1.max(ds.end.1);
+                for i in beg..=end {
+                    self.count[i][ds.beg.0] += 1;
+                }
+            } else if ds.beg.1 == ds.end.1 {
+                let beg = ds.beg.0.min(ds.end.0);
+                let end = ds.beg.0.max(ds.end.0);
+                for i in beg..=end {
+                    self.count[ds.beg.1][i] += 1;
+                }
+            } else {
+                // dbg!(ds);
+            }
+        }
+        // println!("{:?}", &self.count);
+        self.count.iter().map(|v| v.iter().filter(|x| 1 < **x).count()).sum()
     }
-    /// solver for part2
     fn part2(&mut self) -> usize {
-        0
+        for ds in self.line.iter() {
+            if ds.beg.0 == ds.end.0 {
+                let beg = ds.beg.1.min(ds.end.1);
+                let end = ds.beg.1.max(ds.end.1);
+                for i in beg..=end {
+                    self.count[i][ds.beg.0] += 1;
+                }
+            } else if ds.beg.1 == ds.end.1 {
+                let beg = ds.beg.0.min(ds.end.0);
+                let end = ds.beg.0.max(ds.end.0);
+                for i in beg..=end {
+                    self.count[ds.beg.1][i] += 1;
+                }
+            } else if (ds.beg.0 as isize - ds.end.0 as isize).abs() == (ds.beg.0 as isize - ds.end.0 as isize).abs()  {
+                let mut x:isize = ds.beg.0 as isize;
+                let mut y:isize = ds.beg.1 as isize;
+                let diff_x: isize = (ds.end.0 as isize - ds.beg.0 as isize).signum();
+                let diff_y: isize = (ds.end.1 as isize - ds.beg.1 as isize).signum();
+                for i in 0..=(ds.end.0 as isize - ds.beg.0 as isize).abs() {
+                    self.count[y as usize][x as usize] += 1;
+                    x += diff_x;
+                    y += diff_y;
+                }
+            }
+        }
+        // println!("{:?}", &self.count);
+        self.count.iter().map(|v| v.iter().filter(|x| 1 < **x).count()).sum()
     }
 }
 
 impl TryFrom<Description> for Puzzle {
     type Error = ParseError;
     fn try_from(desc: Description) -> Result<Self, ParseError> {
-        Puzzle::parse(desc)
+        Puzzle::parse(&desc)
     }
 }
 
 pub fn go(part: usize, desc: Description) {
-    dbg!(Puzzle::try_from(desc).expect("failed to parse").run(part));
-}
-
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        const TEST1: &str = "0\n1\n2";
-        assert_eq!(
-            Puzzle::parse(Description::TestData(TEST1.to_string()))
-                .expect("-")
-                .run(1),
-            Answer::Part1(0)
-        );
-    }
+    dbg!(Puzzle::solve(&desc, part));
 }
