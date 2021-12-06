ce2bffb9093d0aeb3d040e6ae40e5d3066a8b240
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-06 19:55:54 +0900

    06 done

--- src/y2021/day06.rs
+++ src/y2021/day06.rs
@@ -7,21 +7,53 @@ use regex::Regex;
 use std::collections::HashMap;
 
 #[derive(Debug, PartialEq)]
-struct DataSegment {}
+struct DataSegment {
+    vec: Vec<usize>,
+}
 
 impl TryParse for DataSegment {
     /// make a `Object` from a string block
     fn parse(s: &str) -> Result<Self, ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+        Ok(DataSegment {
+            vec: s
+                .split(',')
+                .map(|i| {
+                    i.strip_suffix('\n')
+                        .unwrap_or(i)
+                        .parse::<usize>()
+                        .expect("bad num")
+                })
+                .collect::<Vec<usize>>(),
+        })
+    }
+}
+
+fn rotating_go_forward(acum: &mut [usize; 7], index: usize, pre1: &mut usize, pre2: &mut usize) {
+    let matured = *pre2;
+    *pre2 = *pre1;
+    *pre1 = acum[index];
+    acum[index] += matured;
+}
+
+fn go_forward(vec: &mut Vec<usize>) {
+    let mut news = 0;
+    for i in vec.iter_mut() {
+        if *i == 0 {
+            news += 1;
+            *i = 6;
+        } else {
+            *i -= 1;
         }
-        let segment = PARSER.captures(s).ok_or(ParseError)?;
-        Err(ParseError)
+    }
+    for _ in 0..news {
+        vec.push(8);
     }
 }
 
 #[derive(Debug, PartialEq)]
-struct Puzzle {}
+struct Puzzle {
+    vec: Vec<usize>,
+}
 
 impl AdventOfCode for Puzzle {
     type Segment = DataSegment;
@@ -31,7 +63,7 @@ impl AdventOfCode for Puzzle {
     const DAY: usize = 6;
     const DELIMITER: &'static str = "\n";
     fn default() -> Self {
-        Self {}
+        Self { vec: Vec::new() }
     }
     // handle header
     // fn header(&mut self, input: &str) -> Maybe<Option<String>> {
@@ -44,18 +76,33 @@ impl AdventOfCode for Puzzle {
     // }
     /// add a data block
     fn insert(&mut self, object: Self::Segment) {
-        // self = object;
+        self.vec = object.vec;
     }
     // finalize
     // fn after_insert(&mut self) {}
     /// solver for part1
     fn part1(&mut self) -> usize {
-        0
+        let mut vec = self.vec.clone();
+        for _ in 0..80 {
+            go_forward(&mut vec);
+        }
+        vec.len()
     }
     /// solver for part2
     fn part2(&mut self) -> usize {
-        0
-    }
+        let mut acum = [0; 7];
+        for i in self.vec.iter() {
+            acum[*i] += 1;
+        }
+        dbg!(&acum);
+        let mut pre1 = 0;
+        let mut pre2 = 0;
+        for i in 0..256 {
+            rotating_go_forward(&mut acum, i % 7, &mut pre1, &mut pre2);
+            // dbg!(acum.iter().sum::<usize>() + pre1 + pre2);
+        }
+        acum.iter().sum::<usize>() + pre1 + pre2
+    } 
 }
 
 pub fn go(part: usize, desc: Description) {
