c78d5d6de9656fcff1b841ba1de7386fd5f106bc
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-07 14:56:24 +0900

    07 done

--- src/y2021/day07.rs
+++ src/y2021/day07.rs
@@ -1,27 +1,20 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use crate::{AdventOfCode, Description, Maybe, ParseError, TryParse};
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::HashMap;
+use crate::{AdventOfCode, Description, ParseError, TryParse};
 
-#[derive(Debug, PartialEq)]
-struct DataSegment {}
+type DataSegment = Vec<usize>;
 
 impl TryParse for DataSegment {
-    /// make a `Object` from a string block
     fn parse(s: &str) -> Result<Self, ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(s).ok_or(ParseError)?;
-        Err(ParseError)
+        Ok(s.trim()
+            .split(',')
+            .map(|i| i.parse::<usize>().unwrap())
+            .collect())
     }
 }
 
 #[derive(Debug, PartialEq)]
-struct Puzzle {}
+struct Puzzle {
+    line: DataSegment,
+}
 
 impl AdventOfCode for Puzzle {
     type Segment = DataSegment;
@@ -31,30 +24,48 @@ impl AdventOfCode for Puzzle {
     const DAY: usize = 7;
     const DELIMITER: &'static str = "\n";
     fn default() -> Self {
-        Self {}
+        Self { line: Vec::new() }
     }
-    // handle header
-    // fn header(&mut self, input: &str) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
-    /// add a data block
     fn insert(&mut self, object: Self::Segment) {
-        // self = object;
+        self.line = object;
     }
-    // finalize
-    // fn after_insert(&mut self) {}
-    /// solver for part1
     fn part1(&mut self) -> usize {
-        0
+        let vec = &self.line;
+        let len: usize = *vec.iter().max().unwrap();
+        let mut fmin: usize = vec.iter().sum::<usize>().pow(2);
+        for pos in 0..=len {
+            fmin = fmin.min(
+                vec.iter()
+                    .map(|i| (*i as isize - pos as isize).abs() as usize)
+                    .sum(),
+            );
+        }
+        fmin
     }
-    /// solver for part2
     fn part2(&mut self) -> usize {
-        0
+        let vec = &self.line;
+        let len: usize = *vec.iter().max().unwrap();
+        let mut fuel_table: Vec<Option<usize>> = Vec::new();
+        fuel_table.resize(len + 1, None);
+        fuel_table[0] = Some(0);
+        fn get(table: &mut Vec<Option<usize>>, n: usize) -> usize {
+            if let Some(k) = table[n] {
+                k
+            } else {
+                let v = n + get(table, n - 1);
+                table[n] = Some(v);
+                v
+            }
+        }
+        let mut fmin: usize = vec.iter().sum::<usize>().pow(2);
+        for pos in 0..=len {
+            fmin = fmin.min(
+                vec.iter()
+                    .map(|i| get(&mut fuel_table, (*i as isize - pos as isize).abs() as usize))
+                    .sum(),
+            );
+        }
+        fmin
     }
 }
 
@@ -62,21 +73,3 @@ pub fn go(part: usize, desc: Description) {
     dbg!(Puzzle::solve(&desc, part));
 }
 
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        const TEST1: &str = "0\n1\n2";
-        assert_eq!(
-            Puzzle::parse(&Description::TestData(TEST1.to_string()))
-                .expect("-")
-                .run(1),
-            Answer::Part1(0)
-        );
-    }
-}
