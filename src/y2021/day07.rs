1a96380c478f931bbb151eecd25a042e7ab64f81
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-07 15:02:59 +0900

    07 beautify

--- src/y2021/day07.rs
+++ src/y2021/day07.rs
@@ -1,19 +1,28 @@
-use crate::{AdventOfCode, Description, ParseError, TryParse};
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use crate::{AdventOfCode, Description, Maybe, ParseError, TryParse};
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::collections::HashMap;
 
-type DataSegment = Vec<usize>;
+#[derive(Debug, PartialEq)]
+struct DataSegment(Vec<usize>);
 
 impl TryParse for DataSegment {
     fn parse(s: &str) -> Result<Self, ParseError> {
-        Ok(s.trim()
-            .split(',')
-            .map(|i| i.parse::<usize>().unwrap())
-            .collect())
+        Ok(DataSegment(
+            s.trim()
+                .split(',')
+                .map(|i| i.parse::<usize>().unwrap())
+                .collect(),
+        ))
     }
 }
 
 #[derive(Debug, PartialEq)]
 struct Puzzle {
-    line: DataSegment,
+    config: Vec<usize>,
 }
 
 impl AdventOfCode for Puzzle {
@@ -24,13 +33,13 @@ impl AdventOfCode for Puzzle {
     const DAY: usize = 7;
     const DELIMITER: &'static str = "\n";
     fn default() -> Self {
-        Self { line: Vec::new() }
+        Self { config: Vec::new() }
     }
     fn insert(&mut self, object: Self::Segment) {
-        self.line = object;
+        self.config = object.0;
     }
     fn part1(&mut self) -> usize {
-        let vec = &self.line;
+        let vec = &self.config;
         let len: usize = *vec.iter().max().unwrap();
         let mut fmin: usize = vec.iter().sum::<usize>().pow(2);
         for pos in 0..=len {
@@ -43,7 +52,7 @@ impl AdventOfCode for Puzzle {
         fmin
     }
     fn part2(&mut self) -> usize {
-        let vec = &self.line;
+        let vec = &self.config;
         let len: usize = *vec.iter().max().unwrap();
         let mut fuel_table: Vec<Option<usize>> = Vec::new();
         fuel_table.resize(len + 1, None);
@@ -57,7 +66,7 @@ impl AdventOfCode for Puzzle {
                 v
             }
         }
-        let mut fmin: usize = vec.iter().sum::<usize>().pow(2);
+        let mut fmin: usize = vec.iter().sum::<usize>() * vec.len();
         for pos in 0..=len {
             fmin = fmin.min(
                 vec.iter()
@@ -73,3 +82,21 @@ pub fn go(part: usize, desc: Description) {
     dbg!(Puzzle::solve(&desc, part));
 }
 
+#[cfg(test)]
+mod test {
+    use {
+        super::*,
+        crate::{Answer, Description},
+    };
+
+    #[test]
+    fn test_part1() {
+        const TEST1: &str = "0\n1\n2";
+        assert_eq!(
+            Puzzle::parse(&Description::TestData(TEST1.to_string()))
+                .expect("-")
+                .run(1),
+            Answer::Part1(0)
+        );
+    }
+}
