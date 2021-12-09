d0cd96fcc8e878769359c4f64a7a6604b8cf1cfe
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-09 18:25:27 +0900

    09 done

--- src/y2021/day09.rs
+++ src/y2021/day09.rs
@@ -1,27 +1,60 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use crate::{AdventOfCode, Description, Maybe, ParseError, TryParse};
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::HashMap;
+use crate::{AdventOfCode, Description, ParseError, TryParse};
 
 #[derive(Debug, PartialEq)]
-struct DataSegment {}
+struct DataSegment(Vec<usize>);
 
 impl TryParse for DataSegment {
-    /// make a `Object` from a string block
     fn parse(s: &str) -> Result<Self, ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(s).ok_or(ParseError)?;
-        Err(ParseError)
+        Ok(DataSegment(
+            s.trim()
+                .chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect::<Vec<usize>>(),
+        ))
     }
 }
 
+fn basin_size(grid: &[Vec<usize>], h: usize, w: usize, j: usize, i: usize) -> usize {
+    let mut to_check: Vec<(usize, usize)> = vec![(j,i)];
+    let mut checked: Vec<(usize, usize)> = Vec::new();
+    while let Some(pos@(j, i)) = to_check.pop() {
+        let here = grid[j][i];
+        if here == 9 {
+            continue;
+        }
+        checked.push(pos);
+        if j != 0 {
+            let new = (j - 1, i);
+            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
+                to_check.push(new);
+            }
+        }
+        if j != h {
+            let new = (j + 1, i);
+            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
+                to_check.push(new);
+            }
+        }
+        if i != 0 {
+            let new = (j, i - 1);
+            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
+                to_check.push(new);
+            }
+        }
+        if i != w {
+            let new = (j, i + 1);
+            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
+                to_check.push(new);
+            }
+        }
+    }
+    checked.len()
+}
+
 #[derive(Debug, PartialEq)]
-struct Puzzle {}
+struct Puzzle {
+    line: Vec<Vec<usize>>,
+}
 
 impl AdventOfCode for Puzzle {
     type Segment = DataSegment;
@@ -31,52 +64,77 @@ impl AdventOfCode for Puzzle {
     const DAY: usize = 9;
     const DELIMITER: &'static str = "\n";
     fn default() -> Self {
-        Self {}
+        Self { line: Vec::new() }
     }
-    // handle header
-    // fn header(&mut self, input: &str) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
-    /// add a data block
     fn insert(&mut self, object: Self::Segment) {
-        // self = object;
+        self.line.push(object.0);
     }
-    // finalize
-    // fn after_insert(&mut self) {}
-    /// solver for part1
     fn part1(&mut self) -> usize {
-        0
+        // dbg!(&self.line);
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let mut risks = 0;
+        for j in 0..height {
+            for i in 0..width {
+                let here = self.line[j][i];
+                if (if j != 0 {
+                    here < self.line[j - 1][i]
+                } else {
+                    true
+                } && if j + 1 != height {
+                    here < self.line[j + 1][i]
+                } else {
+                    true
+                } && if i != 0 {
+                    here < self.line[j][i - 1]
+                } else {
+                    true
+                } && if i + 1 != width {
+                    here < self.line[j][i + 1]
+                } else {
+                    true
+                }) {
+                    risks += self.line[j][i] + 1;
+                }
+            }
+        }
+        risks
     }
-    /// solver for part2
     fn part2(&mut self) -> usize {
-        0
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let mut sizes = Vec::new();
+        for j in 0..height {
+            for i in 0..width {
+                let here = self.line[j][i];
+                if (if j != 0 {
+                    here < self.line[j - 1][i]
+                } else {
+                    true
+                } && if j + 1 != height {
+                    here < self.line[j + 1][i]
+                } else {
+                    true
+                } && if i != 0 {
+                    here < self.line[j][i - 1]
+                } else {
+                    true
+                } && if i + 1 != width {
+                    here < self.line[j][i + 1]
+                } else {
+                    true
+                }) {
+                    dbg!(basin_size(&self.line, height - 1, width - 1, j, i));
+                    sizes.push(basin_size(&self.line, height - 1, width - 1, j, i));
+                }
+            }
+        }
+        sizes.sort_unstable();
+        sizes.reverse();
+        sizes[0] * sizes[1] * sizes[2]
     }
 }
 
 pub fn go(part: usize, desc: Description) {
     dbg!(Puzzle::solve(&desc, part));
-}
-
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        const TEST1: &str = "0\n1\n2";
-        assert_eq!(
-            Puzzle::parse(&Description::TestData(TEST1.to_string()))
-                .expect("-")
-                .run(1),
-            Answer::Part1(0)
-        );
-    }
 }
