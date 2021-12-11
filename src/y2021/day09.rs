093a5e2132f01f35c4e9f8a65107e865723c61a7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-12 00:06:12 +0900

    (09) reimplement with geometric::neighbors

--- src/y2021/day09.rs
+++ src/y2021/day09.rs
@@ -1,45 +1,40 @@
-use crate::framework::{aoc_at, AdventOfCode, Maybe};
-
-fn basin_size(grid: &[Vec<usize>], h: usize, w: usize, j: usize, i: usize) -> usize {
-    let mut to_check: Vec<(usize, usize)> = vec![(j, i)];
-    let mut checked: Vec<(usize, usize)> = Vec::new();
-    while let Some(pos @ (j, i)) = to_check.pop() {
-        let here = grid[j][i];
-        if here == 9 {
-            continue;
-        }
-        checked.push(pos);
-        if j != 0 {
-            let new = (j - 1, i);
-            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
-                to_check.push(new);
-            }
-        }
-        if j != h {
-            let new = (j + 1, i);
-            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
-                to_check.push(new);
-            }
-        }
-        if i != 0 {
-            let new = (j, i - 1);
-            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
-                to_check.push(new);
-            }
-        }
-        if i != w {
-            let new = (j, i + 1);
-            if here < grid[new.0][new.1] && !to_check.contains(&new) && !checked.contains(&new) {
-                to_check.push(new);
-            }
-        }
-    }
-    checked.len()
-}
+use crate::{
+    framework::{aoc_at, AdventOfCode, Maybe},
+    geometric::neighbors,
+};
 
 #[derive(Debug, Default, PartialEq)]
 pub struct Puzzle {
     line: Vec<Vec<usize>>,
+}
+
+impl Puzzle {
+    fn basin_size(&self, h: usize, w: usize, j: usize, i: usize) -> usize {
+        let mut to_check: Vec<(usize, usize)> = vec![(j, i)];
+        let mut checked: Vec<(usize, usize)> = Vec::new();
+        while let Some(pos @ (j, i)) = to_check.pop() {
+            let here = self.line[j][i];
+            if here == 9 {
+                continue;
+            }
+            checked.push(pos);
+            for jj in neighbors(j, h + 1) {
+                for ii in neighbors(i, w + 1) {
+                    if let (Some(y), Some(x)) = (jj, ii) {
+                        if (y == j || x == i)
+                            && !(y == j && x == i)
+                            && here < self.line[y][x]
+                            && !to_check.contains(&(y, x))
+                            && !checked.contains(&(y, x))
+                        {
+                            to_check.push((y, x));
+                        }
+                    }
+                }
+            }
+        }
+        checked.len()
+    }
 }
 
 #[aoc_at(2021, 9)]
@@ -61,27 +56,21 @@ impl AdventOfCode for Puzzle {
         let width = self.line[0].len();
         let mut risks = 0;
         for j in 0..height {
-            for i in 0..width {
+            'next: for i in 0..width {
                 let here = self.line[j][i];
-                if (if j != 0 {
-                    here < self.line[j - 1][i]
-                } else {
-                    true
-                } && if j + 1 != height {
-                    here < self.line[j + 1][i]
-                } else {
-                    true
-                } && if i != 0 {
-                    here < self.line[j][i - 1]
-                } else {
-                    true
-                } && if i + 1 != width {
-                    here < self.line[j][i + 1]
-                } else {
-                    true
-                }) {
-                    risks += self.line[j][i] + 1;
+                for jj in neighbors(j, height) {
+                    for ii in neighbors(i, width) {
+                        if let (Some(jj), Some(ii)) = (jj, ii) {
+                            if (jj == j || ii == i)
+                                && !(jj == j && ii == i)
+                                && self.line[jj][ii] <= here
+                            {
+                                continue 'next;
+                            }
+                        }
+                    }
                 }
+                risks += self.line[j][i] + 1;
             }
         }
         risks
@@ -110,8 +99,8 @@ impl AdventOfCode for Puzzle {
                 } else {
                     true
                 }) {
-                    dbg!(basin_size(&self.line, height - 1, width - 1, j, i));
-                    sizes.push(basin_size(&self.line, height - 1, width - 1, j, i));
+                    dbg!(self.basin_size(height - 1, width - 1, j, i));
+                    sizes.push(self.basin_size(height - 1, width - 1, j, i));
                 }
             }
         }
