7b7aeba026a8f1f8d336b9de6d5bc875ccbd76b7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-11 15:33:21 +0900

    day11 done

--- src/y2021/day11.rs
+++ src/y2021/day11.rs
@@ -1,17 +1,142 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use crate::{
-    framework::{aoc_at, AdventOfCode, Maybe, ParseError},
+    framework::{aoc_at, AdventOfCode, Maybe},
     line_parser,
 };
-use lazy_static::lazy_static;
-use regex::Regex;
-use std::collections::HashMap;
+
+fn progress(
+    vec: &mut [Vec<usize>],
+    flash: Vec<(usize, usize)>,
+    mut step: usize,
+    mut total: usize,
+) -> usize {
+    let h = vec.len();
+    let w = vec[0].len();
+    let mut secondary: Vec<(usize, usize)> = Vec::new();
+    if flash.is_empty() {
+        assert!(vec.iter().all(|v| v.iter().all(|i| *i <= 9)));
+        if step == 0 {
+            for v in vec.iter() {
+                for i in v.iter() {
+                    print!("{}", i);
+                }
+                println!();
+            }
+            println!();
+            return total;
+        }
+        for (j, v) in vec.iter_mut().enumerate() {
+            for (i, p) in v.iter_mut().enumerate() {
+                if *p == 9 {
+                    secondary.push((j, i));
+                }
+                *p += 1;
+            }
+        }
+    } else {
+        for (j, i) in flash.iter() {
+            for dj in [-1isize, 0, 1] {
+                if *j == 0 && dj == -1 {
+                    continue;
+                }
+                if *j + 1 == h && dj == 1 {
+                    continue;
+                }
+                for di in [-1, 0, 1] {
+                    if *i == 0 && di == -1 {
+                        continue;
+                    }
+                    if *i + 1 == w && di == 1 {
+                        continue;
+                    }
+                    if dj == 0 && di == 0 {
+                        continue;
+                    }
+                    let jj = (*j as isize + dj) as usize;
+                    let ii = (*i as isize + di) as usize;
+                    if vec[jj][ii] == 9 {
+                        secondary.push((jj, ii))
+                    }
+                    vec[jj][ii] += 1;
+                }
+            }
+        }
+        total += flash.len();
+    }
+    if secondary.is_empty() {
+        for v in vec.iter_mut() {
+            for i in v.iter_mut() {
+                if 9 < *i {
+                    *i = 0;
+                }
+            }
+        }
+        step -= 1;
+    }
+    // dbg!(step, flash.is_empty());
+    progress(vec, secondary, step, total)
+}
+
+fn progress_check(vec: &mut [Vec<usize>], flash: Vec<(usize, usize)>, mut step: usize) -> usize {
+    let h = vec.len();
+    let w = vec[0].len();
+    let mut secondary: Vec<(usize, usize)> = Vec::new();
+    if flash.is_empty() {
+        if vec.iter().all(|v| v.iter().all(|i| *i == 0)) {
+            return step;
+        }
+        for (j, v) in vec.iter_mut().enumerate() {
+            for (i, p) in v.iter_mut().enumerate() {
+                if *p == 9 {
+                    secondary.push((j, i));
+                }
+                *p += 1;
+            }
+        }
+    } else {
+        for (j, i) in flash.iter() {
+            for dj in [-1isize, 0, 1] {
+                if *j == 0 && dj == -1 {
+                    continue;
+                }
+                if *j + 1 == h && dj == 1 {
+                    continue;
+                }
+                for di in [-1, 0, 1] {
+                    if *i == 0 && di == -1 {
+                        continue;
+                    }
+                    if *i + 1 == w && di == 1 {
+                        continue;
+                    }
+                    if dj == 0 && di == 0 {
+                        continue;
+                    }
+                    let jj = (*j as isize + dj) as usize;
+                    let ii = (*i as isize + di) as usize;
+                    if vec[jj][ii] == 9 {
+                        secondary.push((jj, ii))
+                    }
+                    vec[jj][ii] += 1;
+                }
+            }
+        }
+    }
+    if secondary.is_empty() {
+        for v in vec.iter_mut() {
+            for i in v.iter_mut() {
+                if 9 < *i {
+                    *i = 0;
+                }
+            }
+        }
+        step += 1;
+    }
+    progress_check(vec, secondary, step)
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<usize>>,
 }
 
 #[aoc_at(2021, 11)]
@@ -20,27 +145,14 @@ impl AdventOfCode for Puzzle {
     fn default() -> Self {
         Self { line: Vec::new() }
     }
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Maybe<()> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(object);
+        self.line.push(line_parser::to_digits(block)?);
         Ok(())
     }
-    // fn after_insert(&mut self) {}
     fn part1(&mut self) -> Self::Output1 {
-        0
+        progress(&mut self.line, Vec::new(), 100, 0)
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        progress_check(&mut self.line, Vec::new(), 0)
     }
 }
