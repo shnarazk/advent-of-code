02d578cc4f7c2835ae5f81d448fd04324617b941
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-12 10:18:23 +0900

    add a new feature "y2021"

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,9 @@ pub mod framework;
 pub mod geometric;
 pub mod line_parser;
 pub mod template;
+#[cfg(feature = "y2020")]
 pub mod y2020;
+#[cfg(feature = "y2021")]
 pub mod y2021;
 
 pub use framework::Description;
-pub use y2021::*;
