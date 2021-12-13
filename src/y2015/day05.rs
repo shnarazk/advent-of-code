c9101f26d6379fceae1a9df3dda95aa30cdd6bfa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-13 20:10:57 +0900

    day 05 of 2015 done

--- src/y2015/day05.rs
+++ src/y2015/day05.rs
@@ -1,66 +1,88 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
-    lazy_static::lazy_static,
-    regex::Regex,
-    std::collections::HashMap,
-};
+use crate::framework::{aoc, AdventOfCode, ParseError};
+
+fn is_nice(s: &str) -> bool {
+    3 <= s
+        .chars()
+        .filter(|c| ['a', 'e', 'i', 'o', 'u'].contains(c))
+        .count()
+        && 1 <= {
+            let v: Vec<char> = s.chars().collect::<Vec<char>>();
+            v.iter()
+                .enumerate()
+                .filter(|(i, c)| Some(*c) == v.get(*i + 1))
+                .count()
+        }
+        && !s.contains("ab")
+        && !s.contains("cd")
+        && !s.contains("pq")
+        && !s.contains("xy")
+}
+
+fn is_nicer(s: &str) -> bool {
+    let v: Vec<char> = s.chars().collect::<Vec<char>>();
+    // dbg!(s);
+    let mut found = false;
+    for i in 2..v.len() - 1 {
+        let b2 = v[i - 2];
+        let b1 = v[i - 1];
+        // println!("{}{}|{:?}", b1, b2, &v[i..]);
+        for j in i..v.len() - 1 {
+            // println!(" - {:?}", &v[j..]);
+            if b2 == v[j] && b1 == v[j + 1] {
+                found = true;
+                break;
+            }
+        }
+    }
+    if !found {
+        return false;
+    }
+    for i in 0..v.len() - 2 {
+        // println!("{:?}", &v[i + 2..]);
+        if v[i] == v[i + 2] {
+            return true;
+        }
+    }
+    false
+}
 
 #[derive(Debug, Default)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<String>,
 }
 
 #[aoc(2015, 5)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<Option<String>, ParseError> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(object);
+        self.line.push(block.to_string());
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.line.iter().filter(|s| is_nice(s)).count()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        self.line.iter().filter(|s| is_nicer(s)).count()
     }
 }
 
 #[cfg(feature = "y2015")]
 #[cfg(test)]
 mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
+    use super::*;
 
     #[test]
     fn test_part1() {
-        const TEST1: &str = "0\n1\n2";
-        assert_eq!(
-            Puzzle::solve(Description::TestData(TEST1.to_string()), 1),
-            Answer::Part1(0)
-        );
+        assert!(is_nice("ugknbfddgicrmopn"));
+    }
+    #[test]
+    fn test_part2() {
+        assert!(is_nicer("qjhvhtzxzqqjkmpb"));
+        assert!(is_nicer("xxyxx"));
+        assert!(!is_nicer("uurcxstgmygtbstg"));
+        assert!(!is_nicer("ieodomkazucvgmuy"));
     }
 }
