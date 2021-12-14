82fe2b956296584cd271d0e2acd9cfb0e926b024
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-14 19:59:43 +0900

    2015 day 12 done

--- src/y2015/day12.rs
+++ src/y2015/day12.rs
@@ -1,66 +1,81 @@
 //! <https://adventofcode.com/2015/day/>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
+    crate::framework::{aoc_at, AdventOfCode, ParseError},
     lazy_static::lazy_static,
     regex::Regex,
-    std::collections::HashMap,
+    serde_json,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<isize>>,
+    raw: Vec<String>,
 }
 
-#[aoc(2015, 12)]
+#[aoc_at(2015, 12)]
 impl AdventOfCode for Puzzle {
+    type Output1 = isize;
+    type Output2 = isize;
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<Option<String>, ParseError> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+        const PART1: bool = false;
+        if PART1 {
+            lazy_static! {
+                static ref NON_DIGIT: Regex = Regex::new(r"[^---0-9]+").expect("wrong");
+            }
+            let mut after = NON_DIGIT.replace_all(block, " ").to_string();
+            after = after.trim().to_string();
+            self.line.push(
+                after
+                    .split(' ')
+                    .filter(|s| !s.is_empty())
+                    .map(|s| {
+                        if s.starts_with('-') {
+                            -s.chars()
+                                .skip(1)
+                                .collect::<String>()
+                                .parse::<isize>()
+                                .unwrap()
+                        } else {
+                            s.parse::<isize>().unwrap()
+                        }
+                    })
+                    .collect::<Vec<isize>>(),
+            );
+        } else {
+            self.raw.push(block.to_string());
         }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut num: isize = 0;
+        for l in self.line.iter() {
+            num = l.iter().sum::<isize>();
+            println!("{}", num);
+        }
+        num
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut result: isize = 0;
+        for l in self.raw.iter() {
+            let obj: serde_json::Value = serde_json::from_str(l).expect("");
+            result = collect(&obj);
+            dbg!(result);
+        }
+        result
     }
 }
 
-#[cfg(feature = "y2015")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(
-            Puzzle::solve(Description::TestData("".to_string()), 1),
-            Answer::Part1(0)
-        );
+fn collect(j: &serde_json::Value) -> isize {
+    match j {
+        serde_json::Value::Array(a) => a.iter().map(collect).sum::<isize>(),
+        serde_json::Value::Number(n) => n.as_i64().unwrap() as isize,
+        serde_json::Value::Object(map) if map.values().all(|k| k != "red") => {
+            map.iter().map(|(_, v)| collect(v)).sum::<isize>()
+        }
+        _ => 0,
     }
 }
