13eedbf70b1b61d96b6ecc4bf9b3427cc2a5f571
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-14 12:38:21 +0900

    2015 day 9 done

--- src/y2015/day09.rs
+++ src/y2015/day09.rs
@@ -1,67 +1,115 @@
 //! <https://adventofcode.com/2015/day/9>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     lazy_static::lazy_static,
     regex::Regex,
-    std::collections::HashMap,
+    std::collections::{HashMap, HashSet},
 };
+
+fn search(
+    path: Vec<String>,
+    hash: &HashMap<(String, String), usize>,
+    weight: usize,
+    stop: usize,
+) -> usize {
+    if stop == 0 {
+        // println!("{:?} = {}", path, weight);
+        return weight;
+    }
+    let here: &String = path.last().unwrap();
+    let mut new_weight = usize::MAX;
+    for ((from, to), dist) in hash.iter() {
+        if *from == *here && !path.contains(to) {
+            let mut extended = path.clone();
+            extended.push(to.to_string());
+            let w = search(extended, hash, weight + dist, stop - 1);
+            new_weight = new_weight.min(w);
+        }
+    }
+    new_weight
+}
+
+fn search2(
+    path: Vec<String>,
+    hash: &HashMap<(String, String), usize>,
+    weight: usize,
+    stop: usize,
+) -> usize {
+    if stop == 0 {
+        // println!("{:?} = {}", path, weight);
+        return weight;
+    }
+    let here: &String = path.last().unwrap();
+    let mut new_weight = 0;
+    for ((from, to), dist) in hash.iter() {
+        if *from == *here && !path.contains(to) {
+            let mut extended = path.clone();
+            extended.push(to.to_string());
+            let w = search2(extended, hash, weight + dist, stop - 1);
+            new_weight = new_weight.max(w);
+        }
+    }
+    new_weight
+}
 
 #[derive(Debug, Default)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(String, String, usize)>,
+    path: HashMap<(String, String), usize>,
+    cities: HashSet<String>,
 }
 
 #[aoc(2015, 9)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<Option<String>, ParseError> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+            static ref PARSER: Regex =
+                Regex::new(r"^([A-za-z]+) to ([A-za-z]+) = ([0-9]+)$").expect("wrong");
         }
         let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment);
+        self.line.push((
+            segment[1].to_string(),
+            segment[2].to_string(),
+            segment[3].parse::<usize>()?,
+        ));
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        for (c0, c1, d) in self.line.iter() {
+            self.path.insert((c0.to_string(), c1.to_string()), *d);
+            self.path.insert((c1.to_string(), c0.to_string()), *d);
+            self.cities.insert(c0.to_string());
+            self.cities.insert(c1.to_string());
+        }
+        // dbg!(&self.path);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.cities
+            .iter()
+            .map(|start| {
+                search(
+                    vec![start.to_string()],
+                    &self.path,
+                    0,
+                    self.cities.len() - 1,
+                )
+            })
+            .min()
+            .unwrap()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
-    }
-}
-
-#[cfg(feature = "y2015")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        const TEST1: &str = "0\n1\n2";
-        assert_eq!(
-            Puzzle::solve(Description::TestData(TEST1.to_string()), 1),
-            Answer::Part1(0)
-        );
+        self.cities
+            .iter()
+            .map(|start| {
+                search2(
+                    vec![start.to_string()],
+                    &self.path,
+                    0,
+                    self.cities.len() - 1,
+                )
+            })
+            .max()
+            .unwrap()
     }
 }
