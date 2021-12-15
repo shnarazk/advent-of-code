75574a9ffad3a6cf74c1c923831bfce59d49ce6c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-15 19:04:47 +0900

    2021 day 15 done

--- src/y2021/day15.rs
+++ src/y2021/day15.rs
@@ -1,67 +1,89 @@
 //! <https://adventofcode.com/2021/day/15>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
-    lazy_static::lazy_static,
-    regex::Regex,
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    geometric, line_parser,
 };
 
 #[derive(Debug, Default)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<usize>>,
+}
+
+impl Puzzle {
+    fn build_up(&mut self) -> Vec<Vec<usize>> {
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let mut dist = self.line.clone();
+        for v in dist.iter_mut() {
+            for i in v.iter_mut() {
+                *i = usize::MAX;
+            }
+        }
+        dist[0][0] = 0;
+        dist[0][1] = self.line[0][1];
+        dist[1][0] = self.line[1][0];
+        let mut cands: Vec<(usize, usize)> = vec![(0, 1), (1, 0)];
+        while let Some(n) = cands.iter().min_by_key(|(j, i)| dist[*j][*i]) {
+            let node = (n.0, n.1);
+            cands.retain(|p| *p != node);
+            for neighbor in geometric::neighbors4(node.0, node.1, height, width) {
+                let new_dist = dist[node.0][node.1] + self.line[neighbor.0][neighbor.1];
+                if new_dist < dist[neighbor.0][neighbor.1] {
+                    dist[neighbor.0][neighbor.1] = new_dist;
+                    if !cands.contains(&neighbor) {
+                        cands.push(neighbor);
+                    }
+                }
+            }
+        }
+        dist
+    }
+    fn extend5(&mut self) {
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let mut grid: Vec<Vec<usize>> = Vec::new();
+        for _ in 0..height * 5 {
+            let mut v: Vec<usize> = Vec::new();
+            v.resize(width * 5, 0);
+            grid.push(v);
+        }
+        for dj in 0..5 {
+            for di in 0..5 {
+                for j in 0..height {
+                    for i in 0..width {
+                        grid[height * dj + j][width * di + i] =
+                            (self.line[j][i] + dj + di - 1) % 9 + 1;
+                    }
+                }
+            }
+        }
+        std::mem::swap(&mut self.line, &mut grid);
+    }
 }
 
 #[aoc(2021, 15)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(object);
+        self.line.push(line_parser::to_digits(block)?);
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // for l in self.line.iter() {
+        //     println!{"{:?}", &l};
+        // }
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let dist = self.build_up();
+        dist[height - 1][width - 1]
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
-    }
-}
-
-#[cfg(feature = "y2021")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        const TEST1: &str = "0\n1\n2";
-        assert_eq!(
-            Puzzle::solve(Description::TestData(TEST1.to_string()), 1),
-            Answer::Part1(0)
-        );
+        self.extend5();
+        for l in self.line.iter() {
+            println! {"{:?}", l.iter().map(|c| format!("{}", c)).collect::<Vec<String>>().join(""), };
+        }
+        self.part1()
     }
 }
