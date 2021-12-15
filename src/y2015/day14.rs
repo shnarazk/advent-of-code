333775483065de26c1aa7bafaa51691625b214d5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-15 20:54:37 +0900

    2015 day 14 done

--- src/y2015/day14.rs
+++ src/y2015/day14.rs
@@ -1,66 +1,68 @@
 //! <https://adventofcode.com/2015/day/>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     lazy_static::lazy_static,
     regex::Regex,
-    std::collections::HashMap,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(String, usize, usize, usize)>,
 }
 
 #[aoc(2015, 14)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<Option<String>, ParseError> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+            static ref PARSER: Regex = Regex::new(r"^([A-Za-z]+) can fly ([0-9]+) km/s for ([0-9]+) seconds, but then must rest for ([0-9]+) seconds\.$").expect("wrong");
         }
         let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push((
+            segment[1].to_string(),
+            segment[2].parse::<usize>()?,
+            segment[3].parse::<usize>()?,
+            segment[4].parse::<usize>()?,
+        ));
         Ok(())
     }
-    fn after_insert(&mut self) {
-        dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let t = 2503;
+        let mut longest: usize = 0;
+        for (name, speed, duration, rest) in self.line.iter() {
+            let n_cycle = t / (duration + rest);
+            let extra = (t % (duration + rest)).min(*duration);
+            let length = speed * (n_cycle * duration + extra);
+            longest = longest.max(length);
+            println!(
+                "{} flies {} * ({} * {} + {}) = {} km.",
+                name, speed, n_cycle, duration, extra, length
+            );
+        }
+        longest
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
-    }
-}
+        let end = 2503;
+        let mut point: Vec<usize> = Vec::new();
+        point.resize(self.line.len(), 0);
+        let mut dist = point.clone();
 
-#[cfg(feature = "y2015")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(
-            Puzzle::solve(Description::TestData("".to_string()), 1),
-            Answer::Part1(0)
-        );
+        for t in 0..end {
+            for (i, (_, speed, duration, rest)) in self.line.iter().enumerate() {
+                if (t % (duration + rest)) < *duration {
+                    dist[i] += speed;
+                }
+            }
+            println!("{:?}", dist);
+            let longest = dist.iter().max().unwrap();
+            for (i, d) in dist.iter().enumerate() {
+                if d == longest {
+                    point[i] += 1;
+                }
+            }
+        }
+        // println!("{} flies {} * ({} * {} + {}) = {} km.", name, speed, n_cycle, duration, extra, length);
+        dbg!(&point);
+        point.iter().copied().max().unwrap()
     }
 }
