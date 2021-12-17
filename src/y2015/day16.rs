5d59b36660a890505d92ce44ba2b35f374d0adfd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-17 23:56:17 +0900

    2015 day 16 done

--- src/y2015/day16.rs
+++ src/y2015/day16.rs
@@ -1,66 +1,108 @@
 //! <https://adventofcode.com/2015/day/>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     lazy_static::lazy_static,
     regex::Regex,
-    std::collections::HashMap,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+struct Sue {
+    id: usize,
+    children: Option<usize>,
+    cats: Option<usize>,
+    samoyeds: Option<usize>,
+    pomeranians: Option<usize>,
+    akitas: Option<usize>,
+    vizslas: Option<usize>,
+    goldfish: Option<usize>,
+    trees: Option<usize>,
+    cars: Option<usize>,
+    perfumes: Option<usize>,
+}
+
+#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Sue>,
 }
 
 #[aoc(2015, 16)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<Option<String>, ParseError> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+            static ref PARSER: Regex =
+                Regex::new(r"^Sue ([0-9]+): (([a-z]+: [0-9]+, )*[a-z]+: [0-9]+)$").expect("wrong");
         }
         let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        // print!("{:>4} - ", segment[1].parse::<usize>()?);
+        let mut sue: Sue = Sue {
+            id: segment[1].parse::<usize>()?,
+            ..Sue::default()
+        };
+        for seg in segment[2].split(',') {
+            let mut tokens = seg.split(':');
+            let prop = tokens.next().ok_or(ParseError)?.trim();
+            let val = tokens.next().ok_or(ParseError)?.trim().parse::<usize>()?;
+            // print!("{:>12}: {:>3} ", prop, val);
+            match prop {
+                "children" => sue.children = Some(val),
+                "cats" => sue.cats = Some(val),
+                "samoyeds" => sue.samoyeds = Some(val),
+                "pomeranians" => sue.pomeranians = Some(val),
+                "akitas" => sue.akitas = Some(val),
+                "vizslas" => sue.vizslas = Some(val),
+                "goldfish" => sue.goldfish = Some(val),
+                "trees" => sue.trees = Some(val),
+                "cars" => sue.cars = Some(val),
+                "perfumes" => sue.perfumes = Some(val),
+                _ => panic!(),
+            }
+        }
+        // println!();
+        self.line.push(sue);
         Ok(())
     }
     fn after_insert(&mut self) {
         dbg!(&self.line);
+        // assert_eq!(self.line.len(), 500);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.line
+            .iter()
+            .filter(|sue| {
+                sue.children.map_or(true, |v| v == 3)
+                    && sue.cats.map_or(true, |v| v == 7)
+                    && sue.samoyeds.map_or(true, |v| v == 2)
+                    && sue.pomeranians.map_or(true, |v| v == 3)
+                    && sue.akitas.map_or(true, |v| v == 0)
+                    && sue.vizslas.map_or(true, |v| v == 0)
+                    && sue.goldfish.map_or(true, |v| v == 5)
+                    && sue.trees.map_or(true, |v| v == 3)
+                    && sue.cars.map_or(true, |v| v == 2)
+                    && sue.perfumes.map_or(true, |v| v == 1)
+            })
+            .map(|sue| sue.id)
+            .max()
+            .unwrap()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
-    }
-}
-
-#[cfg(feature = "y2015")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(
-            Puzzle::solve(Description::TestData("".to_string()), 1),
-            Answer::Part1(0)
-        );
+        let they = self
+            .line
+            .iter()
+            .filter(|sue| {
+                sue.children.map_or(true, |v| v == 3)
+                    && sue.cats.map_or(true, |v| v > 7)
+                    && sue.samoyeds.map_or(true, |v| v == 2)
+                    && sue.pomeranians.map_or(true, |v| v < 3)
+                    && sue.akitas.map_or(true, |v| v == 0)
+                    && sue.vizslas.map_or(true, |v| v == 0)
+                    && sue.goldfish.map_or(true, |v| v < 5)
+                    && sue.trees.map_or(true, |v| v > 3)
+                    && sue.cars.map_or(true, |v| v == 2)
+                    && sue.perfumes.map_or(true, |v| v == 1)
+            })
+            .collect::<Vec<_>>();
+        dbg!(&they);
+        they[0].id
     }
 }
