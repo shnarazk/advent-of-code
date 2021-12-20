6f7c97e2a1e54d877e360eafedaa8b396ca98f37
shujinarazaki <shujinarazaki@protonmail.com>
2021-12-20 23:37:15 +0900

    2015 day 18 done

--- src/y2015/day18.rs
+++ src/y2015/day18.rs
@@ -1,50 +1,129 @@
 //! <https://adventofcode.com/2015/day/18>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
-    lazy_static::lazy_static,
-    regex::Regex,
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    geometric, line_parser,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<char>>,
 }
 
 #[aoc(2015, 18)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<Option<String>, ParseError> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(line_parser::to_chars(block)?);
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let count = 100;
+        let mut grid: Vec<Vec<char>> = self.line.clone();
+        let height = grid.len();
+        let width = grid[0].len();
+        for _ in 0..count {
+            let mut next = grid.clone();
+            for (j, v) in grid.iter().enumerate() {
+                for (i, _) in v.iter().enumerate() {
+                    let n = geometric::neighbors8(j, i, height, width)
+                        .iter()
+                        .filter(|(jj, ii)| grid[*jj][*ii] == '#')
+                        .count();
+                    match (grid[j][i], n) {
+                        ('#', 2) | ('#', 3) => {
+                            next[j][i] = '#';
+                        }
+                        ('#', _) => {
+                            next[j][i] = '.';
+                        }
+                        ('.', 3) => {
+                            next[j][i] = '#';
+                        }
+                        ('.', _) => {
+                            next[j][i] = '.';
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+            }
+            std::mem::swap(&mut grid, &mut next);
+            // println!("");
+            // for v in grid.iter() {
+            //     println!("{:?}", v.iter().map(|c| format!("{}", c)).collect::<Vec<_>>().join(""));
+            // }
+        }
+        for v in grid.iter() {
+            println!(
+                "{:?}",
+                v.iter()
+                    .map(|c| format!("{}", c))
+                    .collect::<Vec<_>>()
+                    .join("")
+            );
+        }
+        grid.iter()
+            .map(|v| v.iter().filter(|l| **l == '#').count())
+            .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let count = 100;
+        let mut grid: Vec<Vec<char>> = self.line.clone();
+        let height = grid.len();
+        let width = grid[0].len();
+        grid[0][0] = '#';
+        grid[0][width - 1] = '#';
+        grid[height - 1][0] = '#';
+        grid[height - 1][width - 1] = '#';
+        for _ in 0..count {
+            let mut next = grid.clone();
+            for (j, v) in grid.iter().enumerate() {
+                for (i, _) in v.iter().enumerate() {
+                    let n = geometric::neighbors8(j, i, height, width)
+                        .iter()
+                        .filter(|(jj, ii)| grid[*jj][*ii] == '#')
+                        .count();
+                    match (grid[j][i], n) {
+                        ('#', 2) | ('#', 3) => {
+                            next[j][i] = '#';
+                        }
+                        ('#', _) => {
+                            next[j][i] = '.';
+                        }
+                        ('.', 3) => {
+                            next[j][i] = '#';
+                        }
+                        ('.', _) => {
+                            next[j][i] = '.';
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+            }
+            next[0][0] = '#';
+            next[0][width - 1] = '#';
+            next[height - 1][0] = '#';
+            next[height - 1][width - 1] = '#';
+            std::mem::swap(&mut grid, &mut next);
+            // println!("");
+            // for v in grid.iter() {
+            //     println!("{:?}", v.iter().map(|c| format!("{}", c)).collect::<Vec<_>>().join(""));
+            // }
+        }
+        for v in grid.iter() {
+            println!(
+                "{:?}",
+                v.iter()
+                    .map(|c| format!("{}", c))
+                    .collect::<Vec<_>>()
+                    .join("")
+            );
+        }
+        grid.iter()
+            .map(|v| v.iter().filter(|l| **l == '#').count())
+            .sum()
     }
 }
 
