e22116a023f57da4b654c406b305f393895bffb5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-21 22:33:22 +0900

    2015 day 19 part 1 only

--- src/y2015/day19.rs
+++ src/y2015/day19.rs
@@ -2,6 +2,8 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+
+use std::collections::HashSet;
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
@@ -13,9 +15,23 @@ use {
     std::collections::HashMap,
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Default)]
 pub struct Puzzle {
-    line: Vec<()>,
+    num_atom: usize,
+    dic: HashMap<String, usize>,
+    line: Vec<String>,
+    rule: Vec<(String,Vec<String>)>
+}
+
+fn parse_atoms(mut stream: Vec<String>, line: &str) -> Vec<String> {
+    lazy_static! {
+        static ref ATOM: Regex = Regex::new(r"^([A-Z][^A-Z]*)(.*)$").expect("wrong");
+    }
+    if let Some(segment) = ATOM.captures(line) {
+        stream.push(segment[1].to_string());
+        return parse_atoms(stream, &segment[2]);
+    }
+    stream
 }
 
 #[aoc(2015, 19)]
@@ -31,19 +47,106 @@ impl AdventOfCode for Puzzle {
     // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+            static ref RULE: Regex = Regex::new(r"^([A-Z][a-zA-Z]*) => ([A-Z][a-zA-Z]*)$").expect("wrong");
+            static ref BIGBANG: Regex = Regex::new(r"^e => ([A-Z][a-zA-Z]*)$").expect("wrong");
+            static ref STRING: Regex = Regex::new(r"^([A-Z][a-zA-Z]+)$").expect("wrong");
         }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        if let Some(segment) = RULE.captures(block) {
+            let prec = parse_atoms(Vec::new(), &segment[1]);
+            let post = parse_atoms(Vec::new(), &segment[2]);
+            assert!(prec.len() == 1, "{:?}", prec);
+            for w in prec.iter() {
+                if !self.dic.contains_key(w) {
+                    self.dic.insert(w.to_string(), self.num_atom);
+                    self.num_atom += 1;
+                }
+            }
+            for w in post.iter() {
+                if !self.dic.contains_key(w) {
+                    self.dic.insert(w.to_string(), self.num_atom);
+                    self.num_atom += 1;
+                }
+            }
+            self.rule.push((prec[0].to_string(), post));
+        } else if let Some(segment) = BIGBANG.captures(block) {
+            let post = parse_atoms(Vec::new(), &segment[1]);
+            for w in post.iter() {
+                if !self.dic.contains_key(w) {
+                    self.dic.insert(w.to_string(), self.num_atom);
+                    self.num_atom += 1;
+                }
+            }
+            self.rule.push(("e".to_string(), post));
+        } else if let Some(segment) = STRING.captures(block) {
+            let string = parse_atoms(Vec::new(), &segment[0]);
+            for w in string.iter() {
+                if !self.dic.contains_key(w) {
+                    self.dic.insert(w.to_string(), self.num_atom);
+                    self.num_atom += 1;
+                }
+            }
+            self.line = string;
+        }
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.rule);
+        // dbg!(&self.line);
+        // dbg!(&self.dic);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        dbg!(&self.rule);
+        let mut dic = Vec::new();
+        dic.resize(self.num_atom, "");
+        for (w, n) in self.dic.iter() {
+            dic[*n] = w;
+        }
+        let rule = self.rule.iter()
+            .map( |(atom, a)|
+                    (
+                        *self.dic.get(atom).unwrap(),
+                        a.iter().map(|atom| *self.dic.get(atom).unwrap()).collect::<Vec<usize>>(),
+                    )
+            )
+            .collect::<Vec<(usize, Vec<usize>)>>();
+        let string = self.line.iter()
+            .map(|s| *self.dic.get(s).unwrap())
+            .collect::<Vec<usize>>();
+        let mut bag: HashSet<Vec<usize>> = HashSet::new();
+        bag.insert(string);
+        let mut new_bag: HashSet<Vec<usize>> = HashSet::new();
+        for st in bag.iter() {
+            for (i, n) in st.iter().enumerate() {
+                for (_, seq) in rule.iter().filter(|(key, _)| key == n) {
+                    let mut new_string: Vec<usize> = st.clone();
+                    new_string.remove(i);
+                    for x in seq.iter().rev() {
+                        new_string.insert(i, *x);
+                    }
+                    new_bag.insert(new_string);
+                }
+            }
+        }
+        for s in new_bag.iter() {
+            println!("{:?}", s.iter().map(|n| dic[*n]).collect::<Vec<&str>>().join(""));
+        }
+        new_bag.len()
     }
     fn part2(&mut self) -> Self::Output2 {
+        let mut rule = self.rule.iter()
+            .map(|(k, seq)| {
+                let mut qes = seq.clone();
+                qes.reverse();
+                (qes, k)
+            })
+            .collect::<Vec<_>>();
+        rule.sort_unstable();
+        for r in rule.iter() {
+            println!("{:?} => {}", r.0, r.1);
+        }
+        let mut med = self.line.clone();
+        med.reverse();
+        println!("{:?}", med.join(""));
         0
     }
 }
