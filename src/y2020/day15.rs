788a130e254c25870f016540b55a39fc631c74aa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-21 10:05:46 +0900

    salvage 2020 day 15

--- src/y2020/day15.rs
+++ src/y2020/day15.rs
@@ -1,47 +1,27 @@
 //! <https://adventofcode.com/2020/day/15>
 #![allow(unused_imports)]
 use {
-    crate::y2020::traits::{Description, ProblemObject, ProblemSolver},
+    crate::framework::{aoc, AdventOfCode, ParseError},
     std::collections::HashMap,
 };
 
-pub fn day15(part: usize, desc: Description) {
-    dbg!(Setting::parse(desc).run(part));
-}
-
-#[derive(Debug, PartialEq)]
-struct Object {}
-
-impl ProblemObject for Object {
-    fn parse(_s: &str) -> Option<Self> {
-        None
-    }
-}
-
-#[derive(Debug, PartialEq)]
-struct Setting {
+#[derive(Debug, Default, PartialEq)]
+pub struct Puzzle {
     dic: HashMap<usize, usize>,
     last: usize,
     clock: usize,
 }
 
-impl ProblemSolver<String, usize, usize> for Setting {
-    const YEAR: usize = 2020;
-    const DAY: usize = 15;
+#[aoc(2020, 15)]
+impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = ",";
-    fn default() -> Self {
-        Setting {
-            dic: HashMap::new(),
-            last: 0,
-            clock: 0,
-        }
-    }
-    fn insert(&mut self, cs: String) {
-        if let Ok(n) = cs.trim().parse::<usize>() {
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        if let Ok(n) = block.trim().parse::<usize>() {
             self.clock += 1;
             *self.dic.entry(n).or_insert(0) = self.clock;
             self.last = n;
         }
+        Ok(())
     }
     fn part1(&mut self) -> usize {
         self.run_to(2020)
@@ -51,7 +31,7 @@ impl ProblemSolver<String, usize, usize> for Setting {
     }
 }
 
-impl Setting {
+impl Puzzle {
     fn run_to(&mut self, limit: usize) -> usize {
         self.clock += 1;
         loop {
@@ -76,44 +56,44 @@ impl Setting {
 mod test {
     use {
         super::*,
-        crate::y2020::traits::{Answer, Description},
+        crate::framework::{Answer, Description},
     };
 
     #[test]
     fn test_part1() {
         assert_eq!(
-            Setting::parse(Description::TestData("1,3,2".to_string())).run(1),
+            Puzzle::solve(Description::TestData("1,3,2".to_string()), 1),
             Answer::Part1(1)
         );
         assert_eq!(
-            Setting::parse(Description::TestData("2,1,3".to_string())).run(1),
+            Puzzle::solve(Description::TestData("2,1,3".to_string()), 1),
             Answer::Part1(10)
         );
         assert_eq!(
-            Setting::parse(Description::TestData("1,2,3".to_string())).run(1),
+            Puzzle::solve(Description::TestData("1,2,3".to_string()), 1),
             Answer::Part1(27)
         );
         assert_eq!(
-            Setting::parse(Description::TestData("2,3,1".to_string())).run(1),
+            Puzzle::solve(Description::TestData("2,3,1".to_string()), 1),
             Answer::Part1(78)
         );
         assert_eq!(
-            Setting::parse(Description::TestData("3,2,1".to_string())).run(1),
+            Puzzle::solve(Description::TestData("3,2,1".to_string()), 1),
             Answer::Part1(438)
         );
         assert_eq!(
-            Setting::parse(Description::TestData("3,1,2".to_string())).run(1),
+            Puzzle::solve(Description::TestData("3,1,2".to_string()), 1),
             Answer::Part1(1836)
         );
     }
     #[test]
     fn test_part2() {
         assert_eq!(
-            Setting::parse(Description::TestData("0,3,6".to_string())).run(2),
+            Puzzle::solve(Description::TestData("0,3,6".to_string()), 2),
             Answer::Part2(175594)
         );
         assert_eq!(
-            Setting::parse(Description::TestData("1,3,2".to_string())).run(2),
+            Puzzle::solve(Description::TestData("1,3,2".to_string()), 2),
             Answer::Part2(2578)
         );
     }
