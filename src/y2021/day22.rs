5bfd43d464616ce6d8d8c47f1e38b185bc41f9c0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2021-12-24 00:31:06 +0900

    y2021 day 22 done

--- src/y2021/day22.rs
+++ src/y2021/day22.rs
@@ -2,6 +2,8 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+
+use std::collections::HashSet;
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
@@ -100,15 +102,83 @@ impl AdventOfCode for Puzzle {
             .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut xs: Vec<isize> = self.line.iter().flat_map(|(_, x1, x2, _, _, _, _)| vec![*x1, *x2+1]).collect();
+        let mut ys: Vec<isize> = self.line.iter().flat_map(|(_, _, _, y1, y2, _, _)| vec![*y1, *y2+1]).collect();
+        let mut zs: Vec<isize> = self.line.iter().flat_map(|(_, _, _, _, _, z1, z2)| vec![*z1, *z2+1]).collect();
+
+        let mut hx: HashSet<isize> = HashSet::new();
+        for x in xs.iter() {
+            hx.insert(*x);
+        }
+        xs = hx.iter().copied().collect::<Vec<_>>();
+        let mut hy: HashSet<isize> = HashSet::new();
+        for y in ys.iter() {
+            hy.insert(*y);
+        }
+        ys = hy.iter().copied().collect::<Vec<_>>();
+        let mut hz: HashSet<isize> = HashSet::new();
+        for z in zs.iter() {
+            hz.insert(*z);
+        }
+        zs = hz.iter().copied().collect::<Vec<_>>();
+
+        xs.sort_unstable();
+        ys.sort_unstable();
+        zs.sort_unstable();
+        assert!(xs.windows(2).all(|a| a[0] != a[1]));
+        assert!(ys.windows(2).all(|a| a[0] != a[1]));
+        assert!(zs.windows(2).all(|a| a[0] != a[1]));
+        dbg!(xs.len(), ys.len(), zs.len());
+        dbg!(&xs[1..20]);
+        let mut to_index_x: HashMap<isize, usize> = HashMap::new();
+        let mut to_index_y: HashMap<isize, usize> = HashMap::new();
+        let mut to_index_z: HashMap<isize, usize> = HashMap::new();
+        for (i, x) in xs.iter().enumerate() {
+            to_index_x.insert(*x, i);
+        } 
+        for (i, y) in ys.iter().enumerate() {
+            to_index_y.insert(*y, i);
+        } 
+        for (i, z) in zs.iter().enumerate() {
+            to_index_z.insert(*z, i);
+        }
+        let mut grid: Vec<Vec<Vec<bool>>> = Vec::new();
+        for z in 0..zs.len() {
+            let mut w: Vec<Vec<bool>> = Vec::new();
+            for y in 0..ys.len() {
+                let mut v: Vec<bool> = Vec::new();
+                v.resize(xs.len(), false);
+                w.push(v);
+            }
+            grid.push(w);
+        }
+
+        for (to, x1, x2, y1, y2, z1, z2) in self.line.iter() {
+            for z in *to_index_z.get(z1).unwrap()..*to_index_z.get(&(*z2+1)).unwrap() {
+                for y in *to_index_y.get(y1).unwrap()..*to_index_y.get(&(*y2+1)).unwrap() {
+                    for x in *to_index_x.get(x1).unwrap()..*to_index_x.get(&(*x2+1)).unwrap() {
+                        grid[z][y][x] = *to;
+                    }
+                }
+            }
+        }
+        zs.windows(2)
+            .map(|pz|
+                 ys.windows(2)
+                 .map(|py|
+                      xs.windows(2)
+                      .map(|px|
+                           if grid[*to_index_z.get(&pz[0]).unwrap()][*to_index_y.get(&py[0]).unwrap()][*to_index_x.get(&px[0]).unwrap()] {
+                               ((pz[1] - pz[0]) * (py[1] - py[0]) * (px[1] - px[0])) as usize
+                           } else {
+                               0
+                           }
+                      )
+                      .sum::<usize>()
+                 )
+                 .sum::<usize>()
+            )
+            .sum()
     }
 }
 
-
-struct Voxel {
-    ons: Vec<(usize, usize, usize, usize, usize, usize)>,
-}
-
-impl Voxel {
-    
-}
