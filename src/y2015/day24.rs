1fbdc68e1950e9b3e4f307cee99b2498a0378f74
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-01 08:43:35 +0900

    y2015 day25 part1 done.

--- src/y2015/day24.rs
+++ src/y2015/day24.rs
@@ -15,32 +15,62 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<usize>,
 }
 
 #[aoc(2015, 24)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<Option<String>, ParseError> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(block.parse::<usize>()?);
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        println!("{:?}", self.line.iter().sum::<usize>() / 3);
     }
     fn part1(&mut self) -> Self::Output1 {
+        self.line.reverse();
+        let noptions = self.line.len();
+        let mut test = 0;
+        let target: usize = self.line.iter().sum::<usize>() / 3;
+        let m = self.line.iter().max().unwrap();
+        let mut bag: Vec<(usize, usize, [bool; 29], usize)>
+            = vec![(0, 1, [false; 29], 0)];
+        while let Some(smallest) = bag.iter().min() {
+            // smallest.3 = false;
+            let len = smallest.0;
+            let qe = smallest.1;
+            let sum = smallest.2.iter()
+                .enumerate()
+                .filter(|(_, b)| **b)
+                .map(|(i, _)| self.line[i])
+                .sum::<usize>();
+            if test < sum  {
+                println!("{}/{} (QE={})", sum, len, qe);
+                test = sum;
+            }
+            if sum == target {
+                println!("{:?}(QE = {:>3})",
+                         smallest.2.iter().enumerate().filter(|(_, b)| **b)
+                         .map(|(i, _)| self.line[i])
+                         .collect::<Vec<_>>(),
+                         qe
+                );
+                return qe;
+            }
+            // if 5 <= len { continue; }
+            let s = *smallest;
+            bag.retain(|k| *k != s);
+            for k in s.3..noptions {
+                if s.2[k] || target < sum + self.line[k] { continue; }
+                let mut new = s;
+                new.0 += 1;
+                new.1 *= self.line[k];
+                new.2[k] = true;
+                new.3 = k + 1 ;
+                bag.push(new);
+            }
+        }
         0
     }
     fn part2(&mut self) -> Self::Output2 {
