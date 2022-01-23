aaf0774fbe1696b354553769980a354945c9c4b7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-23 22:28:12 +0900

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,10 @@ pub mod y2021;
 // pub mod y2018;
 // #[cfg(feature = "y2017")]
 // pub mod y2017;
-#[cfg(feature = "y2016")]
-pub mod y2016;
 #[cfg(feature = "y2015")]
 pub mod y2015;
+#[cfg(feature = "y2016")]
+pub mod y2016;
 
 pub use aoc_macro::aoc_arms;
 pub use framework::Description;
