2d589136d0c17d9bb604e21fca3dc5e2570dd4dd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-23 23:16:11 +0900

    y2016 day2 done.

--- src/y2016/day02.rs
+++ src/y2016/day02.rs
@@ -1,66 +1,79 @@
 //! <https://adventofcode.com/2016/day/02>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
-    lazy_static::lazy_static,
-    regex::Regex,
-    std::collections::HashMap,
-};
+use crate::framework::{aoc_at, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<char>>,
 }
 
-#[aoc(2016, 2)]
+#[aoc_at(2016, 2)]
 impl AdventOfCode for Puzzle {
+    type Output1 = Vec<usize>;
+    type Output2 = Vec<char>;
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
-        }
-        let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line.push(block.trim().chars().collect::<Vec<char>>());
         Ok(())
     }
     fn after_insert(&mut self) {
         dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut key: Vec<usize> = Vec::new();
+        let mut x: isize = 0;
+        let mut y: isize = 0;
+        for v in self.line.iter() {
+            for ch in v.iter() {
+                match *ch {
+                    'U' => y = (y - 1).clamp(-1, 1),
+                    'D' => y = (y + 1).clamp(-1, 1),
+                    'L' => x = (x - 1).clamp(-1, 1),
+                    'R' => x = (x + 1).clamp(-1, 1),
+                    _ => unreachable!(),
+                }
+            }
+            key.push((3 * (y + 1) + (x + 1) + 1) as usize);
+        }
+        key
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        fn to_char(y: isize, x: isize) -> char {
+            match (y, x) {
+                (-2, 0) => '1',
+                (-1, -1) => '2',
+                (-1, 0) => '3',
+                (-1, 1) => '4',
+                (0, -2) => '5',
+                (0, -1) => '6',
+                (0, 0) => '7',
+                (0, 1) => '8',
+                (0, 2) => '9',
+                (1, -1) => 'A',
+                (1, 0) => 'B',
+                (1, 1) => 'C',
+                (2, 0) => 'D',
+                _ => unreachable!(),
+            }
+        }
+        fn clamp_2d(val: isize, range: isize) -> isize {
+            let r = 2 - range.abs();
+            val.clamp(-r, r)
+        }
+        let mut key: Vec<char> = Vec::new();
+        let mut x: isize = -2;
+        let mut y: isize = 0;
+        for v in self.line.iter() {
+            for ch in v.iter() {
+                match *ch {
+                    'U' => y = clamp_2d(y - 1, x),
+                    'D' => y = clamp_2d(y + 1, x),
+                    'L' => x = clamp_2d(x - 1, y),
+                    'R' => x = clamp_2d(x + 1, y),
+                    _ => unreachable!(),
+                }
+            }
+            key.push(to_char(y, x));
+        }
+        key
     }
-}
-
-#[cfg(feature = "y2016")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
