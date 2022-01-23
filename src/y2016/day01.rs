9e09db73ff001e532b0ea2d60f2fdd7979e77aaa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-23 22:28:22 +0900

    y2016 day1 done.

--- src/y2016/day01.rs
+++ src/y2016/day01.rs
@@ -1,66 +1,101 @@
 //! <https://adventofcode.com/2016/day/01>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     lazy_static::lazy_static,
     regex::Regex,
-    std::collections::HashMap,
+    std::collections::HashSet,
 };
+
+#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Turn {
+    Right(usize),
+    Left(usize),
+}
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Turn>,
 }
 
 #[aoc(2016, 1)]
 impl AdventOfCode for Puzzle {
-    const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
+    const DELIMITER: &'static str = ", ";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        dbg!(block);
         lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+            static ref PARSER: Regex = Regex::new(r"^([LR])([0-9]+)\n?$").expect("wrong");
         }
         let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        let dist = segment[2].parse::<usize>()?;
+        self.line.push(match &segment[1] {
+            "L" => Turn::Left(dist),
+            "R" => Turn::Right(dist),
+            _ => unreachable!(),
+        });
         Ok(())
     }
     fn after_insert(&mut self) {
         dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
+        let mut rot: isize = 0;
+        let mut x: isize = 0;
+        let mut y: isize = 0;
+        for inst in self.line.iter() {
+            let n: usize;
+            match inst {
+                Turn::Right(rn) => {
+                    rot = (rot + 5) % 4;
+                    n = *rn;
+                }
+                Turn::Left(rn) => {
+                    rot = (rot + 3) % 4;
+                    n = *rn;
+                }
+            }
+            match rot {
+                0 => y += n as isize,
+                1 => x += n as isize,
+                2 => y -= n as isize,
+                3 => x -= n as isize,
+                _ => unreachable!(),
+            }
+        }
+        (x.abs() + y.abs()) as usize
+    }
+    fn part2(&mut self) -> Self::Output1 {
+        let mut pos: HashSet<(isize, isize)> = HashSet::new();
+
+        let mut rot: isize = 0;
+        let mut x: isize = 0;
+        let mut y: isize = 0;
+        for inst in self.line.iter() {
+            let n: usize;
+            match inst {
+                Turn::Right(rn) => {
+                    rot = (rot + 5) % 4;
+                    n = *rn;
+                }
+                Turn::Left(rn) => {
+                    rot = (rot + 3) % 4;
+                    n = *rn;
+                }
+            }
+            for _ in 0..n {
+                match rot {
+                    0 => y += 1,
+                    1 => x += 1,
+                    2 => y -= 1,
+                    3 => x -= 1,
+                    _ => unreachable!(),
+                }
+                if pos.contains(&(x, y)) {
+                    return (x.abs() + y.abs()) as usize;
+                } else {
+                    pos.insert((x, y));
+                }
+            }
+        }
         0
     }
-    fn part2(&mut self) -> Self::Output2 {
-        0
-    }
-}
-
-#[cfg(feature = "y2021")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
