f1ae0eea5b402bf7205e5f7a4589300445c54540
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-01-24 21:34:19 +0900

    y2016 day03 done.

--- src/y2016/day03.rs
+++ src/y2016/day03.rs
@@ -1,66 +1,60 @@
 //! <https://adventofcode.com/2016/day/03>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     lazy_static::lazy_static,
     regex::Regex,
-    std::collections::HashMap,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<usize>>,
 }
 
 #[aoc(2016, 3)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         lazy_static! {
-            static ref PARSER: Regex = Regex::new(r"^([0-9]+)$").expect("wrong");
+            static ref PARSER: Regex =
+                Regex::new(r"^ +([0-9]+) +([0-9]+) +([0-9]+)$").expect("wrong");
         }
         let segment = PARSER.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line.push(vec![
+            segment[1].parse::<usize>()?,
+            segment[2].parse::<usize>()?,
+            segment[3].parse::<usize>()?,
+        ]);
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.line
+            .iter()
+            .filter(|vec| {
+                let mut v: Vec<usize> = vec.to_vec();
+                v.sort_unstable();
+                dbg!(&v);
+                v[2] < v[0] + v[1]
+            })
+            .count()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut count = 0;
+        for j in 0..self.line.len() / 3 {
+            for i in 0..3 {
+                let mut v = vec![
+                    self.line[j * 3][i],
+                    self.line[j * 3 + 1][i],
+                    self.line[j * 3 + 2][i],
+                ];
+                v.sort_unstable();
+                if v[2] < v[0] + v[1] {
+                    count += 1;
+                }
+            }
+        }
+        count
     }
-}
-
-#[cfg(feature = "y2016")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
