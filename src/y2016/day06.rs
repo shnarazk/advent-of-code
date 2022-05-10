af9533730196884f95fdb79c66777a3443027962
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-05-10 09:37:13 +0900

    new file:   bqn/2016/day06.bqn
    modified:   src/bin/adventofcode.rs
    new file:   src/y2016/day06.rs
    modified:   src/y2016/mod.rs

---
+++ src/y2016/day06.rs
@@ -0,0 +1,52 @@
+//! <https://adventofcode.com/2016/day/05>
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use {
+    crate::{
+        framework::{aoc, AdventOfCode, ParseError},
+        geometric::neighbors,
+        line_parser, regex,
+    },
+    md5::{Digest, Md5},
+    std::{collections::HashMap, process::Command},
+};
+
+#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Puzzle {
+    line: Vec<()>,
+}
+
+#[aoc(2016, 6)]
+impl AdventOfCode for Puzzle {
+    const DELIMITER: &'static str = "\n";
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        Ok(())
+    }
+    fn part1(&mut self) -> Self::Output1 {
+        if let Ok(output) = Command::new("bqn/2016/day06.bqn").output() {
+            println!("{}", String::from_utf8_lossy(&output.stdout));
+        }
+        0
+    }
+    fn part2(&mut self) -> Self::Output2 {
+        0
+    }
+}
+
+#[cfg(feature = "y2016")]
+#[cfg(test)]
+mod test {
+    use {
+        super::*,
+        crate::framework::{Answer, Description},
+    };
+
+    // #[test]
+    // fn test_part1() {
+    //     assert_eq!(
+    //         Puzzle::solve(Description::TestData("".to_string()), 1),
+    //         Answer::Part1(0)
+    //     );
+    // }
+}
