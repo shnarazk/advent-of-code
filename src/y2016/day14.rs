ed43000466126cf66d75d26168d5934a01ab00f2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-05-29 14:19:50 +0900

    y2016 day14 part1 done

--- src/y2016/day14.rs
+++ src/y2016/day14.rs
@@ -2,6 +2,7 @@
 use {
     crate::framework::{aoc, AdventOfCode, ParseError},
     md5::{Digest, Md5},
+    std::collections::VecDeque,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -16,15 +17,54 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
+        let limit = 64;
+        let mut passed = 0;
+        let mut queue: VecDeque<(String, Vec<char>)> = VecDeque::new();
+        let salt = "ahsbgdzn";
+        // let salt = "abc";
         let mut hasher = Md5::new();
-        let mut count = 0;
-        for i in 0.. {
-            hasher.update(format!("wtnhxymk{i}"));
+        // let mut count = 0;
+        for i in 0..1000 {
+            hasher.update(format!("{salt}{i}"));
             let result = hasher.finalize_reset();
-            if result[0] == 0 && result[1] == 0 && result[2] >> 4 == 0 {
-                println!("{:x}", result);
-                count += 1;
-                if 8 <= count {
+            let phrase = format!("{:x}", result);
+            let occ5 = phrase
+                .chars()
+                .collect::<Vec<char>>()
+                .windows(5)
+                .filter(|s| s.iter().all(|c| *c == s[0]))
+                .map(|s| s[0])
+                .collect::<Vec<_>>();
+
+            queue.push_back((phrase, occ5));
+        }
+        let mut check = 0;
+        // let's start generate and check phase.
+        'next_phrase: while passed < limit {
+            // generate
+            hasher.update(format!("{}{}", salt, check + 1000));
+            let result = hasher.finalize_reset();
+            let phrase = format!("{:x}", result);
+            let occ5 = phrase
+                .chars()
+                .collect::<Vec<char>>()
+                .windows(5)
+                .filter(|s| s.iter().all(|c| *c == s[0]))
+                .map(|s| s[0])
+                .collect::<Vec<_>>();
+
+            queue.push_back((phrase, occ5));
+            check += 1;
+            // check
+            let phrase = queue.pop_front().unwrap();
+            for s in phrase.0.chars().collect::<Vec<char>>().windows(3) {
+                if s[0] == s[1] && s[0] == s[2] {
+                    let ch = s[0];
+                    if queue.iter().any(|(_, cs)| cs.contains(&ch)) {
+                        passed += 1;
+                        println!("{:3>}, {:9>}: {:?}", passed, check - 1, phrase);
+                        continue 'next_phrase;
+                    }
                     break;
                 }
             }
