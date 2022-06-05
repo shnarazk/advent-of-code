26f41d9b68cfbecba7ae3e06aec73c351e8a9917
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-06-05 17:54:16 +0900

    Catch up

---
+++ src/y2016/day16.rs
@@ -0,0 +1,21 @@
+//! <https://adventofcode.com/2016/day/16>
+use crate::framework::{aoc, AdventOfCode, ParseError};
+
+#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Puzzle {}
+
+#[aoc(2016, 16)]
+impl AdventOfCode for Puzzle {
+    const DELIMITER: &'static str = "\n";
+    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
+        Ok(())
+    }
+    fn part1(&mut self) -> Self::Output1 {
+        println!("This puzzle was solved by Cuis-Smalltalk.");
+        0
+    }
+    fn part2(&mut self) -> Self::Output2 {
+        println!("This puzzle was solved by Cuis-Smalltalk.");
+        0
+    }
+}
