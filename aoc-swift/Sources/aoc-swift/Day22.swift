078ed683e67f47d667542ab7c30c24969dbca5bd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-06-12 10:35:49 +0900

    Prepare for y2016 day23

---
+++ aoc-swift/Sources/aoc-swift/Day22.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by 楢崎修二 on 2022/06/12.
+//
+
+import Foundation
--- src/y2016/day23.rs
+++ src/y2016/day23.rs
@@ -11,22 +11,22 @@ use {
     std::collections::HashMap,
 };
 
-#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
-enum Reg {
-    R(char),
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+enum Val {
+    Reg(u8),
     Lit(isize),
 }
 
-#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 enum Code {
-    Cpy(Reg, Reg),
-    Inc(Reg),
-    Dec(Reg),
-    Jnp(isize, Reg),
-    Tgl(Reg),
+    Cpy(Val, Val),
+    Inc(Val),
+    Dec(Val),
+    Jnz(Val, Val),
+    Tgl(Val),
 }
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
     line: Vec<Code>,
 }
@@ -35,13 +35,46 @@ pub struct Puzzle {
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        if let Ok(segment) = parser.captures(block).ok_or(ParseError) {}
-        // self.line.push(segment[0].parse::<_>());
+        let parse_val = |s: &str| {
+            let c: char = s.chars().next().unwrap();
+            if c.is_ascii_alphabetic() {
+                Val::Reg(c as u8 - b'a')
+            } else {
+                Val::Lit(s.parse::<isize>().unwrap())
+            }
+        };
+        if let Ok(code) = regex!(r"^cpy ([a-z]|-?\d+) ([a-z]|-?\d+)$")
+            .captures(block)
+            .ok_or(ParseError)
+        {
+            self.line
+                .push(Code::Cpy(parse_val(&code[1]), parse_val(&code[2])));
+        } else if let Ok(code) = regex!(r"^inc ([a-z]|-?\d+)$")
+            .captures(block)
+            .ok_or(ParseError)
+        {
+            self.line.push(Code::Inc(parse_val(&code[1])));
+        } else if let Ok(code) = regex!(r"^dec ([a-z]|-?\d+)$")
+            .captures(block)
+            .ok_or(ParseError)
+        {
+            self.line.push(Code::Dec(parse_val(&code[1])));
+        } else if let Ok(code) = regex!(r"^jnz ([a-z]|-?\d+) ([a-z]|-?\d+)$")
+            .captures(block)
+            .ok_or(ParseError)
+        {
+            self.line
+                .push(Code::Jnz(parse_val(&code[1]), parse_val(&code[2])));
+        } else if let Ok(code) = regex!(r"^tgl ([a-z]|-?\d+)$")
+            .captures(block)
+            .ok_or(ParseError)
+        {
+            self.line.push(Code::Tgl(parse_val(&code[1])));
+        }
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
         0
@@ -49,21 +82,4 @@ impl AdventOfCode for Puzzle {
     fn part2(&mut self) -> Self::Output2 {
         0
     }
-}
-
-#[cfg(feature = "y2016")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
