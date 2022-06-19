efa2443092e8a0cf15b33f7ffa73e86517011e71
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-06-19 11:21:09 +0900

    y2019 day1 part1 done

--- AdventOfCodeSwift/AdventOfCodeSwift/Day1.swift
+++ AdventOfCodeSwift/AdventOfCodeSwift/Day1.swift
@@ -11,13 +11,31 @@ let day1 = PuzzleDescripter(year: 2019, day: 1, title: "The Tyranny of the Rocke
 
 class Day1: Solver {
     var input: String?
-    var inputFrom = URL(string: "https://adventofcode.com/2019/day/1/input")!
+    var inputFrom =  "input-y2019-day01" // URL(string: "https://adventofcode.com/2019/day/1/input")!
     private let semaphore = DispatchSemaphore(value: 0)
     func reset() {
-        self.visit(page: inputFrom)
+        // self.visit(page: inputFrom)
+        // input = Bundle.main.path(forResource: inputFrom, ofType: "txt")
+        guard let path = Bundle.main.path(forResource: inputFrom, ofType: "txt") else {
+            fatalError("Couldn't load \(inputFrom).txt")
+        }
+        do {
+            input = try String(contentsOfFile: path)
+        } catch {
+            fatalError("Couldn't read \(inputFrom).txt")
+        }
     }
     func part1() -> String? {
-        return "got input from \(inputFrom): \(input ?? "nil")"
+        var total: Int = 0
+        // return "got input from \(inputFrom): \(input ?? "nil")"
+        guard let input else { return nil }
+        for l in input.split(separator: "\n") {
+            guard let n = Int(l) else {
+                fatalError("Something is wrong.")
+            }
+            total += n / 3 - 2
+        }
+        return "\(total)"
     }
     func part2() -> String? { return nil }
     private func visit(page url: URL) {
