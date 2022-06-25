0e20596b1822f35f21d43f6c71fd347c6afdbed2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-06-25 17:22:40 +0900

    y2019 day03 part2 done.

--- src/y2019/day03.rs
+++ src/y2019/day03.rs
@@ -1,16 +1,10 @@
 //! <https://adventofcode.com/2019/day/3>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-
-use std::collections::HashSet;
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
+        regex,
     },
-    std::collections::HashMap,
+    std::collections::{HashMap, HashSet},
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -76,23 +70,47 @@ impl AdventOfCode for Puzzle {
         dbg!(lowest)
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut checked: HashMap<(isize, isize), usize> = HashMap::new();
+        let mut oy = 0;
+        let mut ox = 0;
+        let mut time = 0;
+        for (dy, dx) in self.line[0].iter() {
+            for y in 0..=dy.abs() {
+                for x in 0..=dx.abs() {
+                    if y == 0 && x == 0 {
+                        continue;
+                    }
+                    oy += dy.signum();
+                    ox += dx.signum();
+                    time += 1;
+                    checked.insert((oy, ox), time);
+                }
+            }
+        }
+        dbg!(checked.len());
+        let mut lowest = usize::MAX / 4;
+        oy = 0;
+        ox = 0;
+        time = 0;
+        checked.remove(&(0, 0));
+        for (dy, dx) in self.line[1].iter() {
+            for y in 0..=dy.abs() {
+                for x in 0..=dx.abs() {
+                    if y == 0 && x == 0 {
+                        continue;
+                    }
+                    oy += dy.signum();
+                    ox += dx.signum();
+                    time += 1;
+                    if let Some(t) = checked.get(&(oy, ox)) {
+                        println!("({}, {}) @ {}", oy, ox, t);
+                        if time + t < lowest {
+                            lowest = time + t;
+                        }
+                    }
+                }
+            }
+        }
+        dbg!(lowest)
     }
-}
-
-#[cfg(feature = "y2019")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
