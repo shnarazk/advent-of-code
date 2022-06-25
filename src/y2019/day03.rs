f142105d17ee9b328a03d9b9bef71a2f82b86017
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-06-25 17:08:23 +0900

    y2019 day03 part1 done

--- src/y2019/day03.rs
+++ src/y2019/day03.rs
@@ -2,6 +2,8 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+
+use std::collections::HashSet;
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
@@ -20,7 +22,7 @@ pub struct Puzzle {
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(&block);
+        // dbg!(&block);
         let parser = regex!(r"^([URDL])(\d+)");
         let mut vec = Vec::new();
         for segment in block.split(',') {
@@ -38,10 +40,40 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut checked: HashSet<(isize, isize)> = HashSet::new();
+        let mut oy = 0;
+        let mut ox = 0;
+        for (dy, dx) in self.line[0].iter() {
+            for y in 0.min(*dy)..=0.max(*dy) {
+                for x in 0.min(*dx)..=0.max(*dx) {
+                    checked.insert((oy + y, ox + x));
+                }
+            }
+            oy += dy;
+            ox += dx;
+        }
+        dbg!(checked.len());
+        let mut lowest = usize::MAX / 4;
+        oy = 0;
+        ox = 0;
+        checked.remove(&(0, 0));
+        for (dy, dx) in self.line[1].iter() {
+            for y in 0.min(*dy)..=0.max(*dy) {
+                for x in 0.min(*dx)..=0.max(*dx) {
+                    let py = oy + y;
+                    let px = ox + x;
+                    if checked.contains(&(py, px)) && (py.abs() + px.abs()) < (lowest as isize) {
+                        lowest = (py.abs() + px.abs()) as usize;
+                    }
+                }
+            }
+            oy += dy;
+            ox += dx;
+        }
+        dbg!(lowest)
     }
     fn part2(&mut self) -> Self::Output2 {
         0
