0a550794917f3a3c1cc4801c7e234077c9d82c78
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-06-28 14:38:55 +0900

    y2019 day6 part1 done

--- src/y2019/day06.rs
+++ src/y2019/day06.rs
@@ -8,55 +8,43 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::collections::HashSet,
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(String, String)>,
+    orbit: HashSet<(String, String)>,
 }
 
 #[aoc(2019, 6)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
+        let parser = regex!(r"^(.{3})\)(.{3})$");
         let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line
+            .push((segment[1].to_string(), segment[2].to_string()));
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
+        for (a, b) in self.line.iter() {
+            self.orbit.insert((a.clone(), b.clone()));
+        }
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut to_visit: Vec<(String, usize)> = vec![("COM".to_string(), 0)];
+        let mut count = 0;
+        while let Some((target, dist)) = to_visit.pop() {
+            count += dist;
+            for (_, child) in self.orbit.iter().filter(|(k, _)| *k == target) {
+                to_visit.push((child.to_string(), dist + 1));
+            }
+        }
+        count
     }
     fn part2(&mut self) -> Self::Output2 {
         0
     }
-}
-
-#[cfg(feature = "y2019")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
