a5f1afa181a459a0c280bf7b0259bbe8426b85d7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-06-29 00:51:34 +0900

    ditto

--- src/y2019/day07.rs
+++ src/y2019/day07.rs
@@ -1,7 +1,10 @@
 //! <https://adventofcode.com/2019/day/7>
-use crate::{
-    framework::{aoc, AdventOfCode, ParseError},
-    line_parser,
+use {
+    crate::{
+        framework::{aoc, AdventOfCode, ParseError},
+        line_parser,
+    },
+    std::collections::VecDeque,
 };
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -22,22 +25,28 @@ impl AdventOfCode for Puzzle {
     fn part1(&mut self) -> Self::Output1 {
         let mut highest_score = 0;
         for init_a in 0..5 {
-            let out_a = self.clone().execute(vec![init_a, 0]);
+            let out_a = self.clone().execute(&mut VecDeque::from(vec![init_a, 0]));
             for init_b in 0..5 {
                 if init_b == init_a {
                     continue;
                 }
-                let out_b = self.clone().execute(vec![init_b, out_a[0]]);
+                let out_b = self
+                    .clone()
+                    .execute(&mut VecDeque::from(vec![init_b, out_a[0]]));
                 for init_c in 0..5 {
                     if init_c == init_b || init_c == init_a {
                         continue;
                     }
-                    let out_c = self.clone().execute(vec![init_c, out_b[0]]);
+                    let out_c = self
+                        .clone()
+                        .execute(&mut VecDeque::from(vec![init_c, out_b[0]]));
                     for init_d in 0..5 {
                         if init_d == init_c || init_d == init_b || init_d == init_a {
                             continue;
                         }
-                        let out_d = self.clone().execute(vec![init_d, out_c[0]]);
+                        let out_d = self
+                            .clone()
+                            .execute(&mut VecDeque::from(vec![init_d, out_c[0]]));
                         for init_e in 0..5 {
                             if init_e == init_d
                                 || init_e == init_c
@@ -46,7 +55,9 @@ impl AdventOfCode for Puzzle {
                             {
                                 continue;
                             }
-                            let out_e = self.clone().execute(vec![init_e, out_d[0]]);
+                            let out_e = self
+                                .clone()
+                                .execute(&mut VecDeque::from(vec![init_e, out_d[0]]));
                             if highest_score < out_e[0] {
                                 println!("{},{},{},{},{}", init_a, init_b, init_c, init_d, init_e);
                                 highest_score = out_e[0];
@@ -59,7 +70,7 @@ impl AdventOfCode for Puzzle {
         dbg!(highest_score) as usize
     }
     fn part2(&mut self) -> Self::Output2 {
-        let mut amp = vec![
+        let mut amp: [Puzzle; 5] = [
             self.clone(),
             self.clone(),
             self.clone(),
@@ -67,17 +78,34 @@ impl AdventOfCode for Puzzle {
             self.clone(),
         ];
         let mut highest_score = 0;
-        // for perm in permutations(0, 5) {
-        //     todo!();
-        // }
+        for perm in permutation(0, 5).into_iter() {
+            let mut channel: [VecDeque<isize>; 5] = [
+                VecDeque::new(),
+                VecDeque::new(),
+                VecDeque::new(),
+                VecDeque::new(),
+                VecDeque::new(),
+            ];
+            for (i, v) in perm.iter().enumerate() {
+                channel[i].push_back(*v as isize);
+            }
+            for i in 0..5 {
+                let output = amp[i].execute(&mut channel[0]);
+                for o in output.iter() {
+                    channel[(i + 1) % 5].push_back(*o);
+                }
+            }
+            if highest_score < *channel[0].front().unwrap() {
+                highest_score = *channel[0].front().unwrap();
+            }
+        }
         dbg!(highest_score) as usize
     }
 }
 
 impl Puzzle {
-    fn execute(&mut self, inputs: Vec<isize>) -> Vec<isize> {
+    fn execute(&mut self, inputs: &mut VecDeque<isize>) -> Vec<isize> {
         let memory = &mut self.line;
-        let mut input_index = 0;
         let mut output: Vec<isize> = Vec::new();
         let mut pc = 0;
         loop {
@@ -116,8 +144,7 @@ impl Puzzle {
                 }
                 3 => {
                     let dst = fetch!(1, true);
-                    memory[dst] = inputs[input_index];
-                    input_index += 1;
+                    memory[dst] = inputs.pop_front().expect("not enough input");
                     pc += 2;
                 }
                 4 => {
@@ -166,4 +193,24 @@ impl Puzzle {
         assert!(1 == output.len());
         output
     }
+}
+
+fn permutation(from: usize, to: usize) -> Vec<Vec<usize>> {
+    fn perm(cands: &[usize]) -> Vec<Vec<usize>> {
+        let mut ret = Vec::new();
+        for c in cands.iter() {
+            let remains = cands
+                .iter()
+                .filter(|i| *i != c)
+                .copied()
+                .collect::<Vec<usize>>();
+            for mut v in perm(&remains).into_iter() {
+                v.push(*c);
+                ret.push(v);
+            }
+        }
+        ret
+    }
+    let cands = (from..to).collect::<Vec<usize>>();
+    perm(&cands)
 }
