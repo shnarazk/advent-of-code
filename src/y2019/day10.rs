3a350d90628870f79d0eaa2ef408c18d6c2a558b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-07-01 18:22:22 +0900

    y2019 day10 part1 done

--- src/y2019/day10.rs
+++ src/y2019/day10.rs
@@ -6,9 +6,9 @@ use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
         geometric::neighbors,
-        line_parser, regex,
+        math::gcd,
     },
-    std::collections::HashMap,
+    std::collections::HashSet,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -25,13 +25,60 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        let height = self.line.len();
+        let width = self.line[0].len();
+        dbg!(height, width);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let mut best = (0, 0);
+        let mut max_visible = 0;
+        for (j, l) in self.line.iter().enumerate() {
+            for (i, p) in l.iter().enumerate() {
+                if !self.at(j, i) {
+                    continue;
+                }
+                let mut visible: HashSet<(isize, isize)> = HashSet::new();
+                for (jj, ll) in self.line.iter().enumerate() {
+                    for (ii, pp) in l.iter().enumerate() {
+                        if self.at(jj, ii) {
+                            let dy = jj as isize - j as isize;
+                            let dx = ii as isize - i as isize;
+                            match (dy, dx) {
+                                (0, 0) => (),
+                                (0, _) => {
+                                    visible.insert((0, dx.signum()));
+                                }
+                                (_, 0) => {
+                                    visible.insert((dy.signum(), 0));
+                                }
+                                _ => {
+                                    let factor = gcd(dy.unsigned_abs(), dx.unsigned_abs()) as isize;
+                                    visible.insert((dy / factor, dx / factor));
+                                }
+                            }
+                        }
+                    }
+                }
+                let n = visible.len();
+                if max_visible < n {
+                    max_visible = n;
+                    best = (j, i);
+                }
+            }
+        }
+        dbg!(&best);
+        max_visible
     }
     fn part2(&mut self) -> Self::Output2 {
         0
+    }
+}
+
+impl Puzzle {
+    fn at(&self, y: usize, x: usize) -> bool {
+        self.line[y][x]
     }
 }
 
