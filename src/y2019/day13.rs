5dc795726c5a02a522cabf7b6a46c65b2c7f934f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-07-03 16:22:20 +0900

    Prepare for y2019 day13-20

---
+++ src/y2019/day13.rs
@@ -0,0 +1,52 @@
+//! <https://adventofcode.com/2019/day/13>
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use {
+    crate::{
+        framework::{aoc, AdventOfCode, ParseError},
+        geometric::neighbors,
+        line_parser, regex,
+    },
+    std::collections::HashMap,
+};
+
+#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Puzzle {
+    line: Vec<isize>,
+}
+
+#[aoc(2019, 13)]
+impl AdventOfCode for Puzzle {
+    const DELIMITER: &'static str = "\n";
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        self.line = line_parser::to_isizes(block, ',')?;
+        Ok(())
+    }
+    fn after_insert(&mut self) {
+        dbg!(&self.line.len());
+    }
+    fn part1(&mut self) -> Self::Output1 {
+        0
+    }
+    fn part2(&mut self) -> Self::Output2 {
+        0
+    }
+}
+
+#[cfg(feature = "y2019")]
+#[cfg(test)]
+mod test {
+    use {
+        super::*,
+        crate::framework::{Answer, Description},
+    };
+
+    // #[test]
+    // fn test_part1() {
+    //     assert_eq!(
+    //         Puzzle::solve(Description::TestData("".to_string()), 1),
+    //         Answer::Part1(0)
+    //     );
+    // }
+}
