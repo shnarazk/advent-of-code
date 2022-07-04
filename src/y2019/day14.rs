7d67b58bc5c9b50f035bf6cf2673a7763c55009b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-07-05 07:56:40 +0900

    Prepare for y2019 day14

--- src/y2019/day14.rs
+++ src/y2019/day14.rs
@@ -11,32 +11,43 @@ use {
     std::collections::HashMap,
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+type ChemicalUnit = (String, usize);
+
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(Vec<ChemicalUnit>, ChemicalUnit)>,
+}
+
+fn parse_chemical_unit(s: &str) -> Result<ChemicalUnit, ParseError> {
+    let parser = regex!(r"^(\d+) ([A-Z]+)");
+    let segment = parser.captures(s).ok_or(ParseError)?;
+    Ok((segment[2].to_string(), segment[1].parse::<usize>()?))
 }
 
 #[aoc(2019, 14)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
+        let parser = regex!(r"^((\d+ [A-Z]+, )*)(\d+ [A-Z]+) => (\d+ [A-Z]+)");
         let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        let mut vec = segment[1]
+            .split(", ")
+            .filter(|seg| !seg.is_empty())
+            .map(|seg| parse_chemical_unit(seg).unwrap())
+            .collect::<Vec<ChemicalUnit>>();
+        vec.push(parse_chemical_unit(&segment[3])?);
+        self.line.push((vec, parse_chemical_unit(&segment[4])?));
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
+        let mut hash: HashMap<&str, (&[ChemicalUnit], usize)> = HashMap::new();
+        for (vec, (c, u)) in self.line.iter() {
+            hash.insert(c, (vec, *u));
+        }
+        dbg!(&hash);
         0
     }
     fn part2(&mut self) -> Self::Output2 {
