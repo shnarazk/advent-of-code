8ce480d38b260e384d61fd61a042d0633fe3caa7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-07-08 17:39:34 +0900

    y2019 day16 part1 done

--- src/y2019/day16.rs
+++ src/y2019/day16.rs
@@ -13,40 +13,45 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<u8>,
+    line: Vec<isize>,
 }
 
 #[aoc(2019, 16)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        self.line = block.chars().map(|c| c as u8 - b'0').collect::<Vec<u8>>();
+        self.line = block
+            .chars()
+            .map(|c| (c as u8 - b'0') as isize)
+            .collect::<Vec<isize>>();
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let base_pattern: Vec<isize> = vec![0, 1, 0, -1];
+        let mut scratch = self.line.clone();
+        for _ in 0..100 {
+            for (step, x) in scratch.iter_mut().enumerate() {
+                let mut result = 0;
+                for (i, p) in self.line.iter().enumerate() {
+                    let ptn = base_pattern[((i + 1) / (step + 1)) % 4];
+                    result += p * ptn;
+                    // print!("{ptn}, ");
+                }
+                // println!("{:?}", result);
+                *x = result;
+            }
+            for (i, v) in scratch.iter().enumerate() {
+                self.line[i] = (*v % 10).abs();
+            }
+            // println!("{:?}", &self.line);
+        }
+        println!("{:?}", &self.line[0..8]);
+        self.line.iter().take(8).fold(0, |sum, d| sum * 10 + *d) as usize
     }
     fn part2(&mut self) -> Self::Output2 {
         0
     }
-}
-
-#[cfg(feature = "y2019")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
