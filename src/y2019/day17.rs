998da7c11b0c438ca576b63980fce0d594419592
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-07-12 00:50:02 +0900

    Under construction; it's hard.

--- src/y2019/day17.rs
+++ src/y2019/day17.rs
@@ -16,6 +16,7 @@ pub struct Puzzle {
     memory: HashMap<usize, isize>,
     pc: usize,
     r_base: usize,
+    cross_points: Vec<Location>,
 }
 
 #[derive(Copy, Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -95,18 +96,17 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line.len());
+        // dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
         let map: HashMap<Location, u8> = self.recognize();
         for y in -1..46 {
             print!("|");
             for x in -2..39 {
-                print!("{}", *map.get(&Location(y, x)).unwrap_or(&b' ') as char);
+                print!("{}", *map.get(&Location(y, x)).unwrap_or(&b'?') as char);
             }
             println!("|");
         }
-        let mut count: usize = 0;
         let mut val: usize = 0;
         for (loc, cell) in map.iter() {
             if *cell != b'#' {
@@ -118,46 +118,179 @@ impl AdventOfCode for Puzzle {
                 .filter(|l| map.get(l) == Some(&b'#'))
                 .count()
             {
-                count += 1;
+                self.cross_points.push(*loc);
                 val += (loc.0 * loc.1) as usize;
             }
         }
-        dbg!(count);
+        dbg!(self.cross_points.len());
         val
     }
     fn part2(&mut self) -> Self::Output2 {
+        #[derive(Clone, Copy, Eq, Debug, Hash, PartialEq)]
+        enum Segment {
+            L(usize),
+            R(usize),
+        }
         let map: HashMap<Location, u8> = self.recognize();
-        let mut location: Location = *map.iter().find(|(_, v)| **v == b'^').unwrap().0;
-        let mut direction = Direction::North;
-        let mut prev_location: Location = location;
-        let mut trace: Vec<u8> = Vec::new();
-        let mut last_action: u8 = 0;
-        while let Some(action) = seek(&map, location, direction, prev_location) {
-            let act = if action == last_action { b'F' } else { action };
-            trace.push(act);
-            match act {
+        for (loc, cell) in map.iter() {
+            if *cell != b'#' {
+                continue;
+            }
+            if 2 < loc
+                .neighbors()
+                .iter()
+                .filter(|l| map.get(l) == Some(&b'#'))
+                .count()
+            {
+                self.cross_points.push(*loc);
+            }
+        }
+        self.cross_points.sort();
+        assert_eq!(12, self.cross_points.len());
+        let mut valids = 0;
+        let mut min_kinds = 0;
+        let mut best: Vec<Segment> = Vec::new();
+        for permutation in 0..3usize.pow(12_u32) {
+            let mut location: Location = *map.iter().find(|(_, v)| **v == b'^').unwrap().0;
+            let mut direction = Direction::North;
+            let mut prev_location: Location = location;
+            let mut trace: Vec<u8> = Vec::new();
+            let mut last_action: u8 = 0;
+            let mut debug_trace = std::collections::HashSet::new();
+            let dir = |i: u32| permutation / 3_usize.pow(i) % 3_usize.pow(i + 1);
+            while let Some(mut action) = seek(&map, location, direction, prev_location) {
+                if let Some((i, _)) = self
+                    .cross_points
+                    .iter()
+                    .enumerate()
+                    .find(|(_, l)| **l == location)
+                {
+                    match dir(i as u32) {
+                        1 => action = b'R',
+                        2 => action = b'L',
+                        _ => action = b'F',
+                    }
+                }
+                // if location == self.cross_points[0] || location == self.cross_points[5] {
+                //     action = b'R';
+                // } else if location == self.cross_points[4] {
+                //     // action = b'L';
+                // }
+                let act = if action == last_action { b'F' } else { action };
+                trace.push(act);
+                debug_trace.insert(location);
+                match act {
+                    b'F' => {
+                        prev_location = location;
+                        location = location + direction.as_location();
+                    }
+                    b'L' => {
+                        direction = direction.rotate_counterclockwise();
+                    }
+                    b'R' => {
+                        direction = direction.rotate_clockwise();
+                    }
+                    _ => unreachable!(),
+                }
+                last_action = act;
+            }
+            debug_trace.insert(location);
+            if 259 == debug_trace.len() {
+                valids += 1;
+                let mut kinds = HashMap::new();
+                // dbg!(valids, permutation);
+                let mut segments: Vec<Segment> = Vec::new();
+                let mut clockwise = false;
+                let mut steps = 0;
+                for a in trace.iter() {
+                    match *a {
+                        b'F' => {
+                            steps += 1;
+                        }
+                        b'R' | b'L' => {
+                            if 0 < steps {
+                                segments.push(if clockwise {
+                                    Segment::R(steps)
+                                } else {
+                                    Segment::L(steps)
+                                });
+                                steps = 0;
+                            }
+                            clockwise = *a == b'R';
+                        }
+                        _ => unreachable!(),
+                    }
+                }
+                segments.push(if clockwise {
+                    Segment::R(steps)
+                } else {
+                    Segment::L(steps)
+                });
+                for seg in segments.iter() {
+                    *kinds.entry(seg).or_insert(0) += 1;
+                }
+                if min_kinds < *kinds.values().min().unwrap() {
+                    min_kinds = *kinds.values().min().unwrap();
+                    best = segments;
+                }
+            }
+            // assert_eq!(259, debug_trace.len());
+        }
+        println!("{min_kinds} by {best:?}");
+        assert_eq!(0, valids);
+        // for y in -1..46 {
+        //     print!("|");
+        //     for x in -2..39 {
+        //         print!(
+        //             "{}",
+        //             if debug_trace.contains(&Location(y, x)) {
+        //                 'R'
+        //             } else {
+        //                 *map.get(&Location(y, x)).unwrap_or(&b'?') as char
+        //             }
+        //         );
+        //     }
+        //     println!("|");
+        // }
+        // dbg!(&location);
+        // for c in trace.iter() {
+        //     print!("{}", *c as char);
+        // }
+        // println!();
+        /*
+        let mut segments: Vec<Segment> = Vec::new();
+        let mut clockwise = false;
+        let mut steps = 0;
+        for a in trace.iter() {
+            match *a {
                 b'F' => {
-                    prev_location = location;
-                    location = location + direction.as_location();
+                    steps += 1;
                 }
-                b'L' => {
-                    direction = direction.rotate_counterclockwise();
-                }
-                b'R' => {
-                    direction = direction.rotate_clockwise();
+                b'R' | b'L' => {
+                    if 0 < steps {
+                        segments.push(if clockwise {
+                            Segment::R(steps)
+                        } else {
+                            Segment::L(steps)
+                        });
+                        steps = 0;
+                    }
+                    clockwise = *a == b'R';
                 }
                 _ => unreachable!(),
             }
-            last_action = act;
         }
-        dbg!(&location);
-        for c in trace.iter() {
-            print!("{}", *c as char);
-        }
-        println!();
+        segments.push(if clockwise {
+            Segment::R(steps)
+        } else {
+            Segment::L(steps)
+        });
+        println!("{segments:?}");
         self.line[0] = 2;
         self.initialize();
         map.len()
+        */
+        0
     }
 }
 
@@ -207,13 +340,13 @@ impl Puzzle {
             map.insert(Location(y, x), cell);
             x += 1;
         }
-        for y in -1..46 {
-            print!("|");
-            for x in -2..39 {
-                print!("{}", *map.get(&Location(y, x)).unwrap_or(&b' ') as char);
-            }
-            println!("|");
-        }
+        // for y in -1..46 {
+        //     print!("|");
+        //     for x in -2..39 {
+        //         print!("{}", *map.get(&Location(y, x)).unwrap_or(&b' ') as char);
+        //     }
+        //     println!("|");
+        // }
         map
     }
 }
