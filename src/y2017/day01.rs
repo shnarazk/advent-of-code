144f525813559a764ecceed8871363696a0d0ec7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-03 20:37:20 +0900

    y2017 day1 part1 done

--- src/y2017/day01.rs
+++ src/y2017/day01.rs
@@ -13,31 +13,30 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<usize>,
 }
 
 #[aoc(2017, 1)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line = block
+            .chars()
+            .map(|c| (c as u8 - b'0') as usize)
+            .collect::<Vec<_>>();
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut data = self.line.clone();
+        data.push(data[0]);
+        dbg!(data
+            .windows(2)
+            .filter(|v| v[0] == v[1])
+            .map(|v| v[0])
+            .sum::<usize>())
     }
     fn part2(&mut self) -> Self::Output2 {
         0
