342cd8dee60bfd9820bae1011026239cb4e19f17
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-06 15:42:00 +0900

    y2017 day5 part2 done.

--- src/y2017/day05.rs
+++ src/y2017/day05.rs
@@ -33,15 +33,28 @@ impl AdventOfCode for Puzzle {
         let mut steps: usize = 0;
         while let Some(ptr) = self.line.get(pc as usize) {
             steps += 1;
-            let addr = *ptr;
+            let offset = *ptr;
             self.line[pc as usize] += 1;
-            pc += addr;
+            pc += offset;
             // println!("{:?}, pc = {}", self.line, pc);
         }
         steps
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut pc: isize = 0;
+        let mut steps: usize = 0;
+        while let Some(ptr) = self.line.get(pc as usize) {
+            steps += 1;
+            let offset = *ptr;
+            if 3 <= offset {
+                self.line[pc as usize] -= 1;
+            } else {
+                self.line[pc as usize] += 1;
+            }
+            pc += offset;
+            // println!("{:?}, pc = {}", self.line, pc);
+        }
+        steps
     }
 }
 
