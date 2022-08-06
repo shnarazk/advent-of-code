736652caf04e3cdf6e804933ea7727bd50459d57
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-06 15:43:05 +0900

    Clean up

--- src/y2017/day05.rs
+++ src/y2017/day05.rs
@@ -1,15 +1,5 @@
 //! <https://adventofcode.com/2017/day/5>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
-};
+use crate::framework::{aoc, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
@@ -52,25 +42,7 @@ impl AdventOfCode for Puzzle {
                 self.line[pc as usize] += 1;
             }
             pc += offset;
-            // println!("{:?}, pc = {}", self.line, pc);
         }
         steps
     }
-}
-
-#[cfg(feature = "y2017")]
-#[cfg(test)]
-mod test {
-    use {
-        super::*,
-        crate::framework::{Answer, Description},
-    };
-
-    // #[test]
-    // fn test_part1() {
-    //     assert_eq!(
-    //         Puzzle::solve(Description::TestData("".to_string()), 1),
-    //         Answer::Part1(0)
-    //     );
-    // }
 }
