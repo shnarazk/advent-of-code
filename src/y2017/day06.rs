3c411e3a140c6549f6f164143c27a1dc8fd8bcf4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-07 14:20:38 +0900

    y2017 day6 part1 done

--- src/y2017/day06.rs
+++ src/y2017/day06.rs
@@ -8,12 +8,13 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::collections::{HashMap, HashSet},
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
     line: Vec<usize>,
+    len: usize,
 }
 
 #[aoc(2017, 6)]
@@ -24,9 +25,29 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        self.len = self.line.len();
+        dbg!(self.len);
     }
     fn part1(&mut self) -> Self::Output1 {
+        let mut visited: HashSet<Vec<usize>> = HashSet::new();
+        for step in 1_usize.. {
+            let mut target = 0;
+            let mut nblocks = 0;
+            for (i, nb) in self.line.iter().enumerate() {
+                if nblocks < *nb {
+                    target = i;
+                    nblocks = *nb;
+                }
+            }
+            self.line[target] = 0;
+            for n in 0..nblocks {
+                self.line[(target + n + 1) % self.len] += 1;
+            }
+            if visited.contains(&self.line) {
+                return step;
+            }
+            visited.insert(self.line.clone());
+        }
         0
     }
     fn part2(&mut self) -> Self::Output2 {
