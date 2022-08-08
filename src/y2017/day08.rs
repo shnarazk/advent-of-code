4f99d0ad8c4b54b07d02b2417f85902e32cd7e13
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-09 01:51:16 +0900

    y2017 day8 part1 done

--- src/y2017/day08.rs
+++ src/y2017/day08.rs
@@ -11,33 +11,88 @@ use {
     std::collections::HashMap,
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Eq, Hash, PartialEq)]
+enum Compare {
+    LessThan,
+    LessEqual,
+    Equal,
+    NotEqual,
+    GreaterThan,
+    GreaterEqual,
+}
+
+#[derive(Debug, Eq, Hash, PartialEq)]
+struct Inst {
+    register: String,
+    offset: isize,
+    cond_reg: String,
+    cond_cmp: Compare,
+    cond_val: isize,
+}
+
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Inst>,
 }
 
 #[aoc(2017, 8)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
+        let parser = regex!(r"^(\w+) (inc|dec) (-?\d+) if (\w+) (>=?|==|!=|<=?) (-?\d+)$");
         let segment = parser.captures(block).ok_or(ParseError)?;
+        let mut offset = segment[3].parse::<isize>()?;
+        if &segment[2] == "dec" {
+            offset *= -1;
+        }
+        let cond_cmp: Compare = match &segment[5] {
+            "<" => Compare::LessThan,
+            "<=" => Compare::LessEqual,
+            "==" => Compare::Equal,
+            "!=" => Compare::NotEqual,
+            ">" => Compare::GreaterThan,
+            ">=" => Compare::GreaterEqual,
+            _ => unreachable!(),
+        };
+        let cond_val: isize = segment[6].parse::<isize>()?;
+        self.line.push(Inst {
+            register: segment[1].to_string(),
+            offset,
+            cond_reg: segment[4].to_string(),
+            cond_cmp,
+            cond_val,
+        });
         // self.line.push(segment[0].parse::<_>());
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut reg: HashMap<String, isize> = HashMap::new();
+        for Inst {
+            register,
+            offset,
+            cond_reg,
+            cond_cmp,
+            cond_val,
+        } in self.line.iter()
+        {
+            let reg_val = *reg.entry(cond_reg.clone()).or_insert(0);
+            let satisfied = match cond_cmp {
+                Compare::LessThan => reg_val < *cond_val,
+                Compare::LessEqual => reg_val <= *cond_val,
+                Compare::Equal => reg_val == *cond_val,
+                Compare::NotEqual => reg_val != *cond_val,
+                Compare::GreaterThan => reg_val > *cond_val,
+                Compare::GreaterEqual => reg_val >= *cond_val,
+            };
+            if satisfied {
+                let reg = reg.entry(register.clone()).or_insert(0);
+                *reg += offset;
+            }
+        }
+        dbg!(*reg.values().max().unwrap_or(&0)) as usize
     }
     fn part2(&mut self) -> Self::Output2 {
         0
