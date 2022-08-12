07f79e8b08364c2c3b2c2570d3a490633abace40
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-12 22:50:49 +0900

    Clean up

--- src/y2017/day13.rs
+++ src/y2017/day13.rs
@@ -1,14 +1,7 @@
 //! <https://adventofcode.com/2017/day/13>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    regex,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
