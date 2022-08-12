5bfb0955e4b61b9fb0acabc06b9a8e78d167d1e6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-12 17:04:26 +0900

    y2017 day13 part1 done

--- src/y2017/day13.rs
+++ src/y2017/day13.rs
@@ -13,31 +13,34 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(usize, usize)>,
 }
 
 #[aoc(2017, 13)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
+        let parser = regex!(r"^(\d+): (\d+)$");
         let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line
+            .push((segment[1].parse::<usize>()?, segment[2].parse::<usize>()?));
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut severity: usize = 0;
+        for (depth, range) in self.line.iter() {
+            if *depth == 0 {
+                continue;
+            }
+            let cycle: usize = 2 * (*range - 1);
+            if *depth % cycle == 0 {
+                severity += depth * *range;
+            }
+        }
+        severity
     }
     fn part2(&mut self) -> Self::Output2 {
         0
