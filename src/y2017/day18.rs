aa42baf6ee522881ace3ebb75763bd3341991589
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-15 14:58:07 +0900

    modified:   src/y2017/day18.rs: parser

--- src/y2017/day18.rs
+++ src/y2017/day18.rs
@@ -11,26 +11,116 @@ use {
     std::collections::HashMap,
 };
 
+#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Val {
+    Reg(char),
+    Lit(isize),
+}
+
+#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Inst {
+    Snd(Val),
+    Set(Val, Val),
+    Add(Val, Val),
+    Mul(Val, Val),
+    Mod(Val, Val),
+    Rcv(Val),
+    Jgz(Val, Val),
+}
+
+impl TryFrom<&str> for Inst {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        dbg!(value);
+        let arg1l = regex!(r"^([[:lower:]]{3}) (-?[[:digit:]]+)$");
+        let arg1r = regex!(r"^([[:lower:]]{3}) ([[:lower:]])$");
+        let arg2ll = regex!(r"^([[:lower:]]{3}) (-?[[:digit:]]+) (-?[[:digit:]]+)$");
+        let arg2lr = regex!(r"^([[:lower:]]{3}) (-?[[:digit:]]+) ([[:lower:]])$");
+        let arg2rl = regex!(r"^([[:lower:]]{3}) ([[:lower:]]) (-?[[:digit:]]+)$");
+        let arg2rr = regex!(r"^([[:lower:]]{3}) ([[:lower:]]) ([[:lower:]])$");
+        if let Some(segment) = arg1l.captures(value) {
+            let val: Val = Val::Lit(segment[2].parse::<isize>()?);
+            let ins = match &segment[1] {
+                "snd" => Inst::Snd(val),
+                "rcv" => Inst::Rcv(val),
+                _ => unreachable!(),
+            };
+            return Ok(ins);
+        }
+        if let Some(segment) = arg1r.captures(value) {
+            let reg: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
+            let ins = match &segment[1] {
+                "snd" => Inst::Snd(reg),
+                "rcv" => Inst::Rcv(reg),
+                _ => unreachable!(),
+            };
+            return Ok(ins);
+        }
+        if let Some(segment) = arg2ll.captures(value) {
+            let op1: Val = Val::Lit(segment[2].parse::<isize>()?);
+            let op2: Val = Val::Lit(segment[3].parse::<isize>()?);
+            let ins = match &segment[1] {
+                "set" => Inst::Set(op1, op2),
+                "add" => Inst::Add(op1, op2),
+                "mul" => Inst::Mul(op1, op2),
+                "mod" => Inst::Mod(op1, op2),
+                "jgz" => Inst::Jgz(op1, op2),
+                _ => unreachable!(),
+            };
+            return Ok(ins);
+        }
+        if let Some(segment) = arg2lr.captures(value) {
+            let op1: Val = Val::Lit(segment[2].parse::<isize>()?);
+            let op2: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
+            let ins = match &segment[1] {
+                "set" => Inst::Set(op1, op2),
+                "add" => Inst::Add(op1, op2),
+                "mul" => Inst::Mul(op1, op2),
+                "mod" => Inst::Mod(op1, op2),
+                "jgz" => Inst::Jgz(op1, op2),
+                _ => unreachable!(),
+            };
+            return Ok(ins);
+        }
+        if let Some(segment) = arg2rl.captures(value) {
+            let op1: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
+            let op2: Val = Val::Lit(segment[3].parse::<isize>()?);
+            let ins = match &segment[1] {
+                "set" => Inst::Set(op1, op2),
+                "add" => Inst::Add(op1, op2),
+                "mul" => Inst::Mul(op1, op2),
+                "mod" => Inst::Mod(op1, op2),
+                "jgz" => Inst::Jgz(op1, op2),
+                _ => unreachable!(),
+            };
+            return Ok(ins);
+        }
+        if let Some(segment) = arg2rr.captures(value) {
+            let op1: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
+            let op2: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
+            let ins = match &segment[1] {
+                "set" => Inst::Set(op1, op2),
+                "add" => Inst::Add(op1, op2),
+                "mul" => Inst::Mul(op1, op2),
+                "mod" => Inst::Mod(op1, op2),
+                "jgz" => Inst::Jgz(op1, op2),
+                _ => unreachable!(),
+            };
+            return Ok(ins);
+        }
+        Err(ParseError)
+    }
+}
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Inst>,
 }
 
 #[aoc(2017, 18)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line.push(Inst::try_from(block)?);
         Ok(())
     }
     fn after_insert(&mut self) {
