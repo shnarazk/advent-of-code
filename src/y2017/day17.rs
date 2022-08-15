608e6a2cd61fbe4b8b8185b761661b3192027f33
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-15 09:35:26 +0900

    y2017 day17 part1 done

--- src/y2017/day17.rs
+++ src/y2017/day17.rs
@@ -6,38 +6,40 @@ use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
         geometric::neighbors,
-        line_parser, regex,
     },
     std::collections::HashMap,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: usize,
 }
 
 #[aoc(2017, 17)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line = block.parse::<usize>()?;
         Ok(())
     }
     fn after_insert(&mut self) {
         dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let step = self.line;
+        let mut ring: Vec<usize> = vec![0];
+        let mut pos = 0;
+        for i in 1..=2017 {
+            pos = (step + pos) % ring.len();
+            if pos == ring.len() - 1 {
+                ring.push(i);
+                pos = ring.len() - 1;
+            } else {
+                ring.insert(pos + 1, i);
+                pos += 1;
+            }
+        }
+        ring[(pos + 1) % ring.len()]
     }
     fn part2(&mut self) -> Self::Output2 {
         0
