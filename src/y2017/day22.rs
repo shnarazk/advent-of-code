fe97325c9d7ee0c1991ff8ffc4351e1b4cfa70e8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-19 12:49:44 +0900

    modified:   src/y2017/day22.rs

--- src/y2017/day22.rs
+++ src/y2017/day22.rs
@@ -8,36 +8,85 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::collections::HashSet,
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+type Location = (isize, isize);
+const UP: Location = (-1, 0);
+const RIGHT: Location = (0, 1);
+const DOWN: Location = (1, 0);
+const LEFT: Location = (0, -1);
+
+fn rotate_cw(dir: Location) -> Location {
+    match dir {
+        UP => RIGHT,
+        RIGHT => DOWN,
+        DOWN => LEFT,
+        LEFT => UP,
+        _ => unreachable!(),
+    }
+}
+
+fn rotate_ccw(dir: Location) -> Location {
+    match dir {
+        RIGHT => UP,
+        DOWN => RIGHT,
+        LEFT => DOWN,
+        UP => LEFT,
+        _ => unreachable!(),
+    }
+}
+
+fn turn_to(dir: Location, infected: bool) -> Location {
+    if infected {
+        rotate_cw(dir)
+    } else {
+        rotate_ccw(dir)
+    }
+}
+
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<bool>>,
+    infection_map: HashSet<Location>,
 }
 
 #[aoc(2017, 22)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line
+            .push(block.chars().map(|c| c == '#').collect::<Vec<_>>());
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(self.line.len());
+        for (j, line) in self.line.iter().enumerate() {
+            for (i, b) in line.iter().enumerate() {
+                if *b {
+                    self.infection_map.insert((j as isize, i as isize));
+                }
+            }
+        }
+        dbg!(self.infection_map.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut carrier_position: Location = (0, 0);
+        let mut carrier_direction: Location = UP;
+        let mut bursts = 0;
+        for _ in 0..50 {
+            let mode = self.infection_map.contains(&carrier_position);
+            carrier_direction = turn_to(carrier_direction, mode);
+            if mode {
+                bursts += 1;
+                self.infection_map.insert(carrier_position);
+            } else {
+                self.infection_map.remove(&carrier_position);
+            }
+            carrier_position.0 += carrier_direction.0;
+            carrier_position.1 += carrier_direction.1;
+        }
+        bursts
     }
     fn part2(&mut self) -> Self::Output2 {
         0
