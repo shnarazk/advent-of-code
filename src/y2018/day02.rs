a16085863d02d09fda4b3ae42614de3f26d16ea5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-20 10:12:42 +0900

    y2018 day2 part2 done.

--- src/y2018/day02.rs
+++ src/y2018/day02.rs
@@ -4,7 +4,7 @@
 #![allow(unused_variables)]
 use {
     crate::{
-        framework::{aoc, AdventOfCode, ParseError},
+        framework::{aoc_at, AdventOfCode, ParseError},
         geometric::neighbors,
         line_parser, regex,
     },
@@ -16,8 +16,10 @@ pub struct Puzzle {
     line: Vec<Vec<u8>>,
 }
 
-#[aoc(2018, 2)]
+#[aoc_at(2018, 2)]
 impl AdventOfCode for Puzzle {
+    type Output1 = usize;
+    type Output2 = String;
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         self.line
@@ -41,6 +43,26 @@ impl AdventOfCode for Puzzle {
         twice * thrice
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        for (j, word1) in self.line.iter().enumerate() {
+            for word2 in self.line[j + 1..].iter() {
+                let mut ndiff: usize = 0;
+                let mut index: usize = 0;
+                for (i, (c1, c2)) in word1.iter().zip(word2.iter()).enumerate() {
+                    if c1 != c2 {
+                        index = i;
+                        ndiff += 1;
+                    }
+                }
+                if ndiff == 1 {
+                    return word1
+                        .iter()
+                        .enumerate()
+                        .filter(|(i, _)| *i != index)
+                        .map(|(_, c)| *c as char)
+                        .collect::<String>();
+                }
+            }
+        }
+        "".to_string()
     }
 }
