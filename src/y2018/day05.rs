fa9658e3a09f2f0388d2494e96747d120cfd4a9f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-21 10:21:17 +0900

    y2018 day5 part1 done

--- src/y2018/day05.rs
+++ src/y2018/day05.rs
@@ -13,31 +13,41 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<u8>,
 }
 
 #[aoc(2018, 5)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line = block.chars().map(|c| c as u8).collect::<Vec<u8>>();
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let dist = b'a' - b'A';
+        let mut updated = true;
+        while updated {
+            updated = false;
+            let mut index = 0;
+            for i in 0..self.line.len() - 1 {
+                if self.line[i] + dist == self.line[i + 1]
+                    || self.line[i] == self.line[i + 1] + dist
+                {
+                    index = i;
+                    updated = true;
+                    break;
+                }
+            }
+            if updated {
+                self.line.remove(index);
+                self.line.remove(index);
+                // dbg!(self.line.iter().map(|c| *c as char).collect::<String>());
+            }
+        }
+        self.line.len()
     }
     fn part2(&mut self) -> Self::Output2 {
         0
