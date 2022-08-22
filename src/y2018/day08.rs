3db848448f41bbc609e248bf4fa6a84a00663307
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-23 08:18:37 +0900

    y2018 day8 part1 done

--- src/y2018/day08.rs
+++ src/y2018/day08.rs
@@ -13,33 +13,44 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<usize>,
 }
 
 #[aoc(2018, 8)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line = line_parser::to_usizes(block, ' ')?;
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.build_span(0).1
     }
     fn part2(&mut self) -> Self::Output2 {
         0
+    }
+}
+
+impl Puzzle {
+    // return:
+    // - the index of the next entry
+    // - and the sum of metadatas in this range
+    fn build_span(&self, index: usize) -> (usize, usize) {
+        let num_children = self.line[index];
+        let num_metadata = self.line[index + 1];
+        let mut sum_metas = 0;
+        let mut i = index + 2;
+        for _ in 0..num_children {
+            let tmp = self.build_span(i);
+            i = tmp.0;
+            sum_metas += tmp.1;
+        }
+        for m in 0..num_metadata {
+            sum_metas += self.line[i + m];
+        }
+        (i + num_metadata, sum_metas)
     }
 }
