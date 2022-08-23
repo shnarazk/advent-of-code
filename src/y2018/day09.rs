ef58dd5a519f6227fe48493ff55dd509b02e4bbd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-23 10:33:16 +0900

    y2018 day9 part1 done

--- src/y2018/day09.rs
+++ src/y2018/day09.rs
@@ -3,40 +3,62 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     std::collections::HashMap,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    players: usize,
+    points: usize,
 }
 
 #[aoc(2018, 9)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
-    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // 410 players; last marble is worth 72059 points
+        self.players = 410;
+        self.points = 72059;
+        // self.players = 13;
+        // self.points = 7999;
+        // self.players = 10;
+        // self.points = 1618;
+        // self.players = 9;
+        // self.points = 25;
     }
     fn part1(&mut self) -> Self::Output1 {
+        let mut points = Vec::new();
+        points.resize(self.players, 0);
+        let mut circle = vec![0];
+        let mut current = 1;
+        let mut next_ball = 1;
+        for e in (1..=self.players).cycle() {
+            let len = circle.len();
+            if len == 0 {
+                circle.push(next_ball);
+                next_ball += 1;
+                // println!("[{e}] {circle:?}");
+                continue;
+            }
+            if next_ball % 23 == 0 {
+                points[e - 1] += next_ball;
+                current = (len + current - 7) % len;
+                points[e - 1] += circle[current];
+                circle.remove(current);
+            } else {
+                current = (current + 1) % len + 1;
+                circle.insert(current, next_ball);
+            }
+            // println!("[{e}] {circle:?}");
+            if self.points == next_ball {
+                return *points.iter().max().unwrap();
+            }
+            next_ball += 1;
+        }
         0
     }
     fn part2(&mut self) -> Self::Output2 {
