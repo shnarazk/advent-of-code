5618ca135bfd285adcf56d531f96ba24c6135325
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-23 09:22:37 +0900

    y2018 day8 part2 done.

--- src/y2018/day08.rs
+++ src/y2018/day08.rs
@@ -30,8 +30,16 @@ impl AdventOfCode for Puzzle {
         self.build_span(0).1
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut table: HashMap<usize, Metadata> = HashMap::new();
+        self.build_table(0, &mut table);
+        sum_meta(0, &table)
     }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum Metadata {
+    Leaf(usize),
+    Link(Vec<usize>),
 }
 
 impl Puzzle {
@@ -52,5 +60,43 @@ impl Puzzle {
             sum_metas += self.line[i + m];
         }
         (i + num_metadata, sum_metas)
+    }
+    /// return:
+    /// - the index of the next entry
+    /// - and the sum of metadatas in this range
+    fn build_table(&self, index: usize, table: &mut HashMap<usize, Metadata>) -> usize {
+        let num_children = self.line[index];
+        let num_metadata = self.line[index + 1];
+        let mut i = index + 2;
+        let mut child_indexes = Vec::new();
+        for _ in 0..num_children {
+            child_indexes.push(i);
+            i = self.build_table(i, table);
+        }
+        if num_children == 0 {
+            let metas: usize = self.line[i..i + num_metadata].iter().sum();
+            table.insert(index, Metadata::Leaf(metas));
+        } else {
+            let mut metas: Vec<usize> = Vec::new();
+            for j in &self.line[i..i + num_metadata] {
+                if *j == 0 {
+                    continue;
+                }
+                if let Some(k) = child_indexes.get(*j - 1) {
+                    metas.push(*k);
+                }
+            }
+            table.insert(index, Metadata::Link(metas));
+        }
+        i + num_metadata
+    }
+}
+
+fn sum_meta(index: usize, table: &HashMap<usize, Metadata>) -> usize {
+    // dbg!(vec.len());
+    match table.get(&index) {
+        Some(Metadata::Leaf(n)) => dbg!(*n),
+        Some(Metadata::Link(v)) => v.iter().map(|n| sum_meta(*n, table)).sum(),
+        None => 0,
     }
 }
