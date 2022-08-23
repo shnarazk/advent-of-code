3b41f15e5c09496bd89a26fb384979e63599cf29
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-23 15:31:28 +0900

    y2018 day9 part2 done.

--- src/y2018/day09.rs
+++ src/y2018/day09.rs
@@ -62,6 +62,72 @@ impl AdventOfCode for Puzzle {
         0
     }
     fn part2(&mut self) -> Self::Output2 {
+        self.points *= 100;
+        // self.players = 10;
+        // self.points = 1618;
+        let mut points = Vec::new();
+        points.resize(self.players, 0);
+        let mut circle_ptr = Vec::new();
+        circle_ptr.resize(self.points, 0);
+        let mut circle_len = 1;
+        let mut current = 0;
+        let mut next_ball = 1;
+        for e in (1..=self.players).cycle() {
+            // if circle_len == 0 {
+            //     next_ball += 1;
+            //     circle_len += 1;
+            //     continue;
+            // }
+            if next_ball % 23 == 0 {
+                points[e - 1] += next_ball;
+                // current = (circle_len + current - 7) % circle_len;
+                let mut ptr = 0;
+                let mut buffer = [0; 8];
+                let mut count = 0;
+                while ptr != current || count < 8 {
+                    count += 1;
+                    buffer[count % 8] = ptr;
+                    ptr = circle_ptr[ptr];
+                }
+                assert!(7 < count);
+                let prev_of_delete = buffer[(count + 1) % 8];
+                let deletion_target = buffer[(count + 2) % 8];
+                // dbg!(count % 8, prev_of_delete, deletion_target, &buffer);
+                points[e - 1] += deletion_target;
+                circle_ptr[prev_of_delete] = circle_ptr[deletion_target];
+                current = circle_ptr[prev_of_delete];
+                circle_len -= 1;
+            } else {
+                // current = (current + 2) % circle_len + 1;
+                let ptr = circle_ptr[current];
+                let next = circle_ptr[ptr];
+                circle_ptr[next_ball] = next;
+                // if ptr == 0 && 1 < circle_len {
+                //     ptr = circle_len;
+                // }
+                circle_ptr[ptr] = next_ball;
+                current = next_ball;
+                circle_len += 1;
+            }
+            // println!("[{e}] {circle:?}");
+            if self.points - 1 == next_ball {
+                dbg!();
+                return *points.iter().max().unwrap();
+            }
+            // print!("[{e}] ({current}): ");
+            // print_link(&circle_ptr);
+            next_ball += 1;
+        }
         0
     }
+}
+
+fn print_link(link: &[usize]) {
+    let mut i = 0;
+    print!("0 ");
+    while link[i] != 0 {
+        print!("{} ", link[i]);
+        i = link[i];
+    }
+    println!();
 }
