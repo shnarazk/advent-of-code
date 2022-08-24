55c44a62f0b6b83b11e8f09c6c7d1392367d6958
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-24 20:13:38 +0900

    y2018 day10 part1 done

--- src/y2018/day10.rs
+++ src/y2018/day10.rs
@@ -2,6 +2,8 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+
+use std::panic::Location;
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
@@ -35,9 +37,62 @@ impl AdventOfCode for Puzzle {
         dbg!(self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut buffer = String::new();
+        let stdin = std::io::stdin();
+        loop {
+            let too_big = self.print();
+            buffer.clear();
+            if !too_big {
+                stdin.read_line(&mut buffer).expect("strange error");
+                if buffer.starts_with('c') {
+                    return 1;
+                }
+            }
+            self.update();
+        }
     }
     fn part2(&mut self) -> Self::Output2 {
         0
+    }
+}
+
+impl Puzzle {
+    fn print(&self) -> bool {
+        let mut map: HashMap<isize, Vec<isize>> = HashMap::new();
+        let mut y_min: isize = isize::MAX;
+        let mut x_min: isize = isize::MAX;
+        let mut y_max: isize = isize::MIN;
+        let mut x_max: isize = isize::MIN;
+        for l in self.line.iter() {
+            map.entry(l[1]).or_insert(Vec::new()).push(l[0]);
+            x_min = x_min.min(l[0]);
+            y_min = y_min.min(l[1]);
+            x_max = x_max.max(l[0]);
+            y_max = y_max.max(l[1]);
+        }
+        if 100 < y_max - y_min || 100 < x_max - x_min {
+            // dbg!(y_max - y_min, x_max - x_min);
+            return true;
+        }
+        for y in y_min..=y_max {
+            if let Some(p) = map.get(&y) {
+                for x in x_min..=x_max {
+                    print!("{}", if p.contains(&x) { '#' } else { '.' });
+                }
+            } else {
+                for x in x_min..=x_max {
+                    print!(".");
+                }
+            }
+            println!();
+        }
+        false
+    }
+    fn update(&mut self) {
+        for l in self.line.iter_mut() {
+            l[0] += l[2];
+            l[1] += l[3];
+        }
+        // todo!();
     }
 }
