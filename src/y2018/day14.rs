c9710194d28735217fe150365665c91f3232fc96
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-26 20:21:04 +0900

    y2018 day14 part1 done

--- src/y2018/day14.rs
+++ src/y2018/day14.rs
@@ -13,30 +13,51 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: usize,
 }
 
 #[aoc(2018, 14)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
-    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        self.line = 635041;
     }
     fn part1(&mut self) -> Self::Output1 {
+        // self.line = 2018;
+        let mut v: Vec<usize> = vec![3, 7];
+        let mut elf1: usize = 0;
+        let mut elf2: usize = 1;
+        let mut remain: Option<usize> = None;
+        for i in 0.. {
+            let sum = v[elf1] + v[elf2];
+            if 9 < sum {
+                let a1 = sum / 10;
+                let a2 = sum % 10;
+                v.push(a1);
+                v.push(a2);
+            } else {
+                v.push(sum);
+            }
+            elf1 = (elf1 + v[elf1] + 1) % v.len();
+            elf2 = (elf2 + v[elf2] + 1) % v.len();
+            // println!("{:?} elf1 = {}, elf2 = {}", v, elf1, elf2);
+            if let Some(n) = remain {
+                if n == 1 {
+                    println!("{:?}", &v[self.line..]);
+                    return v[self.line..].iter().fold(0, |acc, n| acc * 10 + n);
+                }
+                remain = Some(n - 1);
+            } else if self.line == v.len() {
+                dbg!();
+                remain = Some(10);
+            } else if self.line == v.len() + 1 {
+                dbg!();
+                remain = Some(9);
+            }
+        }
         0
     }
     fn part2(&mut self) -> Self::Output2 {
