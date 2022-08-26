229a36c1e7fa66f0b7ecd119ab140ea746459d97
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-26 20:35:37 +0900

    y2018 day14 part2 done.

--- src/y2018/day14.rs
+++ src/y2018/day14.rs
@@ -61,6 +61,38 @@ impl AdventOfCode for Puzzle {
         0
     }
     fn part2(&mut self) -> Self::Output2 {
+        // self.line = 51590;
+        let digits = 6;
+        let mut v: Vec<usize> = vec![3, 7];
+        let mut elf1: usize = 0;
+        let mut elf2: usize = 1;
+        let mut start = 0;
+        for i in 2.. {
+            let sum = v[elf1] + v[elf2];
+            if 9 < sum {
+                let a1 = sum / 10;
+                let a2 = sum % 10;
+                v.push(a1);
+                v.push(a2);
+            } else {
+                v.push(sum);
+            }
+            elf1 = (elf1 + v[elf1] + 1) % v.len();
+            elf2 = (elf2 + v[elf2] + 1) % v.len();
+            if v.len() < digits {
+                continue;
+            }
+            // println!("{:?} elf1 = {}, elf2 = {}", v, elf1, elf2);
+            for _ in 0..=v.len() - digits - start {
+                let val = v[start..start + digits]
+                    .iter()
+                    .fold(0, |acc, n| acc * 10 + n);
+                if val == self.line {
+                    return start;
+                }
+                start += 1;
+            }
+        }
         0
     }
 }
