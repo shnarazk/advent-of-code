fbbdf630a53515886d9496743a5d256a6af813ef
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-27 10:32:11 +0900

    modified:   src/y2018/day15.rs

--- src/y2018/day15.rs
+++ src/y2018/day15.rs
@@ -12,12 +12,87 @@ use {
 
 type Dim2 = (isize, isize);
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Creature {
+    Elf(Dim2, usize),
+    Goblin(Dim2, usize),
+}
+
+impl PartialOrd for Creature {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.position().partial_cmp(other.position())
+    }
+}
+
+impl Ord for Creature {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.position().cmp(other.position())
+    }
+}
+
+impl Creature {
+    fn hit_point(&self) -> &usize {
+        match self {
+            Creature::Elf(_, hp) => hp,
+            Creature::Goblin(_, hp) => hp,
+        }
+    }
+    fn position(&self) -> &Dim2 {
+        match self {
+            Creature::Elf(p, _) => p,
+            Creature::Goblin(p, _) => p,
+        }
+    }
+    //
+    // Moving
+    //
+    fn target_creatures(&self, world: &Puzzle) -> Vec<Creature> {
+        todo!()
+    }
+    fn all_ranges(&self, world: &Puzzle) -> Vec<Dim2> {
+        let v = self.target_creatures(world);
+        todo!()
+    }
+    fn best_moving_position(&self, world: &Puzzle) -> Option<Dim2> {
+        let v = self.all_ranges(world);
+        todo!()
+    }
+    fn is_in_a_range(&self, world: &Puzzle) -> bool {
+        todo!()
+    }
+    //
+    // Attacking
+    //
+    fn best_target_creature(&self, world: &Puzzle) -> Option<Creature> {
+        let mut v = self.target_creatures(world);
+        (!v.is_empty()).then(|| {
+            v.sort();
+            v[0]
+        })
+    }
+    /// return true if I'm killed.
+    fn attacked(&mut self, world: &mut Puzzle) -> bool {
+        let p = match self {
+            Creature::Elf(_, hp) => hp,
+            Creature::Goblin(_, hp) => hp,
+        };
+        if *p < 3 {
+            world.creatures.retain(|c| c != self);
+            return true;
+        }
+        *p -= 3;
+        false
+    }
+    fn turn(&mut self, world: &mut Puzzle) -> bool {
+        todo!()
+    }
+}
+
 #[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
     line: Vec<Vec<u8>>,
     map: HashSet<Dim2>,
-    goblins: HashSet<Dim2>,
-    elves: HashSet<Dim2>,
+    creatures: Vec<Creature>,
 }
 
 #[aoc(2018, 15)]
@@ -37,16 +112,16 @@ impl AdventOfCode for Puzzle {
                 }
                 match *c {
                     b'G' => {
-                        self.goblins.insert(pos);
+                        self.creatures.push(Creature::Elf(pos, 300));
                     }
                     b'E' => {
-                        self.elves.insert(pos);
+                        self.creatures.push(Creature::Goblin(pos, 300));
                     }
                     _ => (),
                 }
             }
         }
-        dbg!(self.goblins.len(), self.elves.len());
+        dbg!(self.creatures.len());
     }
     fn part1(&mut self) -> Self::Output1 {
         0
