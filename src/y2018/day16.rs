e1cb665c1d23833463bc106297e0f2ebbf4048bd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-28 14:52:54 +0900

    y2018 day16 part1 done

--- src/y2018/day16.rs
+++ src/y2018/day16.rs
@@ -5,6 +5,7 @@
 
 use {
     crate::{
+        color,
         framework::{aoc, AdventOfCode, ParseError},
         geometric::neighbors,
         line_parser, regex,
@@ -15,9 +16,9 @@ use {
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
     line: Vec<Vec<usize>>,
-    rules: Vec<(Vec<usize>, Vec<usize>, Vec<usize>)>,
+    rules: Vec<([usize; 4], [usize; 4], [usize; 4])>,
     input_mode: usize,
-    input_buffer: Vec<Vec<usize>>,
+    input_buffer: Vec<[usize; 4]>,
     op_map: [usize; 16],
 }
 
@@ -41,16 +42,24 @@ impl AdventOfCode for Puzzle {
     }
     fn after_insert(&mut self) {
         dbg!(&self.rules.len());
-        let mut n_ops: [usize; 16] = [0; 16];
-        for s in self.rules.iter() {
-            n_ops[s.1[0]] += 1;
-        }
-        dbg!(n_ops);
         dbg!(&self.line.len());
         self.determine_op_code();
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut buffer: [usize; 4] = [0; 4];
+        let mut count = 0;
+        for example in self.rules.iter() {
+            let mut success = 0;
+            for code in 0..16 {
+                let on_note = example.1[0];
+                success +=
+                    (*execute_as(code, &example.1, &example.0, &mut buffer) == example.2) as usize;
+            }
+            if 3 <= success {
+                count += 1;
+            }
+        }
+        count
     }
     fn part2(&mut self) -> Self::Output2 {
         0
@@ -64,21 +73,20 @@ impl Puzzle {
                 let parser = regex!(r"^Before: \[([0-9, ]+)\]$");
                 let segment = parser.captures(block).ok_or(ParseError)?;
                 assert!(self.input_buffer.is_empty());
-                self.input_buffer.push(line_parser::to_usizes_spliting_with(
-                    &segment[1],
-                    &[' ', ','],
-                )?);
+                let v = line_parser::to_usizes_spliting_with(&segment[1], &[' ', ','])?;
+                self.input_buffer.push([v[0], v[1], v[2], v[3]]);
                 self.input_mode = 1;
             }
             1 => {
-                self.input_buffer
-                    .push(line_parser::to_usizes_spliting_with(block, &[' ', ','])?);
+                let v = line_parser::to_usizes_spliting_with(block, &[' ', ','])?;
+                self.input_buffer.push([v[0], v[1], v[2], v[3]]);
                 self.input_mode = 2;
             }
             2 => {
                 let parser = regex!(r"^After:  \[([0-9, ]+)\]$");
                 let segment = parser.captures(block).ok_or(ParseError)?;
-                let t2 = line_parser::to_usizes_spliting_with(&segment[1], &[' ', ','])?;
+                let v = line_parser::to_usizes_spliting_with(&segment[1], &[' ', ','])?;
+                let t2 = [v[0], v[1], v[2], v[3]];
                 let t1 = self.input_buffer.pop().unwrap();
                 let t0 = self.input_buffer.pop().unwrap();
                 self.rules.push((t0, t1, t2));
@@ -93,6 +101,96 @@ impl Puzzle {
         Ok(())
     }
     fn determine_op_code(&mut self) {
-        todo!()
+        let mut result: HashMap<(usize, usize), usize> = HashMap::new();
+        let mut fail: HashMap<(usize, usize), usize> = HashMap::new();
+        let mut tries: HashMap<usize, usize> = HashMap::new();
+        let mut buffer: [usize; 4] = [0; 4];
+        for example in self.rules.iter() {
+            let on_note = example.1[0];
+            *tries.entry(on_note).or_insert(0) += 1;
+        }
+        for code in 0..16 {
+            for example in self.rules.iter() {
+                let on_note = example.1[0];
+                if *execute_as(code, &example.1, &example.0, &mut buffer) == example.2 {
+                    *result.entry((on_note, code)).or_insert(0) += 1;
+                } else {
+                    *fail.entry((on_note, code)).or_insert(0) += 1;
+                }
+            }
+        }
+        for on_note in 0..16 {
+            print!("{:>3}/{:>2}: ", on_note, tries.get(&on_note).unwrap_or(&0),);
+            let mut sum = 0;
+            for code in 0..16 {
+                let occ = *result.get(&(on_note, code)).unwrap_or(&0);
+                let neg = *fail.get(&(on_note, code)).unwrap_or(&0);
+                print!(
+                    "{}{:>3}/{:>2}{}",
+                    if 0 < occ && 0 == neg {
+                        color::CYAN
+                    } else {
+                        color::RESET
+                    },
+                    occ,
+                    neg,
+                    color::RESET
+                );
+                sum += occ;
+            }
+            println!("  | {:>2}", sum);
+        }
     }
+}
+
+fn execute_as<'a, 'b>(
+    op: usize,
+    code: &'a [usize],
+    register: &'a [usize; 4],
+    out: &'b mut [usize; 4],
+) -> &'b mut [usize; 4] {
+    macro_rules! reg {
+        ($num: expr) => {{
+            register[code[$num]]
+        }};
+    }
+    macro_rules! set {
+        ($num: expr) => {{
+            code[$num]
+        }};
+    }
+    macro_rules! val {
+        ($num: expr) => {{
+            code[$num]
+        }};
+    }
+    out[..4].copy_from_slice(&register[..4]);
+    assert_eq!(&register, &out);
+    match op {
+        // addr, addi
+        0 => out[set!(3)] = reg!(1) + reg!(2),
+        1 => out[set!(3)] = reg!(1) + val!(2),
+        // mulr, muli
+        2 => out[set!(3)] = reg!(1) * reg!(2),
+        3 => out[set!(3)] = reg!(1) * val!(2),
+        // barr, bari
+        4 => out[set!(3)] = reg!(1) & reg!(2),
+        5 => out[set!(3)] = reg!(1) & val!(2),
+        // borr, bori
+        6 => out[set!(3)] = reg!(1) | reg!(2),
+        7 => out[set!(3)] = reg!(1) | val!(2),
+        // setr, seti
+        8 => out[set!(3)] = reg!(1),
+        9 => out[set!(3)] = val!(1),
+        // gtir, gtri, gtrr
+        10 => out[set!(3)] = (val!(1) > reg!(2)) as usize,
+        11 => out[set!(3)] = (reg!(1) > val!(2)) as usize,
+        12 => out[set!(3)] = (reg!(1) > reg!(2)) as usize,
+        // eqir, eqri, eqrr
+        13 => out[set!(3)] = (val!(1) == reg!(2)) as usize,
+        14 => out[set!(3)] = (reg!(1) == val!(2)) as usize,
+        15 => out[set!(3)] = (reg!(1) == reg!(2)) as usize,
+        _ => unreachable!(),
+    }
+    out
 }
