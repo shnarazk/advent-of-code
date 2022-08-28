f8c0640c068339bca0b3f74c8eb5d1ed670a0df6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-28 23:00:37 +0900

    modified:   src/y2018/day17.rs

--- src/y2018/day17.rs
+++ src/y2018/day17.rs
@@ -8,25 +8,22 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::collections::HashSet,
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+type Dim2 = (usize, usize);
+
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
     line: Vec<(bool, usize, usize, usize)>,
+    map: HashSet<Dim2>,
+    depth: usize,
+    width: usize,
 }
 
 #[aoc(2018, 17)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         // x=550, y=1443..1454
         let parser = regex!(r"^(x|y)=(\d+), (x|y)=(\d+)\.\.(\d+)$");
@@ -49,11 +46,83 @@ impl AdventOfCode for Puzzle {
     }
     fn after_insert(&mut self) {
         dbg!(&self.line.len());
+        for (horizontal, base, from, to) in self.line.iter() {
+            for i in *from..=*to {
+                let p = if *horizontal { (*base, i) } else { (i, *base) };
+                self.map.insert(p);
+            }
+        }
+        dbg!(self.map.len());
+        self.width = self.map.iter().map(|(y, x)| *x).max().unwrap();
+        self.depth = self.map.iter().map(|(y, x)| *y).max().unwrap();
     }
     fn part1(&mut self) -> Self::Output1 {
+        dbg!(basin_below((0, 500), self));
         0
     }
     fn part2(&mut self) -> Self::Output2 {
         0
     }
+}
+
+fn basin_below(start: Dim2, world: &Puzzle) -> Option<(usize, Option<Dim2>, Option<Dim2>)> {
+    for y in start.0..world.depth {
+        if world.map.contains(&(y, start.1)) {
+            let bottom = (y, start.1);
+            dbg!(bottom);
+            let west = west_end(bottom, world);
+            let east = east_end(bottom, world);
+            // This is a bad idea.
+            // There is a situation with a 'straw' that doesn't touch with the external basin.
+            match west.0.cmp(&east.0) {
+                std::cmp::Ordering::Less => {
+                    println!("west: {:?} > east: {:?}", west, east);
+                }
+                std::cmp::Ordering::Equal => {
+                    println!("west: {:?} = east: {:?}", west, east);
+                }
+                std::cmp::Ordering::Greater => {
+                    println!("west: {:?} < east: {:?}", west, east);
+                }
+            }
+            break;
+        }
+    }
+    None
+}
+
+fn east_end(start: Dim2, world: &Puzzle) -> Dim2 {
+    let mut point = start;
+    loop {
+        let mut check = (point.0, point.1 + 1);
+        if world.map.contains(&check) {
+            point = check;
+            continue;
+        }
+        check = (point.0 - 1, point.1);
+        if world.map.contains(&check) {
+            point = check;
+            continue;
+        }
+        break;
+    }
+    point
+}
+
+fn west_end(start: Dim2, world: &Puzzle) -> Dim2 {
+    let mut point = start;
+    loop {
+        let mut check = (point.0, point.1 - 1);
+        if world.map.contains(&check) {
+            point = check;
+            continue;
+        }
+        check = (point.0 - 1, point.1);
+        if world.map.contains(&check) {
+            point = check;
+            continue;
+        }
+        break;
+    }
+    point
 }
