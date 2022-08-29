2ea81240a902b097d1784aaaba6c31e5ee43546c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-08-30 00:06:14 +0900

    modified:   src/y2018/day19.rs

--- src/y2018/day19.rs
+++ src/y2018/day19.rs
@@ -16,6 +16,58 @@ pub struct Puzzle {
     line: Vec<()>,
 }
 
+#[derive(Debug, Eq, PartialEq)]
+enum Inst {
+    Addr(usize, usize, usize),
+    Addi(usize, usize, usize),
+    Mulr(usize, usize, usize),
+    Muli(usize, usize, usize),
+    Banr(usize, usize, usize),
+    Bani(usize, usize, usize),
+    Borr(usize, usize, usize),
+    Bori(usize, usize, usize),
+    Setr(usize, usize, usize),
+    Seti(usize, usize, usize),
+    Gtir(usize, usize, usize),
+    Gtri(usize, usize, usize),
+    Gtrr(usize, usize, usize),
+    Eqir(usize, usize, usize),
+    Eqri(usize, usize, usize),
+    Eqrr(usize, usize, usize),
+}
+
+impl TryFrom<&str> for Inst {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let parser = regex!(r"^\w{4} (\d+) (\d+) (\d+) (\d+)$");
+        if let Some(segment) = parser.captures(value) {
+            let opr1 = segment[2].parse::<usize>()?;
+            let opr2 = segment[3].parse::<usize>()?;
+            let opr3 = segment[4].parse::<usize>()?;
+            match &segment[1] {
+                "addr" => Ok(Inst::Addr(opr1, opr2, opr3)),
+                "addi" => Ok(Inst::Addi(opr1, opr2, opr3)),
+                "mulr" => Ok(Inst::Mulr(opr1, opr2, opr3)),
+                "muli" => Ok(Inst::Muli(opr1, opr2, opr3)),
+                "banr" => Ok(Inst::Banr(opr1, opr2, opr3)),
+                "bani" => Ok(Inst::Bani(opr1, opr2, opr3)),
+                "bori" => Ok(Inst::Bori(opr1, opr2, opr3)),
+                "borr" => Ok(Inst::Borr(opr1, opr2, opr3)),
+                "setr" => Ok(Inst::Setr(opr1, opr2, opr3)),
+                "seti" => Ok(Inst::Seti(opr1, opr2, opr3)),
+                "gtir" => Ok(Inst::Gtir(opr1, opr2, opr3)),
+                "gtri" => Ok(Inst::Gtri(opr1, opr2, opr3)),
+                "gtrr" => Ok(Inst::Gtrr(opr1, opr2, opr3)),
+                "eqir" => Ok(Inst::Eqir(opr1, opr2, opr3)),
+                "eqri" => Ok(Inst::Eqri(opr1, opr2, opr3)),
+                "eqrr" => Ok(Inst::Eqrr(opr1, opr2, opr3)),
+                _ => Err(ParseError),
+            }
+        } else {
+            Err(ParseError)
+        }
+    }
+}
 #[aoc(2018, 19)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
