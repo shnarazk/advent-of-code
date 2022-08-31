2a7015dfd10ed29bc886f06e1e15967a89b16cb4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-01 08:58:23 +0900

    modified:   src/y2018/day21.rs

--- src/y2018/day21.rs
+++ src/y2018/day21.rs
@@ -13,33 +13,308 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Inst>,
+    pc_index: usize,
+}
+
+#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Inst {
+    Addr(usize, usize, usize),
+    Addi(usize, usize, usize),
+    Mulr(usize, usize, usize),
+    Muli(usize, usize, usize),
+    Banr(usize, usize, usize),
+    Bani(usize, usize, usize),
+    Borr(usize, usize, usize),
+    Bori(usize, usize, usize),
+    Setr(usize, usize, usize),
+    Seti(usize, usize, usize),
+    Gtir(usize, usize, usize),
+    Gtri(usize, usize, usize),
+    Gtrr(usize, usize, usize),
+    Eqir(usize, usize, usize),
+    Eqri(usize, usize, usize),
+    Eqrr(usize, usize, usize),
+}
+
+impl TryFrom<&str> for Inst {
+    type Error = ParseError;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let parser = regex!(r"^(\w{4}) (\d+) (\d+) (\d+)$");
+        if let Some(segment) = parser.captures(value) {
+            let opr1 = segment[2].parse::<usize>()?;
+            let opr2 = segment[3].parse::<usize>()?;
+            let opr3 = segment[4].parse::<usize>()?;
+            match &segment[1] {
+                "addr" => Ok(Inst::Addr(opr1, opr2, opr3)),
+                "addi" => Ok(Inst::Addi(opr1, opr2, opr3)),
+                "mulr" => Ok(Inst::Mulr(opr1, opr2, opr3)),
+                "muli" => Ok(Inst::Muli(opr1, opr2, opr3)),
+                "banr" => Ok(Inst::Banr(opr1, opr2, opr3)),
+                "bani" => Ok(Inst::Bani(opr1, opr2, opr3)),
+                "bori" => Ok(Inst::Bori(opr1, opr2, opr3)),
+                "borr" => Ok(Inst::Borr(opr1, opr2, opr3)),
+                "setr" => Ok(Inst::Setr(opr1, opr2, opr3)),
+                "seti" => Ok(Inst::Seti(opr1, opr2, opr3)),
+                "gtir" => Ok(Inst::Gtir(opr1, opr2, opr3)),
+                "gtri" => Ok(Inst::Gtri(opr1, opr2, opr3)),
+                "gtrr" => Ok(Inst::Gtrr(opr1, opr2, opr3)),
+                "eqir" => Ok(Inst::Eqir(opr1, opr2, opr3)),
+                "eqri" => Ok(Inst::Eqri(opr1, opr2, opr3)),
+                "eqrr" => Ok(Inst::Eqrr(opr1, opr2, opr3)),
+                _ => Err(ParseError),
+            }
+        } else {
+            Err(ParseError)
+        }
+    }
+}
+
+impl Inst {
+    fn disassemble(&self, addr: usize, pc_index: usize) -> String {
+        let l = |n: &usize| match *n {
+            _ if *n == pc_index => "pc",
+            0 => "a",
+            1 => "b",
+            2 => "c",
+            3 => "d",
+            4 => "e",
+            5 => "f",
+            _ => unreachable!(),
+        };
+        let r = |n: &usize| match *n {
+            _ if *n == pc_index => format!("{addr}"),
+            0 => "a".to_string(),
+            1 => "b".to_string(),
+            2 => "c".to_string(),
+            3 => "d".to_string(),
+            4 => "e".to_string(),
+            5 => "f".to_string(),
+            _ => unreachable!(),
+        };
+        match self {
+            Inst::Addr(o1, o2, o3) => format!("{} = {} + {};", l(o3), r(o1), r(o2)),
+            Inst::Addi(o1, o2, o3) => format!("{} = {} + {};", l(o3), r(o1), o2),
+            Inst::Mulr(o1, o2, o3) => format!("{} = {} * {};", l(o3), r(o1), r(o2)),
+            Inst::Muli(o1, o2, o3) => format!("{} = {} * {};", l(o3), r(o1), o2),
+            Inst::Banr(o1, o2, o3) => format!("{} = {} & {};", l(o3), r(o1), r(o2)),
+            Inst::Bani(o1, o2, o3) => format!("{} = {} & {};", l(o3), r(o1), o2),
+            Inst::Borr(o1, o2, o3) => format!("{} = {} | {};", l(o3), r(o1), r(o2)),
+            Inst::Bori(o1, o2, o3) => format!("{} = {} | {};", l(o3), r(o1), o2),
+            Inst::Setr(o1, _, o3) => format!("{} = {};", l(o3), r(o1)),
+            Inst::Seti(o1, _, o3) => format!("{} = {};", l(o3), o1),
+            Inst::Gtir(o1, o2, o3) => format!("{} = ({} > {}) as usize;", l(o3), o1, r(o2)),
+            Inst::Gtri(o1, o2, o3) => format!("{} = ({} > {}) as usize;", l(o3), r(o1), o2),
+            Inst::Gtrr(o1, o2, o3) => format!("{} = ({} > {}) as usize;", l(o3), r(o1), r(o2)),
+            Inst::Eqir(o1, o2, o3) => format!("{} = ({} == {}) as usize;", l(o3), o1, r(o2)),
+            Inst::Eqri(o1, o2, o3) => format!("{} = ({} == {}) as usize;", l(o3), r(o1), o2),
+            Inst::Eqrr(o1, o2, o3) => format!("{} = ({} == {}) as usize;", l(o3), r(o1), r(o2)),
+        }
+    }
+}
+
+fn execute<'a, 'b>(
+    op: &Inst,
+    register: &'a [usize; 6],
+    out: &'b mut [usize; 6],
+) -> Option<&'b mut [usize; 6]> {
+    macro_rules! reg {
+        ($num: expr) => {{
+            register[*$num] as isize
+        }};
+    }
+    macro_rules! set {
+        ($num: expr) => {{
+            *$num
+        }};
+    }
+    macro_rules! val {
+        ($num: expr) => {{
+            *$num as isize
+        }};
+    }
+    macro_rules! check {
+        ($num: expr) => {{
+            if $num < 0 {
+                return None;
+            }
+            $num as usize
+        }};
+    }
+    out[..6].copy_from_slice(&register[..6]);
+    assert_eq!(&register, &out);
+    match op {
+        // addr, addi
+        Inst::Addr(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) + reg!(o1)),
+        Inst::Addi(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) + val!(o1)),
+        // // mulr, muli
+        Inst::Mulr(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) * reg!(o1)),
+        Inst::Muli(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) * val!(o1)),
+        // // banr, bani
+        Inst::Banr(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) & reg!(o1)),
+        Inst::Bani(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) & val!(o1)),
+        // // borr, bori
+        Inst::Borr(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) | reg!(o1)),
+        Inst::Bori(o0, o1, o2) => out[set!(o2)] = check!(reg!(o0) | val!(o1)),
+        // // setr, seti
+        Inst::Setr(o0, _, o2) => out[set!(o2)] = check!(reg!(o0)),
+        Inst::Seti(o0, _, o2) => out[set!(o2)] = check!(val!(o0)),
+        // // gtir, gtri, gtrr
+        Inst::Gtir(o0, o1, o2) => out[set!(o2)] = (val!(o0) > reg!(o1)) as usize,
+        Inst::Gtri(o0, o1, o2) => out[set!(o2)] = (reg!(o0) > val!(o1)) as usize,
+        Inst::Gtrr(o0, o1, o2) => out[set!(o2)] = (reg!(o0) > reg!(o1)) as usize,
+        // // eqir, eqri, eqrr
+        Inst::Eqir(o0, o1, o2) => out[set!(o2)] = (val!(o0) == reg!(o1)) as usize,
+        Inst::Eqri(o0, o1, o2) => out[set!(o2)] = (reg!(o0) == val!(o1)) as usize,
+        Inst::Eqrr(o0, o1, o2) => out[set!(o2)] = (reg!(o0) == reg!(o1)) as usize,
+    }
+    Some(out)
 }
 
 #[aoc(2018, 21)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        let parser = regex!(r"^#ip (\d)");
+        if let Some(segment) = parser.captures(block) {
+            self.pc_index = segment[1].parse::<usize>()?;
+        } else {
+            self.line.push(Inst::try_from(block)?);
+        }
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
+        for (i, c) in self.line.iter().enumerate() {
+            println!("{:>3}: {}", i, c.disassemble(i, self.pc_index));
+        }
+        'next: for n in 1..5 {
+            let mut register: [usize; 6] = [0; 6];
+            let mut work: [usize; 6] = [0; 6];
+            let mut count = 0;
+            while let Some(op) = self.line.get(register[self.pc_index]) {
+                if execute(op, &register, &mut work).is_none() {
+                    return n;
+                }
+                std::mem::swap(&mut register, &mut work);
+                register[self.pc_index] += 1;
+                count += 1;
+                if 1000 < count {
+                    continue 'next;
+                }
+            }
+        }
         0
     }
     fn part2(&mut self) -> Self::Output2 {
         0
     }
 }
+
+/*
+  0: b = 123;
+  1: b = b & 456;
+  2: b = (b == 72) as usize;
+  3: pc = b + 3;
+  4: pc = 0;
+  5: b = 0;
+  6: c = b | 65536;
+  7: b = 6663054;
+  8: e = c & 255;
+  9: b = b + e;
+ 10: b = b & 16777215;
+ 11: b = b * 65899;
+ 12: b = b & 16777215;
+ 13: e = (256 > c) as usize;
+ 14: pc = e + 14;
+ 15: pc = 15 + 1;
+ 16: pc = 27;
+ 17: e = 0;
+ 18: d = e + 1;
+ 19: d = d * 256;
+ 20: d = (d > c) as usize;
+ 21: pc = d + 21;
+ 22: pc = 22 + 1;
+ 23: pc = 25;
+ 24: e = e + 1;
+ 25: pc = 17;
+ 26: c = e;
+ 27: pc = 7;
+ 28: e = (b == a) as usize;
+ 29: pc = e + 29;
+ 30: pc = 5;
+*/
+
+/*
+    0: b = 123;
+=>  1: b = 123 & 456;
+    2: b = (b == 72) as usize;
+    3: pc = b + 3;
+=>  4: pc = 0;
+=>  5: b = 0;
+=>  6: c = b | 65536;
+    7: b = 6663054;
+=>  8: e = c & 255;
+    9: b = b + e;
+   10: b = b & 16777215;
+   11: b = b * 65899;
+   12: b = b & 16777215;
+   13: e = (256 > c) as usize;
+   14: pc = e + 14;
+=> 15: pc = 16;
+=> 16: pc = 27;
+=> 17: e = 0;
+=> 18: d = e + 1;
+   19: d = d * 256;
+   20: d = (d > c) as usize;
+   21: pc = d + 21;
+=> 22: pc = 23;
+=> 23: pc = 25;
+=> 24: e += 1;
+=> 25: pc = 17;
+   26: c = e;
+   27: pc = 7;
+   28: e = (b == a) as usize;
+   29: pc = e + 29;
+=> 30: pc = 5;
+=> 31: halt;
+*/
+
+/*
+    0: b = 123;
+=>  1: b = 123 & 456;
+    2: b = (b == 72) as usize;
+    3: pc = b + 3;
+=>  4: pc = 0;
+=>  5: b = 0;
+    loop {
+        =>  6: c = b | 65536;
+            7: b = 6663054;
+        =>  8: e = c & 255;
+            9: b = b + e;
+           10: b = b & 16777215;
+           11: b = b * 65899;
+           12: b = b & 16777215;
+           13: e = (256 > c) as usize;
+           14: pc = e + 14;
+        => 15: pc = 16;
+        => 16: pc = 27;
+        => 17: e = 0;
+        => 18: d = e + 1;
+           19: d = d * 256;
+           20: d = (d > c) as usize;
+           21: pc = d + 21;
+        => 22: pc = 23;
+        => 23: pc = 25;
+        => 24: e += 1;
+        => 25: pc = 17;
+           26: c = e;
+           27: pc = 7;
+           if b == a {
+               halt;
+           }
+           e = 0;
+}
+*/
