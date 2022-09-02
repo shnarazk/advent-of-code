d1681b323eb45a4cf97568cff286f07f2c993b9a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-02 18:13:37 +0900

    modified:   src/y2018/day23.rs

--- src/y2018/day23.rs
+++ src/y2018/day23.rs
@@ -8,7 +8,7 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::collections::{HashMap, HashSet},
 };
 
 trait Geometry {
@@ -37,9 +37,11 @@ impl Geometry for Dim3 {
     }
 }
 
+type Nanobot = ((isize, isize, isize), usize);
+
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<(Dim3, usize)>,
+    line: Vec<Nanobot>,
 }
 
 #[aoc(2018, 23)]
@@ -60,17 +62,130 @@ impl AdventOfCode for Puzzle {
     }
     fn after_insert(&mut self) {
         dbg!(self.line.len());
+        // let m = (
+        //     self.line.iter().map(|p| p.0 .0).max().unwrap(),
+        //     self.line.iter().map(|p| p.0 .1).max().unwrap(),
+        //     self.line.iter().map(|p| p.0 .2).max().unwrap(),
+        //     self.line.iter().map(|p| p.1).max().unwrap(),
+        //     self.line.iter().map(|p| p.1).min().unwrap(),
+        // );
+        // dbg!(m);
     }
     fn part1(&mut self) -> Self::Output1 {
         let strongest: (&usize, &Dim3) = self.line.iter().map(|(p, r)| (r, p)).max().unwrap();
-        let n: usize = self
-            .line
+        self.line
             .iter()
             .filter(|(p, r)| strongest.1.dist(p) <= *strongest.0)
-            .count();
-        n
+            .count()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        // let mut at_least = 903;
+        let mut at_least = 960;
+        let set_x: HashSet<isize> = self
+            .line
+            .iter()
+            .flat_map(|p| [p.0 .0 - p.1 as isize, p.0 .0, p.0 .0 + p.1 as isize])
+            .collect::<HashSet<_>>();
+        let cand_x = set_x
+            .iter()
+            .map(|x| {
+                (
+                    *x,
+                    self.line
+                        .iter()
+                        .filter(|b| b.0 .0.abs_diff(*x) <= b.1)
+                        .collect::<HashSet<_>>(),
+                )
+            })
+            .filter(|(_, v)| at_least <= v.len())
+            .collect::<HashMap<isize, HashSet<&Nanobot>>>();
+        let set_y: HashSet<isize> = self
+            .line
+            .iter()
+            .flat_map(|p| [p.0 .1 - p.1 as isize, p.0 .1, p.0 .1 + p.1 as isize])
+            .collect::<HashSet<_>>();
+        let cand_y = set_y
+            .iter()
+            .map(|y| {
+                (
+                    *y,
+                    self.line
+                        .iter()
+                        .filter(|b| b.0 .1.abs_diff(*y) <= b.1)
+                        .collect::<HashSet<_>>(),
+                )
+            })
+            .filter(|(_, v)| at_least <= v.len())
+            .collect::<HashMap<isize, HashSet<&Nanobot>>>();
+
+        let set_z: HashSet<isize> = self
+            .line
+            .iter()
+            .flat_map(|p| [p.0 .2 - p.1 as isize, p.0 .2, p.0 .2 + p.1 as isize])
+            .collect::<HashSet<_>>();
+        let cand_z = set_z
+            .iter()
+            .map(|z| {
+                (
+                    *z,
+                    self.line
+                        .iter()
+                        .filter(|b| b.0 .2.abs_diff(*z) <= b.1)
+                        .collect::<HashSet<_>>(),
+                )
+            })
+            .filter(|(_, v)| at_least <= v.len())
+            .collect::<HashMap<isize, HashSet<&Nanobot>>>();
+        dbg!(cand_x.len(), cand_y.len(), cand_z.len());
+
+        let mut best_p = (0, 0, 0);
+        let mut most = 0;
+        for (x, robots_x) in cand_x.iter() {
+            if robots_x.len() < at_least {
+                continue;
+            }
+            for (y, robots_y) in cand_y.iter() {
+                if robots_y.len() < at_least {
+                    continue;
+                }
+                let robots = robots_x
+                    .iter()
+                    .filter(|r| robots_y.contains(*r))
+                    .copied()
+                    .collect::<HashSet<_>>();
+                if robots.len() < at_least {
+                    continue;
+                }
+                for (z, robots_z) in cand_z.iter() {
+                    if robots_z.len() < at_least {
+                        continue;
+                    }
+                    let p = (*x, *y, *z);
+                    let n = robots
+                        .iter()
+                        .filter(|r| robots_z.contains(*r))
+                        .filter(|r| r.0.dist(&p) <= r.1)
+                        .count();
+                    match most.cmp(&n) {
+                        std::cmp::Ordering::Less => {
+                            most = n;
+                            best_p = p;
+                            if at_least < most {
+                                at_least = most;
+                            }
+                            println!("{n:>4}/{at_least:>4}: {p:?}");
+                        }
+                        std::cmp::Ordering::Equal
+                            if p.dist(&(0, 0, 0)) < best_p.dist(&(0, 0, 0)) =>
+                        {
+                            println!("         : {p:?}");
+                            best_p = p;
+                        }
+                        _ => (),
+                    }
+                }
+            }
+        }
+        best_p.dist(&(0, 0, 0))
     }
 }
