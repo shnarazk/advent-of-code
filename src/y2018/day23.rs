adc44e267d3a2b9eead493fd2725ce85e5b3e456
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-05 22:39:20 +0900

    hourly update

--- src/y2018/day23.rs
+++ src/y2018/day23.rs
@@ -28,23 +28,14 @@ const DIRS: [Dim3; 6] = [
 ];
 
 trait Geometry {
-    fn x(&self) -> isize;
-    fn y(&self) -> isize;
-    fn z(&self) -> isize;
+    // fn x(&self) -> isize;
+    // fn y(&self) -> isize;
+    // fn z(&self) -> isize;
     fn abs_dist(&self) -> usize;
-    fn dist(&self, other: &Self) -> usize;
+    fn dist(&self, other: &Dim3) -> usize;
 }
 
 impl Geometry for Dim3 {
-    fn x(&self) -> isize {
-        self.0
-    }
-    fn y(&self) -> isize {
-        self.1
-    }
-    fn z(&self) -> isize {
-        self.2
-    }
     fn abs_dist(&self) -> usize {
         self.0.unsigned_abs() + self.1.unsigned_abs() + self.2.unsigned_abs()
     }
@@ -58,34 +49,28 @@ impl Geometry for Dim3 {
 type Nanobot = ((isize, isize, isize), usize);
 
 trait InArea {
-    fn within_range(&self, target: &Dim3) -> bool;
-    fn abs_dist(&self) -> usize;
-    fn dist(&self, pos: &Dim3) -> usize;
-    fn within_envelop_area(&self, cubic: &Cubic) -> bool;
+    fn within_range(&self, target: &Dim3, offset: isize) -> bool;
 }
 
 impl InArea for Nanobot {
-    fn within_range(&self, target: &Dim3) -> bool {
-        self.dist(target) <= self.1
+    fn within_range(&self, target: &Dim3, offset: isize) -> bool {
+        self.0.dist(target) as isize <= self.1 as isize + offset
     }
+}
+
+impl Geometry for Nanobot {
     fn abs_dist(&self) -> usize {
         self.0.abs_dist()
     }
     fn dist(&self, pos: &Dim3) -> usize {
         self.0.dist(pos)
     }
-    fn within_envelop_area(&self, cubic: &Cubic) -> bool {
-        (((self.0 .1 - cubic.center.0) as f64).powf(2.0)
-            + ((self.0 .1 - cubic.center.1) as f64).powf(2.0)
-            + ((self.0 .2 - cubic.center.2) as f64).powf(2.0))
-        .sqrt()
-            < (self.1 + cubic.radius) as f64
-    }
 }
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
     line: Vec<Nanobot>,
+    num_robots: usize,
 }
 
 #[aoc(2018, 23)]
@@ -105,7 +90,8 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(self.line.len());
+        self.num_robots = self.line.len();
+        dbg!(self.num_robots);
     }
     fn part1(&mut self) -> Self::Output1 {
         let strongest: (&usize, &Dim3) = self.line.iter().map(|(p, r)| (r, p)).max().unwrap();
@@ -115,18 +101,9 @@ impl AdventOfCode for Puzzle {
             .count()
     }
     fn part2(&mut self) -> Self::Output2 {
-        let mut best = Cubic {
-            center: (12, 12, 12),
-            radius: 2,
-            affecting: 0,
-        };
-        best.setup_membership(self);
-        dbg!(best.affecting);
-        dbg!(best.is_coherent(self));
         let mut start = Cubic {
-            center: (0, 0, 0),
             radius: 32,
-            affecting: 0,
+            ..Cubic::default()
         };
         start.radius = self
             .line
@@ -136,7 +113,7 @@ impl AdventOfCode for Puzzle {
             .unwrap()
             .next_power_of_two();
         start.setup_membership(self);
-        dbg!(start.radius, start.affecting);
+        dbg!(start.radius, start.completely_inside);
         let mut to_visit: BinaryHeap<Reverse<Cubic>> = BinaryHeap::new();
         let mut max_value = 1;
         let mut max_position = (0, 0, 0);
@@ -164,20 +141,20 @@ impl AdventOfCode for Puzzle {
             //     p.max_bound(),
             //     p.affecting
             // );
-            if p.affecting < max_value
-                || (p.affecting == max_value && best_distance < p.closest().abs_dist())
+            if p.affecting(self) < max_value
+                || (p.affecting(self) == max_value && best_distance < p.closest().abs_dist())
             {
                 continue;
             }
-            if 8192 * 8192 < p.radius && max_value <= p.affecting {
-                println!(
-                    ":::{:?}(d={}, r={}) {}",
-                    p.center,
-                    p.closest().abs_dist(),
-                    p.radius,
-                    p.affecting
-                );
-            }
+            // if 8192 * 8192 < p.radius && max_value <= p.affecting {
+            //     println!(
+            //         ":::{:?}(d={}, r={}) {}",
+            //         p.center,
+            //         p.closest().abs_dist(),
+            //         p.radius,
+            //         p.affecting
+            //     );
+            // }
             // if to_visit.len() % 1000 == 0 {
             //     dbg!(to_visit.len());
             // }
@@ -227,7 +204,6 @@ impl AdventOfCode for Puzzle {
             }
             for sub in p.divide().iter() {
                 let mut q = sub.clone();
-                q.affecting = 1;
                 q.setup_membership(self);
                 // if 2 < q.affecting {
                 //     println!("{:?}, {}", q.center, q.max_bound());
@@ -248,7 +224,7 @@ impl AdventOfCode for Puzzle {
 }
 impl Puzzle {
     fn count(&self, pos: &Dim3) -> usize {
-        self.line.iter().filter(|r| r.within_range(pos)).count()
+        self.line.iter().filter(|r| r.within_range(pos, 0)).count()
     }
 }
 
@@ -256,39 +232,51 @@ impl Puzzle {
 struct Cubic {
     center: Dim3,
     radius: usize,
-    affecting: usize,
+    completely_inside: usize,
+    completely_outside: usize,
+    not_affect: usize,
 }
 
 impl Cubic {
+    fn affecting(&self, world: &Puzzle) -> usize {
+        world.num_robots - self.completely_outside
+    }
     fn setup_membership(&mut self, world: &Puzzle) {
-        self.affecting = world
+        self.completely_inside = world
             .line
             .iter()
-            .filter(|r| r.within_envelop_area(self))
+            .filter(|r| r.within_range(&self.center, -(self.radius as isize)))
+            .count();
+        self.completely_outside = world
+            .line
+            .iter()
+            .filter(|r| !r.within_range(&self.center, 0))
             .count();
     }
     fn is_coherent(&mut self, world: &Puzzle) -> Option<usize> {
-        if self.radius == 0 {
-            return Some(world.count(&self.center));
-        }
-        let overestimate = world
-            .line
-            .iter()
-            .map(|r| r.within_envelop_area(self))
-            .collect::<Vec<_>>();
-        for (i, robot) in world.line.iter().enumerate() {
-            for dir in DIRS.iter() {
-                let pos = (
-                    self.center.0 + dir.0 * self.radius as isize,
-                    self.center.1 + dir.1 * self.radius as isize,
-                    self.center.2 + dir.2 * self.radius as isize,
-                );
-                if overestimate[i] != robot.within_range(&pos) {
-                    return None;
-                }
-            }
-        }
-        Some(overestimate.iter().filter(|b| **b).count())
+        (world.num_robots == self.completely_inside + self.completely_outside)
+            .then(|| world.count(&self.center))
+        // if self.radius == 0 {
+        //     return Some(world.count(&self.center));
+        // }
+        // let overestimate = world
+        //     .line
+        //     .iter()
+        //     .map(|r| r.within_range(&self.center))
+        //     .collect::<Vec<_>>();
+        // for (i, robot) in world.line.iter().enumerate() {
+        //     for dir in DIRS.iter() {
+        //         let pos = (
+        //             self.center.0 + dir.0 * self.radius as isize,
+        //             self.center.1 + dir.1 * self.radius as isize,
+        //             self.center.2 + dir.2 * self.radius as isize,
+        //         );
+        //         if overestimate[i] != robot.within_range(&pos) {
+        //             return None;
+        //         }
+        //     }
+        // }
+        // Some(overestimate.iter().filter(|b| **b).count())
     }
     fn divide(&self) -> Vec<Cubic> {
         let c = self.center;
@@ -398,33 +386,24 @@ impl Cubic {
 }
 
 impl Geometry for Cubic {
-    fn x(&self) -> isize {
-        self.center.0
-    }
-    fn y(&self) -> isize {
-        self.center.1
-    }
-    fn z(&self) -> isize {
-        self.center.2
-    }
     fn abs_dist(&self) -> usize {
         self.center.0.unsigned_abs() + self.center.1.unsigned_abs() + self.center.2.unsigned_abs()
     }
-    fn dist(&self, other: &Cubic) -> usize {
+    fn dist(&self, other: &Dim3) -> usize {
         unimplemented!()
     }
 }
 
 impl PartialOrd for Cubic {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        self.affecting.partial_cmp(&other.affecting)
+        self.completely_inside.partial_cmp(&other.completely_inside)
         // self.abs_dist().partial_cmp(&other.abs_dist())
     }
 }
 
 impl Ord for Cubic {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.affecting.cmp(&other.affecting)
+        self.completely_inside.cmp(&other.completely_inside)
         // self.abs_dist().cmp(&other.abs_dist())
     }
 }
