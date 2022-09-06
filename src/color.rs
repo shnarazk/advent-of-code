f2e4b5c89c6cb71cceaca5db28645ad4f6fd54e4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-06 15:40:55 +0900

    color::REVERT: Cursor up and move to 1st column, then erase in Line

--- src/color.rs
+++ src/color.rs
@@ -4,4 +4,4 @@ pub const BLUE: &str = "\x1B[001m\x1B[034m";
 pub const MAGENTA: &str = "\x1B[001m\x1B[035m";
 pub const CYAN: &str = "\x1B[001m\x1B[036m";
 pub const RESET: &str = "\x1B[000m";
-pub const REVERT: &str = "\x1B[1A\x1B[1G";
+pub const REVERT: &str = "\x1B[1A\x1B[1G\x1B[1K";
