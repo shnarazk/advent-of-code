d7adc5b37a770ed1f4c3e64c2b2999d2d065f9df
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-07 19:36:51 +0900

    y2018 day25 part1 done.

--- src/y2018/day25.rs
+++ src/y2018/day25.rs
@@ -8,36 +8,75 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::collections::{HashMap, HashSet},
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<isize>>,
 }
 
 #[aoc(2018, 25)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Maybe<Option<String>> {
-    //     let parser: Regex = Regex::new(r"^(.+)\n\n((.|\n)+)$").expect("wrong");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(Some(segment[2].to_string()))
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^([0-9]+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line.push(line_parser::to_isizes(block, ',')?);
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        let mut constellation: HashMap<usize, usize> = HashMap::new();
+        let mut constellation_id = 1;
+        constellation.insert(0, constellation_id);
+        'next_point: for (i, p) in self.line.iter().enumerate().skip(1) {
+            for (j, q) in self.line.iter().enumerate().take(i - 1) {
+                let dist: usize = (p[0] - q[0]).unsigned_abs()
+                    + (p[1] - q[1]).unsigned_abs()
+                    + (p[2] - q[2]).unsigned_abs()
+                    + (p[3] - q[3]).unsigned_abs();
+                if dist <= 3 {
+                    let cid = constellation[&j];
+                    constellation.insert(i, cid);
+                    continue 'next_point;
+                }
+            }
+            constellation_id += 1;
+            constellation.insert(i, constellation_id);
+        }
+        dbg!(constellation_id);
+        let mut rebuild = true;
+        'restart: while rebuild {
+            rebuild = false;
+            for (i, p) in self.line.iter().enumerate() {
+                for (j, q) in self.line.iter().enumerate().skip(i) {
+                    let ci = constellation[&i];
+                    let cj = constellation[&j];
+                    if ci == cj {
+                        continue;
+                    }
+                    let dist: usize = (p[0] - q[0]).unsigned_abs()
+                        + (p[1] - q[1]).unsigned_abs()
+                        + (p[2] - q[2]).unsigned_abs()
+                        + (p[3] - q[3]).unsigned_abs();
+                    if dist <= 3 {
+                        rebuild = true;
+                        // renumber cj to ci
+                        for (_, v) in constellation.iter_mut() {
+                            if *v == cj {
+                                *v = ci;
+                            }
+                        }
+                        constellation_id -= 1;
+                        // dbg!(constellation_id);
+                        continue 'restart;
+                    }
+                }
+            }
+        }
+        dbg!(constellation.values().collect::<HashSet<_>>().len());
+        constellation_id
     }
     fn part2(&mut self) -> Self::Output2 {
         0
