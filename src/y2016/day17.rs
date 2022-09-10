269470ba4371b1a4f28dc49d2105fe2eda69355a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-10 23:45:00 +0900

    y2016 day16: solve in Rust

--- src/y2016/day17.rs
+++ src/y2016/day17.rs
@@ -1,21 +1,67 @@
 //! <https://adventofcode.com/2016/day/17>
-use crate::framework::{aoc, AdventOfCode, ParseError};
+use crate::framework::{aoc_at, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Puzzle {}
+pub struct Puzzle {
+    line: Vec<bool>,
+}
 
-#[aoc(2016, 17)]
+#[aoc_at(2016, 17)]
 impl AdventOfCode for Puzzle {
+    type Output1 = String;
+    type Output2 = String;
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
         Ok(())
     }
+    fn after_insert(&mut self) {
+        self.line = "10001110011110000"
+            .chars()
+            .map(|c| c == '1')
+            .collect::<Vec<_>>();
+    }
     fn part1(&mut self) -> Self::Output1 {
-        println!("This puzzle was solved by Cuis-Smalltalk.");
-        0
+        let disk_size = 272;
+        let mut d = self.line.clone();
+        while d.len() < disk_size {
+            d = lengthen(d);
+        }
+        checksum(disk_size, &d)
+            .iter()
+            .map(|b| if *b { '1' } else { '0' })
+            .collect::<String>()
     }
     fn part2(&mut self) -> Self::Output2 {
-        println!("This puzzle was solved by Cuis-Smalltalk.");
-        0
+        let disk_size = 35651584;
+        let mut d = self.line.clone();
+        while d.len() < disk_size {
+            d = lengthen(d);
+        }
+        checksum(disk_size, &d)
+            .iter()
+            .map(|b| if *b { '1' } else { '0' })
+            .collect::<String>()
+    }
+}
+
+fn lengthen(mut a: Vec<bool>) -> Vec<bool> {
+    let mut b: Vec<bool> = a.iter().map(|b| !*b).collect::<Vec<_>>();
+    b.reverse();
+    a.push(false);
+    a.append(&mut b);
+    a
+}
+
+fn checksum(length: usize, data: &[bool]) -> Vec<bool> {
+    let mut result = data.to_vec();
+    let mut len = length / 2;
+    loop {
+        result = (0..len)
+            .map(|i| result[2 * i] == result[2 * i + 1])
+            .collect::<Vec<_>>();
+        if result.len() % 2 == 1 {
+            return result;
+        }
+        len /= 2;
     }
 }
