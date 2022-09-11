3d00125a83ab0d420f792a8f75fd38d7d1663fed
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-11 15:34:48 +0900

    y2019 day1: solve in Rust

--- src/y2019/day01.rs
+++ src/y2019/day01.rs
@@ -2,20 +2,39 @@
 use crate::framework::{aoc, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Puzzle {}
+pub struct Puzzle {
+    line: Vec<usize>,
+}
 
 #[aoc(2019, 1)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        self.line.push(block.parse::<usize>()?);
         Ok(())
     }
+    fn after_insert(&mut self) {
+        dbg!(self.line.len());
+    }
     fn part1(&mut self) -> Self::Output1 {
-        println!("This puzzle was solved by Swift.");
-        0
+        self.line.iter().map(|v| fuel(*v)).sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        println!("This puzzle was solved by Swift.");
+        self.line.iter().map(|v| fuel2(*v)).sum()
+    }
+}
+
+fn fuel(mass: usize) -> usize {
+    mass / 3 - 2
+}
+
+fn fuel2(mass: usize) -> usize {
+    let mut f = mass / 3;
+    if 2 < f {
+        f -= 2;
+        f += fuel2(f);
+        f
+    } else {
         0
     }
 }
