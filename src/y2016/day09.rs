a9abf283602d60b63b48e6fbe944d0ff678b601f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-09-11 23:12:04 +0900

    y2016 day9: solve in Rust

--- src/y2016/day09.rs
+++ src/y2016/day09.rs
@@ -1,27 +1,52 @@
 //! <https://adventofcode.com/2016/day/09>
-use {
-    crate::framework::{aoc, AdventOfCode, ParseError},
-    std::process::Command,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    regex,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: String,
 }
 
 #[aoc(2016, 9)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        self.line = block.to_string();
         Ok(())
     }
+    fn after_insert(&mut self) {
+        dbg!(self.line.len());
+    }
     fn part1(&mut self) -> Self::Output1 {
-        if let Ok(output) = Command::new("bqn/2016/day09.bqn").output() {
-            println!("{}", String::from_utf8_lossy(&output.stdout));
+        let mut buffer: &str = self.line.trim();
+        let mut length = 0;
+        let parser = regex!(r"([^(]*)\((\d+)x(\d+)\)");
+        while let Some(segment) = parser.captures(buffer) {
+            let len = segment[2].parse::<usize>().unwrap();
+            let rep = segment[3].parse::<usize>().unwrap();
+            length += segment[1].len() + len * rep;
+            buffer = &buffer[segment[0].len() + len..];
         }
-        0
+        length += buffer.len();
+        length
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        span_len(self.line.trim())
     }
+}
+
+fn span_len(mut buffer: &str) -> usize {
+    let mut length = 0;
+    let parser = regex!(r"([^(]*)\((\d+)x(\d+)\)");
+    while let Some(segment) = parser.captures(buffer) {
+        let len = segment[2].parse::<usize>().unwrap();
+        let rep = segment[3].parse::<usize>().unwrap();
+        length +=
+            segment[1].len() + rep * span_len(&buffer[segment[0].len()..segment[0].len() + len]);
+        buffer = &buffer[segment[0].len() + len..];
+    }
+    length += buffer.len();
+    length
 }
