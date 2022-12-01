f68a74792d62da20022339e2828908f24911eebe
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-01 17:09:25 +0900

    y2022 day 1 done.

--- src/y2022/day01.rs
+++ src/y2022/day01.rs
@@ -1,45 +1,36 @@
 //! <https://adventofcode.com/2022/day/1>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    line_parser,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<usize>>,
 }
 
 #[aoc(2022, 1)]
 impl AdventOfCode for Puzzle {
-    const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
+    const DELIMITER: &'static str = "\n\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line
+            .push(line_parser::to_usizes_spliting_with(block, &['\n'])?);
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.line.iter().map(|v| v.iter().sum()).max().unwrap()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut elves = self
+            .line
+            .iter()
+            .map(|v| v.iter().sum())
+            .collect::<Vec<usize>>();
+        // dbg!(&elves);
+        elves.sort();
+        elves.iter().skip(elves.len() - 3).sum()
     }
 }
