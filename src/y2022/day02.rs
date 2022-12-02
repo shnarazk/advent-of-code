2db0ae541cbb16d47923bbcbdba28f8b8bec530d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-02 15:14:11 +0900

    y2022 day2 part1 done

--- src/y2022/day02.rs
+++ src/y2022/day02.rs
@@ -13,31 +13,37 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(char, char)>,
 }
 
 #[aoc(2022, 2)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        let chars = block.chars().collect::<Vec<_>>();
+        self.line.push((chars[0], chars[2]));
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        #[allow(clippy::identity_op)]
+        self.line
+            .iter()
+            .map(|game| match game {
+                ('A', 'X') => 1 + 3,
+                ('A', 'Y') => 2 + 6,
+                ('A', 'Z') => 3 + 0,
+                ('B', 'X') => 1 + 0,
+                ('B', 'Y') => 2 + 3,
+                ('B', 'Z') => 3 + 6,
+                ('C', 'X') => 1 + 6,
+                ('C', 'Y') => 2 + 0,
+                ('C', 'Z') => 3 + 3,
+                _ => unreachable!(),
+            })
+            .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
         0
