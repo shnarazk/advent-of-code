50e1ce53b1dec4c862fe11717ea42674ad57e413
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-04 14:11:54 +0900

    y2022 day4 part1 done

--- src/y2022/day04.rs
+++ src/y2022/day04.rs
@@ -13,31 +13,31 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(usize, usize, usize, usize)>,
 }
 
 #[aoc(2022, 4)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
+        let parser = regex!(r"^(\d+)-(\d+),(\d+)-(\d+)$");
         let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        self.line.push((
+            segment[1].parse::<_>()?,
+            segment[2].parse::<_>()?,
+            segment[3].parse::<_>()?,
+            segment[4].parse::<_>()?,
+        ));
         Ok(())
     }
     fn after_insert(&mut self) {
         dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.line
+            .iter()
+            .filter(|(a, b, x, y)| (a <= x && y <= b) || (x <= a && b <= y))
+            .count()
     }
     fn part2(&mut self) -> Self::Output2 {
         0
