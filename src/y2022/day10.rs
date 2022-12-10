ab6b104c378863c726a597ebabacbf9bce4298dd
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-10 15:32:40 +0900

    y2022 day10 part1 done

--- src/y2022/day10.rs
+++ src/y2022/day10.rs
@@ -4,42 +4,85 @@
 #![allow(unused_variables)]
 use {
     crate::{
-        framework::{aoc, AdventOfCode, ParseError},
+        framework::{aoc_at, AdventOfCode, ParseError},
         geometric::neighbors,
         line_parser, regex,
     },
     std::collections::HashMap,
 };
 
+#[derive(Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum Code {
+    Noop,
+    Addx(isize),
+}
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Code>,
+    pc: usize,
+    register: isize,
+    cycle: usize,
+    auto_sum: isize,
 }
 
-#[aoc(2022, 10)]
+#[aoc_at(2022, 10)]
 impl AdventOfCode for Puzzle {
+    type Output1 = isize;
+    type Output2 = isize;
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        let addx_parser = regex!(r"^addx (-?\d+)$");
+        let noop_parser = regex!(r"^noop$");
+        if let Some(segment) = addx_parser.captures(block) {
+            self.line.push(Code::Addx(segment[1].parse::<isize>()?));
+        } else if noop_parser.captures(block).is_some() {
+            self.line.push(Code::Noop);
+        }
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        self.register = 1;
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        for _ in 0..self.line.len() {
+            self.execute();
+        }
+        self.auto_sum
     }
     fn part2(&mut self) -> Self::Output2 {
         0
+    }
+}
+
+impl Puzzle {
+    fn signal_strength(&self) -> isize {
+        self.cycle as isize * self.register
+    }
+    fn update_cycle(&mut self, n: usize) {
+        for _ in 0..n {
+            self.cycle += 1;
+            if 20 <= self.cycle && (self.cycle - 20) % 40 == 0 {
+                self.auto_sum += self.signal_strength();
+            }
+        }
+    }
+    fn execute(&mut self) {
+        let Some(inst) = &self.line.get(self.pc) else { return; };
+        // if 180 < self.cycle {
+        //     println!("{} {:?}", self.pc, inst);
+        // }
+        match inst {
+            Code::Noop => {
+                self.update_cycle(1);
+            }
+            Code::Addx(n) => {
+                let k = *n;
+                self.update_cycle(1);
+                self.update_cycle(1);
+                self.register += k;
+            }
+        }
+        self.pc += 1;
     }
 }
