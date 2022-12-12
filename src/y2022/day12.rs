f14ea7884ff620658e73ac2a322f7c1c469374a3
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-12 22:39:55 +0900

    clean up

--- src/y2022/day12.rs
+++ src/y2022/day12.rs
@@ -1,20 +1,13 @@
 //! <https://adventofcode.com/2022/day/12>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 
-use std::collections::HashSet;
-
-use crate::geometric;
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
+        geometric,
     },
     std::{
         cmp::Reverse,
-        collections::{BinaryHeap, HashMap},
+        collections::{BinaryHeap, HashSet},
     },
 };
 
@@ -63,8 +56,7 @@ impl AdventOfCode for Puzzle {
         self.width = self.line[0].len();
         self.line[self.start.0][self.start.1] = 0;
         self.line[self.goal.0][self.goal.1] = 'z' as usize - 'a' as usize;
-        dbg!(&self.goal);
-        dbg!(self.height, self.width);
+        dbg!(&self.goal, self.height, self.width);
     }
     fn part1(&mut self) -> Self::Output1 {
         let mut to_visit: BinaryHeap<Reverse<State>> = BinaryHeap::new();
@@ -74,32 +66,23 @@ impl AdventOfCode for Puzzle {
             location: self.start,
         }));
         visited.insert(self.start);
-        let mut level = 0;
-        let mut steps = 0;
         while let Some(Reverse(state)) = to_visit.pop() {
             if state.location == self.goal {
                 return state.cost;
-            }
-            let lv = self.line[state.location.0][state.location.1];
-            if level < lv {
-                level = lv;
-                steps = state.cost;
             }
             for next in
                 geometric::neighbors4(state.location.0, state.location.1, self.height, self.width)
                     .into_iter()
             {
-                if self.line[state.location.0][state.location.1] + 1 < self.line[next.0][next.1] {
+                if self.line[state.location.0][state.location.1] + 1 < self.line[next.0][next.1]
+                    || visited.contains(&next)
+                {
                     continue;
                 }
-                if visited.contains(&next) {
-                    continue;
-                }
-                let ns = State {
+                to_visit.push(Reverse(State {
                     cost: state.cost + 1,
                     location: next,
-                };
-                to_visit.push(Reverse(ns));
+                }));
                 visited.insert(next);
             }
         }
@@ -116,8 +99,7 @@ impl AdventOfCode for Puzzle {
             }
             println!();
         }
-        dbg!(visited.len());
-        steps
+        unreachable!()
     }
     fn part2(&mut self) -> Self::Output2 {
         let mut to_visit: BinaryHeap<Reverse<State>> = BinaryHeap::new();
@@ -131,22 +113,19 @@ impl AdventOfCode for Puzzle {
             if self.line[state.location.0][state.location.1] == 0 {
                 return state.cost;
             }
-            let lv = self.line[state.location.0][state.location.1];
             for next in
                 geometric::neighbors4(state.location.0, state.location.1, self.height, self.width)
                     .into_iter()
             {
-                if self.line[next.0][next.1] + 1 < self.line[state.location.0][state.location.1] {
+                if self.line[next.0][next.1] + 1 < self.line[state.location.0][state.location.1]
+                    || visited.contains(&next)
+                {
                     continue;
                 }
-                if visited.contains(&next) {
-                    continue;
-                }
-                let ns = State {
+                to_visit.push(Reverse(State {
                     cost: state.cost + 1,
                     location: next,
-                };
-                to_visit.push(Reverse(ns));
+                }));
                 visited.insert(next);
             }
         }
