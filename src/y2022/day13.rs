d124b0ebd49006b9bd457705abca7b172cb4cc63
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-13 16:35:58 +0900

    y2022 day12 part1 done

--- src/y2022/day13.rs
+++ src/y2022/day13.rs
@@ -8,36 +8,81 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    nom::{
+        branch::alt, bytes::complete::tag, character::complete::digit1, multi::separated_list0,
+        IResult,
+    },
+    std::{cmp::Ordering, collections::HashMap},
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+enum Expr {
+    Num(usize),
+    Array(Vec<Expr>),
+}
+
+fn parse_expr_num(input: &str) -> IResult<&str, Expr> {
+    let (a, b) = digit1(input)?;
+    Ok((a, Expr::Num(b.parse::<usize>().unwrap())))
+}
+
+fn parse_expr_array(input: &str) -> IResult<&str, Expr> {
+    let (v, _) = tag("[")(input)?;
+    let (e, b) = separated_list0(tag(","), parse_expr)(v)?;
+    let (r, _) = tag("]")(e)?;
+    Ok((r, Expr::Array(b)))
+}
+
+fn parse_expr(input: &str) -> IResult<&str, Expr> {
+    alt((parse_expr_array, parse_expr_num))(input)
+}
+
+fn compare(e1: &Expr, e2: &Expr) -> Ordering {
+    // dbg!(e1, e2);
+    match (e1, e2) {
+        (Expr::Num(a), Expr::Num(b)) => a.cmp(b),
+        (Expr::Array(v1), Expr::Array(v2)) => {
+            for i in 0..v1.len().max(v2.len()) {
+                let Some(i1) = v1.get(i) else { return if i == v2.len() { Ordering::Equal} else { Ordering::Less};};
+                let Some(i2) = v2.get(i) else { return Ordering::Greater;};
+                match compare(i1, i2) {
+                    Ordering::Equal => (),
+                    other => return other,
+                }
+            }
+            Ordering::Equal
+        }
+        (Expr::Num(a), Expr::Array(b)) => compare(&Expr::Array(vec![e1.clone()]), e2),
+        (Expr::Array(a), Expr::Num(b)) => compare(e1, &Expr::Array(vec![e2.clone()])),
+    }
+}
+
+#[derive(Debug, Default, Eq, Hash, PartialEq)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(Expr, Expr)>,
 }
 
 #[aoc(2022, 13)]
 impl AdventOfCode for Puzzle {
-    const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
+    const DELIMITER: &'static str = "\n\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[0].parse::<_>());
+        let mut lines = block.split('\n');
+        self.line.push((
+            parse_expr(lines.next().unwrap()).expect("!!!!").1,
+            parse_expr(lines.next().unwrap()).expect("!!!!").1,
+        ));
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        0
+        self.line
+            .iter()
+            .enumerate()
+            .filter(|(_, (a, b))| compare(a, b) == Ordering::Less)
+            .map(|(i, _)| i + 1)
+            .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
         0
