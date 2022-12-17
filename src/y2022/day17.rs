02ed07f3e951b91493d810b2a40d99953f4f2cd3
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-18 01:57:01 +0900

    y2022 day17 done.

--- src/y2022/day17.rs
+++ src/y2022/day17.rs
@@ -6,9 +6,9 @@ use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
         geometric::neighbors,
-        line_parser, regex,
+        line_parser, math, regex,
     },
-    std::collections::{HashMap, HashSet},
+    std::collections::{HashMap, HashSet, VecDeque},
 };
 
 type Loc = (usize, usize);
@@ -22,7 +22,6 @@ pub struct Puzzle {
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
         if !block.is_empty() {
             self.line = block
                 .trim()
@@ -33,7 +32,7 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line.len());
+        dbg!(self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
         let mut window = self.line.iter().cycle();
@@ -77,13 +76,134 @@ impl AdventOfCode for Puzzle {
                 y = tmp_y;
             }
             bottom = place((x, y), &shape[id], &mut blocks);
-            // print(&blocks);
-            // assert!(i < 3);
         }
         bottom
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let target = 1000000000000;
+        let (header_len, header_height, unit_len, unit_height) = self.cyclic_pattern();
+        let num_units = (target - header_len) / unit_len;
+        let remain_rocks = (target - header_len) % unit_len;
+        let remain_height = self.height_after(header_len + remain_rocks);
+        num_units * unit_height + remain_height
+    }
+}
+
+impl Puzzle {
+    fn cyclic_pattern(&mut self) -> (usize, usize, usize, usize) {
+        let mut memory: HashMap<[usize; 9], (usize, usize)> = HashMap::new();
+        let mut cycles = 0;
+        let mut pre_bottom = 0;
+        let mut pre_x: VecDeque<usize> = VecDeque::new();
+        let mut window = self.line.iter().cycle();
+        let window_cycle = self.line.len();
+        macro_rules! blow {
+            ($x: expr, $w: expr) => {
+                ($x as isize + window.next().unwrap()).clamp(1, 8 - $w) as usize
+            };
+        }
+        let mut blocks: HashSet<Loc> = HashSet::new();
+        for i in 0..7 {
+            blocks.insert((i, 0));
+        }
+        let shape = [
+            vec![(0, 0), (1, 0), (2, 0), (3, 0)],
+            vec![(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)],
+            vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
+            vec![(0, 0), (0, 1), (0, 2), (0, 3)],
+            vec![(0, 0), (1, 0), (0, 1), (1, 1)],
+        ];
+        let shape_w = [4, 3, 3, 1, 2];
+        let shape_h = [1, 3, 3, 4, 2];
+        let mut bottom = 0;
+        for i in 0.. {
+            let id = i % 5;
+            if 0 < i && i % window_cycle == 0 && id == 0 {
+                cycles += 1;
+                let key = [
+                    bottom - pre_bottom,
+                    pre_x[0],
+                    pre_x[1],
+                    pre_x[2],
+                    pre_x[3],
+                    pre_x[4],
+                    pre_x[5],
+                    pre_x[6],
+                    pre_x[7],
+                ];
+                println!("{cycles:>4} i:{i:>7}, key:{:?}", &key);
+                if let Some((header_len, header_height)) = memory.get(&key) {
+                    return dbg!(
+                        *header_len,
+                        *header_height,
+                        i - *header_len,
+                        bottom - *header_height
+                    );
+                }
+                memory.insert(key, (i, bottom));
+                pre_bottom = bottom;
+            }
+            let mut x: usize = 3;
+            let mut y = bottom + 4;
+            loop {
+                let tmp_x = blow!(x, shape_w[id]);
+                if bottom < y || !clash((tmp_x, y), &shape[id], &blocks) {
+                    x = tmp_x;
+                }
+                let tmp_y = y - 1;
+                if tmp_y <= bottom && clash((x, tmp_y), &shape[id], &blocks) {
+                    break;
+                }
+                y = tmp_y;
+            }
+            blocks.retain(|(_, h)| bottom <= *h + 500);
+            bottom = place((x, y), &shape[id], &mut blocks);
+            pre_x.push_front(x);
+            pre_x.resize(8, 0);
+        }
+        unreachable!()
+    }
+    fn height_after(&mut self, num_rocks: usize) -> usize {
+        let mut window = self.line.iter().cycle();
+        let window_cycle = self.line.len();
+        macro_rules! blow {
+            ($x: expr, $w: expr) => {
+                ($x as isize + window.next().unwrap()).clamp(1, 8 - $w) as usize
+            };
+        }
+        let mut blocks: HashSet<Loc> = HashSet::new();
+        for i in 0..7 {
+            blocks.insert((i, 0));
+        }
+        let shape = [
+            vec![(0, 0), (1, 0), (2, 0), (3, 0)],
+            vec![(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)],
+            vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)],
+            vec![(0, 0), (0, 1), (0, 2), (0, 3)],
+            vec![(0, 0), (1, 0), (0, 1), (1, 1)],
+        ];
+        let shape_w = [4, 3, 3, 1, 2];
+        let shape_h = [1, 3, 3, 4, 2];
+        let mut bottom = 0;
+        for i in 0..num_rocks {
+            let id = i % 5;
+            let mut x: usize = 3;
+            let mut y = bottom + 4;
+            loop {
+                let tmp_x = blow!(x, shape_w[id]);
+                if bottom < y || !clash((tmp_x, y), &shape[id], &blocks) {
+                    x = tmp_x;
+                }
+                let tmp_y = y - 1;
+                if tmp_y <= bottom && clash((x, tmp_y), &shape[id], &blocks) {
+                    break;
+                }
+                y = tmp_y;
+            }
+            blocks.retain(|(_, h)| bottom <= *h + 500);
+            bottom = place((x, y), &shape[id], &mut blocks);
+        }
+        bottom
     }
 }
 
