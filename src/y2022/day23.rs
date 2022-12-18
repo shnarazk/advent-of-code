8a06ab176b47e466d13ae3418a9e5482c3089c35
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-18 20:37:29 +0900

    prepare for day19 - 25

---
+++ src/y2022/day23.rs
@@ -0,0 +1,45 @@
+//! <https://adventofcode.com/2022/day/23>
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use {
+    crate::{
+        framework::{aoc, AdventOfCode, ParseError},
+        geometric::neighbors,
+        line_parser, regex,
+    },
+    std::collections::HashMap,
+};
+
+#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Puzzle {
+    line: Vec<()>,
+}
+
+#[aoc(2022, 23)]
+impl AdventOfCode for Puzzle {
+    const DELIMITER: &'static str = "\n";
+    // fn header(&mut self, input: String) -> Result<String, ParseError> {
+    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
+    //     let segment = parser.captures(input).ok_or(ParseError)?;
+    //     for num in segment[1].split(',') {
+    //         let _value = num.parse::<usize>()?;
+    //     }
+    //     Ok(segment[2].to_string())
+    // }
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        let parser = regex!(r"^(\d+)$");
+        let segment = parser.captures(block).ok_or(ParseError)?;
+        // self.line.push(segment[1].parse::<_>());
+        Ok(())
+    }
+    fn after_insert(&mut self) {
+        dbg!(&self.line);
+    }
+    fn part1(&mut self) -> Self::Output1 {
+        1
+    }
+    fn part2(&mut self) -> Self::Output2 {
+        2
+    }
+}
