37696963e719e1e0bd8bd04236ee0e185386bed7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-21 20:16:00 +0900

    y2022 day21 done.

--- src/y2022/day21.rs
+++ src/y2022/day21.rs
@@ -10,23 +10,25 @@ use {
     },
     std::collections::HashMap,
 };
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 enum Expr {
     Num(String, isize),
     Add(String, String, String),
     Sub(String, String, String),
     Mul(String, String, String),
     Div(String, String, String),
+    Var,
 }
 
 impl Expr {
-    fn label(&self) -> &String {
+    fn label(&self) -> &str {
         match self {
-            Expr::Num(_, _) => unreachable!(),
+            Expr::Num(val, _) => val,
             Expr::Add(val, _, _) => val,
             Expr::Sub(val, _, _) => val,
             Expr::Mul(val, _, _) => val,
             Expr::Div(val, _, _) => val,
+            Expr::Var            => "X",
         }
     }
     fn arg1(&self) -> &String {
@@ -36,6 +38,7 @@ impl Expr {
             Expr::Sub(_, val, _) => val,
             Expr::Mul(_, val, _) => val,
             Expr::Div(_, val, _) => val,
+            Expr::Var            => unreachable!(),
         }
     }
     fn arg2(&self) -> &String {
@@ -45,6 +48,7 @@ impl Expr {
             Expr::Sub(_, _, val) => val,
             Expr::Mul(_, _, val) => val,
             Expr::Div(_, _, val) => val,
+            Expr::Var            => unreachable!(),
         }
     }
     fn eval(&self, v1: isize, v2: isize) -> isize {
@@ -54,6 +58,36 @@ impl Expr {
             Expr::Sub(_, _, _) => v1 - v2,
             Expr::Mul(_, _, _) => v1 * v2,
             Expr::Div(_, _, _) => v1 / v2,
+            Expr::Var            => unreachable!(),
+        }
+    }
+    fn mnemonic(&self) -> &str {
+        match self {
+            Expr::Num(_, _) => unreachable!(),
+            Expr::Add(_, _, _) => "+",
+            Expr::Sub(_, _, _) => "-",
+            Expr::Mul(_, _, _) => "*",
+            Expr::Div(_, _, _) => "/",
+            Expr::Var            => unreachable!(),
+        }
+    }
+    fn build(&self, monkeys: &HashMap<String, Expr>, values: &HashMap<String, isize>) {
+        match self {
+            Expr::Num(n, val) if n == "humn" => print!("x"),
+            Expr::Num(_, val) => print!("{val}"),
+            Expr::Add(l, _, _) | Expr::Sub(l, _, _) | Expr::Mul(l, _, _) | Expr::Div(l, _, _) if values.contains_key(l)=> {
+                print!("{}", values.get(l).unwrap());
+                }
+            Expr::Add(_, l, r) | Expr::Sub(_, l, r) | Expr::Mul(_, l, r) | Expr::Div(_, l, r) => {
+                print!("(");
+                let lm = monkeys.get(l).unwrap();
+                lm.build(monkeys, values);
+                print!("{}", self.mnemonic());
+                let rm = monkeys.get(r).unwrap();
+                rm.build(monkeys, values);
+                print!(")");
+            }
+            Expr::Var => print!("X"),
         }
     }
 }
@@ -100,7 +134,8 @@ impl AdventOfCode for Puzzle {
         for m in self.line.iter() {
             let Expr::Num(label, value) = m else { 
                 monkeys.push(m);
-                continue;};
+                continue;
+            };
             values.insert(label.to_string(), *value);
         }
         loop {
@@ -120,12 +155,97 @@ impl AdventOfCode for Puzzle {
                 if m.label() == "root" {
                     return result;
                 }
-                values.insert(m.label().clone(), result);
+                values.insert(m.label().to_string(), result);
             }
             std::mem::swap(&mut monkeys, &mut remain);
         }
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        let mut values: HashMap<String, isize> = HashMap::new();
+        let mut monkeys: Vec<&Expr> = Vec::new();
+        for m in self.line.iter() {
+            let Expr::Num(label, value) = m else { 
+                if "root" != m.label() {
+                    monkeys.push(m);
+                }
+                continue;
+            };
+            if "humn" != m.label() {
+                values.insert(label.to_string(), *value);
+            }
+        }
+        let mut generated = false;
+        while generated {
+            let mut remain = Vec::new();
+            while let Some(m) = monkeys.pop() {
+                let a1 = m.arg1();
+                let a2 = m.arg2();
+                let Some(v1) = values.get(a1) else {
+                    remain.push(m);       
+                    continue;
+                };
+                let Some(v2) = values.get(a2) else {
+                    remain.push(m);       
+                    continue;
+                };
+                let result = m.eval(*v1, *v2);
+                if m.label() == "root" {
+                    continue;
+                }
+                values.insert(m.label().to_string(), result);
+                generated = true;
+            }
+            std::mem::swap(&mut monkeys, &mut remain);
+        }
+        let mut generated = true;
+        while generated {
+            generated = false;
+            let mut remain = Vec::new();
+            while let Some(m) = monkeys.pop() {
+                let a1 = m.arg1();
+                let a2 = m.arg2();
+                let Some(v1) = values.get(a1) else {
+                    remain.push(m);       
+                    continue;
+                };
+                let Some(v2) = values.get(a2) else {
+                    remain.push(m);       
+                    continue;
+                };
+                let result = m.eval(*v1, *v2);
+                if m.label() == "root" {
+                    return result;
+                }
+                values.insert(m.label().to_string(), result);
+                generated = true;
+            }
+            std::mem::swap(&mut monkeys, &mut remain);
+        }
+        let mut monkeys: HashMap<String, Expr> = HashMap::new();
+        for m in self.line.iter() {
+            monkeys.insert(m.label().to_string(), m.clone());
+        }
+        let root = monkeys.get("root").unwrap();
+        let left = root.arg1();
+        let right = root.arg2();
+        dbg!(left, right);
+        let lm = monkeys.get(left).unwrap();
+        lm.build(&monkeys, &values);
+        println!();
+        let rm = monkeys.get(right).unwrap();
+        rm.build(&monkeys, &values);
+        println!();
+        let base = 3_757_272_361_000;
+        for i in 0.. {
+            let x = base + i;
+            let d = 
+ (10*(((131234706858508-((((((((924+((150+(((2*(((905+(((((((((((((2*((((3*(((((((854+(((37*((((((341+(((7*(187+((28+(((((((556+((((x-332)*36)+685)/5))*2)-737)+978)+164)/3)-16))/3)))-849)*2))+748)/7)-349)/4)+793))-61)/7))*8)-983)/7)+974)*2)-531))+181)/2)-548))+750)/2)-657)/11)+85)*28)+877)+709)/3)-430)*2)+839))/2)-60))-635)*2))/4))*2)-478)/2)-818)/3)+39)*7))/5)+553))
+- 46779208742730;
+            dbg!(d, x);
+            if d <= 0 { 
+                return x;
+            }
+        }
+    unreachable!()
     }
 }
