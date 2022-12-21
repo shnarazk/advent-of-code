1fd15f8c1e499f5ea5ed10a007363aeab8d17c76
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-21 13:02:58 +0900

    clean up

--- src/y2022/day20.rs
+++ src/y2022/day20.rs
@@ -1,13 +1,6 @@
 //! <https://adventofcode.com/2022/day/20>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc_at, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
+    crate::framework::{aoc_at, AdventOfCode, ParseError},
     std::collections::HashMap,
 };
 
@@ -76,13 +69,13 @@ impl AdventOfCode for Puzzle {
         let mut prev: Vec<usize> = (0..self.len)
             .map(|i| (i + self.len - 1) % self.len)
             .collect::<Vec<usize>>();
-        self.trace(&next);
+        // self.trace(&next);
         for s in 0..10 {
             for n in 0..self.line.len() {
                 self.debug = s == 5;
                 self.shift(&mut next, &mut prev, n);
             }
-            self.trace(&next);
+            // self.trace(&next);
         }
         self.value(&next)
     }
@@ -92,11 +85,11 @@ impl Puzzle {
     fn shift(&self, next: &mut [usize], prev: &mut [usize], i: usize) {
         let val = self.line[i];
         let mut j = i;
+        let normalized_steps = val.unsigned_abs() % (self.len - 1);
         match val.signum() {
-            0 => return,
-            1 if val.unsigned_abs() % (self.len - 1) == 0 => return,
+            _ if normalized_steps == 0 => return,
             1 => {
-                for _ in 0..(val.unsigned_abs() % (self.len - 1)) {
+                for _ in 0..normalized_steps {
                     j = next[j];
                     // To understand the following, I needed to cheak
                     if j == i {
@@ -104,9 +97,8 @@ impl Puzzle {
                     }
                 }
             }
-            -1 if val.unsigned_abs() % (self.len - 1) == 0 => return,
             -1 => {
-                for _ in 0..((val.unsigned_abs() + 0) % (self.len - 1)) {
+                for _ in 0..normalized_steps {
                     j = prev[j];
                     if j == i {
                         j = prev[j];
@@ -123,17 +115,10 @@ impl Puzzle {
         let prev_i = prev[i];
         let next_i = next[i];
         let next_j = next[j];
-        if next_j == i {
-            if self.debug {
-                let m = (self.len - 1) as isize;
-                dbg!(i, val, val % m);
-            }
-            return;
-        }
-
         assert_ne!(i, j);
         assert_ne!(i, next_i);
         assert_ne!(i, prev_i);
+        assert_ne!(i, next_j); // we are supposed to have skipped this situation.
         assert_ne!(j, next_j);
         next[prev_i] = next_i;
         next[j] = i;
@@ -143,6 +128,7 @@ impl Puzzle {
         prev[i] = j;
         prev[next_i] = prev_i;
     }
+    #[allow(dead_code)]
     fn print(&self, next: &[usize]) {
         let mut i = self.index_zero;
         while next[i] != 0 {
@@ -169,6 +155,7 @@ impl Puzzle {
         }
         self.line[i] == 0
     }
+    #[allow(dead_code)]
     fn trace(&self, next: &[usize]) {
         let mut i = self.index_zero;
         let mut count = 0;
