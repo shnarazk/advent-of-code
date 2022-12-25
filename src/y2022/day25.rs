f0452a7cc1019d789bb9bf6bd1d08327c4f59feb
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-25 14:45:37 +0900

    y2022 done!

--- src/y2022/day25.rs
+++ src/y2022/day25.rs
@@ -4,7 +4,7 @@
 #![allow(unused_variables)]
 use {
     crate::{
-        framework::{aoc, AdventOfCode, ParseError},
+        framework::{aoc_at, AdventOfCode, ParseError},
         geometric::neighbors,
         line_parser, regex,
     },
@@ -13,31 +13,71 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<isize>>,
 }
 
-#[aoc(2022, 25)]
+#[aoc_at(2022, 25)]
 impl AdventOfCode for Puzzle {
+    type Output1 = String;
+    type Output2 = usize;
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(
+            block
+                .chars()
+                .map(|c| match c {
+                    '-' => -1,
+                    '=' => -2,
+                    '0' => 0,
+                    '1' => 1,
+                    '2' => 2,
+                    '3' => 3,
+                    _ => unreachable!(),
+                })
+                .collect::<Vec<isize>>(),
+        );
         Ok(())
     }
     fn after_insert(&mut self) {
-        dbg!(&self.line);
+        dbg!(&self.line.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        let mut sum: usize = self
+            .line
+            .iter()
+            .map(|n| {
+                let v = n
+                    .iter()
+                    .rev()
+                    .enumerate()
+                    .fold(0, |acc, (i, k)| acc + k * 5_isize.pow(i as u32))
+                    as usize;
+                dbg!(v)
+            })
+            .sum();
+        dbg!(sum);
+        let mut buffer: Vec<char> = Vec::new();
+        while 0 < sum {
+            let digit = sum % 5;
+            sum /= 5;
+            let ch = match digit {
+                0 => '0',
+                1 => '1',
+                2 => '2',
+                3 => {
+                    sum += 1;
+                    '='
+                }
+                4 => {
+                    sum += 1;
+                    '-'
+                }
+                _ => unreachable!(),
+            };
+            buffer.push(ch);
+        }
+        buffer.reverse();
+        buffer.iter().collect::<String>()
     }
     fn part2(&mut self) -> Self::Output2 {
         2
