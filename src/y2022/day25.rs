a8d8dbfdc6dec20677481d5c9965b6a5b57d6133
Narazaki, Shuji <shujinarazaki@protonmail.com>
2022-12-25 14:47:16 +0900

    clean up

--- src/y2022/day25.rs
+++ src/y2022/day25.rs
@@ -1,15 +1,5 @@
 //! <https://adventofcode.com/2022/day/25>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc_at, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
-};
+use crate::framework::{aoc_at, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
@@ -19,7 +9,7 @@ pub struct Puzzle {
 #[aoc_at(2022, 25)]
 impl AdventOfCode for Puzzle {
     type Output1 = String;
-    type Output2 = usize;
+    type Output2 = ();
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         self.line.push(
@@ -79,7 +69,5 @@ impl AdventOfCode for Puzzle {
         buffer.reverse();
         buffer.iter().collect::<String>()
     }
-    fn part2(&mut self) -> Self::Output2 {
-        2
-    }
+    fn part2(&mut self) -> Self::Output2 {}
 }
