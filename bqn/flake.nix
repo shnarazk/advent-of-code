c2874ab1bafcf225ef1f4ba7d42a1bdee60008c6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-07-22 12:35:25 +0900

    direnv for bqn part1

---
+++ bqn/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.aoc.url = "github:shnarazk/advent-of-code";
+
+  outputs = { self, nixpkgs, flake-utils, aoc }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      bqn-driver = aoc.packages.${system}.default;
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [ pkgs.bashInteractive bqn-driver ];
+      };
+    });
+}
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A BQN driver for Advent of Code programs";
+  inputs.nixpkgs.url = github:NixOS/nixpkgs;
+  outputs = { self, nixpkgs }:
+  {
+    packages = builtins.listToAttrs
+      (map
+        (system:
+           with import nixpkgs { system = "${system}"; };
+           {
+             name = system;
+             value = {
+               default =
+                 stdenv.mkDerivation rec {
+                   name = "aoc-bqn-driver-${version}";
+                   pname = "aoc-bqn-driver";
+                   version = "20230722-1";
+                   src = self;
+                   # buildInputs = [];
+                   # buildPhase = "";
+                   installPhase = ''
+                       mkdir -p $out/bin;
+                       cp bqn/aoc.bqn $out/bin/2023
+                       cp bqn/aoc.bqn $out/bin/2022
+                       cp bqn/aoc.bqn $out/bin/2021
+                       cp bqn/aoc.bqn $out/bin/2016
+                   '';
+                 };
+           };
+        })
+        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ]
+      );
+  };
+}
