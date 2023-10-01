086619446621614fb20ea08fe9acc202eea18f53
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-10-01 10:40:25 +0900

    Use the uiua's flake.nix

--- uiua/flake.nix
+++ uiua/flake.nix
@@ -3,7 +3,7 @@
   inputs = { 
     nixpkgs.url     = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    uiua.url        = "github:shnarazk/flakes?dir=uiua";
+    uiua.url        = "github:uiua-lang/uiua";
   };
   outputs = { self, nixpkgs, flake-utils, uiua }:
     flake-utils.lib.eachDefaultSystem (system: let
