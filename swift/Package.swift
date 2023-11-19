8b9065cb69392a2d84e1ab76b8f4d5531beb781a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-19 16:31:27 +0900

    (swift) handle years

--- swift/Package.swift
+++ swift/Package.swift
@@ -4,16 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "aoc",
-    dependencies: [.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "aoc",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]
-        ),
-    ]
+  name: "aoc",
+  dependencies: [.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "aoc",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        "Y2022",
+      ]
+    ),
+    .target(
+      name: "Y2022",
+      dependencies: []
+    ),
+  ]
 )
---
+++ swift/Sources/aoc/aoc.swift
@@ -0,0 +1,49 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+import ArgumentParser
+import Foundation
+import Y2022
+
+@main
+struct Aoc: ParsableCommand, Decodable {
+  @Option(help: "year (4 digits)")
+  public var year: Int = 2022
+
+  @Argument(help: "day [1-25]")
+  public var day: Int = 0
+
+  @Argument(help: "part [0-3]")
+  public var part: Int = 0
+
+  @Argument(help: "surfix part of file name for tests")
+  public var test: String = ""
+
+  public var dataFile: String {
+    let d = String(format: "%02d", day)
+    if test.isEmpty {
+      return "../data/\(year)/input-day\(d).txt"
+    } else {
+      return "../data/\(year)/input-day\(d)-\(test).txt"
+    }
+  }
+
+  public func run() throws {
+    print("\u{001B}[34mAoC: \(year)-\(day), part: \(part), file: \(dataFile)\u{001B}[0m")
+    let data: String = try String(contentsOf: URL(fileURLWithPath: dataFile))
+    switch year {
+    case 2023:
+      switch day {
+      // case 1: Y2022.day01(data)
+      // case 2: Y2022.day02(data)
+      default: fatalError()
+      }
+    case 2022:
+      switch day {
+      case 1: Y2022.day01(data)
+      // case 2: Y2022.day02(data)
+      default: fatalError()
+      }
+    default: fatalError()
+    }
+  }
+}
