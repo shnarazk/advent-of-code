4de2ca7b3f77e75460532b4dbdc6d8a6c346adb7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-21 23:57:08 +0900

    (swift) revise

--- swift/Sources/y2016/Day19.swift
+++ swift/Sources/y2016/Day19.swift
@@ -9,7 +9,7 @@ public func day19(_ data: String) {
   let input = 3_014_603
   func part1() {
     var next = [input]
-    next.append(contentsOf: Array(2...input + 1))
+    next.append(contentsOf: 2...input + 1)
     next[input] = 1
     var index = 1
     while next[index] != index {
@@ -21,7 +21,7 @@ public func day19(_ data: String) {
   }
   func part2() {
     var next = [input]
-    next.append(contentsOf: Array(2...input + 1))
+    next.append(contentsOf: 2...input + 1)
     next[input] = 1
     var remain = input
     var index = input / 2
