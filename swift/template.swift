260576552b49b699b088ea0c8f6dccd2cf33dff4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-23 10:36:49 +0900

    new file:   swift/template.swift

---
+++ swift/template.swift
@@ -0,0 +1,15 @@
+public func day(_ data: String) {
+  let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
+  part1(lines)
+  part2(lines)
+}
+
+func part1(_ lines: [String.SubSequence]) {
+  var lines = lines
+  print("Part1: \(1)")
+}
+
+func part2(_ lines: [String.SubSequence]) {
+  var lines = lines
+  print("Part2: \(2)")
+}
