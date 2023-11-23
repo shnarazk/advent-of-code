8840a95315983fb6e78784d85fa4e4b7f0c7ded2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-23 22:42:06 +0900

    swift/Package.swift: set platforms

--- swift/Package.swift
+++ swift/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "aoc",
+  platforms: [.macOS(.v14)],
   dependencies: [.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
