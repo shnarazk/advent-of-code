73957e6e81ea3ee965c5c4516fd10a73f44a134c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-23 16:10:02 +0900

    (swift) y2022 day4 done.

--- swift/Sources/Y2022/day04.swift
+++ swift/Sources/Y2022/day04.swift
@@ -1,14 +1,28 @@
 public func day04(_ data: String) {
+  typealias R = (Int, Int)
   let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
-  func part1(_ lines: [String.SubSequence]) {
-    // var lines = lines
-    print("Part1: \(1)")
+  let inputs = lines.map {
+    let a = Array($0.split(separator: ",")).map {
+      let a = Array($0.split(separator: "-")).map {
+        Int($0)!
+      }
+      return (a[0], a[1])
+    }
+    return (a[0], a[1])
+  }
+  func part1(_ lines: [(R, R)]) {
+    let sum = lines.filter { l in
+      return (l.0.0 <= l.1.0 && l.1.1 <= l.0.1) || (l.1.0 <= l.0.0 && l.0.1 <= l.1.1)
+    }.count
+    print("Part1: \(sum)")
   }
 
-  func part2(_ lines: [String.SubSequence]) {
-    // var lines = lines
-    print("Part2: \(2)")
+  func part2(_ lines: [(R, R)]) {
+    let sum = lines.filter { l in
+      return (l.0.0 <= l.1.0 && l.1.0 <= l.0.1) || (l.1.0 <= l.0.0 && l.0.0 <= l.1.1)
+    }.count
+    print("Part2: \(sum)")
   }
-  part1(lines)
-  part2(lines)
+  part1(inputs)
+  part2(inputs)
 }
