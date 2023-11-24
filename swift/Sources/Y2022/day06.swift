a3910406d6548d043055772e09ccb01d28cb45c7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-24 21:06:37 +0900

    (swift) prepare for day6

---
+++ swift/Sources/Y2022/day06.swift
@@ -0,0 +1,14 @@
+public func day06(_ data: String) {
+  let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
+  func part1(_ lines: [String.SubSequence]) {
+    // var lines = lines
+    print("Part1: \(1)")
+  }
+
+  func part2(_ lines: [String.SubSequence]) {
+    // var lines = lines
+    print("Part2: \(2)")
+  }
+  part1(lines)
+  part2(lines)
+}
--- swift/Sources/aoc/aoc.swift
+++ swift/Sources/aoc/aoc.swift
@@ -45,6 +45,7 @@ struct Aoc: ParsableCommand, Decodable {
       case 3: Y2022.day03(data)
       case 4: Y2022.day04(data)
       case 5: Y2022.day05(data)
+      case 6: Y2022.day06(data)
       default: fatalError()
       }
     case 2016:
