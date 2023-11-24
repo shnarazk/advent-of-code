65cdb28fff8f7f0561392f50fd6cbe5354a5989e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-24 17:46:20 +0900

    (swift) add Swift-collections to dependency

--- swift/Package.swift
+++ swift/Package.swift
@@ -6,7 +6,13 @@ import PackageDescription
 let package = Package(
   name: "aoc",
   platforms: [.macOS(.v14)],
-  dependencies: [.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0"),
+    .package(
+      url: "https://github.com/apple/swift-collections.git",
+      .upToNextMinor(from: "1.0.0")
+    ),
+  ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
@@ -14,6 +20,7 @@ let package = Package(
       name: "aoc",
       dependencies: [
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        .product(name: "Collections", package: "swift-collections"),
         "Y2022",
         "Y2016",
       ]
