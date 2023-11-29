190449da47a04db98f0a8d953bc9533b7416da01
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-29 23:06:26 +0900

    modified:   swift/Sources/Y2022/day09.swift

--- swift/Sources/Y2022/day09.swift
+++ swift/Sources/Y2022/day09.swift
@@ -1,7 +1,25 @@
 fileprivate typealias Dim2 = (Int, Int)
 
-func inc (_ lhs: (Int, Int), _ rhs: (Int, Int)) -> (Int, Int) {
+fileprivate func inc (_ lhs: Dim2, _ rhs: Dim2) -> Dim2 {
   (lhs.0 + rhs.0, lhs.1 + rhs.1)
+}
+fileprivate func move_next(_ next: Dim2, of base: Dim2) -> Dim2 {
+  func signum (_ x: Int) -> Int {
+    if x < 0 {
+      -1
+    } else if x == 0 {
+      0
+    } else {
+      1
+    }
+  }
+  let diff_y = next.0 - base.0
+  let diff_x = next.1 - base.1
+  if 2 <= abs(diff_y) + abs(diff_x) {
+    return (next.0 + signum(diff_y), next.1 + signum(diff_x))
+  } else {
+    return next
+  }
 }
 
 private struct Move {
@@ -17,6 +35,11 @@ private struct Move {
       default: fatalError()
     }
   }
+  var vector: Dim2 {
+    get {
+      (dir.0 * len, dir.1 * len)
+    }
+  }
 }
 
 public func day09(_ data: String) {
@@ -26,9 +49,9 @@ public func day09(_ data: String) {
     var rope: [Dim2] = [(0, 0)]
     for _ in 1..<len { rope.append((0, 0)) }
     for m in moves {
-      rope[0] = inc(rope[0], m.dir)
+      rope[0] = inc(rope[0], m.vector)
       for tail in 1..<len {
-        rope[tail] = inc(rope[tail], m.dir) // FIXME: revise
+        rope[tail] = move_next(rope[tail], of: rope[tail - 1])
       }
     }
     print("Part\(part): \(rope.last!)")
