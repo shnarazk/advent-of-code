d799f21ccb72c269f5a9f7b52cfc1dc080269c56
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-30 01:20:02 +0900

    (swift) y2022 day9 done.

--- swift/Sources/Y2022/day09.swift
+++ swift/Sources/Y2022/day09.swift
@@ -1,24 +1,28 @@
-fileprivate typealias Dim2 = (Int, Int)
-
-fileprivate func inc (_ lhs: Dim2, _ rhs: Dim2) -> Dim2 {
-  (lhs.0 + rhs.0, lhs.1 + rhs.1)
-}
-fileprivate func move_next(_ next: Dim2, of base: Dim2) -> Dim2 {
-  func signum (_ x: Int) -> Int {
-    if x < 0 {
-      -1
-    } else if x == 0 {
-      0
-    } else {
-      1
-    }
+fileprivate struct Dim2: Comparable, Hashable {
+  let y: Int
+  let x: Int
+  // Implement the Comparable operators
+  static func == (lhs: Dim2, rhs: Dim2) -> Bool {
+    return lhs.y == rhs.y && lhs.x == rhs.x
   }
-  let diff_y = next.0 - base.0
-  let diff_x = next.1 - base.1
-  if 2 <= abs(diff_y) + abs(diff_x) {
-    return (next.0 + signum(diff_y), next.1 + signum(diff_x))
-  } else {
-    return next
+  static func < (lhs: Dim2, rhs: Dim2) -> Bool {
+    return lhs.y < rhs.y || (lhs.y == rhs.y && lhs.x < rhs.x)
+  }
+  func inc(_ other: Dim2) -> Dim2 { Dim2(y: y + other.y, x: x + other.x) }
+  func scale(_ s: Int) -> Dim2 { Dim2(y: y * s, x: x * s) }
+  func move(as_next base: Dim2) -> Dim2 {
+    func signum (_ x: Int) -> Int {
+      if x < 0 { -1
+      } else if x == 0 { 0
+      } else { 1 }
+    }
+    let diff_y = base.y - y
+    let diff_x = base.x - x
+    if 2 <= max(abs(diff_y), abs(diff_x)) {
+      return Dim2(y: y + signum(diff_y), x: x + signum(diff_x))
+    } else {
+      return self
+    }
   }
 }
 
@@ -28,34 +32,37 @@ private struct Move {
   init(_ s: String) {
     len = Int(s.dropFirst(2))!
     dir = switch s.first! {
-      case "U": (-1, 0)
-      case "D": (1, 0)
-      case "L": (0, -1)
-      case "R": (0, 1)
+      case "U": Dim2(y: -1, x: 0)
+      case "D": Dim2(y: 1, x: 0)
+      case "L": Dim2(y: 0, x: -1)
+      case "R": Dim2(y: 0, x: 1)
       default: fatalError()
     }
   }
   var vector: Dim2 {
-    get {
-      (dir.0 * len, dir.1 * len)
-    }
+    get { dir.scale(len) }
   }
 }
 
 public func day09(_ data: String) {
   let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
   let moves: [Move] = lines.map { return Move(String($0)) }
-  func part(_ moves: [Move], length len: Int, part: Int) {
-    var rope: [Dim2] = [(0, 0)]
-    for _ in 1..<len { rope.append((0, 0)) }
+  func solve(_ moves: [Move], length len: Int, part: Int) {
+    var rope: [Dim2] = [Dim2(y: 0, x: 0)]
+    var map: Set<Dim2> = []
+    for _ in 1..<len { rope.append(Dim2(y: 0, x: 0)) }
     for m in moves {
-      rope[0] = inc(rope[0], m.vector)
-      for tail in 1..<len {
-        rope[tail] = move_next(rope[tail], of: rope[tail - 1])
+      let vec = m.dir
+      for _ in 0..<m.len {
+        rope[0] = rope[0].inc(vec)
+        for tail in 1..<len {
+          rope[tail] = rope[tail].move(as_next: rope[tail - 1])
+        }
+        map.insert(rope[len - 1])
       }
     }
-    print("Part\(part): \(rope.last!)")
+    print("Part\(part): \(map.count)")
   }
-  part(moves, length: 2, part: 1)
-  part(moves, length: 10, part: 2)
+  solve(moves, length: 2, part: 1)
+  solve(moves, length: 10, part: 2)
 }
