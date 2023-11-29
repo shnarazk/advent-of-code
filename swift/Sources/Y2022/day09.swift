65df2ece3c281b197bed2e9a57af51e70c1a94d0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-11-29 22:17:06 +0900

    modified:   swift/Sources/Y2022/day09.swift

--- swift/Sources/Y2022/day09.swift
+++ swift/Sources/Y2022/day09.swift
@@ -1,5 +1,11 @@
+fileprivate typealias Dim2 = (Int, Int)
+
+func inc (_ lhs: (Int, Int), _ rhs: (Int, Int)) -> (Int, Int) {
+  (lhs.0 + rhs.0, lhs.1 + rhs.1)
+}
+
 private struct Move {
-  let dir: (Int, Int)
+  let dir: Dim2
   let len: Int
   init(_ s: String) {
     len = Int(s.dropFirst(2))!
@@ -16,12 +22,17 @@ private struct Move {
 public func day09(_ data: String) {
   let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
   let moves: [Move] = lines.map { return Move(String($0)) }
-  func part1(_ moves: [Move]) {
-    print("Part1: \(moves.count)")
+  func part(_ moves: [Move], length len: Int, part: Int) {
+    var rope: [Dim2] = [(0, 0)]
+    for _ in 1..<len { rope.append((0, 0)) }
+    for m in moves {
+      rope[0] = inc(rope[0], m.dir)
+      for tail in 1..<len {
+        rope[tail] = inc(rope[tail], m.dir) // FIXME: revise
+      }
+    }
+    print("Part\(part): \(rope.last!)")
   }
-  func part2(_ moves: [Move]) {
-    print("Part2: \(moves.count)")
-  }
-  part1(moves)
-  part2(moves)
+  part(moves, length: 2, part: 1)
+  part(moves, length: 10, part: 2)
 }
