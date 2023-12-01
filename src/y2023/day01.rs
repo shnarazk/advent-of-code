74e0d1d8d85a1f742e95f7e3dad3e03d09d7e011
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-02 02:02:54 +0900

    (y2023/day01.rs) direct calculation

--- src/y2023/day01.rs
+++ src/y2023/day01.rs
@@ -3,11 +3,11 @@ use crate::framework::{aoc, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<Vec<u8>>,
-    line2: Vec<Vec<u8>>,
+    sum1: usize,
+    sum2: usize,
 }
 
-static SUBST: once_cell::sync::OnceCell<Vec<(Vec<u8>, u8)>> = once_cell::sync::OnceCell::new();
+static SUBST: once_cell::sync::OnceCell<Vec<Vec<u8>>> = once_cell::sync::OnceCell::new();
 
 #[aoc(2023, 1)]
 impl AdventOfCode for Puzzle {
@@ -15,46 +15,66 @@ impl AdventOfCode for Puzzle {
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
         let subst = SUBST.get_or_init(|| {
             vec![
-                (b"one".to_vec(), b'1'),
-                (b"two".to_vec(), b'2'),
-                (b"three".to_vec(), b'3'),
-                (b"four".to_vec(), b'4'),
-                (b"five".to_vec(), b'5'),
-                (b"six".to_vec(), b'6'),
-                (b"seven".to_vec(), b'7'),
-                (b"eight".to_vec(), b'8'),
-                (b"nine".to_vec(), b'9'),
+                b"one".to_vec(),
+                b"two".to_vec(),
+                b"three".to_vec(),
+                b"four".to_vec(),
+                b"five".to_vec(),
+                b"six".to_vec(),
+                b"seven".to_vec(),
+                b"eight".to_vec(),
+                b"nine".to_vec(),
             ]
         });
-        let mut b = block.bytes().collect::<Vec<_>>();
-        self.line.push(b.clone());
-        (0..b.len()).for_each(|i| {
-            for (r, s) in subst {
-                if b[i..].starts_with(&r) {
-                    b[i] = *s;
-                    break;
+        let b = block.bytes().collect::<Vec<_>>();
+        let mut not_found = true;
+        for i in 0..b.len() {
+            let x = b[i] - ('0' as u8);
+            if 1 <= x && x <= 9 {
+                let n = 10 * (x as usize);
+                if not_found {
+                    self.sum2 += n;
+                }
+                self.sum1 += n;
+                break;
+            }
+            if not_found {
+                for (j, r) in subst.iter().enumerate() {
+                    if b[i..].starts_with(&r) {
+                        self.sum2 += 10 * (j + 1);
+                        not_found = false;
+                        break;
+                    }
                 }
             }
-        });
-        self.line2.push(b);
+        }
+        not_found = true;
+        for i in (0..b.len()).rev() {
+            let x = b[i] - ('0' as u8);
+            if 1 <= x && x <= 9 {
+                let n = x as usize;
+                if not_found {
+                    self.sum2 += n;
+                }
+                self.sum1 += n;
+                break;
+            }
+            if not_found {
+                for (j, r) in subst.iter().enumerate() {
+                    if b[i..].starts_with(&r) {
+                        self.sum2 += j + 1;
+                        not_found = false;
+                        break;
+                    }
+                }
+            }
+        }
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
-        sum(&self.line)
+        self.sum1
     }
     fn part2(&mut self) -> Self::Output2 {
-        sum(&self.line2)
+        self.sum2
     }
-}
-
-fn sum(l: &[Vec<u8>]) -> usize {
-    l.iter()
-        .map(|v| {
-            let d = v
-                .iter()
-                .filter(|c| (**c as char).is_digit(10))
-                .collect::<Vec<_>>();
-            10 * (*d[0] - ('0' as u8)) as usize + (*d[d.len() - 1] - ('0' as u8)) as usize
-        })
-        .sum()
 }
