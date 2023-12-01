d2633beec09cd655d3f2a451198760a696d1a59e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-01 15:03:25 +0900

    y2023 day01 done.

--- src/y2023/day01.rs
+++ src/y2023/day01.rs
@@ -14,37 +14,73 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<char>>,
+    line2: Vec<Vec<char>>,
 }
 
 #[aoc(2023, 1)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(block.chars().collect::<Vec<_>>());
+        let mut b = block.to_owned();
+        let subst = [
+            ("one", '1'),
+            ("two", '2'),
+            ("three", '3'),
+            ("four", '4'),
+            ("five", '5'),
+            ("six", '6'),
+            ("seven", '7'),
+            ("eight", '8'),
+            ("nine", '9'),
+        ];
+        let mut acc: Vec<char> = Vec::new();
+        'next_char: while !b.is_empty() {
+            for (r, s) in subst {
+                if b.starts_with(r) {
+                    acc.push(s);
+                    // b = b.strip_prefix(r).unwrap().to_string();
+                    // Wow! letters can be overwapped!
+                    let mut bs = b.chars();
+                    let c = bs.next().unwrap();
+                    b = bs.collect::<String>();
+                    continue 'next_char;
+                }
+            }
+            let mut bs = b.chars();
+            let c = bs.next().unwrap();
+            acc.push(c);
+            b = bs.collect::<String>();
+        }
+        self.line2.push(acc);
         Ok(())
     }
-    fn wrap_up(&mut self) {
-        for i in 0..100 {
-            progress!(i);
-            std::thread::sleep(std::time::Duration::from_millis(20));
-        }
-        dbg!(&self.line);
-    }
+    fn wrap_up(&mut self) {}
     fn part1(&mut self) -> Self::Output1 {
-        1
+        self.line
+            .iter()
+            .map(|v| {
+                let d = v.iter().filter(|c| c.is_digit(10)).collect::<Vec<_>>();
+                vec![d[0], d[d.len() - 1]]
+                    .into_iter()
+                    .collect::<String>()
+                    .parse::<usize>()
+                    .unwrap()
+            })
+            .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        self.line2
+            .iter()
+            .map(|v| {
+                let d = v.iter().filter(|c| c.is_digit(10)).collect::<Vec<_>>();
+                vec![d[0], d[d.len() - 1]]
+                    .into_iter()
+                    .collect::<String>()
+                    .parse::<usize>()
+                    .unwrap()
+            })
+            .sum()
     }
 }
