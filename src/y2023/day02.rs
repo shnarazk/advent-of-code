de0c6ba1b4bd9d7d03ff30a3fdeb9c3e34d40493
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-02 14:37:43 +0900

    y2023 day2 done.

--- src/y2023/day02.rs
+++ src/y2023/day02.rs
@@ -1,45 +1,57 @@
 //! <https://adventofcode.com/2023/day/2>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    regex,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    index: usize,
+    result1: usize,
+    result2: usize,
 }
 
 #[aoc(2023, 2)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
+        self.index += 1;
+        let parser = regex!(r"Game \d+: (.*)$");
+        let parser2 = regex!(r"(\d+) (red|green|blue)");
         let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        let x = segment[1]
+            .split(';')
+            .map(|set| {
+                let s = set
+                    .split(", ")
+                    .map(|b| {
+                        let c = parser2.captures(b).unwrap();
+                        match &c[2] {
+                            "red" => (c[1].to_owned().parse::<usize>().unwrap(), 0, 0),
+                            "green" => (0, c[1].to_owned().parse::<usize>().unwrap(), 0),
+                            "blue" => (0, 0, c[1].to_owned().parse::<usize>().unwrap()),
+                            _ => panic!("cant"),
+                        }
+                    })
+                    .collect::<Vec<_>>();
+                s.iter().fold((0, 0, 0), |acc, val| {
+                    (acc.0 + val.0, acc.1 + val.1, acc.2 + val.2)
+                })
+            })
+            .collect::<Vec<_>>();
+        let maxs = x.iter().fold((0, 0, 0), |acc, val| {
+            (acc.0.max(val.0), acc.1.max(val.1), acc.2.max(val.2))
+        });
+        if maxs.0 <= 12 && maxs.1 <= 13 && maxs.2 <= 14 {
+            self.result1 += self.index;
+        }
+        self.result2 += maxs.0 * maxs.1 * maxs.2;
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        self.result1
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        self.result2
     }
 }
