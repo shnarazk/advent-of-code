07926cec2fd343a3c013705b6d6c13c53ef8ae1c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-03 14:58:54 +0900

    y2023 day3 done.

--- src/y2023/day03.rs
+++ src/y2023/day03.rs
@@ -1,45 +1,77 @@
 //! <https://adventofcode.com/2023/day/3>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     std::collections::HashMap,
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Default)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: usize,
+    number: HashMap<(usize, usize, usize), usize>,
+    symbol: Vec<(usize, usize, char)>,
 }
 
 #[aoc(2023, 3)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        let cs = block.chars().collect::<Vec<_>>();
+        let mut acc: Option<usize> = None;
+        let mut pos_start = 0;
+        for (j, c) in cs.iter().enumerate() {
+            if c.is_digit(10) {
+                let n = (*c as u8 - b'0') as usize;
+                if let Some(a) = acc {
+                    acc = Some(a * 10 + n);
+                } else {
+                    acc = Some(n);
+                    pos_start = j;
+                }
+            } else {
+                if *c != '.' {
+                    self.symbol.push((self.line, j, *c));
+                }
+                if let Some(n) = acc {
+                    // dbg!((self.line, pos_start, j), n);
+                    self.number.insert((self.line, pos_start, j - 1), n);
+                    acc = None;
+                }
+            }
+        }
+        if let Some(n) = acc {
+            self.number.insert((self.line, pos_start, cs.len()), n);
+        }
+        self.line += 1;
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        let mut sum = 0;
+        for ((y, x1, x2), val) in self.number.iter() {
+            if self
+                .symbol
+                .iter()
+                .any(|(sy, sx, _)| y.abs_diff(*sy) <= 1 && *x1 <= *sx + 1 && *sx <= *x2 + 1)
+            {
+                sum += val;
+            }
+        }
+        sum
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        let mut sum = 0;
+        for (y, x, _) in self.symbol.iter().filter(|d| d.2 == '*') {
+            let mut count = 0;
+            let mut s = 1;
+            for ((ny, x1, x2), val) in self.number.iter() {
+                if y.abs_diff(*ny) <= 1 && *x1 <= *x + 1 && *x <= *x2 + 1 {
+                    count += 1;
+                    s *= val;
+                }
+            }
+            if count == 2 {
+                sum += s;
+            }
+        }
+        sum
     }
 }
