21bc80df01384fdfc850110e7823e7b770b74c4a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-05 18:43:58 +0900

    y2023 day5 done.

--- src/y2023/day05.rs
+++ src/y2023/day05.rs
@@ -1,16 +1,12 @@
 //! <https://adventofcode.com/2023/day/5>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    line_parser,
 };
+
+// Range(1,3) means an integer set that contains {1, 2} not containing 3.
+type Range = (usize, usize);
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
@@ -33,30 +29,89 @@ impl AdventOfCode for Puzzle {
                 continue;
             }
             let t = line_parser::to_usizes(line, ' ')?;
-            v.push((t[0], t[1], t[2]));
+            v.push((t[0], t[1], t[1] + t[2]));
         }
         self.line.push(v);
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
         let mut locs = self.seeds.clone();
-        let map = |pos: usize, dest: usize, source: usize, range: usize| {
-            (source <= pos && pos < source + range).then(|| dest + pos - source)
-        };
-        for (i, trans) in self.line.iter().enumerate() {
+        for trans in self.line.iter() {
             for p in locs.iter_mut() {
-                for (d, s, r) in trans.iter() {
-                    if let Some(d) = map(*p, *d, *s, *r) {
+                for (d, s, t) in trans.iter() {
+                    let map = |pos: usize| (*s <= pos && pos < *t).then(|| *d + pos - *s);
+                    if let Some(d) = map(*p) {
                         *p = d;
                         break;
                     }
                 }
             }
-            dbg!(&locs);
         }
-        dbg!(*locs.iter().min().unwrap())
+        *locs.iter().min().unwrap()
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        let mut ranges: Vec<Range> = self
+            .seeds
+            .windows(2)
+            .enumerate()
+            .filter(|i| i.0 % 2 == 0)
+            .map(|(_, v)| (v[0], v[0] + v[1])) // store as half-close range
+            .collect::<Vec<Range>>();
+        for trans in self.line.iter() {
+            let mut handled: Vec<Range> = Vec::new();
+            for (d, s, t) in trans.iter() {
+                let mapb = |pos: usize| (*s <= pos && pos < *t).then(|| *d + pos - *s);
+                let mape = |pos: usize| (*s <= pos && pos <= *t).then(|| *d + pos - *s);
+                let mut unhandled: Vec<Range> = Vec::new();
+                for r in ranges.iter() {
+                    if (r.0 < *s) && (r.1 < *s) {
+                        unhandled.push(*r);
+                        continue;
+                    }
+                    if (r.0 < *s) && (r.1 <= *t) {
+                        // divide two segments
+                        let div: usize = *s;
+                        let r1: Range = (r.0, div);
+                        let r2: Range = (mapb(div).unwrap(), mape(r.1).unwrap());
+                        unhandled.push(r1);
+                        handled.push(r2);
+                        continue;
+                    }
+                    if (r.0 < *s) && (*t < r.1) {
+                        // divide three segments
+                        let div1: usize = *s;
+                        let div2: usize = *t;
+                        let r1: Range = (r.0, div1);
+                        let r2: Range = (mapb(div1).unwrap(), mape(div2).unwrap());
+                        let r3: Range = (div2, r.1);
+                        unhandled.push(r1);
+                        handled.push(r2);
+                        unhandled.push(r3);
+                        continue;
+                    }
+                    assert!(*s <= r.0);
+                    if (r.0 <= *t) && (r.1 <= *t) {
+                        // shifted the entire range
+                        let r0 = (mapb(r.0).unwrap(), mape(r.1).unwrap());
+                        handled.push(r0);
+                        continue;
+                    }
+                    if (r.0 <= *t) && (*t < r.1) {
+                        // divide two segments
+                        let div: usize = *t;
+                        let r1: Range = (mapb(r.0).unwrap(), mape(div).unwrap());
+                        let r2: Range = (div, r.1);
+                        handled.push(r1);
+                        unhandled.push(r2);
+                        continue;
+                    }
+                    assert!(*t < r.0);
+                    unhandled.push(*r);
+                }
+                ranges = unhandled;
+            }
+            ranges.append(&mut handled);
+        }
+        ranges.iter().map(|(b, _)| *b).min().unwrap()
     }
 }
