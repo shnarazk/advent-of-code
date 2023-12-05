491e09f87d4403c109381e28ba4b8100c00564cb
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-05 14:43:51 +0900

    y2023 day5 part1 done

--- src/y2023/day05.rs
+++ src/y2023/day05.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
@@ -13,31 +14,47 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    seeds: Vec<usize>,
+    line: Vec<Vec<(usize, usize, usize)>>,
 }
 
 #[aoc(2023, 5)]
 impl AdventOfCode for Puzzle {
-    const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
+    const DELIMITER: &'static str = "\n\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        let mut v = Vec::new();
+        for (i, line) in block.trim().split('\n').enumerate() {
+            if line.starts_with("seeds:") {
+                let vals = line.split(": ").nth(1).unwrap().trim();
+                self.seeds = line_parser::to_usizes(vals, ' ')?;
+                continue;
+            }
+            if i == 0 {
+                continue;
+            }
+            let t = line_parser::to_usizes(line, ' ')?;
+            v.push((t[0], t[1], t[2]));
+        }
+        self.line.push(v);
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        let mut locs = self.seeds.clone();
+        let map = |pos: usize, dest: usize, source: usize, range: usize| {
+            (source <= pos && pos < source + range).then(|| dest + pos - source)
+        };
+        for (i, trans) in self.line.iter().enumerate() {
+            for p in locs.iter_mut() {
+                for (d, s, r) in trans.iter() {
+                    if let Some(d) = map(*p, *d, *s, *r) {
+                        *p = d;
+                        break;
+                    }
+                }
+            }
+            dbg!(&locs);
+        }
+        dbg!(*locs.iter().min().unwrap())
     }
     fn part2(&mut self) -> Self::Output2 {
         2
