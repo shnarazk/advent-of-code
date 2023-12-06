7bff2c445260cc3798ca6086256b583518d9b47b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-06 14:49:18 +0900

    y2023 day6 done.

--- src/y2023/day06.rs
+++ src/y2023/day06.rs
@@ -1,52 +1,50 @@
 //! <https://adventofcode.com/2023/day/6>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    line_parser,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    index: usize,
+    line: Vec<Vec<usize>>,
+    setting1: Vec<(usize, usize)>,
+    setting2: (usize, usize),
 }
-
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
 
 #[aoc(2023, 6)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        let s = block.split(':').nth(1).unwrap().trim();
+        self.line.push(line_parser::to_usizes(s, ' ').unwrap());
         Ok(())
     }
     fn end_of_data(&mut self) {
-        dbg!(&self.line);
+        for (i, t) in self.line[0].iter().enumerate() {
+            self.setting1.push((*t, self.line[1][i]));
+        }
+        let s2 = self
+            .line
+            .iter()
+            .map(|vec| {
+                let mut s = 0;
+                for x in vec.iter() {
+                    s = s * 10_usize.pow(x.ilog(10) + 1) + *x;
+                }
+                s
+            })
+            .collect::<Vec<_>>();
+        self.setting2 = (s2[0], s2[1]);
     }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        self.setting1
+            .iter()
+            .map(|(t, d)| (1..*t - 1).filter(|x| *x * (*t - *x) > *d).count())
+            .product::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        let (t, d) = self.setting2;
+        (1..t - 1).filter(|x| *x * (t - *x) > d).count()
     }
 }
