c5d92d9d05631c15537da3e5bdea1b6fa19023e6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-09 14:20:41 +0900

    y2034 day9 done.

--- src/y2023/day09.rs
+++ src/y2023/day09.rs
@@ -1,52 +1,43 @@
 //! <https://adventofcode.com/2023/day/9>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
+use crate::{
+    framework::{aoc, AdventOfCode, ParseError},
+    line_parser,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<isize>>,
 }
-
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
 
 #[aoc(2023, 9)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(line_parser::to_isizes(block, ' ').unwrap());
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        self.line.iter().map(extrapolate).sum::<isize>() as usize
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        self.line.iter().map(extrapolate_backword).sum::<isize>() as usize
     }
+}
+
+fn extrapolate(vec: &Vec<isize>) -> isize {
+    if vec.iter().all(|n| *n == 0) {
+        return 0;
+    }
+    let last = vec.last().unwrap();
+    let v = vec.windows(2).map(|p| p[1] - p[0]).collect::<Vec<isize>>();
+    *last + extrapolate(&v)
+}
+
+fn extrapolate_backword(vec: &Vec<isize>) -> isize {
+    if vec.iter().all(|n| *n == 0) {
+        return 0;
+    }
+    let base = vec.first().unwrap();
+    let v = vec.windows(2).map(|p| p[1] - p[0]).collect::<Vec<isize>>();
+    base - extrapolate_backword(&v)
 }
