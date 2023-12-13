f3bef6fc3dc0d7b15e7c755edef242ab47088cc4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-13 17:04:13 +0900

    y2023 day13 part1 done.

--- src/y2023/day13.rs
+++ src/y2023/day13.rs
@@ -1,52 +1,66 @@
 //! <https://adventofcode.com/2023/day/13>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
-        line_parser, regex,
-    },
-    std::collections::HashMap,
-};
+// #![allow(dead_code)]
+// #![allow(unused_imports)]
+// #![allow(unused_variables)]
+use crate::framework::{aoc, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<Vec<bool>>>,
 }
-
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
 
 #[aoc(2023, 13)]
 impl AdventOfCode for Puzzle {
-    const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
+    const DELIMITER: &'static str = "\n\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(
+            block
+                .trim()
+                .split('\n')
+                .map(|l| l.chars().map(|c| c == '#').collect::<Vec<bool>>())
+                .collect::<Vec<_>>(),
+        );
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        self.line.iter().map(evaluate).sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
         2
     }
+}
+
+fn evaluate(m: &Vec<Vec<bool>>) -> usize {
+    let width = m[0].len();
+    let height = m.len();
+    // check horizontal mirror
+    'next_line: for y in 0..height - 1 {
+        if m[y] != m[y + 1] {
+            continue 'next_line;
+        }
+        let mir = y;
+        for offset in 1..(mir + 1).min(height - y - 1) {
+            if m[mir - offset] != m[mir + 1 + offset] {
+                continue 'next_line;
+            }
+        }
+        return 100 * (mir + 1);
+    }
+    'next_column: for x in 0..width - 1 {
+        for y in 0..height {
+            if m[y][x] != m[y][x + 1] {
+                continue 'next_column;
+            }
+        }
+        let mir = x;
+        for offset in 1..(mir + 1).min(width - x - 1) {
+            for y in 0..height {
+                if m[y][mir - offset] != m[y][mir + 1 + offset] {
+                    continue 'next_column;
+                }
+            }
+        }
+        return mir + 1;
+    }
+    unreachable!()
 }
