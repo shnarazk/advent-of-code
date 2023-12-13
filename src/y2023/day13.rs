c6447e87d41c4343d71a34784be329a71a3bd5f5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-13 18:07:45 +0900

    y2023 day13 done.

--- src/y2023/day13.rs
+++ src/y2023/day13.rs
@@ -1,7 +1,4 @@
 //! <https://adventofcode.com/2023/day/13>
-// #![allow(dead_code)]
-// #![allow(unused_imports)]
-// #![allow(unused_variables)]
 use crate::framework::{aoc, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -26,8 +23,55 @@ impl AdventOfCode for Puzzle {
         self.line.iter().map(evaluate).sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        self.line.iter_mut().map(cleanup).sum::<usize>()
     }
+}
+
+fn cleanup(m: &mut Vec<Vec<bool>>) -> usize {
+    let width = m[0].len();
+    let height = m.len();
+    // let mut smudge = (0, 0);
+    for mir in 0..width - 1 {
+        let ed = (0_..(mir + 1).min(width - mir - 1))
+            .map(|offset| {
+                (0..height)
+                    .map(|y| {
+                        if m[y][mir - offset] != m[y][mir + 1 + offset] {
+                            // smudge = (y, mir - offset);
+                            1
+                        } else {
+                            0
+                        }
+                    })
+                    .sum::<usize>()
+            })
+            .sum::<usize>();
+        if ed == 1 {
+            // m[smudge.0][smudge.1] = !m[smudge.0][smudge.1];
+            return mir + 1;
+        }
+    }
+    for mir in 0..height - 1 {
+        let ed = (0_..(mir + 1).min(height - mir - 1))
+            .map(|offset| {
+                (0..width)
+                    .map(|x| {
+                        if m[mir - offset][x] != m[mir + 1 + offset][x] {
+                            // smudge = (mir - offset, x);
+                            1
+                        } else {
+                            0
+                        }
+                    })
+                    .sum::<usize>()
+            })
+            .sum::<usize>();
+        if ed == 1 {
+            // m[smudge.0][smudge.1] = !m[smudge.0][smudge.1];
+            return 100 * (mir + 1);
+        }
+    }
+    unreachable!()
 }
 
 fn evaluate(m: &Vec<Vec<bool>>) -> usize {
@@ -39,7 +83,7 @@ fn evaluate(m: &Vec<Vec<bool>>) -> usize {
             continue 'next_line;
         }
         let mir = y;
-        for offset in 1..(mir + 1).min(height - y - 1) {
+        for offset in 1..(mir + 1).min(height - mir - 1) {
             if m[mir - offset] != m[mir + 1 + offset] {
                 continue 'next_line;
             }
@@ -53,7 +97,7 @@ fn evaluate(m: &Vec<Vec<bool>>) -> usize {
             }
         }
         let mir = x;
-        for offset in 1..(mir + 1).min(width - x - 1) {
+        for offset in 1..(mir + 1).min(width - mir - 1) {
             for y in 0..height {
                 if m[y][mir - offset] != m[y][mir + 1 + offset] {
                     continue 'next_column;
