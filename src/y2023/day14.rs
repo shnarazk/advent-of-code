e569aac3ec5ae063bef4048125375791e917c4d4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-14 15:30:13 +0900

    y2023 day14 part1 done

--- src/y2023/day14.rs
+++ src/y2023/day14.rs
@@ -13,40 +13,133 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<usize>>,
 }
-
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
 
 #[aoc(2023, 14)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(
+            block
+                .chars()
+                .map(|c| match c {
+                    'O' => 1,
+                    '#' => 2,
+                    _ => 0,
+                })
+                .collect::<Vec<_>>(),
+        );
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        dbg!(self.line.len(), self.line[0].len());
+        let depth = self.line.len();
+        let m = transpose(&self.line);
+        m.iter()
+            .map(|line| {
+                let mut res = Vec::new();
+                let mut base = 0;
+                for (i, k) in line.iter().enumerate() {
+                    match *k {
+                        1 => {
+                            res.push(base);
+                            base += 1;
+                        }
+                        2 => base = i + 1,
+                        _ => (),
+                    }
+                }
+                res.iter().map(|val| depth - val).sum::<usize>()
+            })
+            .sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
+        // print(&add_force(self.line.clone()));
+        print(&cycle(cycle(self.line.clone())));
         2
     }
+}
+
+fn transpose(v: &Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let h = v.len();
+    let w = v[0].len();
+    (0..w)
+        .map(|x| (0..h).map(|y| v[y][x]).collect::<Vec<_>>())
+        .collect::<Vec<_>>()
+}
+
+fn add_force(mat: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let m = transpose(&mat);
+    let depth = m[0].len();
+    let map = m
+        .iter()
+        .map(|line| {
+            let mut res = Vec::new();
+            let mut base = 0;
+            for (i, k) in line.iter().enumerate() {
+                match *k {
+                    1 => {
+                        res.push(base);
+                        base += 1;
+                    }
+                    2 => base = i + 1,
+                    _ => (),
+                }
+            }
+            res
+        })
+        .collect::<Vec<_>>();
+    let mut n = mat.clone();
+    for l in n.iter_mut() {
+        for p in l.iter_mut() {
+            if *p == 1 {
+                *p = 0;
+            }
+        }
+    }
+    for (x, l) in map.iter().enumerate() {
+        for y in l.iter() {
+            n[*y][x] = 1;
+        }
+    }
+    n
+}
+
+fn rotate_clockwise(m: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    let h = m.len();
+    let w = m[0].len();
+    let mut n = m.clone();
+    for (y, l) in n.iter_mut().enumerate() {
+        for (x, p) in l.iter_mut().enumerate() {
+            *p = m[h - x - 1][y];
+        }
+    }
+    n
+}
+
+fn print(m: &Vec<Vec<usize>>) {
+    for l in m.iter() {
+        for p in l.iter() {
+            print!(
+                "{}",
+                match p {
+                    1 => 'O',
+                    2 => '#',
+                    _ => '.',
+                }
+            );
+        }
+        println!();
+    }
+}
+fn cycle(m: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    rotate_clockwise(add_force(rotate_clockwise(add_force(rotate_clockwise(
+        add_force(rotate_clockwise(add_force(m))),
+    )))))
+    // rotate_clockwise(rotate_clockwise(rotate_clockwise(rotate_clockwise(m))))
+    // rotate_clockwise(rotate_clockwise(rotate_clockwise(rotate_clockwise(m))))
+    // print(&m);
+    // println!();
+    // rotate_clockwise(m)
 }
