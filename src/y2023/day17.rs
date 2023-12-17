3a343506d5ca40e7ff1f5f6dd35cc5a9aac8cab9
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-17 17:55:48 +0900

    y2023 day17 part1 done

--- src/y2023/day17.rs
+++ src/y2023/day17.rs
@@ -2,49 +2,142 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
 #![allow(unused_variables)]
+
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
+        geometric::{neighbors, Dim2, Direction, GeometricAddition, GeometricRotation},
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::{
+        cmp::Reverse,
+        collections::{BinaryHeap, HashMap},
+    },
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<usize>>,
 }
 
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub struct Crucible {
+    estimate: usize,
+    cost: usize,
+    pos: Dim2<usize>,
+    dir: Direction,
+    dist: usize,
+    domain: Dim2<usize>,
+}
 
+impl Crucible {
+    fn possible_dirs(&self) -> Vec<Crucible> {
+        let mut res: Vec<Crucible> = Vec::new();
+        // go straight
+        if self.dist < 2 {
+            if let Some(p) = <Dim2<usize> as GeometricAddition<usize>>::move_to(
+                self.pos,
+                self.dir.as_vec2(),
+                self.domain.0,
+                self.domain.1,
+            ) {
+                let mut c = self.clone();
+                c.pos = p;
+                c.dist += 1;
+                res.push(c);
+            }
+        }
+        // turn right
+        let dir = GeometricRotation::turn_right(&self.dir);
+        if let Some(p) = <Dim2<usize> as GeometricAddition<usize>>::move_to(
+            self.pos,
+            dir.as_vec2(),
+            self.domain.0,
+            self.domain.1,
+        ) {
+            let mut c = self.clone();
+            c.pos = p;
+            c.dir = dir;
+            c.dist = 0;
+            res.push(c);
+        }
+        // turn left
+        let dir = GeometricRotation::turn_left(&self.dir);
+        if let Some(p) = <Dim2<usize> as GeometricAddition<usize>>::move_to(
+            self.pos,
+            dir.as_vec2(),
+            self.domain.0,
+            self.domain.1,
+        ) {
+            let mut c = self.clone();
+            c.pos = p;
+            c.dir = dir;
+            c.dist = 0;
+            res.push(c);
+        }
+        res
+    }
+}
 #[aoc(2023, 17)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(
+            block
+                .trim()
+                .bytes()
+                .map(|c| (c as u8 - b'0') as usize)
+                .collect::<Vec<_>>(),
+        );
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
+    // fn end_of_data(&mut self) { dbg!(&self.line); }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let goal = (height - 1, width - 1);
+        let mut to_visit: BinaryHeap<Reverse<Crucible>> = BinaryHeap::new();
+        // mapping (pos, from) => (cost, range, route)
+        let mut visited: HashMap<(Dim2<usize>, Direction), (usize, usize)> = HashMap::new();
+        let mut cost = self
+            .line
+            .iter()
+            .map(|v| v.iter().sum::<usize>())
+            .sum::<usize>();
+        let start = Crucible {
+            estimate: 9 * height + width,
+            cost: 0,
+            pos: (0, 0),
+            dir: Direction::EAST,
+            dist: 0,
+            domain: (height, width),
+        };
+        to_visit.push(Reverse(start));
+        while let Some(Reverse(c)) = to_visit.pop() {
+            if c.dist == 0 {
+                if let Some(d) = visited.get(&(c.pos, c.dir)) {
+                    if d.0 <= c.cost && d.1 <= c.dist {
+                        continue;
+                    }
+                }
+            }
+            if c.pos == goal || cost < c.cost {
+                if c.cost < cost {
+                    cost = dbg!(c.cost);
+                }
+                continue;
+            }
+            if c.dist == 0 {
+                visited.insert((c.pos, c.dir), (c.cost, c.dist));
+            }
+            let mut cands = c.possible_dirs();
+            while let Some(mut cand) = cands.pop() {
+                cand.cost += self.line[cand.pos.0][cand.pos.1];
+                cand.estimate = goal.0 - cand.pos.0 + goal.1 - cand.pos.1;
+                to_visit.push(Reverse(cand));
+            }
+        }
+        cost
     }
     fn part2(&mut self) -> Self::Output2 {
         2
