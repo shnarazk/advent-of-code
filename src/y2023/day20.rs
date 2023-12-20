39adfe40fa31bdf62cf0195bb03b2b327ac51c3a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-20 17:59:05 +0900

    y2023 day20 part1 done

--- src/y2023/day20.rs
+++ src/y2023/day20.rs
@@ -8,45 +8,141 @@ use {
         geometric::neighbors,
         line_parser, regex,
     },
-    std::collections::HashMap,
+    itertools::Itertools,
+    std::collections::{HashMap, HashSet, VecDeque},
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
-    line: Vec<()>,
+    modules: HashMap<String, Module>,
+    pulses: VecDeque<(String, String, bool)>,
+    pulse_counts: (usize, usize),
 }
 
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
+#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+enum ModuleType {
+    #[default]
+    Broadcast,
+    FlipFlop,
+    Conjunction,
+}
+#[derive(Debug, Default, Eq, PartialEq)]
+struct Module {
+    module_type: ModuleType,
+    bool_state: bool,
+    hash: HashMap<String, bool>,
+    dests: Vec<String>,
+}
+
+impl<'a> Module {
+    fn receive_pulse(&'a mut self, from: &'a String, high_pulse: bool) -> Option<bool> {
+        match self.module_type {
+            ModuleType::Broadcast => Some(high_pulse),
+            ModuleType::FlipFlop => (!high_pulse).then(|| {
+                self.bool_state = !self.bool_state;
+                self.bool_state
+            }),
+            ModuleType::Conjunction => {
+                self.hash.insert(from.to_string(), high_pulse);
+                Some(!self.hash.values().all(|b| *b))
+            }
+        }
+    }
+}
 
 #[aoc(2023, 20)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        let segment = block.split(" -> ").collect::<Vec<_>>();
+        let mut label = segment[0].chars();
+        let (module_type, module_name) = match label.next().unwrap() {
+            '%' => (ModuleType::FlipFlop, label.collect::<String>()),
+            '&' => (ModuleType::Conjunction, label.collect::<String>()),
+            'b' if segment[0] == "broadcaster" => (ModuleType::Broadcast, segment[0].to_string()),
+            _ => unreachable!(),
+        };
+        let dests = segment[1]
+            .split(", ")
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>();
+        let module = Module {
+            module_type,
+            dests,
+            ..Module::default()
+        };
+        self.modules.insert(module_name, module);
         Ok(())
     }
     fn end_of_data(&mut self) {
-        dbg!(&self.line);
+        // build hashes
+        let mut map: HashMap<String, Vec<String>> = HashMap::new();
+        for (from, m) in self.modules.iter() {
+            for to in m.dests.iter() {
+                let entry = map.entry(to.clone()).or_insert(Vec::new());
+                entry.push(from.to_string());
+            }
+        }
+        for (name, m) in self.modules.iter_mut() {
+            if let Some(inputs) = map.get(name) {
+                m.hash = inputs
+                    .iter()
+                    .map(|n| (n.to_string(), false))
+                    .collect::<HashMap<String, bool>>();
+            }
+        }
+
+        // dbg!(&self.modules);
     }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        self.start();
+        dbg!(self.pulse_counts);
+        self.pulse_counts.0 * self.pulse_counts.1
     }
     fn part2(&mut self) -> Self::Output2 {
         2
+    }
+}
+
+impl Puzzle {
+    fn start(&mut self) {
+        let mut _history = HashSet::from([self.dump()]);
+        for _ in 0..1000 {
+            self.pulses
+                .push_back(("".to_string(), "broadcaster".to_string(), false));
+            self.pulse_counts.0 += 1;
+            self.dispatch();
+        }
+    }
+    fn dispatch(&mut self) {
+        while let Some((from, to, pulse)) = self.pulses.pop_front() {
+            let Some(target) = self.modules.get_mut(&to) else {
+                continue;
+            };
+            if let Some(output) = target.receive_pulse(&from, pulse) {
+                target
+                    .dests
+                    .iter()
+                    .for_each(|d| self.pulses.push_back((to.clone(), d.clone(), output)));
+                if output {
+                    self.pulse_counts.1 += target.dests.len();
+                } else {
+                    self.pulse_counts.0 += target.dests.len();
+                }
+            }
+        }
+    }
+    fn dump(&self) -> Vec<(bool, Vec<(&String, &bool)>)> {
+        self.modules
+            .iter()
+            .map(|(name, m)| {
+                (
+                    name,
+                    (m.bool_state, m.hash.iter().sorted().collect::<Vec<_>>()),
+                )
+            })
+            .sorted()
+            .map(|(_, m)| m)
+            .collect::<Vec<_>>()
     }
 }
