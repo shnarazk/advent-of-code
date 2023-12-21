d2ff8e94d48c94ee9346543d093801e8469fa7e7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-21 22:08:01 +0900

    modified:   src/y2023/day21.rs

--- src/y2023/day21.rs
+++ src/y2023/day21.rs
@@ -6,7 +6,7 @@ use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
         geometric::{neighbors, Dim2, GeometricMath},
-        line_parser, regex,
+        line_parser, progress, regex,
     },
     std::collections::HashMap,
 };
@@ -75,7 +75,98 @@ impl AdventOfCode for Puzzle {
             .map(|l| l.iter().filter(|c| **c == steps).count())
             .sum::<usize>()
     }
-    fn part2(&mut self) -> Self::Output2 {
-        2
+    fn part2(&mut self) -> Self::Output1 {
+        dbg!(self
+            .line
+            .iter()
+            .map(|v| v.iter().filter(|p| !*p).count())
+            .sum::<usize>());
+        let height = self.line.len() as isize;
+        let width = self.line[0].len() as isize;
+        let steps = 500; // 2650u365;
+        let mut to_visit: Vec<Dim2<isize>> = Vec::new();
+        let mut next: Vec<Dim2<isize>> = Vec::new();
+        let mut history: HashMap<Dim2<isize>, Vec<(usize, usize)>> = HashMap::new();
+        to_visit.push((self.start.0 as isize, self.start.1 as isize));
+        for n in 0..steps {
+            progress!(n);
+            let mut memo = [[0; 7]; 7];
+            while let Some(p) = to_visit.pop() {
+                for q in p
+                    .neighbors4((isize::MIN, isize::MIN), (isize::MAX, isize::MAX))
+                    .iter()
+                {
+                    let mod_y = abs_mod(q.0, height);
+                    let mod_x = abs_mod(q.1, width);
+                    if !self.line[mod_y][mod_x] && !next.contains(q) {
+                        next.push(*q);
+                        let i = if 0 <= q.0 {
+                            q.0 / height
+                        } else {
+                            q.0 / height - 1
+                        };
+                        let j = if 0 <= q.1 {
+                            q.1 / width
+                        } else {
+                            q.1 / width - 1
+                        };
+                        if -3 <= i && i < 4 && -3 <= j && j < 4 {
+                            memo[(i + 3) as usize][(j + 3) as usize] += 1;
+                        }
+                    }
+                }
+            }
+            std::mem::swap(&mut to_visit, &mut next);
+            assert!(next.is_empty());
+            for y in -3..=3 {
+                for x in -3..=3 {
+                    let reach = memo[(y + 3) as usize][(x + 3) as usize];
+                    if 0 < reach {
+                        let entry = history.entry((y, x)).or_default();
+                        entry.push((n, reach));
+                    }
+                }
+            }
+        }
+        for y in -3..=3 {
+            for x in -3..=3 {
+                let base = (y * height, x * width);
+                let reach = to_visit
+                    .iter()
+                    .filter(|(yy, xx)| {
+                        base.0 <= *yy
+                            && *yy < base.0 + height
+                            && base.1 <= *xx
+                            && *xx < base.1 + width
+                    })
+                    .count();
+                print!("{:10}", reach);
+            }
+            println!();
+        }
+        for y in -3..=3 {
+            for x in -3..=3 {
+                if let Some(v) = history.get(&(y, x)) {
+                    let start = v[0].0;
+                    let series = v.iter().map(|(t, n)| *n).collect::<Vec<_>>();
+                    println!("({y},{x}) {start} | {:?}", series);
+                } else {
+                    println!("({y},{x}) -");
+                }
+            }
+            println!();
+        }
+
+        let count = 0;
+        count
+    }
+}
+
+fn abs_mod(a: isize, b: isize) -> usize {
+    let tmp = a % b;
+    if 0 <= tmp {
+        tmp as usize
+    } else {
+        (b + tmp) as usize
     }
 }
