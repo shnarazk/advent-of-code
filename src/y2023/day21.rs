a62226e0032374890fc5a598c3a660ef627254b5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-21 15:05:00 +0900

    y2023 day21 part1 done

--- src/y2023/day21.rs
+++ src/y2023/day21.rs
@@ -5,7 +5,7 @@
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
+        geometric::{neighbors, Dim2, GeometricMath},
         line_parser, regex,
     },
     std::collections::HashMap,
@@ -13,38 +13,67 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<bool>>,
+    start: Dim2<usize>,
 }
-
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
 
 #[aoc(2023, 21)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        if let Some(i) = block.chars().enumerate().find(|(i, c)| *c == 'S') {
+            self.start = (self.line.len(), i.0);
+        }
+        self.line
+            .push(block.chars().map(|c| c == '#').collect::<Vec<_>>());
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
-    }
+    fn end_of_data(&mut self) {}
     fn part1(&mut self) -> Self::Output1 {
-        1
+        let steps = 64;
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let mut to_visit: Vec<Dim2<usize>> = Vec::new();
+        let mut next: Vec<Dim2<usize>> = Vec::new();
+        to_visit.push(self.start);
+        let mut map = self
+            .line
+            .iter()
+            .map(|l| l.iter().map(|_| usize::MAX).collect::<Vec<_>>())
+            .collect::<Vec<_>>();
+        for n in 0..=steps {
+            while let Some(p) = to_visit.pop() {
+                if self.line[p.0][p.1] {
+                    continue;
+                }
+                map[p.0][p.1] = n;
+                for q in p.neighbors4((0, 0), (height, width)).iter() {
+                    if !next.contains(q) {
+                        next.push(*q);
+                    }
+                }
+            }
+            std::mem::swap(&mut to_visit, &mut next);
+        }
+        // println!("{:?}", map);
+        // for (y, l) in map.iter().enumerate() {
+        //     for (x, c) in l.iter().enumerate() {
+        //         print!(
+        //             "{}",
+        //             if self.line[y][x] {
+        //                 '#'
+        //             } else if *c == usize::MAX {
+        //                 ' '
+        //             } else {
+        //                 (b'0' + *c as u8) as char
+        //             }
+        //         );
+        //     }
+        //     println!();
+        // }
+        map.iter()
+            .map(|l| l.iter().filter(|c| **c == steps).count())
+            .sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
         2
