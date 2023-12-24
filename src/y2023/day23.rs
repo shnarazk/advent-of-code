4f85135af2e8967fb1ded0bfb0ce0caa4b269d54
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-24 19:57:33 +0900

    y2023 day23 part1 done

--- src/y2023/day23.rs
+++ src/y2023/day23.rs
@@ -5,46 +5,68 @@
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
+        geometric::{neighbors, Dim2, GeometricMath},
         line_parser, regex,
     },
-    std::collections::HashMap,
+    std::collections::HashSet,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<u8>>,
 }
-
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
 
 #[aoc(2023, 23)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     Ok(segment[2].to_string())
-    // }
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
+        self.line.push(block.bytes().collect::<Vec<_>>());
         Ok(())
     }
     fn end_of_data(&mut self) {
-        dbg!(&self.line);
+        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        let height = self.line.len();
+        let width = self.line[0].len();
+        let goal = (height - 1, width - 2);
+        let mut to_visit: Vec<(Dim2<usize>, HashSet<Dim2<usize>>)> = vec![];
+        let mut longest = 0;
+        to_visit.push(((0, 1), HashSet::new()));
+        while let Some(p) = to_visit.pop() {
+            if p.0 == goal {
+                longest = dbg!(longest.max(p.1.len()));
+                continue;
+            }
+            for q0 in p.0.neighbors4((0, 0), (height, width)).iter_mut() {
+                let mut q = p.clone();
+                match self.line[q0.0][q0.1] {
+                    b'^' => {
+                        q.1.insert(*q0);
+                        q0.0 -= 1;
+                    }
+                    b'<' => {
+                        q.1.insert(*q0);
+                        q0.1 -= 1;
+                    }
+                    b'>' => {
+                        q.1.insert(*q0);
+                        q0.1 += 1;
+                    }
+                    b'v' => {
+                        q.1.insert(*q0);
+                        q0.0 += 1;
+                    }
+                    _ => (),
+                }
+                if self.line[q0.0][q0.1] != b'#' && !p.1.contains(q0) {
+                    q.0 = *q0;
+                    q.1.insert(*q0);
+                    to_visit.push(q);
+                }
+            }
+        }
+        longest
     }
     fn part2(&mut self) -> Self::Output2 {
         2
