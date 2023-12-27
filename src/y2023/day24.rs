f98a1553e8891598d17065e4bf4fc0d26a3af439
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-12-27 22:29:17 +0900

    modified:   src/y2023/day24.rs

--- src/y2023/day24.rs
+++ src/y2023/day24.rs
@@ -1,15 +1,11 @@
 //! <https://adventofcode.com/2023/day/24>
-// #![allow(dead_code)]
-// #![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
         geometric::{Dim2, Dim3},
-        line_parser, progress,
+        line_parser,
     },
-    itertools::Itertools,
-    std::collections::{HashMap, HashSet},
+    std::collections::HashSet,
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -21,7 +17,6 @@ pub struct Puzzle {
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        // 144788461200241, 195443318499267, 285412990927879 @ 227, 158, 5
         let b = block.split(" @ ").collect::<Vec<&str>>();
         self.line.push((
             {
@@ -35,9 +30,7 @@ impl AdventOfCode for Puzzle {
         ));
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(self.line.len());
-    }
+    // fn end_of_data(&mut self) { dbg!(self.line.len()); }
     fn part1(&mut self) -> Self::Output1 {
         const LEAST: f64 = 200000000000000.0;
         const MOST: f64 = 400000000000000.0;
@@ -61,180 +54,54 @@ impl AdventOfCode for Puzzle {
             .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        let forbiddens = self
-            .line
-            .iter()
-            .map(|(p, v)| v.0)
-            .collect::<HashSet<_>>()
-            .iter()
-            .sorted()
-            .cloned()
-            .collect::<Vec<_>>();
-        let min = self.line.iter().map(|(p, v)| p.0).min().unwrap();
-        let max = self.line.iter().map(|(p, v)| p.0).max().unwrap();
-        panic!();
-        self.sweep(&forbiddens, (min, max));
-        println!(
-            "lifting pos:{:?}",
-            self.line
-                .iter()
-                .flat_map(|(p, v)| (0 < v.0).then_some(p.0))
-                .sorted()
-                .rev()
-                .take(4)
-                .collect::<Vec<_>>()
-        );
-        println!(
-            "lifting pos:{:?}",
-            self.line
-                .iter()
-                .flat_map(|(p, v)| (0 < v.0).then_some(p.0))
-                .sorted()
-                .take(4)
-                .collect::<Vec<_>>()
-        );
-        println!(
-            "lifting vec:{:?}",
-            self.line
-                .iter()
-                .flat_map(|(_, v)| (0 < v.0).then_some(v.0))
-                .sorted()
-                .rev()
-                .take(4)
-                .rev()
-                .collect::<Vec<_>>()
-        );
-        println!(
-            "falling pos:{:?}",
-            self.line
-                .iter()
-                .flat_map(|(p, v)| (0 > v.0).then_some(p.0))
-                .sorted()
-                .take(4)
-                .collect::<Vec<_>>()
-        );
-        println!(
-            "falling pos:{:?}",
-            self.line
-                .iter()
-                .flat_map(|(p, v)| (0 > v.0).then_some(p.0))
-                .sorted()
-                .rev()
-                .take(4)
-                .collect::<Vec<_>>()
-        );
-        println!(
-            "falling vec:{:?}",
-            self.line
-                .iter()
-                .flat_map(|(_, v)| (0 > v.0).then_some(v.0))
-                .sorted()
-                .take(4)
-                .collect::<Vec<_>>()
-        );
-        let ax = self
-            .line
-            .iter()
-            .enumerate()
-            .flat_map(|(i, (p, v))| (v.0 < 0).then_some((p.0, v.0, i)))
-            .sorted()
-            .last()
-            .unwrap();
-        let bx = self
-            .line
-            .iter()
-            .enumerate()
-            .flat_map(|(i, (p, v))| (v.0 > 0).then_some((p.0, v.0, i)))
-            .sorted()
-            .last()
-            .unwrap();
-        // sort on X-axis: it leads a constrain about rock's initial position and velocity. Solve it. Then repeat the same procedure on Y and Z.
-
-        let mut range = 3_000_000_000;
-        let mut v = 0;
-        while range > 0 {
-            range /= 2;
-            let l_diff = self.check(v - range);
-            let r_diff = self.check(v + range);
-            if l_diff.abs() < r_diff.abs() {
-                v -= range;
-                println!("{:10}< | {:10}/{:10}", range, v, l_diff);
-            } else {
-                v += range;
-                println!("{:10}> | {:10}/{:10}", range, v, r_diff);
+        let forbiddens = self.line.iter().map(|(_, v)| v.0).collect::<HashSet<_>>();
+        dbg!(self.line.len() - forbiddens.len());
+        let forbiddens = self.line.iter().map(|(_, v)| v.0).collect::<HashSet<_>>();
+        for (i, (p0, v0)) in self.line.iter().enumerate() {
+            for (_, (p1, v1)) in self.line.iter().enumerate().skip(i) {
+                if v0.0 != v1.0 {
+                    continue;
+                }
+                for s in -400..=400 {
+                    if s == v0.0 {
+                        continue;
+                    }
+                    let m = (p1.0 - p0.0) % (s - v0.0);
+                    if m == 0 {
+                        dbg!(s);
+                    }
+                }
             }
         }
-        for o in -5..=5 {
-            println!("v={}: {}", v + o, self.check(v + o));
-        }
-        2
-    }
-}
-impl Puzzle {
-    fn sweep(&self, forbidden: &[isize], range: (isize, isize)) {
-        let mut converge: HashMap<isize, Vec<(usize, isize)>> = HashMap::new();
-        let width = range.1 - range.0;
-        for test in -50..=50 {
-            if test == 0 || forbidden.contains(&test) {
+        return 0;
+        'next_speed: for s in -400..=400 {
+            if forbiddens.contains(&s) {
                 continue;
             }
-            let mut countup: HashMap<isize, usize> = HashMap::new();
-            for (x, v) in self.line.iter().map(|(p, v)| (p.0, v.0)) {
-                let mut diffs: HashSet<isize> = HashSet::new();
-                for t in 1..=dbg!((width / test).abs()) {
-                    let diff = x + t * v - t * test;
-                    diffs.insert(diff);
+            // if s != -356 { continue; }
+            let mut mods: HashSet<isize> = HashSet::new();
+            // for (p, v) in self.line.iter() {
+            //     if s - v.0 == 0 {
+            //         continue 'next_speed;
+            //     }
+            //     let m = p.0 % (s - v.0);
+            //     mods.insert(m);
+            // }
+            for (p, v) in self.line.iter() {
+                for (q, w) in self.line.iter() {
+                    // if v.0 == w.0 {
+                    let m = (q.0 - p.0) % (s + v.0);
+                    mods.insert(m);
                 }
-                for d in diffs.iter() {
-                    *countup.entry(*d).or_default() += 1;
-                }
+                let m = p.0 % (s - v.0);
+                mods.insert(m);
             }
-            for (dist, count) in countup.iter() {
-                converge.entry(*dist).or_default().push((*count, test));
-            }
-        }
-        for value in converge.values_mut() {
-            value.sort();
-            value.reverse();
-        }
-        dbg!(converge.len());
-        dbg!(converge
-            .iter()
-            .map(|(x, l)| (l[0], x))
-            .sorted()
-            .rev()
-            .take(2)
-            .collect::<Vec<_>>());
-        // for value in converge.values_mut() {
-        //     value.sort();
-        //     value.reverse();
-        // }
-        // dbg!(converge.iter().map(|(x, l)| (l[0], x)).max().unwrap());
-    }
-    fn check(&self, at: isize) -> isize {
-        let mut converge: HashMap<isize, Vec<(usize, isize)>> = HashMap::new();
-        for test in 0..=0 {
-            let mut countup: HashMap<isize, usize> = HashMap::new();
-            for (x, v) in self.line.iter().map(|(p, v)| (p.0, v.0)) {
-                let mut diffs: HashSet<isize> = HashSet::new();
-                for t in 1..2000isize {
-                    let diff = x + t * v - t * (test + at);
-                    diffs.insert(diff);
-                }
-                for d in diffs.iter() {
-                    *countup.entry(*d).or_default() += 1;
-                }
-            }
-            for (dist, count) in countup.iter() {
-                converge.entry(*dist).or_default().push((*count, test + at));
+            dbg!(mods.len());
+            if mods.len() <= 6 {
+                return dbg!(s as usize);
             }
         }
-        // for value in converge.values_mut() {
-        //     value.sort();
-        //     value.reverse();
-        // }
-        // dbg!(converge.iter().map(|(x, l)| (l[0], x)).max().unwrap());
-        return converge.iter().map(|(dist, l)| dist.abs()).min().unwrap();
+        0
     }
 }
 
@@ -250,21 +117,4 @@ fn intersect_in_plane(a: (Dim2<f64>, Dim2<f64>), b: (Dim2<f64>, Dim2<f64>)) -> O
         return None;
     }
     Some((a.1 .0 * ta + a.0 .0, a.1 .1 * ta + a.0 .1))
-    /*
-    f = a.1*ta + a.0
-    g = b.1*tb + b.0
-    a.1*ta + a.0 = b.1*tb + b.0
-
-    a.1.0*ta + a.0.0 = b.1.0*tb + b.0.0
-    a.1.1*ta + a.0.1 = b.1.1*tb + b.0.1
-
-    ta = (b.1.0*tb + b.0.0 - a.0.0) / a.1.0
-    a.1.1*(b.1.0*tb + b.0.0 - a.0.0) = a.1.0 * (b.1.1*tb + b.0.1 - a.0.1)
-    (a.1.1*b.1.0 - a.1.0 * b.1.1) tb = a.1.0 * (b.0.1 - a.0.1) - a.1.1*(b.0.0 - a.0.0)
-
-    tb = (a.1.1*ta + a.0.1 - b.0.1) / b.1.1
-    a.1.0*ta + a.0.0 = b.1.0 * (a.1.1*ta + a.0.1 - b.0.1) / b.1.1 + b.0.0
-    b.1.1* a.1.0*ta + b.1.1*(a.0.0 - b.0.0) = b.1.0 * a.1.1*ta + b.1.0 * (a.0.1 - b.0.1)
-    (b.1.1*a.1.0 - b.1.0*a.1.1)*ta = b.1.0*(a.0.1 - b.0.1) - b.1.1*(a.0.0 - b.0.0)
-    */
 }
