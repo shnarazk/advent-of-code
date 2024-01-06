9740abdfe384e552c4be56227fcd3168f27b2fb2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-01-06 22:10:33 +0900

    y2021 day22: implementation improvement

--- src/y2021/day22.rs
+++ src/y2021/day22.rs
@@ -5,6 +5,7 @@ use {
         framework::{aoc, AdventOfCode, ParseError},
         line_parser, regex,
     },
+    itertools::Itertools,
     std::collections::HashMap,
 };
 
@@ -33,21 +34,6 @@ impl AdventOfCode for Puzzle {
         ));
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        dbg!(self
-            .line
-            .iter()
-            .map(|(_, x, _, y, _, z, _)| x.min(y).min(z))
-            .min()
-            .unwrap());
-        dbg!(self
-            .line
-            .iter()
-            .map(|(_, _, x, _, y, _, z)| x.max(y).max(z))
-            .max()
-            .unwrap());
-        // dbg!(&self.line);
-    }
     fn part1(&mut self) -> Self::Output1 {
         let offset = |p: isize| (p + 50isize) as usize;
         let mut grid: Vec<Vec<Vec<bool>>> = Vec::new();
@@ -61,14 +47,14 @@ impl AdventOfCode for Puzzle {
             grid.push(v);
         }
         for (to, x1, x2, y1, y2, z1, z2) in self.line.iter() {
-            print!("{}..{},{}..{},{}..{}", x1, x2, y1, y2, z1, z2);
-            println!(
-                " - {:?},{:?},{:?}",
-                50 < *z1 || *z2 < -50,
-                50 < *y1 || *y2 < -50,
-                50 < *x1 || *x2 < -50,
-            );
-            assert!(x1 <= x2 && y1 <= y2 && z1 <= z2);
+            // print!("{}..{},{}..{},{}..{}", x1, x2, y1, y2, z1, z2);
+            // println!(
+            //     " - {:?},{:?},{:?}",
+            //     50 < *z1 || *z2 < -50,
+            //     50 < *y1 || *y2 < -50,
+            //     50 < *x1 || *x2 < -50,
+            // );
+            debug_assert!(x1 <= x2 && y1 <= y2 && z1 <= z2);
             if 50 < *z1 || *z2 < -50 {
                 continue;
             }
@@ -95,109 +81,82 @@ impl AdventOfCode for Puzzle {
             .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        let mut xs: Vec<isize> = self
+        let xs: Vec<isize> = self
             .line
             .iter()
             .flat_map(|(_, x1, x2, _, _, _, _)| vec![*x1, *x2 + 1])
-            .collect();
-        let mut ys: Vec<isize> = self
+            .collect::<HashSet<isize>>()
+            .iter()
+            .copied()
+            .sorted()
+            .collect::<Vec<isize>>();
+        let ys: Vec<isize> = self
             .line
             .iter()
             .flat_map(|(_, _, _, y1, y2, _, _)| vec![*y1, *y2 + 1])
-            .collect();
-        let mut zs: Vec<isize> = self
+            .collect::<HashSet<isize>>()
+            .iter()
+            .copied()
+            .sorted()
+            .collect::<Vec<isize>>();
+        let zs: Vec<isize> = self
             .line
             .iter()
             .flat_map(|(_, _, _, _, _, z1, z2)| vec![*z1, *z2 + 1])
-            .collect();
+            .collect::<HashSet<isize>>()
+            .iter()
+            .copied()
+            .sorted()
+            .collect::<Vec<isize>>();
+        let to_index_x: HashMap<isize, usize> = xs
+            .iter()
+            .enumerate()
+            .map(|(i, x)| (*x, i))
+            .collect::<HashMap<isize, usize>>();
+        let to_index_y: HashMap<isize, usize> = ys
+            .iter()
+            .enumerate()
+            .map(|(i, y)| (*y, i))
+            .collect::<HashMap<isize, usize>>();
+        let to_index_z: HashMap<isize, usize> = zs
+            .iter()
+            .enumerate()
+            .map(|(i, z)| (*z, i))
+            .collect::<HashMap<isize, usize>>();
+        let mut grid: Vec<Vec<Vec<bool>>> = zs
+            .iter()
+            .map(|_| {
+                ys.iter()
+                    .map(|_| xs.iter().map(|_| false).collect::<Vec<_>>())
+                    .collect::<Vec<_>>()
+            })
+            .collect::<Vec<_>>();
 
-        let mut hx: HashSet<isize> = HashSet::new();
-        for x in xs.iter() {
-            hx.insert(*x);
-        }
-        xs = hx.iter().copied().collect::<Vec<_>>();
-        let mut hy: HashSet<isize> = HashSet::new();
-        for y in ys.iter() {
-            hy.insert(*y);
-        }
-        ys = hy.iter().copied().collect::<Vec<_>>();
-        let mut hz: HashSet<isize> = HashSet::new();
-        for z in zs.iter() {
-            hz.insert(*z);
-        }
-        zs = hz.iter().copied().collect::<Vec<_>>();
-
-        xs.sort_unstable();
-        ys.sort_unstable();
-        zs.sort_unstable();
-        debug_assert!(xs.windows(2).all(|a| a[0] != a[1]));
-        debug_assert!(ys.windows(2).all(|a| a[0] != a[1]));
-        debug_assert!(zs.windows(2).all(|a| a[0] != a[1]));
-        dbg!(xs.len(), ys.len(), zs.len());
-        dbg!(&xs[1..20]);
-        let mut to_index_x: HashMap<isize, usize> = HashMap::new();
-        let mut to_index_y: HashMap<isize, usize> = HashMap::new();
-        let mut to_index_z: HashMap<isize, usize> = HashMap::new();
-        for (i, x) in xs.iter().enumerate() {
-            to_index_x.insert(*x, i);
-        }
-        for (i, y) in ys.iter().enumerate() {
-            to_index_y.insert(*y, i);
-        }
-        for (i, z) in zs.iter().enumerate() {
-            to_index_z.insert(*z, i);
-        }
-        let mut grid: Vec<Vec<Vec<bool>>> = Vec::new();
-        for _ in 0..zs.len() {
-            let mut w: Vec<Vec<bool>> = Vec::new();
-            for _ in 0..ys.len() {
-                let mut v: Vec<bool> = Vec::new();
-                v.resize(xs.len(), false);
-                w.push(v);
-            }
-            grid.push(w);
-        }
-
-        for (to, x1, x2, y1, y2, z1, z2) in self.line.iter() {
-            for k in grid
-                .iter_mut()
-                .take(*to_index_z.get(&(*z2 + 1)).unwrap())
-                .skip(*to_index_z.get(z1).unwrap())
-            {
-                for l in k
-                    .iter_mut()
-                    .take(*to_index_y.get(&(*y2 + 1)).unwrap())
-                    .skip(*to_index_y.get(y1).unwrap())
-                {
-                    for m in l
-                        .iter_mut()
-                        .take(*to_index_x.get(&(*x2 + 1)).unwrap())
-                        .skip(*to_index_x.get(x1).unwrap())
-                    {
-                        *m = *to;
+        for (to, ref x1, x2, y1, ref y2, ref z1, z2) in self.line.iter() {
+            let i1 = *to_index_z.get(z1).unwrap();
+            let i2 = *to_index_z.get(&(*z2 + 1)).unwrap();
+            let j1 = *to_index_y.get(y1).unwrap();
+            let j2 = *to_index_y.get(&(*y2 + 1)).unwrap();
+            let k1 = *to_index_x.get(x1).unwrap();
+            let k2 = *to_index_x.get(&(*x2 + 1)).unwrap();
+            for i in i1..i2 {
+                for j in j1..j2 {
+                    for k in k1..k2 {
+                        grid[i][j][k] = *to;
                     }
                 }
             }
         }
-        zs.windows(2)
-            .map(|pz| {
-                ys.windows(2)
-                    .map(|py| {
-                        xs.windows(2)
-                            .map(|px| {
-                                if grid[*to_index_z.get(&pz[0]).unwrap()]
-                                    [*to_index_y.get(&py[0]).unwrap()]
-                                    [*to_index_x.get(&px[0]).unwrap()]
-                                {
-                                    ((pz[1] - pz[0]) * (py[1] - py[0]) * (px[1] - px[0])) as usize
-                                } else {
-                                    0
-                                }
-                            })
-                            .sum::<usize>()
-                    })
-                    .sum::<usize>()
-            })
-            .sum()
+        let mut sum = 0;
+        for (i, c) in grid.iter().enumerate() {
+            for (j, l) in c.iter().enumerate() {
+                for (k, b) in l.iter().enumerate() {
+                    if *b {
+                        sum += (zs[i + 1] - zs[i]) * (ys[j + 1] - ys[j]) * (xs[k + 1] - xs[k]);
+                    }
+                }
+            }
+        }
+        sum as usize
     }
 }
