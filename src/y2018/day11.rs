41ecfc44b8f34d690f9ab3adfc76243ed549a44e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-01-08 00:48:23 +0900

    y2018 day11: implementation improvement

--- src/y2018/day11.rs
+++ src/y2018/day11.rs
@@ -1,35 +1,38 @@
 //! <https://adventofcode.com/2018/day/11>
 use {
-    crate::framework::{aoc, AdventOfCode, ParseError},
+    crate::{
+        framework::{aoc_at, AdventOfCode, ParseError},
+        geometric::Dim2,
+    },
     std::collections::HashMap,
 };
 
-type Dim2 = (usize, usize);
-
 #[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
-    line: usize,
-    map: HashMap<Dim2, isize>,
+    grid_serial_number: usize,
+    map: HashMap<Dim2<usize>, isize>,
 }
 
-#[aoc(2018, 11)]
+#[aoc_at(2018, 11)]
 impl AdventOfCode for Puzzle {
+    type Output1 = String;
+    type Output2 = String;
     const DELIMITER: &'static str = "\n";
-    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        self.grid_serial_number = block.parse::<usize>()?;
         Ok(())
     }
-    fn end_of_data(&mut self) {
-        self.line = 3463;
-    }
     fn part1(&mut self) -> Self::Output1 {
-        let mut loc: Dim2 = (1, 1);
+        let map = self.build_value_power_level_map();
+        let mut loc: Dim2<usize> = (1, 1);
         let mut point_max: isize = 0;
         for y in 1..=300 - 2 {
             for x in 1..=300 - 2 {
                 let mut sum = 0;
                 for yy in y..y + 3 {
                     for xx in x..x + 3 {
-                        sum += self.get((xx, yy));
+                        // sum += self.get((xx, yy));
+                        sum += map[yy - 1][xx - 1];
                         if point_max < sum {
                             point_max = sum;
                             loc = (x, y);
@@ -38,10 +41,10 @@ impl AdventOfCode for Puzzle {
                 }
             }
         }
-        dbg!(loc);
-        0
+        format!("{},{}", loc.0, loc.1)
     }
     fn part2(&mut self) -> Self::Output2 {
+        let map = self.build_value_power_level_map();
         let mut loc = (1, 1, 1);
         let mut point_max: isize = 0;
         for y in 1..=300 {
@@ -50,10 +53,10 @@ impl AdventOfCode for Puzzle {
                 let mut sum = 0;
                 for size in 1..=max_square {
                     for yy in y..y + size {
-                        sum += self.get((x + size - 1, yy));
+                        sum += map[yy - 1][x + size - 2]; // ((x + size - 1, yy));
                     }
                     for xx in x..x + size - 1 {
-                        sum += self.get((xx, y + size - 1));
+                        sum += map[y + size - 2][xx - 1]; //self.get((xx, y + size - 1));
                     }
                     if point_max < sum {
                         point_max = sum;
@@ -62,35 +65,27 @@ impl AdventOfCode for Puzzle {
                 }
             }
         }
-        dbg!(loc);
-        0
+        format!("{},{},{}", loc.0, loc.1, loc.2)
     }
 }
 
 impl Puzzle {
-    fn get(&mut self, at: Dim2) -> isize {
-        if let Some(p) = self.map.get(&at) {
-            return *p;
-        }
-        let rack_id = at.0 as isize + 10;
-        let mut power_level = rack_id * at.1 as isize;
-        power_level += self.line as isize;
-        power_level *= rack_id;
-        power_level /= 100;
-        power_level %= 10;
-        power_level -= 5;
-        self.map.insert(at, power_level);
-        power_level
+    fn build_value_power_level_map(&self) -> Vec<Vec<isize>> {
+        (1..=300)
+            .map(|y| {
+                (1..=300)
+                    .map(|x| {
+                        let rack_id = x as isize + 10;
+                        let mut power_level = rack_id * (y as isize);
+                        power_level += self.grid_serial_number as isize;
+                        power_level *= rack_id;
+                        power_level /= 100;
+                        power_level %= 10;
+                        power_level -= 5;
+                        power_level
+                    })
+                    .collect::<Vec<isize>>()
+            })
+            .collect::<Vec<_>>()
     }
-}
-
-#[test]
-fn y2018day11part1_test1() {
-    let mut p = Puzzle {
-        line: 8,
-        ..Puzzle::default()
-    };
-    assert_eq!(p.get((3, 5)), 4);
-    p.line = 57;
-    assert_eq!(p.get((122, 79)), -5);
 }
