583f2f67482f26779ecb66925993934a4b686e4a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-01-07 19:20:56 +0900

    y2015 day22: implementation improvement

--- src/y2015/day22.rs
+++ src/y2015/day22.rs
@@ -20,14 +20,13 @@ const SPELL: [(usize, usize, (usize, usize, usize, usize)); 5] = [
 enum GameState {
     #[default]
     Impossible,
-    Won,
+    Won(usize),
     Lose,
 }
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Player {
     hit_point: usize,
-    damage: usize,
     mana: usize,
     state: [Option<usize>; 5], // Magic Missile, Drain, Shild, Poison, Rechage
 }
@@ -43,41 +42,54 @@ pub struct Puzzle {
     used_mana: usize,
     player: Player,
     boss: Boss,
-    hit_point: usize,
-    damage: usize,
 }
 
 impl Puzzle {
     // turn consists of your spell then boss's attack
-    fn turn(&self, kind: usize) -> Result<Puzzle, GameState> {
-        if self.player.state[kind].is_some() && self.player.mana < SPELL[kind].0 {
+    fn turn(&self, kind: usize, dec: usize) -> Result<Puzzle, GameState> {
+        if 2000 < self.used_mana {
             return Err(GameState::Impossible);
         }
         let mut new_state = self.clone();
-        // spelling
-        new_state.player.state[kind] = Some(SPELL[kind].1);
-        new_state.used_mana -= SPELL[kind].0;
         let mut armor = 0;
-        // your spell effects just before the boss's turn
-        for (kind, _) in SPELL.iter().enumerate() {
-            if let Some(remain) = &mut new_state.player.state[kind] {
-                if 0 == *remain {
-                    new_state.player.state[kind] = None;
-                } else {
-                    *remain -= 1;
-                    new_state.player.hit_point += SPELL[kind].2 .0;
-                    new_state.boss.hit_point = new_state
-                        .boss
-                        .hit_point
-                        .checked_sub(SPELL[kind].2 .1)
-                        .ok_or(GameState::Won)?;
-                    armor += SPELL[kind].2 .2;
-                    new_state.player.mana += SPELL[kind].2 .3;
+        macro_rules! change_state {
+            () => {
+                new_state.player.hit_point = new_state
+                    .player
+                    .hit_point
+                    .checked_sub(dec)
+                    .ok_or(GameState::Lose)?;
+                for (kind, _) in SPELL.iter().enumerate() {
+                    if let Some(remain) = &mut new_state.player.state[kind] {
+                        assert!(*remain != 0);
+                        new_state.player.hit_point += SPELL[kind].2 .0;
+                        new_state.boss.hit_point = new_state
+                            .boss
+                            .hit_point
+                            .checked_sub(SPELL[kind].2 .1)
+                            .ok_or(GameState::Won(new_state.used_mana))?;
+                        armor += ((kind == 2) as usize) * SPELL[kind].2 .2;
+                        new_state.player.mana += SPELL[kind].2 .3;
+                        if 1 == *remain {
+                            new_state.player.state[kind] = None;
+                        } else {
+                            *remain -= 1;
+                        }
+                    }
                 }
-            }
+            };
         }
+        change_state!();
+        if new_state.player.mana < SPELL[kind].0 || new_state.player.state[kind].is_some() {
+            return Err(GameState::Impossible);
+        }
+        new_state.player.mana -= SPELL[kind].0;
+        new_state.used_mana += SPELL[kind].0;
+        new_state.player.state[kind] = Some(SPELL[kind].1);
+        armor = 0;
+        change_state!();
         // boss atack
-        new_state.player.hit_point = (new_state.player.hit_point + 7 * armor)
+        new_state.player.hit_point = (new_state.player.hit_point + armor)
             .checked_sub(new_state.boss.damage)
             .ok_or(GameState::Lose)?;
         Ok(new_state)
@@ -88,37 +100,51 @@ impl Puzzle {
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let hp = regex!(r"^Hit Points: ([0-9]+)$");
-        let damage = regex!(r"^Damage: ([0-9]+)$");
+        let hp = regex!(r"^Hit Points: (\d+)$");
+        let damage = regex!(r"^Damage: (\d+)$");
         if let Ok(segment) = hp.captures(block).ok_or(ParseError) {
-            self.hit_point = segment[1].parse::<usize>()?;
+            self.boss.hit_point = segment[1].parse::<usize>()?;
         }
         if let Ok(segment) = damage.captures(block).ok_or(ParseError) {
-            self.damage = segment[1].parse::<usize>()?;
+            self.boss.damage = segment[1].parse::<usize>()?;
         }
         Ok(())
     }
     fn end_of_data(&mut self) {
-        self.player.hit_point = 50 - 1;
+        self.player.hit_point = 49;
         self.player.mana = 500;
-        self.boss.hit_point = self.hit_point - 1;
-        self.boss.damage = self.damage;
+        self.boss.hit_point -= 1;
     }
     fn part1(&mut self) -> Self::Output1 {
         let mut to_visit: BinaryHeap<Reverse<Puzzle>> = BinaryHeap::new();
         to_visit.push(Reverse(self.clone()));
         while let Some(Reverse(w)) = to_visit.pop() {
             for (i, _) in SPELL.iter().enumerate() {
-                match w.turn(i) {
+                match w.turn(i, 0) {
                     Ok(ww) => to_visit.push(Reverse(ww)),
-                    Err(GameState::Won) => return w.used_mana,
+                    Err(GameState::Won(mana)) => {
+                        return mana;
+                    }
                     Err(GameState::Impossible) | Err(GameState::Lose) => continue,
                 }
             }
         }
-        unreachable!();
+        unreachable!()
     }
     fn part2(&mut self) -> Self::Output2 {
-        0
+        let mut to_visit: BinaryHeap<Reverse<Puzzle>> = BinaryHeap::new();
+        to_visit.push(Reverse(self.clone()));
+        while let Some(Reverse(w)) = to_visit.pop() {
+            for (i, _) in SPELL.iter().enumerate() {
+                match w.turn(i, 1) {
+                    Ok(ww) => to_visit.push(Reverse(ww)),
+                    Err(GameState::Won(mana)) => {
+                        return mana;
+                    }
+                    Err(GameState::Impossible) | Err(GameState::Lose) => continue,
+                }
+            }
+        }
+        unreachable!()
     }
 }
