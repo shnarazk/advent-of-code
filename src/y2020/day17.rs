46f5be787ba42c77a38aa718d7a9743ff33b1fe0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-01-08 07:02:22 +0900

    y2020 day17: implementation improvement

--- src/y2020/day17.rs
+++ src/y2020/day17.rs
@@ -1,49 +1,45 @@
 //! <https://adventofcode.com/2020/day/17>
-
-use crate::framework::ConfigAoC;
 use {
-    crate::framework::{aoc, AdventOfCode, Answer, ParseError},
-    std::{cmp::PartialEq, collections::HashMap, fmt::Debug, hash::Hash},
+    crate::{
+        framework::{aoc, AdventOfCode, ParseError},
+        geometric::Dim2,
+    },
+    std::{collections::HashSet, hash::Hash},
 };
 
 #[derive(Debug, Default)]
-pub struct Puzzle {}
+pub struct Puzzle {
+    line: Vec<Vec<bool>>,
+    map: HashSet<Dim2<isize>>,
+}
 
 #[aoc(2020, 17)]
 impl AdventOfCode for Puzzle {
-    const DELIMITER: &'static str = "";
-    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
+    const DELIMITER: &'static str = "\n";
+    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+        self.line
+            .push(block.chars().map(|c| c == '#').collect::<Vec<_>>());
         Ok(())
     }
-    fn part1(&mut self) -> Self::Output1 {
-        0
-    }
-    fn part2(&mut self) -> Self::Output2 {
-        0
-    }
-    fn solve(config: ConfigAoC) -> Answer<Self::Output1, Self::Output2> {
-        match config.part {
-            1 => World::<LOC>::solve(config),
-            2 => World::<LOC4>::solve(config),
-            _ => {
-                let c1 = ConfigAoC {
-                    part: 1,
-                    ..ConfigAoC::default()
-                };
-                let c2 = ConfigAoC {
-                    part: 2,
-                    ..ConfigAoC::default()
-                };
-                match (World::<LOC>::solve(c1), World::<LOC4>::solve(c2)) {
-                    (Answer::Part1(a), Answer::Part2(b)) => Answer::Answers(a, b),
-                    _ => unreachable!(),
+    fn end_of_data(&mut self) {
+        let offset_y = ((self.line.len() - 1) / 2) as isize;
+        let offset_x = ((self.line[0].len() - 1) / 2) as isize;
+        for (y, l) in self.line.iter().enumerate() {
+            for (x, b) in l.iter().enumerate() {
+                if *b {
+                    self.map
+                        .insert((y as isize - offset_y, x as isize - offset_x));
                 }
             }
         }
     }
+    fn part1(&mut self) -> Self::Output1 {
+        World::<LOC>::solve(&self.map)
+    }
+    fn part2(&mut self) -> Self::Output2 {
+        World::<LOC4>::solve(&self.map)
+    }
 }
-
-const RANGE: isize = 17;
 
 #[allow(clippy::upper_case_acronyms)]
 type LOC = (isize, isize, isize);
@@ -51,93 +47,80 @@ type LOC = (isize, isize, isize);
 #[allow(clippy::upper_case_acronyms)]
 type LOC4 = (isize, isize, isize, isize);
 
-#[derive(Debug, Default, PartialEq)]
-struct World<DIM: Eq + Default + Hash + PartialEq> {
-    loc: HashMap<DIM, bool>,
+#[derive(Clone, Debug, Default, PartialEq)]
+struct World<DIM: Eq + Hash> {
+    loc: HashSet<DIM>,
     generation: usize,
 }
 
-#[derive(Debug, Default, PartialEq)]
-struct World0 {
-    loc: HashMap<LOC, bool>,
-    generation: usize,
-}
-
-#[derive(Debug, Default, PartialEq)]
-struct World4 {
-    loc: HashMap<LOC4, bool>,
-    generation: usize,
-}
-
-trait CellParser {
-    type Element;
-    fn default_() -> Self;
-    fn parse_(self, block: &str) -> Self;
-    fn neighbors(&mut self, l: Self::Element) -> (usize, usize);
+trait CellParser
+where
+    Self: Sized,
+{
+    type INDEX;
+    fn from_map(map: &HashSet<Dim2<isize>>) -> Self;
+    fn neighbors(&self, l: Self::INDEX) -> usize;
     fn next(&mut self) -> Self;
     fn actives(&self) -> usize;
+    fn solve(map: &HashSet<Dim2<isize>>) -> usize {
+        Self::from_map(map)
+            .next()
+            .next()
+            .next()
+            .next()
+            .next()
+            .next()
+            .actives()
+    }
 }
 
 impl CellParser for World<LOC> {
-    type Element = LOC;
-    fn default_() -> Self {
-        Self {
-            loc: HashMap::new(),
-            generation: 0,
+    type INDEX = LOC;
+    fn from_map(map: &HashSet<Dim2<isize>>) -> Self {
+        let mut instance = Self::default();
+        for (y, x) in map.iter() {
+            instance.loc.insert((0, *y, *x));
         }
+        instance
     }
-    fn parse_(mut self, block: &str) -> Self {
-        let mut vec: Vec<LOC> = Vec::new();
-        let z = 0;
-        let offset = (block.split('\n').count() as isize - 1) / 2;
-        let mut i = -offset;
-        let mut j = -offset;
-        for l in block.split('\n') {
-            for c in l.chars() {
-                if c == '#' {
-                    vec.push((z, i, j));
-                }
-                j += 1;
-            }
-            j = -offset;
-            i += 1;
-        }
-        for l in vec {
-            *self.loc.entry(l).or_insert(false) = true;
-        }
-        self
-    }
-    fn neighbors(&mut self, l: LOC) -> (usize, usize) {
-        let mut actives = 0;
-        let mut inactives = 0;
-        for z in -1..=1 {
-            for i in -1..=1 {
-                for j in -1..=1 {
-                    if z == 0 && i == 0 && j == 0 {
-                        continue;
-                    }
-                    if *self.get_entry((l.0 + z, l.1 + i, l.2 + j)) {
-                        actives += 1;
-                    } else {
-                        inactives += 1;
-                    }
-                }
-            }
-        }
-        (inactives, actives)
+    fn neighbors(&self, l: Self::INDEX) -> usize {
+        (-1..=1)
+            .map(|z| {
+                (-1..=1)
+                    .map(|y| {
+                        (-1..=1)
+                            .map(|x| {
+                                (((z, y, x) != (0, 0, 0))
+                                    && self.loc.contains(&(l.0 + z, l.1 + y, l.2 + x)))
+                                    as usize
+                            })
+                            .sum::<usize>()
+                    })
+                    .sum::<usize>()
+            })
+            .sum::<usize>()
     }
     fn next(&mut self) -> Self {
-        let mut next = Self::default_();
-        for z in -RANGE..=RANGE {
-            for i in -RANGE..=RANGE {
-                for j in -RANGE..=RANGE {
-                    let l = (z, i, j);
-                    let na = self.neighbors(l).1;
-                    let new_entry = next.get_entry(l);
-                    if *self.get_entry(l) {
-                        *new_entry = na == 2 || na == 3;
-                    } else {
-                        *new_entry = na == 3;
+        let mut next = self.clone();
+        let mut z_range = (isize::MAX, isize::MIN);
+        let mut y_range = (isize::MAX, isize::MIN);
+        let mut x_range = (isize::MAX, isize::MIN);
+        for (z, y, x) in self.loc.iter() {
+            z_range.0 = z_range.0.min(*z - 1);
+            z_range.1 = z_range.1.max(*z + 1);
+            y_range.0 = y_range.0.min(*y - 1);
+            y_range.1 = y_range.1.max(*y + 1);
+            x_range.0 = x_range.0.min(*x - 1);
+            x_range.1 = x_range.1.max(*x + 1);
+        }
+        next.loc.clear();
+        for z in z_range.0..=z_range.1 {
+            for y in y_range.0..=y_range.1 {
+                for x in x_range.0..=x_range.1 {
+                    let l = (z, y, x);
+                    let na = self.neighbors(l);
+                    if na == 3 || (self.loc.contains(&l) && na == 2) {
+                        next.loc.insert(l);
                     }
                 }
             }
@@ -146,84 +129,69 @@ impl CellParser for World<LOC> {
         next
     }
     fn actives(&self) -> usize {
-        let mut count = 0;
-        for z in -RANGE..=RANGE {
-            for i in -RANGE..=RANGE {
-                for j in -RANGE..=RANGE {
-                    if let Some(true) = self.loc.get(&(z, i, j)) {
-                        count += 1;
-                    }
-                }
-            }
-        }
-        count
+        self.loc.len()
     }
 }
 
 impl CellParser for World<LOC4> {
-    type Element = LOC4;
-    fn default_() -> Self {
-        Self {
-            loc: HashMap::new(),
-            generation: 0,
+    type INDEX = LOC4;
+    fn from_map(map: &HashSet<Dim2<isize>>) -> Self {
+        let mut instance = Self::default();
+        for (y, x) in map.iter() {
+            instance.loc.insert((0, 0, *y, *x));
         }
+        instance
     }
-    fn parse_(mut self, block: &str) -> Self {
-        let mut vec: Vec<LOC4> = Vec::new();
-        let t = 0;
-        let z = 0;
-        let offset = (block.split('\n').count() as isize - 1) / 2;
-        let mut i = -offset;
-        let mut j = -offset;
-        for l in block.split('\n') {
-            for c in l.chars() {
-                if c == '#' {
-                    vec.push((t, z, i, j));
-                }
-                j += 1;
-            }
-            j = -offset;
-            i += 1;
-        }
-        for l in vec {
-            *self.loc.entry(l).or_insert(false) = true;
-        }
-        self
-    }
-    fn neighbors(&mut self, l: LOC4) -> (usize, usize) {
-        let mut actives = 0;
-        let mut inactives = 0;
-        for t in -1..=1 {
-            for z in -1..=1 {
-                for i in -1..=1 {
-                    for j in -1..=1 {
-                        if t == 0 && z == 0 && i == 0 && j == 0 {
-                            continue;
-                        }
-                        if *self.get_entry((l.0 + t, l.1 + z, l.2 + i, l.3 + j)) {
-                            actives += 1;
-                        } else {
-                            inactives += 1;
-                        }
-                    }
-                }
-            }
-        }
-        (inactives, actives)
+    fn neighbors(&self, l: Self::INDEX) -> usize {
+        (-1..=1)
+            .map(|t| {
+                (-1..=1)
+                    .map(|z| {
+                        (-1..=1)
+                            .map(|y| {
+                                (-1..=1)
+                                    .map(|x| {
+                                        (((t, z, y, x) != (0, 0, 0, 0))
+                                            && self.loc.contains(&(
+                                                l.0 + t,
+                                                l.1 + z,
+                                                l.2 + y,
+                                                l.3 + x,
+                                            ))) as usize
+                                    })
+                                    .sum::<usize>()
+                            })
+                            .sum::<usize>()
+                    })
+                    .sum::<usize>()
+            })
+            .sum::<usize>()
     }
     fn next(&mut self) -> Self {
-        let mut next = Self::default_();
-        for t in -RANGE..=RANGE {
-            for z in -RANGE..=RANGE {
-                for i in -RANGE..=RANGE {
-                    for j in -RANGE..=RANGE {
-                        let l = (t, z, i, j);
-                        let na = self.neighbors(l).1;
-                        let new_entry = next.get_entry(l);
-                        if *self.get_entry(l) {
-                            *new_entry = na == 2 || na == 3;
-                        } else {
-                            *new_entry = na == 3;
+        let mut next = self.clone();
+        let mut t_range = (isize::MAX, isize::MIN);
+        let mut z_range = (isize::MAX, isize::MIN);
+        let mut y_range = (isize::MAX, isize::MIN);
+        let mut x_range = (isize::MAX, isize::MIN);
+        for (t, z, y, x) in self.loc.iter() {
+            t_range.0 = t_range.0.min(*t - 1);
+            t_range.1 = t_range.1.max(*t + 1);
+            z_range.0 = z_range.0.min(*z - 1);
+            z_range.1 = z_range.1.max(*z + 1);
+            y_range.0 = y_range.0.min(*y - 1);
+            y_range.1 = y_range.1.max(*y + 1);
+            x_range.0 = x_range.0.min(*x - 1);
+            x_range.1 = x_range.1.max(*x + 1);
+        }
+        next.loc.clear();
+        for t in t_range.0..=t_range.1 {
+            for z in z_range.0..=z_range.1 {
+                for y in y_range.0..=y_range.1 {
+                    for x in x_range.0..=x_range.1 {
+                        let l = (t, z, y, x);
+                        let na = self.neighbors(l);
+                        if na == 3 || (self.loc.contains(&l) && na == 2) {
+                            next.loc.insert(l);
                         }
                     }
                 }
@@ -233,67 +201,6 @@ impl CellParser for World<LOC4> {
         next
     }
     fn actives(&self) -> usize {
-        let mut count = 0;
-        for t in -RANGE..=RANGE {
-            for z in -RANGE..=RANGE {
-                for i in -RANGE..=RANGE {
-                    for j in -RANGE..=RANGE {
-                        if let Some(true) = self.loc.get(&(t, z, i, j)) {
-                            count += 1;
-                        }
-                    }
-                }
-            }
-        }
-        count
+        self.loc.len()
     }
-}
-
-#[aoc(2020, 17)]
-impl<DIM: Debug + Default + Eq + Hash + PartialEq> AdventOfCode for World<DIM>
-where
-    World<DIM>: CellParser,
-{
-    const DELIMITER: &'static str = "\n";
-    fn insert(&mut self, _block: &str) -> Result<(), ParseError> {
-        Ok(())
-    }
-    fn parse(config: ConfigAoC) -> Result<Self, ParseError> {
-        Ok(Self::default().parse_(&Self::load(config)?))
-    }
-    fn part1(&mut self) -> usize {
-        self.next().next().next().next().next().next().actives()
-    }
-    fn part2(&mut self) -> usize {
-        self.next().next().next().next().next().next().actives()
-    }
-}
-
-impl<DIM: Default + Eq + Hash + PartialEq> World<DIM>
-where
-    World<DIM>: CellParser,
-{
-    fn get_entry(&mut self, l: DIM) -> &mut bool {
-        self.loc.entry(l).or_insert(false)
-    }
-    /*
-    fn print(&mut self) {
-        const R: isize = 5;
-        // for z in -R..=R {
-        for z in -1..=1 {
-            for i in -R..=R {
-                for j in -R..=R {
-                    if *self.get_entry((z, i, j)) {
-                        print!("#");
-                    // print!("{:?}, ", (z, i, j));
-                    } else {
-                        print!(".");
-                    }
-                }
-                println!();
-            }
-            println!();
-        }
-    }
-     */
 }
