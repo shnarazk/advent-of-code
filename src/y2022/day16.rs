e4cd8ff035cff30250426578c3bbf04f147169df
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-01-08 17:00:51 +0900

    y2022 day16: implementation improvement

--- src/y2022/day16.rs
+++ src/y2022/day16.rs
@@ -1,5 +1,4 @@
 //! <https://adventofcode.com/2022/day/16>
-
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
@@ -7,15 +6,16 @@ use {
     },
     std::{
         cmp::Reverse,
-        collections::{BinaryHeap, HashMap, HashSet},
+        collections::{hash_map::Entry, BinaryHeap, HashMap, HashSet},
     },
 };
 
 #[derive(Debug, Default, Eq, PartialEq)]
 pub struct Puzzle {
     line: Vec<(String, usize, Vec<String>)>,
-    map: HashMap<String, (usize, Vec<String>)>,
-    distance: HashMap<(String, String), usize>,
+    map: HashMap<usize, (usize, Vec<usize>)>,
+    distance: HashMap<(usize, usize), usize>,
+    label_id: HashMap<String, usize>,
 }
 
 #[aoc(2022, 16)]
@@ -36,24 +36,49 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn end_of_data(&mut self) {
-        for (label, flow, linked) in self.line.iter() {
-            self.map.insert(label.clone(), (*flow, linked.clone()));
+        let mut id = 0;
+        for (label, _flow, linked) in self.line.iter() {
+            if let Entry::Vacant(e) = self.label_id.entry(label.clone()) {
+                e.insert(id);
+                id += 1;
+            }
+            for dest in linked.iter() {
+                if let Entry::Vacant(e) = self.label_id.entry(dest.clone()) {
+                    e.insert(id);
+                    id += 1;
+                }
+            }
         }
-        self.initialize_distacnes();
+        for (label, flow, linked) in self.line.iter() {
+            self.map.insert(
+                *self.label_id.get(label).unwrap(),
+                (
+                    *flow,
+                    linked
+                        .iter()
+                        .map(|l| *self.label_id.get(l).unwrap())
+                        .collect::<Vec<_>>(),
+                ),
+            );
+        }
+
+        self.initialize_distances();
     }
     fn part1(&mut self) -> Self::Output1 {
+        let aa = *self.label_id.get("AA").unwrap();
         let init = State {
-            path: vec!["AA".to_string()],
+            path: vec![aa],
             // contribution: vec![(0, 0)],
             ..Default::default()
         };
         self.traverse(init)
     }
     fn part2(&mut self) -> Self::Output2 {
+        let aa = *self.label_id.get("AA").unwrap();
         let init = State2 {
-            path: vec!["AA".to_string()],
-            state1: (0, "AA".to_string()),
-            state2: (0, "AA".to_string()),
+            path: vec![aa],
+            state1: (0, aa),
+            state2: (0, aa),
             ..Default::default()
         };
         let mut best: usize = 0;
@@ -65,7 +90,7 @@ impl AdventOfCode for Puzzle {
 
 #[derive(Debug, Default, Eq, PartialEq)]
 struct State {
-    path: Vec<String>,
+    path: Vec<usize>,
     time: usize,
     total_flow: usize,
     // contribution: Vec<(usize, usize)>,
@@ -74,39 +99,38 @@ struct State {
 #[derive(Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd)]
 struct State2 {
     total_flow: usize,
-    path: Vec<String>,
-    state1: (usize, String),
-    state2: (usize, String),
+    path: Vec<usize>,
+    state1: (usize, usize),
+    state2: (usize, usize),
 }
 
 impl Puzzle {
-    fn initialize_distacnes(&mut self) {
-        let labels = self.map.keys().cloned().collect::<Vec<_>>();
-        for l in labels.iter() {
-            self.initialize_distances_from(l.to_string());
+    fn initialize_distances(&mut self) {
+        for i in 0..self.label_id.len() {
+            self.initialize_distances_from(i);
         }
     }
-    fn initialize_distances_from(&mut self, label: String) {
+    fn initialize_distances_from(&mut self, label: usize) {
         #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-        struct State<'a> {
+        struct State {
             time: usize,
-            loc: &'a String,
+            loc: usize,
         }
-        self.distance.insert((label.clone(), label.clone()), 0);
+        self.distance.insert((label, label), 0);
         let mut to_visit = BinaryHeap::new();
         to_visit.push(Reverse(State {
-            loc: &label,
             time: 0,
+            loc: label,
         }));
-        let mut visited: HashSet<&String> = HashSet::new();
-        visited.insert(&label);
+        let mut visited: HashSet<usize> = HashSet::new();
+        visited.insert(label);
         while let Some(Reverse(state)) = to_visit.pop() {
-            for next in self.map.get(state.loc).unwrap().1.iter() {
-                let key = (label.clone(), next.clone());
+            for next in self.map.get(&state.loc).unwrap().1.iter() {
+                let key = (label, *next);
                 if let std::collections::hash_map::Entry::Vacant(e) = self.distance.entry(key) {
                     e.insert(state.time + 1);
                     to_visit.push(Reverse(State {
-                        loc: next,
+                        loc: *next,
                         time: state.time + 1,
                     }));
                 }
@@ -134,7 +158,7 @@ impl Puzzle {
             }
             let total_flow = state.total_flow + (REMAIN - time) * flow;
             let mut path = state.path.clone();
-            path.push(next.clone());
+            path.push(*next);
             // let mut contribution = state.contribution.clone();
             // contribution.push((time, self.map.get(next).unwrap().0));
             best = self
@@ -149,7 +173,7 @@ impl Puzzle {
         best
     }
     // calculate the expected reward in a cheap way
-    fn reward_bound(&self, remain: usize, path: &[String]) -> usize {
+    fn reward_bound(&self, remain: usize, path: &[usize]) -> usize {
         self.map
             .iter()
             .filter(|(l, _)| !path.contains(l))
@@ -173,13 +197,13 @@ impl Puzzle {
             let mut next = state.clone();
             let working = (&mut next.state1).min(&mut next.state2);
             working.0 += *dist + 1;
-            working.1 = label.clone();
+            working.1 = *label;
             let flow = self.map.get(label).unwrap().0;
             if DURATION <= working.0 || flow == 0 {
                 continue;
             }
             next.total_flow += (DURATION - working.0) * flow;
-            next.path.push(label.clone());
+            next.path.push(*label);
             if next.total_flow
                 + self.reward_bound(DURATION - next.state1.0.max(next.state2.0) - 1, &next.path)
                 < *best
