066c006b0029a689f1d727b4bd8de7153df83732
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-01-13 13:23:15 +0900

    y2018 day05: better idea

--- src/y2018/day05.rs
+++ src/y2018/day05.rs
@@ -1,110 +1,50 @@
 //! <https://adventofcode.com/2018/day/5>
-use {
-    crate::framework::{aoc, AdventOfCode, ParseError},
-    std::collections::HashSet,
-};
+use crate::framework::{aoc, AdventOfCode, ParseError};
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<u8>,
+    line: Vec<isize>,
 }
 
 #[aoc(2018, 5)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        self.line = block.chars().map(|c| c as u8).collect::<Vec<u8>>();
+        self.line = block
+            .chars()
+            .map(|c| {
+                if c.is_ascii_uppercase() {
+                    (c as u8 as isize) - (b'A' as isize) + 1
+                } else if c.is_ascii_lowercase() {
+                    (b'a' as isize) - (c as u8 as isize) - 1
+                } else {
+                    panic!();
+                }
+            })
+            .collect::<Vec<isize>>();
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
-        let dist = b'a' - b'A';
-        let mut updated = true;
-        while updated {
-            updated = false;
-            let mut index = 0;
-            for i in 0..self.line.len() - 1 {
-                if self.line[i] + dist == self.line[i + 1]
-                    || self.line[i] == self.line[i + 1] + dist
-                {
-                    index = i;
-                    updated = true;
-                    break;
-                }
-            }
-            if updated {
-                self.line.remove(index);
-                self.line.remove(index);
-                // dbg!(self.line.iter().map(|c| *c as char).collect::<String>());
-            }
-        }
-        self.line.len()
+        self.len(0)
     }
     fn part2(&mut self) -> Self::Output2 {
-        let dist = b'a' - b'A';
-        let mut units: HashSet<u8> = HashSet::new();
-        for c in self.line.iter().filter(|c| b'A' <= **c && **c <= b'Z') {
-            units.insert(*c);
-        }
-        let mut len = usize::MAX;
-        for unit in units.iter() {
-            let v = self
-                .line
-                .iter()
-                .filter(|c| **c != *unit && **c != *unit + dist)
-                .copied()
-                .collect::<Vec<_>>();
-            len = len.min(shrinkable(v));
-        }
-        len
+        (1..=26).map(|ex| self.len(ex)).min().unwrap()
     }
 }
 
-fn shrinkable(mut p: Vec<u8>) -> usize {
-    let dist = b'a' - b'A';
-    let mut updated = true;
-    while updated {
-        updated = false;
-        let mut index = 0;
-        for i in 0..p.len() - 1 {
-            if p[i] + dist == p[i + 1] || p[i] == p[i + 1] + dist {
-                index = i;
-                updated = true;
-                break;
+impl Puzzle {
+    fn len(&self, ex: isize) -> usize {
+        let mut result = Vec::new();
+        for c in self.line.iter() {
+            if c.abs() == ex {
+                continue;
             }
-        }
-        if updated {
-            p.remove(index);
-            p.remove(index);
-        }
-    }
-    p.len()
-}
-
-#[allow(dead_code)]
-fn shrinkable2(p: &[u8]) -> usize {
-    let dist = b'a' - b'A';
-    let mut count = 0;
-    let mut pre_index = 0;
-    let mut pre_char = b' ';
-    let mut index = 0;
-    while let Some(c) = p.get(index) {
-        if *c + dist == pre_char || *c == pre_char + dist {
-            if 0 < pre_index {
-                // TODO: This is imcomplete. we need to make 'holes' to skip already
-                // shrunk letters, then skip them.
-                // while p[pre_index] == b' ' { if 0 < pre_index { pre_index -= 1; } else {...}  }
-                pre_index -= 1;
-                pre_char = p[pre_index];
+            if result.last().map_or(false, |l| *l + *c == 0) {
+                result.pop();
             } else {
-                pre_index = index;
-                pre_char = b' ';
+                result.push(*c);
             }
-        } else {
-            count += 1;
-            pre_index = index;
-            pre_char = *c;
         }
-        index += 1;
+        result.len()
     }
-    count
 }
