1b5b98a6287b34c8f1036d6eab3e3414f66daf53
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-29 22:57:44 +0900

    (lean4) check elapsed time

--- lean/Main.lean
+++ lean/Main.lean
@@ -1,4 +1,5 @@
 import «AoC»
+import Aesop
 import Std.Data.String
 import Std.Data.List
 import Std.Data.Nat
@@ -6,18 +7,23 @@ import Std.Data.Nat
 def readData (datafilename : String) : IO (Array String) := do
      IO.FS.lines datafilename
 
-def solve1 (s : String) : Nat :=
+def solve1_line (s : String) : Nat :=
   (sk.head!.toNat - '0'.toNat) * 10 + (sk.getLast!.toNat - '0'.toNat)
   where
     sk := s.toList.filter fun c => c.isDigit
 
-#eval solve1 "aa2aa3"
+#eval solve1_line "aa2aa3"
+
+def solve1 : IO Unit := do
+  let lines ← readData "../data/2023/input-day01.txt"
+  -- IO.println s!" => {lines}"
+  let nums := lines.map solve1_line
+  -- IO.println s!" => {nums}"
+  let sum := Array.foldl (. + .) 0 nums
+  IO.println s!" part1: {sum}"
+  return ()
 
 def main : IO Unit := do
   IO.println s!"Hello, {hello}!"
-  let lines ← readData "../data/2023/input-day01.txt"
-  let output := lines.map solve1
-  -- IO.println s!" => {lines}"
-  IO.println s!" => {output}"
-  let sum := Array.foldl (. + .) 0 output
-  IO.println s!" => {sum}"
+  let result ← Aesop.time' solve1
+  IO.println s!" => {result.printAsMillis}"
