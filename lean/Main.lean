2199555fd4fac35dabbc57a5c3c2871ddd8ae30f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-29 22:06:21 +0900

    (lean) y2023 day01 part1 done.

--- lean/Main.lean
+++ lean/Main.lean
@@ -1,9 +1,23 @@
 import «AoC»
+import Std.Data.String
+import Std.Data.List
+import Std.Data.Nat
 
 def readData (datafilename : String) : IO (Array String) := do
      IO.FS.lines datafilename
 
+def solve1 (s : String) : Nat :=
+  (sk.head!.toNat - '0'.toNat) * 10 + (sk.getLast!.toNat - '0'.toNat)
+  where
+    sk := s.toList.filter fun c => c.isDigit
+
+#eval solve1 "aa2aa3"
+
 def main : IO Unit := do
   IO.println s!"Hello, {hello}!"
   let lines ← readData "../data/2023/input-day01.txt"
-  IO.println s!" => {lines}"
+  let output := lines.map solve1
+  -- IO.println s!" => {lines}"
+  IO.println s!" => {output}"
+  let sum := Array.foldl (. + .) 0 output
+  IO.println s!" => {sum}"
