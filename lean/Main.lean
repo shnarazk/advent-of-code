bbea91e599849f688e2774c4970e6c0e178c3a46
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-02-29 23:19:41 +0900

    better monad work

--- lean/Main.lean
+++ lean/Main.lean
@@ -14,16 +14,16 @@ def solve1_line (s : String) : Nat :=
 
 #eval solve1_line "aa2aa3"
 
-def solve1 : IO Unit := do
-  let lines ← readData "../data/2023/input-day01.txt"
-  -- IO.println s!" => {lines}"
+def solve1 (lines : Array String): IO Unit := do
   let nums := lines.map solve1_line
-  -- IO.println s!" => {nums}"
   let sum := Array.foldl (. + .) 0 nums
   IO.println s!" part1: {sum}"
   return ()
 
 def main : IO Unit := do
   IO.println s!"Hello, {hello}!"
-  let result ← Aesop.time' solve1
+  let lines ← readData "../data/2023/input-day01.txt"
+  -- let solver1 := bind (pure lines) solve1
+  let solver1 := pure lines >>= solve1
+  let result ← Aesop.time' solver1
   IO.println s!" => {result.printAsMillis}"
