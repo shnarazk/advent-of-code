c54ba1e6f0f0d50243b752750b16c4744ff1c51c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-01 14:49:40 +0900

    (lean) implementing y2023 day2

--- lean/AoC/Day02.lean
+++ lean/AoC/Day02.lean
@@ -3,15 +3,34 @@ import «AoC».Basic
 
 namespace Day02
 
-def collect (_line : String) : Std.HashMap String Nat :=
-  Std.HashMap.insert hash "red" 3 
-  where 
-    hash : Std.HashMap String Nat := Std.HashMap.empty
+def toHashMap (source : String) /- : Std.HashMap String Nat -/ :=
+  List.foldl
+    (fun hash items =>
+      List.foldl
+        (fun hash assoc =>
+          let (l, n) := association assoc
+          hash.insert l n)
+        hash
+        items
+    )
+    Std.HashMap.empty
+    items_in_bags
+  where
+    body := List.getD (String.split source (. == ':')) 1 ""
+    turns := String.split body (. == ';')
+    items_in_bags := turns.map (fun b => String.split b (. == ','))
+    association := fun s =>
+      let pair := String.split s.trim (. == ' ')
+      let value := (List.getD pair 0 "no num").trim.toInt!
+      let label := (List.getD pair 1 "no name").trim
+      (label, value)
+
+#eval (toHashMap "case 1: 3 red; 5 blue").findEntry? "blue" = some ("blue", 5)
 
 def solve1_line (line : String) : Nat :=
-  0
+  Std.HashMap.size hash
   where
-    hash := collect line
+    hash := toHashMap line
 
 def solve1 (lines : Array String) : IO Unit := do
   let points : Array Nat := Array.map solve1_line lines
@@ -20,7 +39,7 @@ def solve1 (lines : Array String) : IO Unit := do
   return ()
 
 def solve2 (lines : Array String) : IO Unit := do
-  IO.println s!" part1: not yet implemented {lines.size}"
+  IO.println s!" part2: not yet implemented {lines.size}"
   return ()
 
 end Day02
