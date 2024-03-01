2cb9a528faa914a7a32aa20b46b231c5afc91f60
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-01 20:52:12 +0900

    ditto

--- lean/AoC/Day04.lean
+++ lean/AoC/Day04.lean
@@ -3,6 +3,20 @@ import «AoC».Basic
 
 namespace Day04
 
+def parsed (source : String) : List Nat × List Nat :=
+  (toNats targets, toNats cands)
+  where
+    body := List.getD (String.split source (. == ':')) 1 ""
+    parts := String.split body (. == '|')
+    targets := List.getD parts 0 "no num"
+    cands := List.getD parts 1 "no name"
+    toNats := fun (s : String) =>
+      let pair := String.split s.trim (. == ' ')
+      let numbers := List.filter (fun s => s != "") pair
+      List.map String.toNat! numbers
+
+#eval parsed "Card 1: 41 48 | 83  1 41"
+
 def solve1_line (_line : String) : Nat := 0
 
 def solve1 (lines : Array String) : IO Unit := do
