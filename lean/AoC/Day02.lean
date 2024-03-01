37bca285e09051adf3760ec4073371df951ea5e0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-01 15:51:47 +0900

    (lean) y2023 day2 done.

--- lean/AoC/Day02.lean
+++ lean/AoC/Day02.lean
@@ -18,13 +18,13 @@ def toHashMap (source : String) /- : Std.HashMap String Nat -/ :=
     (Std.HashMap.empty.insert "«id»" id)
     items_in_bags
   where
-    id := ((List.getD (String.split source (. == ':')) 0 "-1").drop 5).trim.toInt!
+    id := ((List.getD (String.split source (. == ':')) 0 "-1").drop 5).trim.toNat!
     body := List.getD (String.split source (. == ':')) 1 ""
     turns := String.split body (. == ';')
     items_in_bags := turns.map (fun b => String.split b (. == ','))
     association := fun s =>
       let pair := String.split s.trim (. == ' ')
-      let value := (List.getD pair 0 "no num").trim.toInt!
+      let value := (List.getD pair 0 "no num").trim.toNat!
       let label := (List.getD pair 1 "no name").trim
       (label, value)
 
@@ -34,7 +34,7 @@ def solve1_line (line : String) : Nat :=
   if   Std.HashMap.findD hash "red" 0   <= 12
     && Std.HashMap.findD hash "green" 0 <= 13
     && Std.HashMap.findD hash "blue" 0  <= 14
-  then (Std.HashMap.find! hash "«id»").toNat
+  then Std.HashMap.find! hash "«id»"
   else 0
   where
     hash := toHashMap line
@@ -45,8 +45,17 @@ def solve1 (lines : Array String) : IO Unit := do
   IO.println s!" part1: {sum}"
   return ()
 
+def solve2_line (line : String) : Nat :=
+  (Std.HashMap.findD hash "red" 0)
+  * (Std.HashMap.findD hash "green" 0)
+  * (Std.HashMap.findD hash "blue" 0)
+  where
+    hash := toHashMap line
+
 def solve2 (lines : Array String) : IO Unit := do
-  IO.println s!" part2: not yet implemented {lines.size}"
+  let points : Array Nat := Array.map solve2_line lines
+  let sum := Array.foldl (. + .) 0 points
+  IO.println s!" part1: {sum}"
   return ()
 
 end Day02
