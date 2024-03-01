1287aae1112e4caf3c630b3e79c8f67b97434ec7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-02 00:40:13 +0900

    (lean) new file:   template.lean

---
+++ lean/template.lean
@@ -0,0 +1,33 @@
+import Std
+import «AoC».Basic
+
+namespace Day0?
+
+def parsed (source : String) : sorry := sorry
+
+def solve1_line (line : String) : Nat :=
+  sorry
+
+#eval solve1_line ""
+
+def solve1 (lines : Array String) : IO Unit := do
+  let points : Array Nat := Array.map solve1_line lines
+  let sum := Array.foldl (. + .) 0 points
+  IO.println s!" part1: {sum}"
+  return ()
+
+def solve2_line (line : String) : Nat :=
+  sorry
+
+def solve2 (lines : Array String) : IO Unit := do
+  let points : Array Nat := Array.map solve2_line lines
+  let sum := Array.foldl (. + .) 0 points
+  IO.println s!" part2: {sum}"
+  return ()
+
+end Day0?
+
+def day0? : IO Unit := do
+  let data ← dataFor 2023 ?
+  pure data >>= Day0?.solve1
+  pure data >>= Day0?.solve2
