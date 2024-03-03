1407822bb3efb7d496f4d76681dad9e6b1038987
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-03 15:55:29 +0900

    (lean) y2023 day7 part1 done

--- lean/AoC/Day07.lean
+++ lean/AoC/Day07.lean
@@ -37,25 +37,66 @@ def occurences (cs : Array Char) : Array Nat :=
 
 #eval occurences #['A', '3', '9', 'A', 'A']
 
-def evaluation (h : Hand) : Nat × Nat :=
-  match occurences h.cards with
-  | #[5]          => (7, h.bid) -- Five of a kind
-  | #[4, 1]       => (6, h.bid) -- Four of a kind
-  | #[3, 2]       => (5, h.bid) -- Full house
-  | #[3, 1, 1]    => (4, h.bid) -- Three of a kind
-  | #[2, 2, 1]    => (3, h.bid) -- Two pair
-  | #[2, 1, 1, 1] => (2, h.bid) -- One pair
-  | _             => (1, h.bid) -- High card
+/--
+larger is higher
+-/
+def order : Char -> Nat
+  | 'A' => 14
+  | 'K' => 13
+  | 'Q' => 12
+  | 'J' => 11
+  | 'T' => 10
+  | '9' => 9
+  | '8' => 8
+  | '7' => 7
+  | '6' => 6
+  | '5' => 5
+  | '4' => 4
+  | '3' => 3
+  | '2' => 2
+  | _ => 0
+
+#eval order 'A'
+
+def orderOf (h : Hand) : Array Nat :=
+  Array.map order h.cards
+
+def evaluation (h : Hand) : (Nat × List Nat) × Nat :=
+  let ord := orderOf h |> Array.toList
+    match occurences h.cards with
+  | #[5]          => ((7, ord), h.bid) -- Five of a kind
+  | #[4, 1]       => ((6, ord), h.bid) -- Four of a kind
+  | #[3, 2]       => ((5, ord), h.bid) -- Full house
+  | #[3, 1, 1]    => ((4, ord), h.bid) -- Three of a kind
+  | #[2, 2, 1]    => ((3, ord), h.bid) -- Two pair
+  | #[2, 1, 1, 1] => ((2, ord), h.bid) -- One pair
+  | _             => ((1, ord), h.bid) -- High card
 
 #eval evaluation $ Hand.hand #['A', '3', '9', 'A', 'A'] 33
+
+def compareList (a b : List Nat) : Ordering :=
+  match a, b with
+  | [], []             => .eq
+  | [], _              => .lt
+  | _, []              => .lt
+  | a₀ :: a', b₀ :: b' =>
+    if a₀ == b₀
+    then compareList a' b'
+    else compare a₀ b₀
+
+#eval compareList [2, 4] [2, 4] == Ordering.eq
+#eval compareList [2, 3, 1] [2, 1] == Ordering.gt
 
 def solve1 (data : String) : IO Unit := do
   match AoCParser.parse parser.parser data with
   | none   => IO.println s!"  part1: parse error"
   | some d =>
-    let test := d[0]!
-    IO.println s!"{evaluation test}"
-    IO.println s!"  part1: {d.size}"
+    let o := Array.qsort (d.map evaluation) (fun a b =>
+      match compare a.fst.fst b.fst.fst, compareList a.fst.snd b.fst.snd with
+      | Ordering.eq, ord => ord == Ordering.lt
+      | ord, _ => ord == Ordering.lt)
+    let winnings := Array.foldl (fun acc r => (acc.fst + acc.snd * r.snd, acc.snd + 1)) (0, 1) o
+    IO.println s!"  part1: {winnings.fst}"
   return ()
 
 def solve2_line (_line : String) : Nat := 0
