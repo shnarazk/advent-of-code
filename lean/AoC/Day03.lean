5a4a659bcf576090ec96afa6aa9acb1734d8161b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-04 11:16:44 +0900

    (lean) y2023 day3 part2 done

--- lean/AoC/Day03.lean
+++ lean/AoC/Day03.lean
@@ -1,26 +1,65 @@
 import Std
 import «AoC».Basic
+import Mathlib.Data.List.BigOperators.Defs
 
 namespace Day03
 
-def asterisks (lines : Array String) : List (Nat × Nat) :=
+structure Number where
+  new ::
+  row : Int
+  beg : Int
+  en  : Int
+  val : Nat
+deriving Ord, Repr
+
+instance : ToString Number where
+  toString n := String.join [toString n.val, toString (n.row, n.beg, n.en)]
+
+def asterisks (lines : Array String) : List (Int × Int) :=
   lines.foldl (fun (i, l) line =>
       (i + 1, line.foldl (fun (j, l) ch =>
-          (j + 1, if ch == '.' || ch.isDigit then l else l ++ [(i, j)]))
+          (j + 1, if ch == '*' then l ++ [(i, j)] else l))
         (0, l)
       |>.snd))
-    ((0, []) : Nat × List (Nat × Nat))
+    ((0, []) : Int × List (Int × Int))
   |>.snd
 
-def solve1 (lines : Array String) : IO Unit := do
-  let points := dbgTraceVal $ asterisks lines
-  let sum := points.length -- Array.foldl (. + .) 0 points
-  IO.println s!"  part1: {sum}"
+def numbers_in_line (line : String) : List (Int × Int × Nat) :=
+  (line ++ ".").foldl
+    (fun (result, index, tmp) ch =>
+      match ch.isDigit, tmp with
+      | true, none => (result, index + 1, some (index-1, ch.toNat - '0'.toNat))
+      | true, some (s, v) => (result, index + 1, some (s, v * 10 + ch.toNat - '0'.toNat))
+      | false, some (s, v) => (result++[(s, index, v)], index + 1, none)
+      | _, _ => (result, index + 1, tmp)
+    )
+    (([] : List (Int × Int × Nat)), 0, (none : Option (Int × Nat)))
+  |>.fst
+
+#eval numbers_in_line "467..114.."
+
+def numbers (lines : Array String) : List Number :=
+  lines.foldl (fun (row, result) line =>
+      line |> numbers_in_line |>.map (fun n => Number.new row n.fst n.snd.fst n.snd.snd) |> (row + 1, result ++ .))
+    ((0, []) : Int × List Number)
+  |>.snd
+
+#eval (3 : Int).natAbs
+
+def solve2 (lines : Array String) : IO Unit := do
+  let cands := asterisks lines
+  let numbers := numbers lines
+  let gears := cands.foldl
+    (fun acc (y₀, x₀) =>
+      let neighbors := numbers.filter (fun num => (num.row - y₀).natAbs ≤ 1 && num.beg ≤ x₀ && x₀ ≤ num.en)
+      if neighbors.length == 2 then (neighbors.map (·.val) |>.foldl (fun a x => a * x) 1 |> (acc ++ [·])) else acc)
+    ([] : List Nat)
+  IO.println s!"  part1: {gears.sum}"
   return ()
 
 def solve2_line (_line : String) : Nat := 0
 
-def solve2 (lines : Array String) : IO Unit := do
+def solve1 (lines : Array String) : IO Unit := do
   let points : Array Nat := Array.map solve2_line lines
   let sum := Array.foldl (. + .) 0 points
   IO.println s!"  part2: {sum}"
