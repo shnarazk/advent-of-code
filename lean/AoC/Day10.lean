bc3463a614b627f33a88104566914f15b86182e3
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-06 20:15:02 +0900

    modified:   AoC/Day10.lean

--- lean/AoC/Day10.lean
+++ lean/AoC/Day10.lean
@@ -1,47 +1,74 @@
 import Std
 import Lean.Data.Parsec
-import Mathlib.Data.Matrix.Basic
 import «AoC».Basic
 import «AoC».Parser
 
 namespace Day10
 open Std
 
-structure Data where
-  new ::
-  grid : Array (Array Char)
-deriving Repr
-
--- def Data.of (self : Data) (y : Nat) (x : Nat) : Char := (self.grid[y]!)[x]!
-def Data.at (self : Data) : Matrix Nat Nat Char := fun y x => (self.grid[y]!)[x]!
-
-#eval (Data.new #[#['0', '1'], #['2', '3']]).at 0 0
-#eval (Data.new #[#['0', '1'], #['2', '3']]).at 1 1
-
 inductive Circuit where
+  | v : Circuit
   | h : Circuit
   | l : Circuit
   | j : Circuit
   | k : Circuit
   | f : Circuit
   | S : Circuit
+  | x : Circuit
+deriving BEq, Repr
 
-def Circuit.dest (c : Circuit) (pre : Nat × Nat) (pos : Nat × Nat) : (Nat × Nat) × (Nat × Nat) :=
+instance : ToString Circuit where
+  toString s :=
+  match s with
+  | .v => "|"
+  | .h => "-"
+  | .l => "L" 
+  | .j => "J" 
+  | .k => "7" 
+  | .f => "F" 
+  | .S => "S" 
+  | _  => " "
+    
+def Circuit.ofChar (c : Char) : Circuit :=
+  match c with
+  | '|' => .v
+  | '-' => .h
+  | 'L' => .l
+  | 'J' => .j
+  | '7' => .k
+  | 'F' => .f
+  | 'S' => .S
+  |  _  => .x
+
+#eval (Circuit.ofChar 'f') |> toString
+
+structure Data where
+  new ::
+  grid : Array (Array Circuit)
+deriving BEq, Repr
+
+#eval (none : Option Nat).map (fun _ => 3)
+
+def Data.at (self : Data) (pos : Nat × Nat) : Option Circuit :=
+  (self.grid[pos.fst]?) >>= (·[pos.snd]?)
+
+partial def Data.dest (c : Data) (pre : Nat × Nat) (pos : Nat × Nat) : (Nat × Nat) × (Nat × Nat) :=
   let dy := pos.fst - pre.fst
   let dx := pos.snd - pre.snd
-  match c with
-  | .h /- - -/ => (pos, (pos.fst      , pos.snd + dx))
-  | .l /- L -/ => (pos, (pos.fst + dy , pos.snd     ))
-  | .j /- J -/ => (pos, (pos.fst + dx , pos.snd     ))
-  | .k /- 7 -/ => (pos, (pos.fst      , pos.snd     ))
-  | .f /- F -/ => (pos, (pos.fst      , pos.snd     ))
-  | .S /- . -/ => (pos, pos)
+  match c.at pos with
+  | some .v /- - -/ => (pos, (pos.fst + dy , pos.snd     ))
+  | some .h /- - -/ => (pos, (pos.fst      , pos.snd + dx))
+  | some .l /- L -/ => (pos, (pos.fst      , pos.snd + dy))
+  | some .j /- J -/ => (pos, (pos.fst - dx , pos.snd     ))
+  | some .k /- 7 -/ => (pos, (pos.fst + dx , pos.snd     ))
+  | some .f /- F -/ => (pos, (pos.fst      , pos.snd - dy))
+  | _ /- . -/ => (pos, pos)
 
 #eval #['a', 'b'].toList.toString
 #eval #["aa", "bb"].foldl (fun x y => x ++ y) ""
 
  instance : ToString Data where
-   toString self := self.grid.map (· |>.foldl String.push ""|>.append "\n")
+   toString self := self.grid.map (·|>.map toString |>.foldl String.append ""|>.append "\n")
       |>.foldl String.append ""
 
 namespace parser
@@ -56,9 +83,9 @@ def cell := pchar '|'
     <|> pchar '.'
     <|> pchar 'S'
 
-def parser := do
-  let cs ← many (many cell <* eol)
-  return (Data.new cs)
+def pcircuit := (return Circuit.ofChar) <*> cell
+
+def parser := (return Data.new) <*>many (many pcircuit <* eol)
 
 end parser
 
