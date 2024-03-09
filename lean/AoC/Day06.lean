7ed4644fe9133c9975c27684466990169228f80f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-09 13:20:19 +0900

    revise day6 to use binary search and namespaces

--- lean/AoC/Day06.lean
+++ lean/AoC/Day06.lean
@@ -2,7 +2,6 @@ import Std
 import Lean.Data.Parsec
 import «AoC».Basic
 import «AoC».Parser
--- import Mathlib.Data.List.BigOperators.Defs
 
 namespace Day06
 
@@ -11,6 +10,29 @@ structure Race where
   time : Nat
   dist : Nat
 deriving Repr
+
+namespace Race
+
+private def qualify (r : Race) (t : Nat) : Ordering := compare ((r.time - t) * t) r.dist
+
+private def absDiff (i j : Nat) : Nat := max (i - j) (j - i)
+
+private def seek (r : Race) (span : Nat) (i j : Nat) (inc : Bool) : Nat :=
+  if span = 0 then if inc then j else i
+  else
+    let m := (i + j) / 2
+    match qualify r m with
+    | Ordering.lt => if inc then seek r (span / 2) m j inc else seek r (span / 2) i m inc
+    | Ordering.eq => m
+    | Ordering.gt => if inc then seek r (span / 2) i m inc else seek r (span / 2) m j inc
+
+def evaluate (r : Race) : Nat :=
+  let m := r.time / 2
+  let i := seek r m 0 m true
+  let j := seek r m m r.time false
+  j - i + 1
+
+end Race
 
 namespace parser
 open Lean Parsec AoCParser
@@ -37,32 +59,32 @@ def parse (data : String) :=
 
 end parser
 
-def evaluate₁ (race : Race) : Nat :=
-  let ts := List.iota race.time
-  (ts.filter (fun t => race.dist ≤ (race.time - t) * t)).length
+namespace Part1
 
-def solve1 (data : String) : IO Unit := do
+def solve (data : String) : IO Unit := do
   match parser.parse data with
   | some races =>
-    -- let _ ← List.mapM (fun r => IO.println s!"{r.time}, {r.dist}") races
-    let vars := List.map evaluate₁ races
-    IO.println s!"  part1: {vars.foldl Nat.mul 0}"
+    let vars := List.map Race.evaluate races
+    IO.println s!"  part1: {vars.foldl Nat.mul 1}"
   | _ =>
     IO.println s!"  part1: parse error"
   return ()
 
-def solve2_line (_line : String) : Nat :=
-  0
+end Part1
 
-def solve2 (data : String) : IO Unit := do
+namespace Part2
+
+def solve (data : String) : IO Unit := do
   let x := (data.split (. == '\n')).map (fun l =>
     List.foldl (fun n d => n * 10 + d.toNat - '0'.toNat) 0 (l.toList.filter Char.isDigit))
-  let res := evaluate₁ ({ time := x.get! 0, dist := x.get! 1} : Race)
-  IO.println s!"  part2: {res}"
+  let r := Race.race (x.get! 0) (x.get! 1)
+  IO.println s!"  part2: {r.evaluate}"
+
+end Part2
 
 end Day06
 
 def day06 (ext : Option String): IO Unit := do
   let data ← dataOf 2023 6 ext
-  Day06.solve1 data
-  Day06.solve2 data
+  Day06.Part1.solve data
+  Day06.Part2.solve data
