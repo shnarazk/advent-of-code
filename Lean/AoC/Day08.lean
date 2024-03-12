5020326a91bddfd19bb1f7ef67a80fed43584226
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-13 00:04:49 +0900

    ditto

--- Lean/AoC/Day08.lean
+++ Lean/AoC/Day08.lean
@@ -31,30 +31,36 @@ def parser := do
 
 end parser
 
-partial def trace₁ : Puzzle → Nat → String → Nat
-  | _     , step, "ZZZ" => step
-  | puzzle, step, pos   =>
+def trace₁ : Puzzle → Nat → Nat → String → Nat
+  | _     ,      _,  step, "ZZZ" => step
+  | _     ,       0, step,     _ => step
+  | puzzle, lim + 1, step,   pos =>
     let (left, right) := puzzle.branches.find! pos
     let dir := puzzle.path[step % puzzle.path.length]!
-    trace₁ puzzle (step + 1) <| if dir == 'L' then left else right
+    trace₁ puzzle lim (step + 1) <| if dir == 'L' then left else right
 
 def solve1 (data : String) : IO Unit := do
   if let some p := AoCParser.parse parser.parser data then
-    IO.println s!"  part1: {trace₁ p 0 "AAA"}"
+    let limit := Nat.lcm p.path.length p.branches.size
+    IO.println s!"  part1: {trace₁ p limit 0 "AAA"}"
 
-partial def trace₂ (puzzle : Puzzle) (step : Nat) (pos : String) : Nat :=
-  if pos.endsWith "Z"
-  then step
-  else
-    let (left, right) := puzzle.branches.find! pos
-    let dir := puzzle.path[step % puzzle.path.length]!
-    trace₂ puzzle (step + 1) <| if dir == 'L' then left else right
+def trace₂ (puzzle : Puzzle) (limit : Nat) (step : Nat) (pos : String) : Nat :=
+  match limit with
+  | 0 => 0
+  | lim + 1 =>
+    if pos.endsWith "Z"
+    then step
+    else
+      let (left, right) := puzzle.branches.find! pos
+      let dir := puzzle.path[step % puzzle.path.length]!
+      trace₂ puzzle lim (step + 1) <| if dir == 'L' then left else right
 
 -- #eval Nat.lcm 1 9
 
 def analyze (p : Puzzle) : Nat :=
+  let limit := Nat.lcm p.path.length p.branches.size
   p.branches.toList.filter (String.endsWith ·.fst "A")
-    |>.map (trace₂ p 0 ·.fst)
+    |>.map (trace₂ p limit 0 ·.fst)
     |>.foldl Nat.lcm 1
 
 def solve2 (data : String) : IO Unit := do
