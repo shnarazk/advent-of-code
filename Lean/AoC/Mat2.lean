7ce51d02ad4a58e3ff9ac3c1c3378748a74a7f95
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-18 09:42:55 +0900

    modified:   AoC/Mat2.lean

--- Lean/AoC/Mat2.lean
+++ Lean/AoC/Mat2.lean
@@ -8,23 +8,62 @@ instance [ToString α] [Inhabited α] : ToString (Mat1 α) where
   toString m := s!"{m.width}{toString m.vector}"
 
 namespace Mat1
+/--
+return a new instance of Mat1 of an array
+-/
 def new {α : Type} [Inhabited α] (vec : Array α) (w : Nat) : Mat1 α :=
-  ({ width := w, vector := vec } : Mat1 α)
+  ({width := w, vector := vec} : Mat1 α)
 
+/--
+return a new instacne of Mat1 of an 2D array
+-/
+def of2DMatrix {α : Type} [Inhabited α] (m : Array (Array α)) : Mat1 α :=
+  ({width := (m.getD 1 #[]).size, vector := m.foldl Array.append #[]} : Mat1 α)
+
+/--
+return the `(i,j)`-th element of Mat1 instance
+-/
 def get {α : Type} [Inhabited α] (mat : Mat1 α) (i j : Nat) : α :=
   let ix := i * mat.width + j
   if h : mat.vector.size > 0
   then mat.vector.get (Fin.ofNat' ix h)
   else (default : α)
 
-def x := new #[true, false, true, false] 2
+/--
+set the `(i,j)`-th element to `val` and return the modified Mat1 instance
+-/
+def set {α : Type} [Inhabited α] (mat : Mat1 α) (i j : Nat) (val : α) : Mat1 α :=
+  let ix := i * mat.width + j
+  if h : mat.vector.size > 0
+  then { mat with vector := mat.vector.set (Fin.ofNat' ix h) val}
+  else mat
 
-#eval x
-#check x
-#check get
+-- def x := new #[true, false, true, false] 2
+-- def y := of2DMatrix #[#[1,2,3], #[4,5,6]]
 
-def y := get x 0 0
-#eval y
+-- #eval x
+-- #check x
+-- #eval y
+-- #check y
+-- #check get
 
+-- #eval get x 0 0
+-- #eval get y 0 1
 
 end Mat1
+
+-- def x := Mat1.new #[true, false, true, false] 2
+-- def y := Mat1.of2DMatrix #[#[1,2,3], #[4,5,6]]
+
+-- #eval x
+-- #check x
+-- #eval y
+-- #check y
+-- #check get
+
+-- #eval x.get 0 0
+-- #eval y.get 0 1
+-- #eval y.get 100 100
+
+-- #eval x.set 0 0 false
+-- #eval y.set 1 1 10000
