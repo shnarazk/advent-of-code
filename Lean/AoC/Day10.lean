aaaf15a7aed3ca1ac8f253d86dd64f283c5e03d8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-03-22 17:02:26 +0900

    modified:   AoC/Day10.lean

--- Lean/AoC/Day10.lean
+++ Lean/AoC/Day10.lean
@@ -89,6 +89,16 @@ def destinationVector (mat : Mat1 Circuit) (vec : Pos × Pos) : Pos × Pos :=
   |       _ => (  0,   0)
   (now, both2 trans now diff)
 
+def findLoop (mat : Mat1 Circuit) (limit : Nat) (start : Pos) (path : List Pos) (v : Pos × Pos)
+    : List Pos :=
+  match limit with
+  | 0       => []
+  | lim + 1 =>
+    let v' := destinationVector mat v
+    if v'.fst == v'.snd
+    then if v'.snd == start then v'.fst :: path else []
+    else findLoop mat lim start (v'.fst :: path) v'
+
 namespace parser
 open Lean.Parsec AoCParser
 
@@ -107,40 +117,25 @@ def parser := (return Mat1.of2DMatrix) <*>many (many pcircuit <* eol)
 
 end parser
 
-namespace part1
-
-def loop_len (self : Mat1 Circuit) (limit : Nat) (start : Pos) (len : Nat) (vec : Pos × Pos) : Nat :=
-  match limit with
-  | 0        => 0
-  | lim' + 1 =>
-    let v' := destinationVector self vec
-    if v'.fst == v'.snd
-    then if v'.snd == start then len + 1 else 0
-    else loop_len self lim' start (len + 1) v'
-
-def solve (m : Mat1 Circuit) : Nat :=
-  makeVecs m.shape (startPosition m)
-    |>.map (loop_len m m.size (startPosition m) 0 .)
-    |>.maximum? |>.getD 0 |> (· / 2)
-
-end part1
+def Part1.solve (m : Mat1 Circuit) : Nat :=
+  let st := startPosition m
+  makeVecs m.shape st
+  |>.map (findLoop m m.size st [st] .)
+  |>.map List.length
+  |>.maximum?
+  |>.getD 0
+  |> (· / 2)
 
 structure Map where
   new ::
   size : Pos
   cells : Array Bool
 
-def mappingOf (size : Pos) (locs : List Pos) : Mat1 Bool :=
-  let s' := (max 1 size.fst, max 1 size.snd)
-  let noneZero : s'.fst * s'.snd > 0 := by simp
-  locs.foldl (fun mat pos => mat.set₂ pos true) (Mat1.new! s' noneZero false)
-
--- #eval mappingOf (2, 2) [(0,0), (1,1)]
-
 namespace part2
 open Lean Data
 
 /-!
+  We need a matrix of uncheked, block, open, encoded as none, some true, some false
   1. pick the looping route
   2. double the scale
   3. draw the loop
@@ -148,15 +143,15 @@ open Lean Data
   5. count the unmarked cells
 -/
 
-def mkLoop (mat : Mat1 Circuit) (limit : Nat) (start : Pos) (path : List Pos) (vec : Pos × Pos)
-    : List Pos :=
-  match limit with
-  | 0       => []
-  | lim + 1 =>
-    let v' := destinationVector mat vec
-    if v'.fst == v'.snd
-    then if v'.snd == start then v'.fst :: path else []
-    else mkLoop mat lim start (v'.fst :: path) v'
+def OBool := Option Bool
+deriving BEq, Inhabited
+
+def mappingOf (size : Pos) (locs : List Pos) : Mat1 OBool :=
+  let s' := (max 1 size.fst, max 1 size.snd)
+  let noneZero : s'.fst * s'.snd > 0 := by simp
+  locs.foldl (fun mat pos => mat.set₂ pos (some true)) (Mat1.new! s' noneZero none)
+
+-- #eval mappingOf (2, 2) [(0,0), (1,1)]
 
 def Pos.interpolate (p : Pos) (q : Pos) : Pos :=
   let p' := Pos.double p
@@ -175,33 +170,55 @@ def scaleUp : List Pos → List Pos
 
 -- #eval makeNeighbors (10, 10) (0, 0)
 
-partial def propagate (limit : Nat) (self : Mat1 Bool) (toVisit : List Pos) : Mat1 Bool :=
-  match toVisit, limit with
-  | [], _ | _, 0 => self
-  |  _, lim + 1  =>
-    toVisit.map (makeNeighbors self.shape)
-      |>.join
-      |>.foldl
-        (fun lt e => if lt.fst.get₂ e then lt else (lt.fst.set₂ e true, e :: lt.snd))
-        (self, [])
-      |> uncurry (propagate lim)
+-- FIXME: use `foldWithIdx`
+partial def propagate (mat : Mat1 OBool) (toVisit : Mat1 Bool) : Mat1 OBool :=
+  let (progress, mat', toVisit') := toVisit.foldWithIdx
+    (fun curr i j e =>
+      match e with
+      | false => curr
+      | true  =>
+        makeNeighbors mat.shape (i, j)
+        |>.foldl
+          (fun curr@(_, mat, toVisit) next =>
+            match mat.get₂ next with
+            | some _ => curr
+            | none   => (true, mat.set₂ next (some false), toVisit.set₂ next true))
+          curr)
+    (false, mat, toVisit.cloneWith false)
+  if progress then propagate mat' toVisit' else mat'
 
-def solve (m: Mat1 Circuit) : Nat :=
+def solve (m: Mat1 Circuit) : IO (Nat × Nat × Nat) := do
   let st := startPosition m
   let sp := m.shape
   let loop := makeVecs sp st
-    |>.map (mkLoop m m.size st [st] .)
+    |>.map (findLoop m m.size st [st] .)
     |>.foldl (fun best cand => if best.length < cand.length then cand else best) []
     |> scaleUp
-  -- loop.length
-  let map := propagate (min 1 m.size) (mappingOf (Pos.double sp) loop) [(0, 0)]
-  List.range sp.fst
-    |>.foldl (fun count y =>
-      List.range sp.snd
-        |>.filter (fun x => !map.get₂ (Pos.double (y, x)))
-        |>.length
-        |> (· + count))
-      0
+  let r := propagate (mappingOf (Pos.double sp) loop) (m.cloneWith false |>.set 0 0 true)
+  let _ ← List.range sp.1
+    |>.mapM
+      (fun i => do
+        let _ ← List.range sp.2
+          |>.mapM
+            (fun j => do
+              match r.get (i * 2) (j * 2) with
+              | none       => IO.print "."
+              | some false => IO.print "x"
+              | some true  => IO.print "#"
+            )
+        IO.print "\n"
+        return ())
+  return (
+    r.countWithIdx (fun i j e => i % 2 == 0 && j % 2 == 0 && e == none),
+    r.countWithIdx (fun i j e => i % 2 == 0 && j % 2 == 0 && e == some false),
+    r.countWithIdx (fun i j e => i % 2 == 0 && j % 2 == 0 && e == some true),
+  )
+  -- let r := mappingOf (Pos.double sp) (dbgTraceVal loop)
+  -- (
+  --   r.countWithIdx (fun i j e => i % 2 == 0 && j % 2 == 0 && e == none),
+  --   r.countWithIdx (fun i j e => i % 2 == 0 && j % 2 == 0 && e == some false),
+  --   r.countWithIdx (fun i j e => i % 2 == 0 && j % 2 == 0 && e == some true),
+  -- )
 
 -- #eval List.range 9
 
@@ -211,5 +228,6 @@ end Day10
 
 def day10 (ext : Option String) : IO Answers := do
   if let some (some m) := AoCParser.parse Day10.parser.parser (← dataOf 2023 10 ext)
-  then return (s!"{Day10.part1.solve m}", s!"{Day10.part2.solve m}")
+  then
+    return (s!"{Day10.Part1.solve m}", s!"{← Day10.part2.solve m}")
   else return ("parse error", "")
