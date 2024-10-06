7a2398180dc922be2a7f4641a7adf927e6f70b8a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-10-07 00:24:48 +0900

    ditto

--- Lean/Y2024/Day01.lean
+++ Lean/Y2024/Day01.lean
@@ -8,8 +8,7 @@ import «AoC».Parser
 namespace Y2024.Day01
 open Accumulation
 
-def year : Nat := 2024
-def day : Nat := 1
+def date : AocProblem := AocProblem.mk 2024 1 (by omega) (by omega)
 
 structure Input where
 deriving BEq, Repr
@@ -40,9 +39,8 @@ def solve (_ : Input) : Nat := 0
 end Part2
 
 protected def solve (alt : Option String): IO Answers := do
-  if let some d := parser.parse (← dataOf year day alt)
+  if let some d := parser.parse (← date.getData alt)
   then return (s!"{Part1.solve d}", s!"{Part2.solve d}")
   else return ("parse error", "")
 
 end Y2024.Day01
-
