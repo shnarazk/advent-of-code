a093791b5f705741b2aac97e137d05d164ee8ffe
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-10-17 09:15:38 +0900

    modified (WIP):   Y2023/Day13.lean

--- Lean4/Y2023/Day13.lean
+++ Lean4/Y2023/Day13.lean
@@ -3,14 +3,49 @@ import «AoC».Combinator
 import «AoC».Parser
 import «AoC».Rect
 
+namespace TwoDimensionalVector.Rect
+
+def mirrored_horizontally (frame : Rect Bool) (p : Dim2) (h : Nat) : Option Dim2 :=
+  if p.y < h then
+    let y' := h + h - p.y + 1
+    if y' < frame.shape.y then some { p with y := y' } else none
+  else
+    let y' := p.y - h
+    if h < y' then none else some { p with y := h - y' - 1 }
+
+def r99 := Rect.ofDim2 (Dim2.mk 9 9) (by rfl : NonNegDim (Dim2.mk 2 3)) false
+#eval r99.mirrored_horizontally (Dim2.mk 1 5) 2
+
+-- #eval mirrored_horizontally r99 (Dim2.mk 4 5) 4
+
+def mirrored_vertically (frame : Rect Bool) (p : Dim2) (v : Nat) : Option Dim2 :=
+  if p.x < v then
+    let x' := v + v - p.x + 1
+    if x' < frame.shape.x then some { p with x := x' } else none
+  else
+    let x' := p.x - v
+    if v < x' then none else some { p with x := v - x' - 1 }
+
+#eval r99.mirrored_horizontally (Dim2.mk 4 5) 4
+
+end TwoDimensionalVector.Rect
+
 namespace Y2023.Day13
 
 open Std Accumulation CoP CiCL
-open TwoDimentionalVector
+open TwoDimensionalVector
 
-structure Input where
-deriving BEq, Repr
--- instance : ToString Input where toString s := s!""
+variable (r : Rect Bool)
+
+def cut_horizontally (n : Nat) : Option Nat :=
+  if r.shape.toList.all (fun p ↦
+    if let some q := r.mirrored_horizontally p n then
+      r.get p = r.get q
+    else
+      false) then
+    some n
+  else
+    none
 
 namespace parser
 
@@ -36,8 +71,10 @@ namespace Part1
 def solve (pls : Array (Rect Bool)) : Nat :=
   pls.map evaluate |> sum
   where
-    evaluate (p : Rect Bool) : Nat :=
-      0
+    pick_axis (n : Int) : List Nat := List.range n.toNat |>.drop 1 |>.dropLast
+    valid_h_cut (m : Rect Bool) (h : Nat) : Option Nat := some 0
+    evaluate (m : Rect Bool) : Nat :=
+      dbgTrace s!"{pick_axis m.shape.y}" m.area
      -- List.range p.shape.y.toNat ** List.range p.shape.y.toNat
      --  |>.map
 
