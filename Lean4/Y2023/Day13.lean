37468bff9505c5110237c19021e735cac7b37d4d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-10-17 15:22:33 +0900

    modified (debug):   Y2023/Day13.lean

--- Lean4/Y2023/Day13.lean
+++ Lean4/Y2023/Day13.lean
@@ -3,6 +3,8 @@ import «AoC».Combinator
 import «AoC».Parser
 import «AoC».Rect
 
+def dbg {α : Type} [ToString α] (a : α) : α := dbgTrace s!"{a}" (fun _ ↦ a)
+
 namespace Option
 
 /--
@@ -20,43 +22,67 @@ end Option
 -- #eval some "abc" |>.mapOr (·.length) 99
 -- #eval none |>.mapOr (·.length) 99
 
+namespace Dim2
+
+def cutsₕ (g : Dim2) : List Nat := List.range g.y.toNat |>.drop 1
+def cutsᵥ (g : Dim2) : List Nat := List.range g.x.toNat |>.drop 1
+
+-- #eval (Dim2.mk 3 1).cutsₕ
+
+end Dim2
+
 namespace TwoDimensionalVector.Rect
 
 variable (r : Rect Bool)
 
-def mirrored_horizontally (p : Dim2) (h : Nat) : Option Dim2 :=
+def mirroredₕ (p : Dim2) (h : Nat) : Option Dim2 :=
   if p.y < h then
-    let y' := h + h - p.y + 1
+    let y' := h + h - p.y - 1
     if y' < r.shape.y then some { p with y := y' } else none
   else
     let y' := p.y - h
     if h < y' then none else some { p with y := h - y' - 1 }
 
 def r99 := Rect.ofDim2 (Dim2.mk 9 9) (by simp [NonNegDim]) false
-#eval r99.mirrored_horizontally (Dim2.mk 1 5) 2
+def p := Dim2.mk 1 5
+-- #eval r99.mirroredₕ p 2
+-- #eval if let some q := r99.mirroredₕ p 2 then r99.mirroredₕ q 2 else none
 
 -- #eval mirrored_horizontally r99 (Dim2.mk 4 5) 4
 
-def mirrored_vertically (p : Dim2) (v : Nat) : Option Dim2 :=
+def mirroredᵥ (p : Dim2) (v : Nat) : Option Dim2 :=
   if p.x < v then
-    let x' := v + v - p.x + 1
+    let x' := v + v - p.x - 1
     if x' < r.shape.x then some { p with x := x' } else none
   else
     let x' := p.x - v
     if v < x' then none else some { p with x := v - x' - 1 }
 
-#eval r99.mirrored_horizontally (Dim2.mk 4 5) 4
+-- #eval r99.mirroredᵥ (Dim2.mk 4 5) 4
 
-def cut_horizontally (n : Nat) : Option Nat :=
-  if r.shape.toList.all
-       -- 対応するものがなければ `true`
-      (fun p ↦ r.mirrored_horizontally p n |>.mapOr (r.get p = r.get ·) true)
-  then
+def cutₕ (n : Nat) : Option Nat :=
+  -- 対応するものがなければ `true`
+  if r.shape.toList.all (fun p ↦ r.mirroredₕ p n |>.mapOr (r.get p false = r.get · false) true) then
+    some (n * 100)
+  else
+    none
+
+-- #eval r99.cutₕ 1
+-- #eval r99.get (Dim2.mk 1 1) true = r99.get (Dim2.mk 1 2) true
+
+def cutᵥ (n : Nat) : Option Nat :=
+  if r.shape.toList.all (fun p ↦ r.mirroredᵥ p n |>.mapOr (r.get p false = r.get · false) true) then
     some n
   else
     none
 
-#eval r99.cut_horizontally 1
+-- #eval r99.cutᵥ 1
+
+def find_cutₕ : Nat := r.shape.cutsₕ.foldl (fun acc k ↦ r.cutₕ k |>.mapOr (· + acc) acc) 0
+def find_cutᵥ : Nat := r.shape.cutsᵥ.foldl (fun acc k ↦ r.cutᵥ k |>.mapOr (· + acc) acc) 0
+def evaluate : Nat := r.find_cutₕ + r.find_cutᵥ
+
+-- #eval r99.evaluate
 
 end TwoDimensionalVector.Rect
 
@@ -85,15 +111,7 @@ end parser
 
 namespace Part1
 
-def solve (pls : Array (Rect Bool)) : Nat :=
-  pls.map evaluate |> sum
-  where
-    pick_axis (n : Int) : List Nat := List.range n.toNat |>.drop 1 |>.dropLast
-    valid_h_cut (_m : Rect Bool) (_h : Nat) : Option Nat := some 0
-    evaluate (m : Rect Bool) : Nat :=
-      dbgTrace s!"{pick_axis m.shape.y}" m.area
-     -- List.range p.shape.y.toNat ** List.range p.shape.y.toNat
-     --  |>.map
+def solve (pls : Array (Rect Bool)) : Nat := pls.map (·.evaluate) |> sum
 
 end Part1
 
@@ -106,7 +124,8 @@ end Part2
 -- #eval (some #[3, 5]).map (·.shrink 1)
 
 def solve := AocProblem.config 2023 13
-  ((·.map ((⎊toString‿dbgTrace‿K) ∘ (·.shrink 1))) ∘ parser.parse)
+  parser.parse
+  -- ((·.map ((⎊toString‿dbgTrace‿K) ∘ (·.shrink 1))) ∘ parser.parse)
   Part1.solve
   Part2.solve
 
