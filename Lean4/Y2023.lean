f9042d5c80bd556020b473e9217b238ad5fea3e5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-10-25 15:33:10 +0900

    (Lean4) prepare for Y2023 day16

--- Lean4/Y2023.lean
+++ Lean4/Y2023.lean
@@ -15,6 +15,7 @@ import «Y2023».Day12
 import «Y2023».Day13
 import «Y2023».Day14
 import «Y2023».Day15
+import «Y2023».Day16
 
 namespace Y2023
 
@@ -34,10 +35,11 @@ def solvers := [
   Y2023.Day13.solve,
   Y2023.Day14.solve,
   Y2023.Day15.solve,
+  Y2023.Day16.solve,
 ]
 
 def there_are_solvers : 0 < solvers.length := by
-  have count : solvers.length = 15 := by exact rfl
+  have count : solvers.length = 16 := by exact rfl
   simp [count]
 
 protected def solvedDays : Nat := solvers.length
