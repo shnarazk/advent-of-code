f995cf7ef980f33c00d61f540e9701f6c1ff712e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-10-29 19:21:45 +0900

    modified:   Y2023/Day18.lean

--- Lean4/Y2023/Day18.lean
+++ Lean4/Y2023/Day18.lean
@@ -1,11 +1,12 @@
 import «AoC».Basic
 import «AoC».Combinator
 import «AoC».Parser
--- import «AoC».Rect64
+import «AoC».Rect64
 
 namespace Y2023.Day18
 
 open Accumulation CiCL
+open TwoDimensionalVector64
 
 structure Input where
   dir : Char
@@ -29,9 +30,9 @@ def line := do
   return (Input.mk
       dist
       num
-      (hexs.map (fun c ↦ ("0123456789abcdef".enumerate.find? (Prod.snd · == c)) |>.mapOr (·.fst) 0)
+      (hexs.map
+          (fun c ↦ ("0123456789abcdef".enumerate.find? (Prod.snd · == c)).mapOr (·.fst) 0)
         |>.foldl (fun (acc v : Nat) ↦ acc * 16 + v) 0))
--- #check ['o'].map (fun c ↦ "-yo".enumerate.find? (·.snd == c) |>.mapOr (·.fst) 0)
 
 def parse : String → Option (Array Input) := AoCParser.parse parser
   where
@@ -41,7 +42,15 @@ end parser
 
 namespace Part1
 
-def solve (_ : Array Input) : Nat := 0
+def solve (l : Array Input) : Nat :=
+  let heightₚ := l.filter (·.dir = 'D') |>.map (·.length) |> sum
+  let heightₘ := l.filter (·.dir = 'U') |>.map (·.length) |> sum
+  let widthₚ  := l.filter (·.dir = 'R') |>.map (·.length) |> sum
+  let widthₘ  := l.filter (·.dir = 'L') |>.map (·.length) |> sum
+  let width := widthₚ - widthₘ
+  let height := heightₚ - heightₘ
+  let r : Rect Nat := Rect.ofDim2 height.toUInt64 width.toUInt64 0
+  0
 
 end Part1
 
