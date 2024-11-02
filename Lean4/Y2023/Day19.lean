feb842bf17cff3e076d361dc1ffc01ebae10c267
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-11-02 15:24:34 +0900

    modified (completed about parser):   Y2023/Day19.lean

--- Lean4/Y2023/Day19.lean
+++ Lean4/Y2023/Day19.lean
@@ -5,6 +5,7 @@ import «AoC».Parser
 namespace Y2023.Day19
 
 open Accumulation CiCL Std
+abbrev HashMap := Std.HashMap
 
 inductive Operator where | Lt | Gt deriving BEq
 
@@ -55,6 +56,7 @@ namespace parser
 open AoCParser
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
+open Std.HashMap
 
 def prule := do
   let var ← alphabets
@@ -73,29 +75,46 @@ def pdecl := do
   return Decl.mk label rules (Target.new drule)
 -- #eval AoCParser.parse pdecl "rfg{s<537:gd,x>2440:R,A}"
 
-def parse : String → Option (Array Decl) := AoCParser.parse parser
-  where
-    parser : Parser (Array Decl):= sepBy1 pdecl eol
--- #eval parse "rfg{s<537:gd,x>2440:R,A}\nqs{s>3448:A,lnx}"
+def prating := do
+  let name ← alphabets <* pchar '='
+  let value ← number
+  return (name, value)
+
+def pratings := do
+  let rs ← pchar '{' *> sepBy1 prating (pchar ',') <* pchar '}'
+  let h := rs.foldl (fun h p ↦ HashMap.insert h (Prod.fst p) (Prod.snd p)) (HashMap.empty : HashMap String Nat)
+  return h
+-- #eval AoCParser.parse (sepBy1 pratings eol) "{x=768,m=2655}\n{x=167,m=44}"
+
+def parse : String → Option (HashMap String Decl × Array (HashMap String Nat)) := AoCParser.parse parser
+ where
+    parser := do
+      let d ← sepBy1 pdecl eol <* eol
+      let p ← eol *> sepBy1 pratings eol
+      return (makeInstruction d, p)
+-- #eval parse "rfg{s<537:gd,x>2440:R,A}\nqs{s>3448:A,lnx}\n\n{x=787}" |>.mapOr (·.fst) #[]
+-- #eval parse "rfg{s<537:gd,lnx}\n\n{x=787,m=2655}" |>.mapOr (·.snd.map (·.toList)) #[]
 
 end parser
 
 namespace Part1
 
-def solve (a : Array Decl) : Nat :=
- dbg s!"{makeInstruction a |>.toList}" 0
+def solve (input : HashMap String Decl × Array (HashMap String Nat)) : Nat :=
+  let (decls, ratings) := input
+  if let some label_in := decls.get? "in" then
+    dbg s!"got: {label_in}" 0
+  else
+    dbg s!"{decls |>.toList}, rating{ratings.map (·.toList)}" 0
 
 end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := 0
+def solve (_input : HashMap String Decl × Array (HashMap String Nat)) : Nat :=
+  0
 
 end Part2
 
-def solve := AocProblem.config 2023 19
-  ((dbg "parsed as ") ∘ parser.parse)
-  Part1.solve
-  Part2.solve
+def solve := AocProblem.config 2023 19 parser.parse Part1.solve Part2.solve
 
 end Y2023.Day19
