f8dbf77b13bf3c00ec950d873cb458072309c62b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-11-03 16:15:31 +0900

    modified (completed parser):   Y2023/Day20.lean

--- Lean4/Y2023/Day20.lean
+++ Lean4/Y2023/Day20.lean
@@ -1,80 +1,112 @@
 import «AoC».Basic
 import «AoC».Combinator
 import «AoC».Parser
--- import «AoC».Rect64
 
 namespace Y2023.Day20
 
 open Accumulation CiCL
+open Std
+
+abbrev HashMap := Std.HashMap
+abbrev Label := String
 
 structure Input where
 deriving BEq, Repr
 instance : ToString Input where toString _ := s!""
 
-inductive Module where
+inductive Mdl where
   | Broadcaster
-  | FlipFlop (label : String)
-  | Conjunction (label : String)
-deriving BEq
+  | FlipFlop (label : Label) (state : Bool)
+  | Conjunction (label : Label) (state : HashMap Label Bool)
 
-instance : ToString Module where
-  toString : Module → String
-    | Module.Broadcaster => "Broadcaster"
-    | Module.FlipFlop s => s!"FF:{s}"
-    | Module.Conjunction s => s!"Con:{s}"
--- #eval Module.FlipFlop "ab"
--- #eval Module.Conjunction "ab"
+instance : BEq Mdl where
+  beq : Mdl → Mdl → Bool
+  | .Broadcaster     , .Broadcaster      => true
+  | .FlipFlop l₁ _   , .FlipFlop l₂ _    => l₁ == l₂
+  | .Conjunction l₁ _, .Conjunction l₂ _ => l₁ == l₂
+  | _                , _                 => false
 
-inductive Link where | To | Low | High
-deriving BEq
+instance : ToString Mdl where
+  toString : Mdl → String
+    | Mdl.Broadcaster     => "Broadcaster"
+    | Mdl.FlipFlop l s    => s!"FF:{l}({s})"
+    | Mdl.Conjunction l s => s!"Con:{l}({s.toList})"
+-- #eval Mdl.FlipFlop "ab"
+-- #eval Mdl.Conjunction "ab"
 
-instance : ToString Link where
-  toString : Link → String
-    | Link.To   => "->"
-    | Link.Low  => "-low->"
-    | Link.High => "-high->"
+def Mdl.label : Mdl → Label
+  | Mdl.Broadcaster    => ""
+  | Mdl.FlipFlop l _    => l
+  | Mdl.Conjunction l _ => l
+
+def Mdl.pulse : Mdl → (Label × Bool) → Mdl × Option (Label × Bool)
+  | Mdl.Broadcaster,           _ => (Mdl.Broadcaster     , some ("", false))
+  | Mdl.FlipFlop l b, (_, false) => (Mdl.FlipFlop l !b   , some (l, !b))
+  | Mdl.FlipFlop l b,  (_, true) => (Mdl.FlipFlop l b    , none) 
+  | Mdl.Conjunction l s, (l', b) =>
+     let s' := s.insert l' b      ; (Mdl.Conjunction l s', some (l, !s'.values.all I))
+-- #eval (Mdl.FlipFlop "ff" false).pulse ("a", false)
+
+def Mdl.link : Mdl → Mdl → Mdl
+  | Mdl.Broadcaster     , Mdl.Conjunction l s => Mdl.Conjunction l (s.insert "" false)
+  | Mdl.FlipFlop l' _   , Mdl.Conjunction l s => Mdl.Conjunction l (s.insert l' false)
+  | Mdl.Conjunction l' _, Mdl.Conjunction l s => Mdl.Conjunction l (s.insert l' false)
+  | _                   , d                   => d
 
 structure Rule where
-  module : Module
-  link   : Link
-  outputs : Array String
-deriving BEq
+  module : Mdl
+  dests  : Array String
 
 instance : ToString Rule where
-  toString r := s!"{r.module} {r.link} {r.outputs}"
+  toString r := s!"{r.module} -> {r.dests}"
+
+abbrev Circuit := HashMap Label (Mdl × Array Label)
+ 
+def Circuit.new (rules : Array Rule) : Circuit :=
+  rules.foldl
+    (fun c r => 
+       r.dests.foldl
+         (fun c destLabel => 
+           if let some target := c.get? destLabel then
+             c.insert destLabel (r.module.link target.fst, target.snd)
+           else
+             c
+          )
+         c
+    )
+    (rules.foldl (fun c r => c.insert r.module.label (r.module, r.dests)) HashMap.empty)
+
+instance : ToString Circuit where toString self := self.toList.toString
 
 namespace parser
 
 open AoCParser
+open Std
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
 def pmodule := pbroadcaster <|> pflipflop <|> pconjunction
   where
-    pbroadcaster := pstring "broadcaster"  *> return Module.Broadcaster
-    pflipflop    := pchar '%' *> alphabets >>= pure ∘ Module.FlipFlop
-    pconjunction  := pchar '&' *> alphabets >>= pure ∘ Module.Conjunction
+    pbroadcaster := pstring "broadcaster"  *> return Mdl.Broadcaster
+    pflipflop := do
+      let l ← pchar '%' *> alphabets
+      return Mdl.FlipFlop l false
+    pconjunction := do
+      let l ← pchar '&' *> alphabets
+      return Mdl.Conjunction l HashMap.empty
+
 -- #eval AoCParser.parse pmodule "broadcaster"
 -- #eval AoCParser.parse pmodule "%a"
 -- #eval AoCParser.parse pmodule "&inv"
 
-def plink := plink_ <|> plinkL <|> plinkH
-  where
-    plink_ := pstring "->"      *> return Link.To
-    plinkL := pstring "-low->"  *> return Link.Low
-    plinkH := pstring "-high->" *> return Link.High
--- #eval AoCParser.parse plink "->"
--- #eval AoCParser.parse plink "-high->"
-
-def poutputs := sepBy1 alphabets (pstring ", ")
--- #eval AoCParser.parse outputs "a"
--- #eval AoCParser.parse outputs "a, b, c"
+def pdests := sepBy1 alphabets (pstring ", ")
+-- #eval AoCParser.parse pdests  "a"
+-- #eval AoCParser.parse .dests "a, b, c"
 
 def pline := do
-  let m ← pmodule <* whitespaces
-  let l ← plink <* whitespaces
-  let o ← poutputs
-  return Rule.mk m l o
+  let m ← pmodule <* whitespaces <* pstring "->" <* whitespaces
+  let o ← pdests
+  return Rule.mk m o
 -- #check pline
 -- #eval AoCParser.parse pline "%a -> inv, con"
 
@@ -87,7 +119,9 @@ end parser
 
 namespace Part1
 
-def solve (_ : Array Rule) : Nat := 0
+def solve (a : Array Rule) : Nat :=
+  let c := Circuit.new a
+  dbg s!"{c}" 0
 
 end Part1
 
