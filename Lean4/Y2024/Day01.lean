eb6225b89d0dcc7ee93312804d10fcbea3817bb2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-01 15:08:05 +0900

    (Lean4) Y2024 day1 done.

--- Lean4/Y2024/Day01.lean
+++ Lean4/Y2024/Day01.lean
@@ -8,8 +8,8 @@ namespace Y2024.Day01
 open Accumulation
 
 structure Input where
-deriving BEq, Repr
--- instance : ToString Input where toString s := s!""
+  line : Array (Nat × Nat)deriving BEq, Repr
+instance : ToString Input where toString s := s!"{s.line.size}"
 
 namespace parser
 
@@ -17,21 +17,40 @@ open AoCParser
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
+def line_parser := do
+  let left ← number <* ws
+  let right ← number <* eol
+  return (left, right)
+
 def parse : String → Option Input := AoCParser.parse parser
   where
-    parser : Parser Input := return Input.mk
+    parser : Parser Input := do
+      let v ← many1 line_parser
+      return Input.mk v
 
 end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := 0
+def solve (input : Input) : Nat :=
+  let l : Array Nat := input.line.map (·.1) |>.heapSort (·<·)
+  let r : Array Nat := input.line.map (·.2) |>.heapSort (·<·)
+  l.zip r
+    |>.map (fun (l, r) ↦ if l < r then r - l else l - r)
+    |> sum
 
 end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := 0
+open Std
+
+def solve (input : Input) : Nat :=
+  let hash := input.line.map (·.2)
+    |>.foldl (fun h i ↦ h.insert i (1 + h.getD i 0)) Std.HashMap.empty
+  input.line.map (·.1)
+    |>.map (fun i ↦ i * hash.getD i 0)
+    |> sum
 
 end Part2
 
