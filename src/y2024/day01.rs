9fb0c2384ca27813d400268d095d5610c0c7865c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-01 14:17:59 +0900

    y2024 day01 part1 done

--- src/y2024/day01.rs
+++ src/y2024/day01.rs
@@ -10,9 +10,9 @@ use {
     },
     nom::{
         bytes::complete::{tag, take},
-        character::complete::{alpha1, alphanumeric1, anychar, digit1, newline, u64},
-        multi::{many_till, separated_list1},
-        sequence::{separated_pair, terminated, tuple},
+        character::complete::{alpha1, alphanumeric1, anychar, digit1, newline, space1, u64},
+        multi::{many1, many_till, separated_list1},
+        sequence::{pair, separated_pair, terminated, tuple},
         IResult,
     },
     serde::Serialize,
@@ -21,39 +21,37 @@ use {
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<(u64, u64)>,
 }
 
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
+fn parse(str: &str) -> IResult<&str, Vec<(u64, u64)>> {
+    let (remain1, v) = many1(pair(terminated(u64, space1), terminated(u64, newline)))(str)?;
+    Ok((remain1, v))
+}
 
 #[aoc(2024, 1)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
-    // fn parse(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
-    //     // Ok("".to_string())
-    //     Ok(segment[2].to_string())
-    // }
-    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        dbg!(block);
-        // let parser = regex!(r"^(\d+)$");
-        // let segment = parser.captures(block).ok_or(ParseError)?;
-        // self.line.push(segment[1].parse::<_>());
-        Ok(())
-    }
-    fn end_of_data(&mut self) {
-        dbg!(&self.line);
+    fn parse(&mut self, input: String) -> Result<String, ParseError> {
+        let str = input.as_str();
+        let Ok((_remain1, v)) = parse(str) else {
+            return Err(ParseError);
+        };
+        self.line = v;
+        Ok("".to_string())
     }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        let mut l = self.line.iter().map(|p| p.0).collect::<Vec<u64>>();
+        let mut r = self.line.iter().map(|p| p.1).collect::<Vec<u64>>();
+        l.sort();
+        r.sort();
+        dbg!(l
+            .iter()
+            .zip(r.iter())
+            .collect::<Vec<_>>()
+            .iter()
+            .map(|(a, b)| a.abs_diff(**b) as usize)
+            .sum::<usize>())
     }
     fn part2(&mut self) -> Self::Output2 {
         2
