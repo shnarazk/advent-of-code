17a723f0880418daec75e9fb047be4e1f3f41854
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-03 00:57:03 +0900

    ditto

--- Lean4/Y2024/Day02.lean
+++ Lean4/Y2024/Day02.lean
@@ -5,10 +5,9 @@ import «AoC».Combinator
 import «AoC».Parser
 
 namespace Y2024.Day02
+open CiCL
 
-structure Input where
-  line : Array (Array Nat) deriving BEq, Repr
-instance : ToString Input where toString s := s!"{s.line.size}"
+abbrev Input := Array (Array Nat)
 
 namespace parser
 
@@ -19,36 +18,29 @@ open Std.Internal.Parsec.String
 def line_parser := do sepBy1 number (pchar ' ')
 -- #eval AoCParser.parse line_parser "3 5 8"
 
-def parse : String → Option Input := AoCParser.parse parser
-  where
-    parser : Parser Input := do
-      let v ← sepBy1 line_parser eol
-      return Input.mk v
+def parse : String → Option Input := AoCParser.parse (sepBy1 line_parser eol)
 -- #eval parse "3 8\n1 2 3\n6 9"
 
 end parser
 
 def abs_diff (a b : Nat) : Nat := if a < b then b - a else a - b
 
-def between (a : Nat) (b : Nat × Nat): Bool :=
-  b.1 ≤ a && a ≤ b.2
+def between (a : Nat) (b : Nat × Nat): Bool := b.1 ≤ a && a ≤ b.2
 
 def satisfy (levels : Array Nat) : Bool :=
-  (levels.windows2.all (CiCL.uncurry (· < ·))
-    || levels.windows2.all (CiCL.uncurry (· > ·)))
+  (levels.windows2.all (uncurry (· < ·)) || levels.windows2.all (uncurry (· > ·)))
     && levels.windows2.all (fun (a, b) ↦ between (abs_diff a b) (1, 3))
 
 namespace Part1
 
-def solve (input : Input) : Nat := input.line.filter satisfy |>.size
+def solve (input : Input) : Nat := input.filter satisfy |>.size
 
 end Part1
 
 namespace Part2
 
-def solve (input : Input) : Nat :=
-  input.line.filter (fun (l : Array Nat) ↦
-      (satisfy l || (List.range l.size |>.map l.eraseIdx! |>.any satisfy)))
+def solve (inp : Input) : Nat :=
+  inp.filter (fun l ↦ satisfy l || (List.range l.size |>.map l.eraseIdx! |>.any satisfy))
     |>.size
 
 end Part2
