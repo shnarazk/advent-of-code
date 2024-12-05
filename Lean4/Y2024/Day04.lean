4349abb6043f02ff27ce943e5ee48ef9759b0470
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-05 22:43:02 +0900

    modified (parser):   Lean4/Y2024/Day04.lean

--- Lean4/Y2024/Day04.lean
+++ Lean4/Y2024/Day04.lean
@@ -8,10 +8,10 @@ open Accumulation CiCL
 abbrev HashMap := Std.HashMap
 
 structure Input where
-  line: Array String
-deriving BEq, Repr
+  plane : HashMap (Nat × Nat) Char
+deriving Repr
 
-instance : ToString Input where toString _ := s!""
+instance : ToString Input where toString self := s!"{self.plane.toList}"
 
 namespace parser
 
@@ -19,8 +19,10 @@ open AoCParser
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
-def parse_line : Parser String := alphabets
--- #eval AoCParser.parse alphabets "eocb\n"
+def parse_line : Parser (Array Char) := do
+  let as ← alphabets
+  return as.toList.toArray
+-- #eval AoCParser.parse parse_line "eocb\n"
 
 def parse_lines := sepBy1 parse_line eol
 -- #eval AoCParser.parse parse_lines "eocb\nABC\n"
@@ -28,14 +30,19 @@ def parse_lines := sepBy1 parse_line eol
 def parse : String → Option Input := AoCParser.parse parser
   where parser := do
     let v ← parse_lines
-    return Input.mk v
+    let h := v.toList.enum.map (fun (i, l) ↦ l.toList.enum.map (fun (x : Nat × Char) ↦ ((i, x.1), x.2)))
+      |>.flatten
+    return Input.mk (h.foldl (fun h (p, v) ↦ h.insert p v) Std.HashMap.empty)
 -- #eval parse "ABC\nXYZ"
 
 end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := 0
+def solve (input : Input) : Nat :=
+  input.plane.toList.filter
+      (fun (_p, _c) ↦ true)
+    |>.length
 
 end Part1
 
