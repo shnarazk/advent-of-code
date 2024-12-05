6226b970e5285de0bcf8a69c870a701f181be11c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-06 00:36:08 +0900

    (Lean4) Y2024 day4 part1 done

--- Lean4/Y2024/Day04.lean
+++ Lean4/Y2024/Day04.lean
@@ -7,11 +7,78 @@ namespace Y2024.Day04
 open Accumulation CiCL
 abbrev HashMap := Std.HashMap
 
+def asNat : Bool -> Nat
+  | false => 0
+  | true => 1
+
 structure Input where
-  plane : HashMap (Nat × Nat) Char
+  plane : HashMap (Int × Int) Char
 deriving Repr
 
 instance : ToString Input where toString self := s!"{self.plane.toList}"
+
+def Input.xmas_n (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1 - 1, p.2) == some 'M'
+      && self.plane.get? (p.1 - 2, p.2) == some 'A'
+      && self.plane.get? (p.1 - 3, p.2) == some 'S'
+    |> asNat
+
+--
+def Input.xmas_s (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1 + 1, p.2) == some 'M'
+      && self.plane.get? (p.1 + 2, p.2) == some 'A'
+      && self.plane.get? (p.1 + 3, p.2) == some 'S'
+    |> asNat
+
+--
+def Input.xmas_e (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1, p.2 + 1) == some 'M'
+      && self.plane.get? (p.1, p.2 + 2) == some 'A'
+      && self.plane.get? (p.1, p.2 + 3) == some 'S'
+    |> asNat
+
+--
+def Input.xmas_w (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1, p.2 - 1) == some 'M'
+      && self.plane.get? (p.1, p.2 - 2) == some 'A'
+      && self.plane.get? (p.1, p.2 - 3) == some 'S'
+    |> asNat
+
+--
+def Input.xmas_ne (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1 - 1, p.2 + 1) == some 'M'
+      && self.plane.get? (p.1 - 2, p.2 + 2) == some 'A'
+      && self.plane.get? (p.1 - 3, p.2 + 3) == some 'S'
+    |> asNat
+
+--
+def Input.xmas_es (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1 + 1, p.2 + 1) == some 'M'
+      && self.plane.get? (p.1 + 2, p.2 + 2) == some 'A'
+      && self.plane.get? (p.1 + 3, p.2 + 3) == some 'S'
+    |> asNat
+
+--
+def Input.xmas_sw (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1 + 1, p.2 - 1) == some 'M'
+      && self.plane.get? (p.1 + 2, p.2 - 2) == some 'A'
+      && self.plane.get? (p.1 + 3, p.2 - 3) == some 'S'
+    |> asNat
+
+--
+def Input.xmas_wn (self : Input) (p : Int × Int) : Nat :=
+  self.plane.get? p == some 'X'
+      && self.plane.get? (p.1 - 1, p.2 - 1) == some 'M'
+      && self.plane.get? (p.1 - 2, p.2 - 2) == some 'A'
+      && self.plane.get? (p.1 - 3, p.2 - 3) == some 'S'
+    |> asNat
 
 namespace parser
 
@@ -30,7 +97,7 @@ def parse_lines := sepBy1 parse_line eol
 def parse : String → Option Input := AoCParser.parse parser
   where parser := do
     let v ← parse_lines
-    let h := v.toList.enum.map (fun (i, l) ↦ l.toList.enum.map (fun (x : Nat × Char) ↦ ((i, x.1), x.2)))
+    let h := v.toList.enum.map (fun (i, l) ↦ l.toList.enum.map (fun (x : Nat × Char) ↦ (((i : Int), (x.1 : Int)), x.2)))
       |>.flatten
     return Input.mk (h.foldl (fun h (p, v) ↦ h.insert p v) Std.HashMap.empty)
 -- #eval parse "ABC\nXYZ"
@@ -40,21 +107,19 @@ end parser
 namespace Part1
 
 def solve (input : Input) : Nat :=
-  input.plane.toList.filter
-      (fun (_p, _c) ↦ true)
-    |>.length
+  input.plane.toList.map
+      (fun (p, _) ↦ input.xmas_n p + input.xmas_s p + input.xmas_e p + input.xmas_w p
+        + input.xmas_ne p + input.xmas_es p + input.xmas_sw p + input.xmas_wn p)
+    |> sum
 
 end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := 0
+def solve (input : Input) : Nat := input.plane.toList.length
 
 end Part2
 
-def solve := AocProblem.config 2024 04
-  ((dbg "parsed as ") ∘ parser.parse)
-  Part1.solve
-  Part2.solve
+def solve := AocProblem.config 2024 04 parser.parse Part1.solve Part2.solve
 
 end Y2024.Day04
