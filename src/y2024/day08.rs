5380ac2cb9653f7b6aeec1cc7779bdbf162bfca4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-08 14:49:23 +0900

    Y2024 day8 part1 done

--- src/y2024/day08.rs
+++ src/y2024/day08.rs
@@ -5,11 +5,11 @@
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
+        geometric::*,
         parser::parse_usize,
     },
     serde::Serialize,
-    std::collections::HashMap,
+    std::collections::{HashMap, HashSet},
     winnow::{
         ascii::newline,
         combinator::{repeat, repeat_till, separated, terminated},
@@ -18,31 +18,81 @@ use {
     },
 };
 
-#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
-pub struct Puzzle {
-    line: Vec<()>,
+#[derive(Debug, Eq, Hash, PartialEq, Serialize)]
+enum Kind {
+    Empty,
+    Antinode,
 }
 
-// impl Default for Puzzle {
-//     fn default() -> Self {
-//         Puzzle { }
-//     }
-// }
+fn kind_from(c: &char) -> Kind {
+    match *c {
+        '.' => Kind::Empty,
+        'a' => Kind::Antinode,
+        _ => unreachable!(),
+    }
+}
 
-// fn parse(str: &muut &str) -> PResult<()> {}
+#[derive(Clone, Debug, Default, Eq, PartialEq, Serialize)]
+pub struct Puzzle {
+    plane: Vec<Vec<char>>,
+    antenna: Vec<(Vec2, char)>,
+    antinode: HashSet<Vec2>,
+    size: Vec2,
+    // trail: HashMap<Vec2, Option<(Vec2, Direction)>>,
+}
+
+impl Puzzle {
+    fn check_pos(&self, p: Vec2) -> Option<Vec2> {
+        ((0..self.size.0).contains(&p.0) && (0..self.size.1).contains(&p.1)).then_some(p)
+    }
+}
+
+fn parse_line(input: &mut &str) -> PResult<Vec<char>> {
+    let v = repeat(1.., one_of(['.', '#', 'a'])).parse_next(input)?;
+    Ok(v)
+}
+
+fn parse(input: &mut &str) -> PResult<Vec<Vec<char>>> {
+    separated(1.., parse_line, newline).parse_next(input)
+}
 
 #[aoc(2024, 8)]
 impl AdventOfCode for Puzzle {
-    // fn parse(&mut self, input: String) -> Result<String, ParseError> {
-    //     let s = &mut input.as_str();
-    //     self.line = parse(s)?;
-    //     Self::parsed()
-    // }
+    fn parse(&mut self, input: String) -> Result<String, ParseError> {
+        self.plane = input
+            .split('\n')
+            .filter(|l| !l.is_empty())
+            .map(|l| l.chars().collect::<Vec<char>>())
+            .collect::<Vec<_>>();
+        Self::parsed()
+    }
     fn end_of_data(&mut self) {
-        dbg!(&self.line);
+        for (i, l) in self.plane.iter().enumerate() {
+            for (j, c) in l.iter().enumerate() {
+                if *c != '.' {
+                    self.antenna.push(((i as isize, j as isize), *c));
+                }
+                self.size.1 = j as isize + 1;
+            }
+            self.size.0 = i as isize + 1;
+        }
+        dbg!(self.antenna.len());
     }
     fn part1(&mut self) -> Self::Output1 {
-        1
+        for (i, (p, f1)) in self.antenna.iter().enumerate() {
+            for (j, (q, f2)) in self.antenna.iter().enumerate() {
+                if i != j && f1 == f2 {
+                    let d = q.sub(&p);
+                    if let Some(a) = self.check_pos(p.sub(&d)) {
+                        self.antinode.insert(a);
+                    }
+                    if let Some(b) = self.check_pos(q.add(&d)) {
+                        self.antinode.insert(b);
+                    }
+                }
+            }
+        }
+        self.antinode.len()
     }
     fn part2(&mut self) -> Self::Output2 {
         2
