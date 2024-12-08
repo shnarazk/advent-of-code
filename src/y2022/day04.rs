6438f02e066356d42f73dffb27102e5082ffbbb7
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-08 11:21:28 +0900

    Y2022 day04: use winnow instead of regex

--- src/y2022/day04.rs
+++ src/y2022/day04.rs
@@ -1,7 +1,10 @@
 //! <https://adventofcode.com/2022/day/4>
-use crate::{
-    framework::{aoc, AdventOfCode, ParseError},
-    regex,
+use {
+    crate::{
+        framework::{aoc, AdventOfCode, ParseError},
+        parser::parse_usize,
+    },
+    winnow::{combinator::terminated, PResult, Parser},
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -9,18 +12,31 @@ pub struct Puzzle {
     line: Vec<(usize, usize, usize, usize)>,
 }
 
+fn parse_line(s: &mut &str) -> PResult<(usize, usize, usize, usize)> {
+    (
+        terminated(parse_usize, "-"),
+        terminated(parse_usize, ","),
+        terminated(parse_usize, "-"),
+        parse_usize,
+    )
+        .parse_next(s)
+}
+
 #[aoc(2022, 4)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        let parser = regex!(r"^(\d+)-(\d+),(\d+)-(\d+)$");
+        /* let parser = regex!(r"^(\d+)-(\d+),(\d+)-(\d+)$");
         let segment = parser.captures(block).ok_or(ParseError)?;
         self.line.push((
             segment[1].parse::<_>()?,
             segment[2].parse::<_>()?,
             segment[3].parse::<_>()?,
             segment[4].parse::<_>()?,
-        ));
+        )); */
+        let s = block.to_string();
+        let p = &mut s.as_str();
+        self.line.push(parse_line(p)?);
         Ok(())
     }
     fn end_of_data(&mut self) {
--- src/y2023/template.rs
+++ src/y2023/template.rs
@@ -27,18 +27,9 @@ pub struct Puzzle {
 impl AdventOfCode for Puzzle {
     // const DELIMITER: &'static str = "\n";
     // fn header(&mut self, input: String) -> Result<String, ParseError> {
-    //     let parser = regex!(r"^(.+)\n\n((.|\n)+)$");
-    //     let segment = parser.captures(input).ok_or(ParseError)?;
-    //     for num in segment[1].split(',') {
-    //         let _value = num.parse::<usize>()?;
-    //     }
     //     Ok("".to_string())
     // }
     // fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-    //     dbg!(block);
-    //     // let parser = regex!(r"^(\d+)$");
-    //     // let segment = parser.captures(block).ok_or(ParseError)?;
-    //     // self.line.push(segment[1].parse::<_>());
     //     Ok(())
     // }
     fn end_of_data(&mut self) {
