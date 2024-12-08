cbccc42e9b977e0c9c1307b815f22ea7f46da772
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-09 06:25:27 +0900

    modified:   src/y2022/day04.rs

--- src/y2022/day04.rs
+++ src/y2022/day04.rs
@@ -4,7 +4,7 @@ use {
         framework::{aoc, AdventOfCode, ParseError},
         parser::parse_usize,
     },
-    winnow::{combinator::terminated, PResult, Parser},
+    winnow::{combinator::seq, PResult, Parser},
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -13,34 +13,17 @@ pub struct Puzzle {
 }
 
 fn parse_line(s: &mut &str) -> PResult<(usize, usize, usize, usize)> {
-    (
-        terminated(parse_usize, "-"),
-        terminated(parse_usize, ","),
-        terminated(parse_usize, "-"),
-        parse_usize,
-    )
-        .parse_next(s)
+    seq!(parse_usize, _: "-", parse_usize, _: ",", parse_usize, _: "-", parse_usize).parse_next(s)
 }
 
 #[aoc(2022, 4)]
 impl AdventOfCode for Puzzle {
     const DELIMITER: &'static str = "\n";
     fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        /* let parser = regex!(r"^(\d+)-(\d+),(\d+)-(\d+)$");
-        let segment = parser.captures(block).ok_or(ParseError)?;
-        self.line.push((
-            segment[1].parse::<_>()?,
-            segment[2].parse::<_>()?,
-            segment[3].parse::<_>()?,
-            segment[4].parse::<_>()?,
-        )); */
         let s = block.to_string();
         let p = &mut s.as_str();
         self.line.push(parse_line(p)?);
         Ok(())
-    }
-    fn end_of_data(&mut self) {
-        // dbg!(&self.line);
     }
     fn part1(&mut self) -> Self::Output1 {
         self.line
