43283ec19720165a56375951748192fbd8efaaba
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-10 08:42:53 +0900

    Y2024 day9: optimize with chunk-based calculation

--- src/y2024/day09.rs
+++ src/y2024/day09.rs
@@ -11,9 +11,9 @@ use {
 pub struct Puzzle {
     line: Vec<usize>,
     // id to its length
-    file: Vec<usize>,
+    file_len: Vec<usize>,
     // id to its length
-    free: Vec<usize>,
+    free_len: Vec<usize>,
 }
 
 #[aoc(2024, 9)]
@@ -25,86 +25,71 @@ impl AdventOfCode for Puzzle {
     fn end_of_data(&mut self) {
         for (i, x) in self.line.iter().enumerate() {
             if i % 2 == 0 {
-                self.file.push(*x);
+                self.file_len.push(*x);
             } else {
-                self.free.push(*x);
+                self.free_len.push(*x);
             }
         }
     }
     fn part1(&mut self) -> Self::Output1 {
-        let len = self.line.iter().cloned().sum::<usize>();
-        let mut tmp: Vec<Option<u32>> = vec![None; len];
+        let disc_len = self.line.iter().cloned().sum::<usize>();
+        let mut disc_image: Vec<Option<u32>> = vec![None; disc_len];
         let mut p: usize = 0;
         for (i, n) in self.line.iter().enumerate() {
             let v = (i % 2 == 0).then_some(i as u32 / 2);
-            for q in tmp.iter_mut().skip(p).take(*n) {
+            for q in disc_image.iter_mut().skip(p).take(*n) {
                 *q = v;
             }
             p += n;
         }
         let mut left: usize = 0;
-        let mut right: usize = tmp.len() - 1;
+        let mut right: usize = disc_image.len() - 1;
         'stop: while left < right {
-            if tmp[left].is_none() {
-                while tmp[right].is_none() {
+            if disc_image[left].is_none() {
+                while disc_image[right].is_none() {
                     if right <= left {
                         break 'stop;
                     }
                     right -= 1;
                 }
-                tmp.swap(left, right);
+                disc_image.swap(left, right);
             }
             left += 1;
         }
-        tmp.iter()
+        disc_image
+            .iter()
             .enumerate()
             .map(|(i, x)| x.map_or(0, |v| i * (v as usize)))
             .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        let len = self.line.iter().cloned().sum::<usize>();
-        let mut tmp: Vec<Option<u32>> = vec![None; len];
-        let mut start_at: Vec<usize> = vec![0; self.file.len()];
-        let mut p: usize = 0;
-        for (i, n) in self.line.iter().enumerate() {
+        let mut file_start: Vec<usize> = vec![0; self.file_len.len()];
+        let mut free_start: Vec<usize> = vec![0; self.free_len.len()];
+        let mut start = 0;
+        for (i, &l) in self.line.iter().enumerate() {
             if i % 2 == 0 {
-                start_at[i / 2] = p;
+                file_start[i / 2] = start;
+            } else {
+                free_start[i / 2] = start;
             }
-            let v = (i % 2 == 0).then_some(i as u32 / 2);
-            for q in tmp.iter_mut().skip(p).take(*n) {
-                *q = v;
-            }
-            p += n;
+            start += l;
         }
-        'next_file: for (id, &ln) in self.file.iter().enumerate().rev() {
-            let mut left: Option<usize> = None;
-            let mut right = 0;
-            while right < start_at[id] {
-                if let Some(l) = tmp[right] {
-                    left = None;
-                    right += self.file[l as usize];
-                } else {
-                    if left.is_none() {
-                        left = Some(right);
-                    }
-                    if let Some(l) = left {
-                        if ln == right - l + 1 {
-                            for i in tmp.iter_mut().skip(l).take(ln) {
-                                *i = Some(id as u32);
-                            }
-                            for i in tmp.iter_mut().skip(start_at[id]).take(self.file[id]) {
-                                *i = None;
-                            }
-                            continue 'next_file;
-                        }
-                    }
-                    right += 1;
+        'next_file: for (id, &len) in self.file_len.iter().enumerate().rev() {
+            let mut target = 0;
+            while free_start[target] < file_start[id] {
+                if len <= self.free_len[target] {
+                    file_start[id] = free_start[target];
+                    free_start[target] += len;
+                    self.free_len[target] -= len;
+                    continue 'next_file;
                 }
+                target += 1;
             }
         }
-        tmp.iter()
+        self.file_len
+            .iter()
             .enumerate()
-            .map(|(i, x)| x.map_or(0, |v| i * (v as usize)))
+            .map(|(id, &len)| (0..len).map(|i| id * (i + file_start[id])).sum::<usize>())
             .sum()
     }
 }
