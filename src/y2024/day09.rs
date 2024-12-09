2f86ab206ec5eec5a4b111e7c48aecc27130fab3
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-09 20:08:54 +0900

    Clean up

--- src/y2024/day09.rs
+++ src/y2024/day09.rs
@@ -1,21 +1,10 @@
 //! <https://adventofcode.com/2024/day/9>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::neighbors,
         parser,
     },
     serde::Serialize,
-    std::collections::HashMap,
-    winnow::{
-        ascii::newline,
-        combinator::{repeat, repeat_till, separated, seq, terminated},
-        token::one_of,
-        PResult, Parser,
-    },
 };
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
@@ -28,7 +17,6 @@ pub struct Puzzle {
 #[aoc(2024, 9)]
 impl AdventOfCode for Puzzle {
     fn parse(&mut self, input: String) -> Result<String, ParseError> {
-        let s = &mut input.as_str();
         self.line = parser::to_digits(input.as_str())?;
         Self::parsed()
     }
@@ -40,9 +28,6 @@ impl AdventOfCode for Puzzle {
                 self.free.push(*x);
             }
         }
-        // dbg!(&self.line);
-        // dbg!(&self.free.len());
-        // dbg!(&self.file.len());
     }
     fn part1(&mut self) -> Self::Output1 {
         let len = self.line.iter().cloned().sum::<usize>();
@@ -50,8 +35,8 @@ impl AdventOfCode for Puzzle {
         let mut p: usize = 0;
         for (i, n) in self.line.iter().enumerate() {
             let v = (i % 2 == 0).then_some(i as u32 / 2);
-            for q in p..p + n {
-                tmp[q] = v;
+            for q in tmp.iter_mut().skip(p).take(*n) {
+                *q = v;
             }
             p += n;
         }
@@ -65,11 +50,7 @@ impl AdventOfCode for Puzzle {
                     }
                     right -= 1;
                 }
-                assert!(tmp[left].is_none());
-                assert!(tmp[right].is_some());
                 tmp.swap(left, right);
-                assert!(tmp[left].is_some());
-                assert!(tmp[right].is_none());
             }
             left += 1;
         }
@@ -88,16 +69,11 @@ impl AdventOfCode for Puzzle {
                 end[i / 2] = p;
             }
             let v = (i % 2 == 0).then_some(i as u32 / 2);
-            for q in p..p + n {
-                tmp[q] = v;
+            for q in tmp.iter_mut().skip(p).take(*n) {
+                *q = v;
             }
             p += n;
         }
-        let after = tmp
-            .iter()
-            .map(|p| p.map_or('.', |x| (b'0' + (x as u8)) as char))
-            .collect::<String>();
-        // println!("{after}");
         'next_file: for (id, &ln) in self.file.iter().enumerate().rev() {
             let mut left: Option<usize> = None;
             let mut right = 0;
@@ -108,19 +84,14 @@ impl AdventOfCode for Puzzle {
                     }
                     if let Some(l) = left {
                         if ln == right - l + 1 {
-                            for i in l..l + ln {
-                                tmp[i] = Some(id as u32);
+                            for i in tmp.iter_mut().skip(l).take(ln) {
+                                *i = Some(id as u32);
                             }
-                            for i in l + ln..len {
-                                if tmp[i] == Some(id as u32) {
-                                    tmp[i] = None;
+                            for i in tmp.iter_mut().take(len).skip(l + ln) {
+                                if *i == Some(id as u32) {
+                                    *i = None;
                                 }
                             }
-                            let after = tmp
-                                .iter()
-                                .map(|p| p.map_or('.', |x| (b'0' + (x as u8)) as char))
-                                .collect::<String>();
-                            // println!("{after}");
                             continue 'next_file;
                         }
                     }
@@ -130,11 +101,6 @@ impl AdventOfCode for Puzzle {
                 right += 1;
             }
         }
-        let after = tmp
-            .iter()
-            .map(|p| p.map_or('.', |x| (b'0' + (x as u8)) as char))
-            .collect::<String>();
-        // println!("{after}");
         tmp.iter()
             .enumerate()
             .map(|(i, x)| x.map_or(0, |v| i * (v as usize)))
