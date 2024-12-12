f3ed0bb86c6adaa70a4c463f8e6f6678012243bb
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-12 17:40:57 +0900

    Y2024 day12 done.

--- src/y2024/day12.rs
+++ src/y2024/day12.rs
@@ -1,21 +1,15 @@
 //! <https://adventofcode.com/2024/day/12>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
         geometric::{GeometricMath, Vec2},
-        parser,
         rect::Rect,
     },
-    rayon::prelude::*,
     serde::Serialize,
-    std::collections::{HashMap, VecDeque},
+    std::collections::{HashMap, HashSet},
     winnow::{
         ascii::{alpha1, newline},
-        combinator::{repeat, repeat_till, separated, seq, terminated},
-        token::one_of,
+        combinator::separated,
         PResult, Parser,
     },
 };
@@ -25,18 +19,38 @@ pub struct Puzzle {
     mapping: Rect<char>,
 }
 impl Puzzle {
-    fn area_for(&self, pos: Vec2) -> usize {
+    fn evaluate1_area(&self, accum: &mut Rect<bool>, pos: Vec2) -> usize {
+        if accum[&pos] {
+            return 0;
+        };
         let Some(c) = self.mapping.get(&pos) else {
             return 0;
         };
         let mut count = 0;
         let mut r: Rect<Option<bool>> = self.mapping.map(|_| None);
         let mut to_visid: Vec<Vec2> = vec![pos];
+        let mut h_segs: HashSet<Vec2> = HashSet::new();
+        let mut v_segs: HashSet<Vec2> = HashSet::new();
         while let Some(p) = to_visid.pop() {
             if let Some(None) = r.get(&p) {
                 if self.mapping[&p] == *c {
                     count += 1;
+                    accum[&p] = true;
                     r[&p] = Some(true);
+
+                    if self.mapping.get(&(p.0 - 1, p.1)) != Some(c) {
+                        h_segs.insert(p);
+                    }
+                    if self.mapping.get(&(p.0 + 1, p.1)) != Some(c) {
+                        h_segs.insert((p.0 + 1, p.1));
+                    }
+                    if self.mapping.get(&(p.0, p.1 - 1)) != Some(c) {
+                        v_segs.insert(p);
+                    }
+                    if self.mapping.get(&(p.0, p.1 + 1)) != Some(c) {
+                        v_segs.insert((p.0, p.1 + 1));
+                    }
+
                     for q in p.neighbors4((0, 0), self.mapping.size()).iter() {
                         to_visid.push(*q);
                     }
@@ -45,7 +59,106 @@ impl Puzzle {
                 }
             }
         }
-        count
+        // println!("({pos:?}) => area: {count}, sides: {}", num_hseg + num_vseg);
+        count * (v_segs.len() + h_segs.len())
+    }
+    fn evaluate2_area(&self, accum: &mut Rect<bool>, pos: Vec2) -> usize {
+        if accum[&pos] {
+            return 0;
+        };
+        let Some(c) = self.mapping.get(&pos) else {
+            return 0;
+        };
+        let mut count = 0;
+        let mut r: Rect<Option<bool>> = self.mapping.map(|_| None);
+        let mut to_visid: Vec<Vec2> = vec![pos];
+        let mut h_segs: HashSet<(Vec2, bool)> = HashSet::new();
+        let mut v_segs: HashSet<(Vec2, bool)> = HashSet::new();
+        while let Some(p) = to_visid.pop() {
+            if let Some(None) = r.get(&p) {
+                if self.mapping[&p] == *c {
+                    count += 1;
+                    accum[&p] = true;
+                    r[&p] = Some(true);
+
+                    if self.mapping.get(&(p.0 - 1, p.1)) != Some(c) {
+                        h_segs.insert((p, false));
+                    }
+                    if self.mapping.get(&(p.0 + 1, p.1)) != Some(c) {
+                        h_segs.insert(((p.0 + 1, p.1), true));
+                    }
+                    if self.mapping.get(&(p.0, p.1 - 1)) != Some(c) {
+                        v_segs.insert((p, false));
+                    }
+                    if self.mapping.get(&(p.0, p.1 + 1)) != Some(c) {
+                        v_segs.insert(((p.0, p.1 + 1), true));
+                    }
+
+                    for q in p.neighbors4((0, 0), self.mapping.size()).iter() {
+                        to_visid.push(*q);
+                    }
+                } else {
+                    r[&p] = Some(false);
+                }
+            }
+        }
+        // build longer segment
+        let hss: HashMap<usize, Vec<(usize, bool)>> = h_segs.iter().fold(
+            HashMap::new(),
+            |mut m: HashMap<usize, Vec<(usize, bool)>>, &(pos, spin): &((isize, isize), bool)| {
+                m.entry(pos.0 as usize)
+                    .or_default()
+                    .push((pos.1 as usize, spin));
+                m
+            },
+        );
+        let num_hseg: usize = hss
+            .into_iter()
+            .map(|(_, mut v)| {
+                v.sort();
+                let mut num = 1;
+                let mut end = v[0].0 + 1;
+                let mut spin = v[0].1;
+                for &(st, sp) in v.iter().skip(1) {
+                    if end != st || spin != sp {
+                        num += 1;
+                    }
+                    end = st + 1;
+                    spin = sp;
+                }
+                num
+            })
+            .sum::<usize>();
+
+        let vss: HashMap<usize, Vec<(usize, bool)>> = v_segs.iter().fold(
+            HashMap::new(),
+            |mut m: HashMap<usize, Vec<(usize, bool)>>, &(pos, spin): &((isize, isize), bool)| {
+                m.entry(pos.1 as usize)
+                    .or_default()
+                    .push((pos.0 as usize, spin));
+                m
+            },
+        );
+        let num_vseg: usize = vss
+            .into_iter()
+            .map(|(_, mut v)| {
+                v.sort();
+                let mut num = 1;
+                let mut end = v[0].0 + 1;
+                let mut spin = v[0].1;
+                for &(st, sp) in v.iter().skip(1) {
+                    if end != st || spin != sp {
+                        num += 1;
+                    }
+                    end = st + 1;
+                    spin = sp;
+                }
+                num
+            })
+            .sum::<usize>();
+
+        // println!("({pos:?}) => area: {count}, sides: {}", num_hseg + num_vseg);
+        count * (num_hseg + num_vseg)
     }
 }
 
@@ -66,21 +179,17 @@ impl AdventOfCode for Puzzle {
         Self::parsed()
     }
     fn part1(&mut self) -> Self::Output1 {
+        let mut accum: Rect<bool> = self.mapping.map(|_| false);
         self.mapping
             .iter()
-            .map(|(pos, c)| {
-                let a = pos
-                    .neighbors4((-1, -1), self.mapping.size().add(&(1, 1)))
-                    .iter()
-                    .map(|q| (self.mapping.get(q) != Some(c)) as usize)
-                    .sum::<usize>();
-                let b = self.area_for(pos);
-                println!("{pos:?}: {a}, {b} = {}", a * b);
-                a * b
-            })
+            .map(|(pos, _)| self.evaluate1_area(&mut accum, pos))
             .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
-        self.mapping.iter().map(|(pos, c)| 1).sum()
+        let mut accum: Rect<bool> = self.mapping.map(|_| false);
+        self.mapping
+            .iter()
+            .map(|(pos, _)| self.evaluate2_area(&mut accum, pos))
+            .sum()
     }
 }
