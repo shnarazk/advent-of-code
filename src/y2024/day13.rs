f3b8771f22c4b5cb3b2b548a987e970e78e6abe9
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-13 16:39:29 +0900

    Clean up

--- src/y2024/day13.rs
+++ src/y2024/day13.rs
@@ -1,32 +1,27 @@
 //! <https://adventofcode.com/2024/day/13>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
     crate::{
         framework::{aoc, AdventOfCode, ParseError},
-        geometric::{neighbors, GeometricMath},
-        math::{gcd, lcm},
-        parser::{self, parse_usize},
+        geometric::{Dim2, GeometricMath},
+        parser::parse_usize,
     },
     rayon::prelude::*,
-    rustc_data_structures::fx::{FxHashMap, FxHasher},
     serde::Serialize,
-    std::{collections::HashMap, hash::BuildHasherDefault},
     winnow::{
         ascii::newline,
-        combinator::{repeat, repeat_till, separated, seq, terminated},
+        combinator::{separated, seq},
         token::one_of,
         PResult, Parser,
     },
 };
+type Setting = (Dim2<usize>, Dim2<usize>, Dim2<usize>);
 
 #[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct Puzzle {
-    line: Vec<((usize, usize), (usize, usize), (usize, usize))>,
+    line: Vec<Setting>,
 }
 
-fn parse_button(s: &mut &str) -> PResult<(usize, usize)> {
+fn parse_button(s: &mut &str) -> PResult<Dim2<usize>> {
     seq!(_: "Button ", _: one_of(['A', 'B']),
         _: ": X+", parse_usize,
         _: ", Y+", parse_usize,
@@ -34,7 +29,7 @@ fn parse_button(s: &mut &str) -> PResult<(usize, usize)> {
     .parse_next(s)
 }
 
-fn parse_prize(s: &mut &str) -> PResult<(usize, usize)> {
+fn parse_prize(s: &mut &str) -> PResult<Dim2<usize>> {
     seq!(_: "Prize",
         _: ": X=", parse_usize,
         _: ", Y=", parse_usize,
@@ -42,11 +37,11 @@ fn parse_prize(s: &mut &str) -> PResult<(usize, usize)> {
     .parse_next(s)
 }
 
-fn parse_block(s: &mut &str) -> PResult<((usize, usize), (usize, usize), (usize, usize))> {
+fn parse_block(s: &mut &str) -> PResult<Setting> {
     seq!(parse_button, _: newline, parse_button, _: newline, parse_prize).parse_next(s)
 }
 
-fn parse(s: &mut &str) -> PResult<Vec<((usize, usize), (usize, usize), (usize, usize))>> {
+fn parse(s: &mut &str) -> PResult<Vec<Setting>> {
     separated(1.., parse_block, (newline, newline)).parse_next(s)
 }
 
@@ -62,22 +57,18 @@ impl AdventOfCode for Puzzle {
     }
     fn part1(&mut self) -> Self::Output1 {
         self.line
-            .iter()
+            .par_iter()
             .map(|(a, b, g)| solve(g, a, b).map_or(0, |(a, b)| a * 3 + b))
             .sum()
     }
     fn part2(&mut self) -> Self::Output2 {
         self.line
-            .iter()
+            .par_iter()
             .map(|(a, b, g)| {
                 let gg = g.add(&(10_000_000_000_000, 10_000_000_000_000));
                 if let Some((i, j)) = solve(&gg, a, b) {
-                    assert_eq!(gg.0, a.0 * i + b.0 * j);
-                    assert_eq!(gg.1, a.1 * i + b.1 * j);
-                    // let bb = b.mul_scalar(2);
-                    // if let Some((ii, jj)) = solve(&gg, a, &bb) {
-                    //     dbg!(3 * i + j, 3 * ii + 2 * jj);
-                    // }
+                    debug_assert_eq!(gg.0, a.0 * i + b.0 * j);
+                    debug_assert_eq!(gg.1, a.1 * i + b.1 * j);
                     return 3 * i + j;
                 }
                 0
@@ -86,13 +77,7 @@ impl AdventOfCode for Puzzle {
     }
 }
 
-fn solve(goal: &(usize, usize), a: &(usize, usize), b: &(usize, usize)) -> Option<(usize, usize)> {
-    if lcm(a.0, b.0) == lcm(a.1, b.1) {
-        println!("lcm: {a:?}, {b:?} == ({},{})", lcm(a.0, b.0), lcm(a.1, b.1));
-    }
-    // if gcd(a.0, b.0) == gcd(a.1, b.1) {
-    //     println!("gcd: {a:?}, {b:?} == ({},{})", gcd(a.0, b.0), gcd(a.1, b.1));
-    // }
+fn solve(goal: &Dim2<usize>, a: &Dim2<usize>, b: &Dim2<usize>) -> Option<Dim2<usize>> {
     /*
       . a.0 * i + b.0 * j = goal.0
       . a.1 * i + b.1 * j = goal.1
@@ -122,11 +107,6 @@ fn solve(goal: &(usize, usize), a: &(usize, usize), b: &(usize, usize)) -> Optio
                 return Some((i, j));
             }
         }
-    } else {
-        /*
-          . a.0 * i = goal.0
-          . a.1 * i = goal.1
-        */
     }
     None
 }
