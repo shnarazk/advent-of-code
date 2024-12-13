d36bdcbc93b03a809f6a25ffcc21223b5ba0b17a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-14 00:00:54 +0900

    Clean up

--- src/y2024/day13.rs
+++ src/y2024/day13.rs
@@ -53,23 +53,20 @@ impl AdventOfCode for Puzzle {
         Self::parsed()
     }
     fn part1(&mut self) -> Self::Output1 {
-        self.line
-            .par_iter()
-            .map(|(a, b, g)| solve(g, a, b).map_or(0, |(i, j)| i * 3 + j))
-            .sum()
+        self.line.par_iter().map(|(a, b, g)| solve(g, a, b)).sum()
     }
     fn part2(&mut self) -> Self::Output2 {
         self.line
             .par_iter()
             .map(|(a, b, g)| {
                 let gg = g.add(&(10_000_000_000_000, 10_000_000_000_000));
-                solve(&gg, a, b).map_or(0, |(i, j)| 3 * i + j)
+                solve(&gg, a, b)
             })
             .sum()
     }
 }
 
-fn solve(goal: &Dim2<usize>, a: &Dim2<usize>, b: &Dim2<usize>) -> Option<Dim2<usize>> {
+fn solve(goal: &Dim2<usize>, a: &Dim2<usize>, b: &Dim2<usize>) -> usize {
     // . a.0 * i + b.0 * j = goal.0
     // . a.1 * i + b.1 * j = goal.1
     // i = (goal.0 - b.0 * j) / a.0
@@ -78,16 +75,23 @@ fn solve(goal: &Dim2<usize>, a: &Dim2<usize>, b: &Dim2<usize>) -> Option<Dim2<us
     //   . a.0 * (goal.1 - b.1 * j) + a.1 * b.0 * j = a.1 * goal.0
     //   . a.0 * goal.1 - a.0 * b.1 * j + a.1 * b.0 * j = a.1 * goal.0
     //   . (a.1 * b.0 - a.0 * b.1) * j = a.1 * goal.0 - a.0 * goal.1
+    //
+    // b.0 * j = goal.0 - a.0 * i
+    // b.1 * j = goal.1 - a.1 * i
+    // b.1 * (goal.0 - a.0 * i) = b.0 * (goal.1 - a.1 * i)
+    // b.1 * goal.0 - a.0 * b.1 * i = b.0 * goal.1 - a.1 * b.0 * i
+    // (a.1 * b.0 - a.0 * b.1) * i = b.0 * goal.1 - b.1 * goal.0
     if a.1 * b.0 != a.0 * b.1 {
         let tmp1 = (a.1 * b.0).abs_diff(a.0 * b.1);
-        let tmp2 = (a.1 * goal.0).abs_diff(a.0 * goal.1);
-        if tmp2 % tmp1 == 0 {
-            let j = tmp2 / tmp1;
-            if (goal.0 - b.0 * j) % a.0 == 0 {
-                let i = (goal.0 - b.0 * j) / a.0;
-                return Some((i, j));
-            }
+        let tmp2 = (b.0 * goal.1).abs_diff(b.1 * goal.0);
+        let tmp3 = (a.1 * goal.0).abs_diff(a.0 * goal.1);
+        let i = tmp2 / tmp1;
+        let im = tmp2 % tmp1;
+        let j = tmp3 / tmp1;
+        let jm = tmp3 % tmp1;
+        if im == 0 && jm == 0 {
+            return 3 * i + j;
         }
     }
-    None
+    0
 }
