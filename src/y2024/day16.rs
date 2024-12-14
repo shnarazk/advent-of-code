a5de83287ba64d940ac1643d68b74e79f0a50eb4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-14 16:34:57 +0900

    Prepare for day15-19

---
+++ src/y2024/day16.rs
@@ -0,0 +1,58 @@
+//! <https://adventofcode.com/2024/day/16>
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+use {
+    crate::{
+        framework::{aoc, AdventOfCode, ParseError},
+        geometric::neighbors,
+        parser::parse_usize,
+    },
+    rayon::prelude::*,
+    rustc_data_structures::fx::{FxHashMap, FxHasher},
+    serde::Serialize,
+    std::{collections::HashMap, hash::BuildHasherDefault},
+    winnow::{
+        ascii::newline,
+        combinator::{repeat, repeat_till, separated, terminated},
+        token::one_of,
+        PResult, Parser,
+    },
+};
+
+#[derive(Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
+pub struct Puzzle {
+    line: Vec<()>,
+}
+
+// impl Default for Puzzle {
+//     fn default() -> Self {
+//         Puzzle { }
+//     }
+// }
+
+// fn parse(s: &mut &str) -> PResult<()> {}
+
+#[aoc(2024, 16)]
+impl AdventOfCode for Puzzle {
+    // const DELIMITER: &'static str = "\n";
+    // fn parse(&mut self, input: String) -> Result<String, ParseError> {
+    //     let s = &mut input.as_str();
+    //     self.line = parser(s)?;
+    //     Self::parsed()
+    // }
+    // fn insert(&mut self, block: &str) -> Result<(), ParseError> {
+    //     Ok(())
+    // }
+    fn end_of_data(&mut self) {
+        dbg!(&self.line);
+    }
+    fn part1(&mut self) -> Self::Output1 {
+        let mut ret: FxHashMap<usize, usize> =
+            HashMap::<usize, usize, BuildHasherDefault<FxHasher>>::default();
+        1
+    }
+    fn part2(&mut self) -> Self::Output2 {
+        2
+    }
+}
