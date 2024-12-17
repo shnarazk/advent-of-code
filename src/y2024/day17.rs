dab7608c7e2a66185337d23a87ec92a87b369083
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-18 00:01:48 +0900

    Y2024 day17 done.

--- src/y2024/day17.rs
+++ src/y2024/day17.rs
@@ -7,11 +7,17 @@ use {
         framework::{aoc_at, AdventOfCode, ParseError},
         geometric::neighbors,
         parser::{parse_isize, parse_usize},
+        progress,
     },
+    itertools::Itertools,
     rayon::prelude::*,
     rustc_data_structures::fx::{FxHashMap, FxHasher},
     serde::Serialize,
-    std::{collections::HashMap, hash::BuildHasherDefault},
+    std::{
+        cmp::{Ordering, Reverse},
+        collections::{BinaryHeap, HashMap},
+        hash::BuildHasherDefault,
+    },
     winnow::{
         ascii::{dec_uint, newline},
         combinator::{repeat, repeat_till, separated, seq, terminated},
@@ -25,10 +31,26 @@ pub struct Puzzle {
     reg: [isize; 3],
     code: Vec<u8>,
     ip: usize,
-    output: String,
+    input: String,
+    output: Vec<u8>,
+    break_flag: bool,
+}
+
+fn to_num(vec: &[u8]) -> isize {
+    let mut result: isize = 0;
+    for d in vec.iter() {
+        result *= 8;
+        result += *d as isize;
+    }
+    result
 }
 
 impl Puzzle {
+    fn reset(&self, i: isize) -> Puzzle {
+        let mut pc = self.clone();
+        pc.reg[0] = i;
+        pc
+    }
     fn fetch(&mut self) -> Option<(u8, u8)> {
         if let Some(a) = self.code.get(self.ip).map(|p| *p) {
             if let Some(b) = self.code.get(self.ip + 1).map(|p| *p) {
@@ -79,10 +101,10 @@ impl Puzzle {
             }
             5 => {
                 let val = self.combo_opland(lit_opl) % 8;
-                if !self.output.is_empty() {
-                    self.output.push(',');
+                self.output.push(val as u8);
+                if self.break_flag && self.output != self.code[0..self.output.len()] {
+                    self.ip = self.code.len();
                 }
-                self.output.push_str(format!("{val}").as_str());
                 self.inc_ip();
             }
             6 => {
@@ -102,12 +124,19 @@ impl Puzzle {
             _ => (),
         }
     }
-    fn run(&mut self) {
+    fn run(&mut self) -> &[u8] {
         self.ip = 0;
         while let Some((c, l)) = self.fetch() {
             self.execute(c, l);
         }
-        println!("{}", self.output);
+        &self.output
+    }
+    fn run_breakable(&mut self) {
+        self.break_flag = true;
+        self.ip = 0;
+        while let Some((c, l)) = self.fetch() {
+            self.execute(c, l);
+        }
     }
 }
 
@@ -143,6 +172,7 @@ impl AdventOfCode for Puzzle {
         Self::parsed()
     }
     fn end_of_data(&mut self) {
+        self.input = self.code.iter().map(|n| format!("{n}")).join(",");
         // dbg!(&self.reg);
         // self.reg[2] = 9;
         // let mut pc = Puzzle {
@@ -155,10 +185,106 @@ impl AdventOfCode for Puzzle {
     }
     fn part1(&mut self) -> Self::Output1 {
         self.run();
-        dbg!(&self.reg);
-        self.output.clone()
+        self.output.iter().map(|n| format!("{n}")).join(",")
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        // 2,4(A): B(3bit) <- A % 8
+        // 1,5   : B(3bit) <- B ^ b101
+        // 7,5(B): C <- A / 2**B
+        // 0,3   : A <- A / 8
+        // 4.1   : B <- B ^ C
+        // 1,6   : B <- B ^ 6
+        // 5,5(B): output <- B % 8
+        // 3,0   : if A != 0 { goto 0 }
+        //
+        let mut cond = self.code.clone();
+        cond.reverse();
+        dbg!(&cond);
+        let mut result: Vec<u8> = Vec::new();
+        fn compatible(base: &[u8], cand: &[u8]) -> bool {
+            let len = cand.len();
+            cand.iter()
+                .enumerate()
+                .all(|(i, n)| *n == base[len - i - 1])
+        }
+        type SEARCH = (usize, Vec<u8>);
+        let mut to_visit: BinaryHeap<Reverse<SEARCH>> = BinaryHeap::new();
+        to_visit.push(Reverse((0, Vec::new())));
+        while let Some(Reverse((done, vec))) = to_visit.pop() {
+            if vec.len() == cond.len() {
+                dbg!(vec);
+                let ans = to_num(&[3, 0, 6, 2, 3, 4, 6, 3, 1, 2, 2, 0, 4, 2, 3, 3]);
+                let mut pc = self.reset(ans);
+                pc.run();
+                dbg!(&pc.output);
+                return ans as usize;
+            }
+            let base = to_num(&vec);
+            for i in 0..7_u8 {
+                let cand = base * 8 + i as isize;
+                let mut pc = self.reset(cand);
+                if compatible(&cond, pc.run()) {
+                    let mut v = vec.clone();
+                    v.push(i);
+                    to_visit.push(Reverse((done + 1, v)));
+                }
+            }
+        }
+        'next: for target in cond.iter() {
+            let base = to_num(&result);
+            for i in 0..7_u8 {
+                let cand = base * 8 + i as isize;
+                let mut pc = self.reset(cand);
+                if compatible(&cond, pc.run()) {
+                    result.push(i);
+                    dbg!(&result);
+                    continue 'next;
+                }
+            }
+            dbg!(&result);
+            break;
+        }
+
+        // for i in 0..64 {
+        //     let mut pc = self.clone();
+        //     pc.reg[0] = i;
+        //     pc.run();
+        //     println!("{i:>2o} {:?}", &pc.output);
+        // }
+        return 0;
+        dbg!(self.code.len());
+        let mut digits = 0;
+        for i in 2_isize.pow(30)..=2_isize.pow(48) {
+            let mut pc = self.clone();
+            pc.reg[0] = i;
+            pc.run();
+            if pc.code == pc.output {
+                return i as usize;
+            }
+            if i % 100 == 0 {
+                progress!(format!("{:>2.6}", i as f64 / 2_isize.pow(18) as f64));
+            }
+            digits = digits.max(pc.output.len());
+        }
+        digits
+        // let mut len = 0;
+        /* println!("      {}", &self.input);
+        // for i in 2_isize.pow(16)..=2_isize.pow(18) {
+        //     let mut pc = self.clone();
+        //     pc.reg[0] = i;
+        //     pc.run();
+        //     println!("{} => {}", i, &pc.output);
+        // } */
+        /* for i in 0..=64 {
+            let mut pc = self.clone();
+            pc.reg[0] = i;
+            pc.run_breakable();
+            println!("{:o}: {:?}", i, pc.output);
+            /* if i % 100 == 0 {
+                progress!(format!("{:>2.6}", (i as f64).log2()));
+            } */
+            // println!("{}:{}", i, pc.output.len());
+        }
+        0 */
     }
 }
