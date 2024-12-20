bb4a65bec7c3aae8f96e9a2e235a906ccb57382e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-20 12:21:28 +0900

    add tests:   src/array.rs

--- src/array.rs
+++ src/array.rs
@@ -8,7 +8,6 @@ pub fn rotate_clockwise<I: Clone>(m: Vec<Vec<I>>) -> Vec<Vec<I>> {
     }
     n
 }
-
 #[allow(clippy::ptr_arg)]
 pub fn transpose<I: Clone>(v: &Vec<Vec<I>>) -> Vec<Vec<I>> {
     let h = v.len();
@@ -16,4 +15,24 @@ pub fn transpose<I: Clone>(v: &Vec<Vec<I>>) -> Vec<Vec<I>> {
     (0..w)
         .map(|x| (0..h).map(|y| v[y][x].clone()).collect::<Vec<_>>())
         .collect::<Vec<_>>()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_rotate_clockwise() {
+        let input = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let expected_output = vec![vec![7, 4, 1], vec![8, 5, 2], vec![9, 6, 3]];
+
+        assert_eq!(rotate_clockwise(input), expected_output);
+    }
+    #[test]
+    fn test_transpose() {
+        let input = vec![vec![1, 2, 3], vec![4, 5, 6], vec![7, 8, 9]];
+        let expected_output = vec![vec![1, 4, 7], vec![2, 5, 8], vec![3, 6, 9]];
+
+        assert_eq!(transpose(&input), expected_output);
+    }
 }
