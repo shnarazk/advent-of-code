af61a89156bd3f94c5581ed25d92cc1192a727f6
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-22 14:28:50 +0900

    Y2024 day22 part1 done

--- src/y2024/day22.rs
+++ src/y2024/day22.rs
@@ -26,27 +26,61 @@ use {
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<usize>,
 }
 
-fn parse(s: &mut &str) -> PResult<()> {
-    ().parse_next(s)
+fn parse(s: &mut &str) -> PResult<Vec<usize>> {
+    separated(1.., parse_usize, newline).parse_next(s)
 }
 
 #[aoc(2024, 22)]
 impl AdventOfCode for Puzzle {
     fn parse(&mut self, input: String) -> Result<String, ParseError> {
-        // self.line = parse(&mut input.as_str())?;
+        self.line = parse(&mut input.as_str())?;
         Self::parsed()
     }
     fn end_of_data(&mut self) {
-        dbg!(&self.line);
+        // let mut secret = 123;
+        // dbg!(next(&mut secret));
+        // dbg!(next(&mut secret));
+        // dbg!(next(&mut secret));
+        // dbg!(next(&mut secret));
+        // dbg!(next(&mut secret));
+        // dbg!(next(&mut secret));
     }
     fn part1(&mut self) -> Self::Output1 {
-        // let mut _: FxHashMap<_, _> = HashMap::<_, _, BuildHasherDefault<FxHasher>>::default();
-        1
+        self.line
+            .iter()
+            .map(|seed| generate(*seed, 2000))
+            .sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
         2
     }
+}
+
+fn mix(seed: &mut usize, value: usize) {
+    let ret = *seed ^ value;
+    *seed = ret;
+}
+
+fn prune(seed: &mut usize) {
+    let ret = *seed % 16777216;
+    *seed = ret;
+}
+fn next(secret: &mut usize) -> usize {
+    let s1 = *secret * 64;
+    mix(secret, s1);
+    prune(secret);
+    let s2 = *secret / 32;
+    mix(secret, s2);
+    prune(secret);
+    let s3 = *secret * 2048;
+    mix(secret, s3);
+    prune(secret);
+    *secret
+}
+
+fn generate(seed: usize, limit: usize) -> usize {
+    (0..limit).fold(seed, |mut n, _| next(&mut n))
 }
