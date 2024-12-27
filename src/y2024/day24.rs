983cddc9d22d3311c556db9ff3f8bd5e7d916719
Narazaki, Shuji <shujinarazaki@protonmail.com>
2024-12-28 01:28:41 +0900

    Clean up

--- src/y2024/day24.rs
+++ src/y2024/day24.rs
@@ -1,26 +1,19 @@
 //! <https://adventofcode.com/2024/day/24>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 #![allow(clippy::type_complexity)]
 use {
     crate::{
-        color,
         framework::{aoc_at, AdventOfCode, ParseError},
         parser::parse_usize,
         progress,
     },
     itertools::Itertools,
-    rayon::{prelude::*, slice::Windows},
+    rayon::prelude::*,
     rustc_data_structures::fx::{FxHashMap, FxHashSet, FxHasher},
-    serde::{Deserialize, Serialize},
+    serde::Serialize,
     std::{
         cmp::Reverse,
         collections::{BinaryHeap, HashMap, HashSet},
-        fmt,
-        fs::File,
         hash::BuildHasherDefault,
-        io::prelude::*,
         sync::OnceLock,
     },
     winnow::{
@@ -30,8 +23,6 @@ use {
         PResult, Parser,
     },
 };
-
-const PERSISTENT_STORAGE: &'static str = "misc/2024/day24-diff-table.json";
 
 /// convert a `usize` to its binary representation
 fn int_to_bit_vector(mut n: usize, l: usize) -> Vec<bool> {
@@ -59,18 +50,6 @@ fn fmt(v: &[bool]) -> String {
     )
 }
 
-fn fmt_(v: &[usize]) -> String {
-    format!(
-        "{}|{}|0({})",
-        v.len(),
-        v.iter()
-            .rev()
-            .map(|n| format!("{n:>2}"))
-            .collect::<String>(),
-        v.iter().filter(|n| 0 < **n).count(),
-    )
-}
-
 #[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd, Serialize)]
 enum Gate {
     #[default]
@@ -85,16 +64,12 @@ type GateSpec = (Gate, Wire, Wire, Wire);
 
 /// convert from 'ord', 0 to 43, to wire
 fn ord_to_wire(n: usize, prefix: u8) -> Wire {
-    (
-        prefix,
-        (b'0' + ((n / 10) as u8)) as u8,
-        (b'0' + ((n % 10) as u8)) as u8,
-    )
+    (prefix, b'0' + ((n / 10) as u8), b'0' + ((n % 10) as u8))
 }
 
 /// convert a `Wire` type to its 'ord', 0 to 43
 fn wire_to_ord((_, b, c): &Wire) -> usize {
-    (((*b as u8) - b'0') as usize) * 10 + (((*c as u8) - b'0') as usize)
+    ((*b - b'0') as usize) * 10 + ((*c - b'0') as usize)
 }
 
 /// convert  `Wire` to its string representation
@@ -132,23 +107,21 @@ impl Adder {
         }
 
         let mut propagated = true;
-        // let base_config = BASE_LINK.get().unwrap();
         while propagated {
             propagated = false;
             for (gate, output) in self.mapper.iter() {
-                match (values.get(&gate.1), values.get(&gate.2), values.get(output)) {
-                    (Some(b1), Some(b2), None) => {
-                        values.insert(
-                            *output,
-                            match gate.0 {
-                                Gate::And => b1 & b2,
-                                Gate::Or => b1 | b2,
-                                Gate::Xor => b1 ^ b2,
-                            },
-                        );
-                        propagated = true;
-                    }
-                    _ => (),
+                if let (Some(b1), Some(b2), None) =
+                    (values.get(&gate.1), values.get(&gate.2), values.get(output))
+                {
+                    values.insert(
+                        *output,
+                        match gate.0 {
+                            Gate::And => b1 & b2,
+                            Gate::Or => b1 | b2,
+                            Gate::Xor => b1 ^ b2,
+                        },
+                    );
+                    propagated = true;
                 }
             }
         }
@@ -253,7 +226,6 @@ impl Descriptor {
             });
         let x = 27339360157359;
         let y = 34293989942303;
-        let added = adder.add(x, y).0;
         let samples = [x, y, 0, 1];
         let one_more = samples
             .iter()
@@ -285,7 +257,7 @@ impl Descriptor {
         let ret1 = ret
             .iter()
             .zip(one_more.iter())
-            .map(|(a, b)| a + (*b as usize))
+            .map(|(a, b)| a + *b)
             .collect::<Vec<_>>();
         if ret1.iter().all(|n| *n == 0) {
             None
@@ -294,13 +266,19 @@ impl Descriptor {
         }
     }
     /// return `(down_tree, up_tree)`
-    fn wire_trees(&self) -> (HashMap<Wire, HashSet<Wire>>, HashMap<Wire, HashSet<Wire>>) {
+    fn wire_trees(
+        &self,
+    ) -> (
+        FxHashMap<Wire, FxHashSet<Wire>>,
+        FxHashMap<Wire, FxHashSet<Wire>>,
+    ) {
         let adder = self.build_adder();
-        let base_links = BASE_LINK.get().unwrap();
-        let mut wires: HashSet<Wire> = HashSet::new();
-        let mut down_tree: HashMap<Wire, HashSet<Wire>> = HashMap::new();
-        let mut up_tree: HashMap<Wire, HashSet<Wire>> = HashMap::new();
-        for ((g, i1, i2), o) in adder.mapper.iter() {
+        let mut wires: FxHashSet<Wire> = HashSet::<Wire, BuildHasherDefault<FxHasher>>::default();
+        let mut down_tree: FxHashMap<Wire, FxHashSet<Wire>> =
+            HashMap::<_, _, BuildHasherDefault<FxHasher>>::default();
+        let mut up_tree: FxHashMap<Wire, FxHashSet<Wire>> =
+            HashMap::<_, _, BuildHasherDefault<FxHasher>>::default();
+        for ((_, i1, i2), o) in adder.mapper.iter() {
             wires.insert(*i1);
             wires.insert(*i2);
             wires.insert(*o);
@@ -311,8 +289,8 @@ impl Descriptor {
         }
         (down_tree, up_tree)
     }
-    fn wire_affects(&self, tree: &HashMap<Wire, HashSet<Wire>>, wire: Wire) -> HashSet<Wire> {
-        let mut subtree: HashSet<Wire> = HashSet::new();
+    fn wire_affects(&self, tree: &FxHashMap<Wire, FxHashSet<Wire>>, wire: Wire) -> FxHashSet<Wire> {
+        let mut subtree: FxHashSet<Wire> = HashSet::<_, BuildHasherDefault<FxHasher>>::default();
         if let Some(linked) = tree.get(&wire) {
             let mut to_visit: Vec<Wire> = linked.iter().cloned().collect::<Vec<_>>();
             subtree.insert(wire);
@@ -342,26 +320,15 @@ impl Descriptor {
     }
 }
 
-fn wrong_bit(memo: &mut HashMap<Vec<Wire>, Option<usize>>, swaps: Vec<Wire>) -> Option<usize> {
-    unimplemented!()
-}
-
 static WIRE_NAMES: OnceLock<HashSet<Wire>> = OnceLock::new();
 static BASE_LINK: OnceLock<Vec<(Gate, Wire, Wire, Wire)>> = OnceLock::new();
 static INPUT_BITS: OnceLock<usize> = OnceLock::new();
-static BASE_OUTPUT: OnceLock<HashMap<(usize, usize), usize>> = OnceLock::new();
-static DIFF_MAP: OnceLock<HashMap<(Wire, Wire), Vec<usize>>> = OnceLock::new();
 static BASE_PROPAGATION_TABLE: OnceLock<FxHashMap<(Gate, Wire, Wire), Wire>> = OnceLock::new();
 
 fn build_swapped_pair((pick1, pick2): &(Wire, Wire)) -> (GateSpec, GateSpec) {
     assert!(![b'x', b'y'].contains(&pick1.0));
     assert!(![b'x', b'y'].contains(&pick2.0));
 
-    let (p1, p2) = if pick1 < pick2 {
-        (pick1, pick2)
-    } else {
-        (pick2, pick1)
-    };
     let specs = BASE_LINK.get().unwrap();
     let spec1: &GateSpec = specs.iter().find(|(_, _, _, o)| *o == *pick1).unwrap();
     let spec2: &GateSpec = specs.iter().find(|(_, _, _, o)| *o == *pick2).unwrap();
@@ -453,8 +420,7 @@ impl AdventOfCode for Puzzle {
         let links = BASE_LINK.get().unwrap();
         let mut data = links
             .iter()
-            .enumerate()
-            .map(|(i, (g, i1, i2, o))| {
+            .map(|(_, i1, i2, o)| {
                 (
                     wire_to_string(o),
                     vec![wire_to_string(i1), wire_to_string(i2)],
@@ -486,7 +452,6 @@ impl AdventOfCode for Puzzle {
         Adder::new(&Vec::new()).add(x, y).0
     }
     fn part2(&mut self) -> Self::Output2 {
-        let input_bits = *INPUT_BITS.get().unwrap();
         let wire_names = WIRE_NAMES.get().unwrap();
         let mut to_visit: BinaryHeap<Reverse<Descriptor>> = BinaryHeap::new();
 
@@ -503,7 +468,7 @@ impl AdventOfCode for Puzzle {
             }
             visited.insert(desc.clone());
             best = best.min(desc.num_broken);
-            let (d_tree, u_tree) = desc.wire_trees();
+            let (d_tree, _) = desc.wire_trees();
             if let Some(brokens) = desc.check_correctness() {
                 let num_broken = brokens.iter().filter(|n| 0 < **n).count();
                 // if adder.num_broken < num_broken {
@@ -520,44 +485,16 @@ impl AdventOfCode for Puzzle {
                 ));
                 let index = brokens.iter().position(|n| 0 < *n).unwrap();
                 if desc.overrides.len() < 4 {
-                    let input_tree = desc.wire_affects(&u_tree, ord_to_wire(index, b'z'));
-                    let inputs = input_tree
-                        .iter()
-                        .filter(|w| w.0 == b'x' || w.0 == b'y')
-                        .sorted()
-                        .cloned()
-                        .collect::<Vec<_>>();
-                    let required = (0..=index)
-                        .flat_map(|i| [ord_to_wire(i, b'x'), ord_to_wire(i, b'y')])
-                        .collect::<Vec<_>>();
-                    let missing = required
-                        .iter()
-                        .filter(|w| !inputs.contains(w))
-                        .cloned()
-                        .collect::<Vec<_>>();
-                    // println!(
-                    //     "bit{}: {:?}",
-                    //     index,
-                    //     missing.iter().map(wire_to_string).collect::<Vec<_>>()
-                    // );
-                    // find upper-side nodes (wires)
                     let related_wires = wire_names
                         .iter()
                         .filter(|w| ![b'x', b'y'].contains(&w.0))
                         .filter(|&w| {
-                            let w_input_tree = desc.wire_affects(&u_tree, *w);
-                            let w_inputs = w_input_tree
-                                .iter()
-                                .filter(|w| [b'x', b'y'].contains(&w.0))
-                                .sorted()
-                                .cloned()
-                                .collect::<Vec<_>>();
                             let w_output_tree = desc.wire_affects(&d_tree, *w);
                             let w_level = w_output_tree
                                 .iter()
                                 .filter(|w| w.0 == b'z')
                                 .min()
-                                .map_or(usize::MAX, |w| wire_to_ord(w));
+                                .map_or(usize::MAX, wire_to_ord);
                             // index <= w_level && w_inputs.iter().all(|w| required.contains(w))
                             w_level <= index
                             // && index <= wire_to_ord(w_outputs.first().unwrap())
@@ -578,11 +515,6 @@ impl AdventOfCode for Puzzle {
                                 if visited.contains(&new_adder) {
                                     continue;
                                 }
-                                // println!(
-                                //     " - try {index}: {}-{}",
-                                //     wire_to_string(upper),
-                                //     wire_to_string(lower)
-                                // );
                                 to_visit.push(Reverse(new_adder));
                             }
                         }
@@ -613,17 +545,6 @@ impl AdventOfCode for Puzzle {
                 }
             }
         }
-
         "".to_string()
-
-        /*
-        if BASE_OUTPUT.get().is_none() {
-            let hash = adder.all_outputs();
-            let input_bits = *INPUT_BITS.get().unwrap();
-            assert_eq!(hash.len(), input_bits * (input_bits - 1) / 2);
-            BASE_OUTPUT.set(hash).unwrap();
-        }
-        self.build_search_table();
-        */
     }
 }
