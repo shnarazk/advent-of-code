07c9c8c862cf053d10b0a89d9836bbb94c84c000
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-01-06 17:48:40 +0900

    (Lean4) Y2024 day8 done.

--- Lean4/Y2024/Day08.lean
+++ Lean4/Y2024/Day08.lean
@@ -15,6 +15,8 @@ instance : Hashable Int64 where hash a := a.toUInt64
 
 def add (a b : Vec2) : Vec2 := (a.1 + b.1, a.2 + b.2)
 def sub (a b : Vec2) : Vec2 := (a.1 - b.1, a.2 - b.2)
+def contains (size pos : Vec2) : Bool :=
+  0 ≤ pos.1 && pos.1 < size.1 && 0 ≤ pos.2 && pos.2 < size.2
 
 structure Input where
   anntena : List (Char × Vec2)
@@ -46,21 +48,19 @@ def parse : String → Option Input := AoCParser.parse parser
 end parser
 
 namespace Part1
-def contains (size pos : Vec2) : Bool :=
-  0 ≤ pos.1 && pos.1 < size.1 && 0 ≤ pos.2 && pos.2 < size.2
+
+partial def inbound_antinodes' (size a offset : Vec2) : List Vec2 :=
+  let next := add a offset
+  if contains size next then [next] else []
 
 def inbound_antinodes (size : Vec2) (a b : Char × Vec2) : List Vec2 :=
   if a.1 == b.1
-    then
-      let d := sub b.2 a.2
-      [sub a.2 d, add b.2 d].filter (fun p ↦ contains size p)
+    then inbound_antinodes' size a.2 (sub a.2 b.2) ++ inbound_antinodes' size b.2 (sub b.2 a.2)
     else []
 
 def solve (input : Input) : Nat :=
   input.anntena.enum.map
-    (fun (i, a1) ↦
-        input.anntena.drop (i + 1)
-            |>.flatMap (fun a2 ↦ inbound_antinodes input.size a1 a2))
+    (fun (i, a1) ↦ input.anntena.drop (i + 1) |>.flatMap (fun a2 ↦ inbound_antinodes input.size a1 a2))
     |>.flatten
     |> HashSet.ofList
     |>.size
@@ -69,7 +69,21 @@ end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := 0
+partial def inbound_antinodes' (size a offset : Vec2) : List Vec2 :=
+  let next := add a offset
+  if contains size next then [next] ++ inbound_antinodes' size next offset else []
+
+def inbound_antinodes (size : Vec2) (a b : Char × Vec2) : List Vec2 :=
+  if a.1 == b.1
+    then [a.2, b.2] ++ inbound_antinodes' size a.2 (sub a.2 b.2) ++ inbound_antinodes' size b.2 (sub b.2 a.2)
+    else []
+
+def solve (input : Input) : Nat :=
+    input.anntena.enum.map
+    (fun (i, a1) ↦ input.anntena.drop (i + 1) |>.flatMap (fun a2 ↦ inbound_antinodes input.size a1 a2))
+    |>.flatten
+    |> HashSet.ofList
+    |>.size
 
 end Part2
 
