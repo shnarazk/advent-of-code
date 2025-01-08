9623ae5034a33ecbf076ec018b7ead08855a0ec5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-01-08 22:42:26 +0900

    ditto

--- Lean4/Y2024/Day09.lean
+++ Lean4/Y2024/Day09.lean
@@ -38,7 +38,24 @@ end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := 0
+partial def next (disc : Array (Option Nat)) ( limit_left limit_right i : Nat) (increase : Bool) : Nat :=
+  if disc.getD i none == none then i
+    else
+      if not increase && i == limit_left then i
+        else if increase && i == limit_right then i
+          else
+            next disc limit_left limit_right (if increase then i + 1 else i - 1) increase
+
+partial def swap (disc : Array (Option Nat)) (left right : Nat) : Array (Option Nat) :=
+  if left < right then disc
+    else
+      let l := next disc 0 (disc.size - 1) left true
+      let r := next disc 0 (disc.size - 1) right false
+      if r <= l then disc else swap (disc.swapIfInBounds l r) (l + 1) (r - 1)
+
+def solve (input : Input) : Nat :=
+  let moved := swap input.line 0 (input.line.size - 1)
+  moved.size
 
 end Part1
 
