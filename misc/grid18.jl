fccb84f555567fdea01bc6b5fcacf2d92746fd3e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-01-12 01:05:23 +0900

    modified:   misc/grid18.jl

--- misc/grid18.jl
+++ misc/grid18.jl
@@ -1,48 +1,37 @@
 #!/usr/bin/env julia
 import Pkg
-# Pkg.add("JSON")
-# Pkg.add("Plots")
-# Pkg.add("GridVisualize")
-Pkg.add("Colors")
-Pkg.add("Images")
-using JSON, Plots # , GridVisualize
-using Colors, Images
+# Pkg.add("JSON"),
+# Pkg.add("Plots"),
+# Pkg.add("GridVisualize"),
+# Pkg.add("Colors"),
+using JSON, Plots, Colors
 
-serialized = read("2024/day18.json", String)
-data = JSON.parse(serialized)
-grid_data = data["line"]
+grid_data = JSON.parse(read("2024/day18.json", String))["line"]
+
 # Extract dimensions of the grid
 rows = 1 + maximum([g[1] for g in grid_data])
 cols = 1 + maximum([g[2] for g in grid_data])
-println(rows)
-println(cols)
+len = length(grid_data)
+
 # Create an empty matrix for plotting purposes
-
-grid_matrix = fill(:white, rows, cols)
-
-# Populate the grid matrix with :black or :white based on grid data values
-for g in grid_data
-    grid_matrix[g[1]+1, g[2]+1] = :black
+grid_matrix = fill(RGB(1.0, 1.0, 1.0), rows, cols)
+for (i, g) in enumerate(grid_data)
+    c = (len - i) / len
+    grid_matrix[g[1]+1, g[2]+1] = RGB(c, c, c)
 end
+grid_matrix[1, 1] = RGB(1.0, 0.0, 0.0)
 
-color_map = Dict(:white => RGB(1.0, 1.0, 1.0), :black => RGB(0.0, 0.0, 0.0))
-color_matrix = [color_map[element] for element in grid_matrix]
-
-mosaicview(color_matrix)
-# Plot the grid using Plots package
-# gridplot(grid_matrix,
-    # xlims=(0.5, cols + 0.5),
-    # ylims=(0.5, rows + 0.5),
-    # aspect_ratio=:equal,
-    # markerstrokecolor=(:black, :white)[1],
-    # markersize=10,
-    # legend=false
+# color_map = Dict(
+#     :red => RGB(1.0, 0.0, 0.0),
+#     :white => RGB(1.0, 1.0, 1.0),
+#     :black => RGB(0.0, 0.0, 0.0),
 # )
-# plot!(
-#     ktd.x,
-#     ktd.density,
-#     ylims=(0, 0.4),
-#     color=:red,
-#     linewidth=2)
+# color_matrix = [color_map[element] for element in grid_matrix]
 
-# savefig("hist2024.png")
+heatmap(
+    # color_matrix,
+    grid_matrix,
+    title="Y2024 day18",
+    aspect_ratio=:equal
+)
+savefig("day18-grid.png")
