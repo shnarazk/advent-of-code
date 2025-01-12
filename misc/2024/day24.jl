da388ee6b70b029c3e24faa4dacb88ec2502f364
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-01-12 10:49:42 +0900

    new file:   misc/2024/day24.jl

--- misc/2024/day24.jl
+++ misc/2024/day24.jl
@@ -1,12 +1,13 @@
 #!/usr/bin/env julia
 # using Pkg
 # Pkg.add("DrawSimpleGraphs")
+# Pkg.add("JSON")
 # Pkg.add("Plots")
 # Pkg.add("SimpleGraphs")
 using DrawSimpleGraphs, JSON, Plots, SimpleGraphs
 
 # Load the JSON file
-json_path = "day23.json"
+json_path = "day24.json"
 json_data = JSON.parsefile(json_path)
 
 # Initialize a graph
@@ -14,13 +15,15 @@ g = UndirectedGraph()
 
 # Add edges from JSON data
 for edge in json_data
-    add!(g, edge[1], edge[2])
+    if 2 <= length(edge[2])
+        add!(g, edge[1], edge[2][1])
+        add!(g, edge[1], edge[2][2])
+    end
 end
 
 # Plot the graph
 embed(g, :spring)
 set_vertex_size(g, 1)
-draw(
-    g,
-)
-savefig("day23.png")
+draw(g)
+
+savefig("day24.png")
