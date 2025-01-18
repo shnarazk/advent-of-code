7de33b959e7d52d268cb38df779ea612960f5801
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-01-18 11:45:58 +0900

    modified:   misc/hist.jl

--- misc/hist.jl
+++ misc/hist.jl
@@ -13,25 +13,37 @@ data = JSON.parse(execution_data)
 times = [item["time"] for item in data if haskey(item, "time")]
 tl = log2.(times)
 ktd = kde(tl)
-
+xlims = (-5, 15)
+ylims = (0, 0.25)
 histogram(
     tl,
-    xlims=(-5, 20),
-    ylims=(0, 0.4),
-    bins=20,
+    xlims=xlims,
+    ylims=ylims,
+    bins=5,
     normalize=true,
     title="AoC$year execution time distribution",
     xlabel="log2(time[ms])",
     # xlabel="時間",
-    ylabel="Freq",
+    ylabel="Frequency",
     legend=false,
     # fontfamily="Noto Sans CJK JP"
+    grid=true,
+    c=:lightblue,
+    framestyle=:box,
+    xtickfont=font(12, "Arial"),
+    ytickfont=font(12, "Arial"),
+    guidefont=font(14, "Arial"),
+    titlefont=font(16, "Arial Bold")
 )
 plot!(
     ktd.x,
     ktd.density,
-    ylims=(0, 0.4),
+    xlims=xlims,
+    ylims=ylims,
     color=:red,
-    linewidth=2)
+    linewidth=2,
+    label="Density",
+    grid=true
+)
 
 savefig("hist-$year.png")
