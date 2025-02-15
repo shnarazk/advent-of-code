27fcc0853301f038231e370f91d18f1e69633e2f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-02-15 23:50:05 +0900

    Use winnow:   src/y2017/day18.rs

--- src/y2017/day18.rs
+++ src/y2017/day18.rs
@@ -1,9 +1,6 @@
 //! <https://adventofcode.com/2017/day/18>
 use {
-    crate::{
-        framework::{aoc, AdventOfCode, ParseError},
-        regex,
-    },
+    crate::framework::{aoc, AdventOfCode, ParseError},
     std::collections::{HashMap, VecDeque},
 };
 
@@ -24,90 +21,6 @@ enum Inst {
     Mod(Val, Val),
     Rcv(Val),
     Jgz(Val, Val),
-}
-
-impl TryFrom<&str> for Inst {
-    type Error = ParseError;
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        // dbg!(value);
-        let arg1l = regex!(r"^([[:lower:]]{3}) (-?[[:digit:]]+)$");
-        let arg1r = regex!(r"^([[:lower:]]{3}) ([[:lower:]])$");
-        let arg2ll = regex!(r"^([[:lower:]]{3}) (-?[[:digit:]]+) (-?[[:digit:]]+)$");
-        let arg2lr = regex!(r"^([[:lower:]]{3}) (-?[[:digit:]]+) ([[:lower:]])$");
-        let arg2rl = regex!(r"^([[:lower:]]{3}) ([[:lower:]]) (-?[[:digit:]]+)$");
-        let arg2rr = regex!(r"^([[:lower:]]{3}) ([[:lower:]]) ([[:lower:]])$");
-        if let Some(segment) = arg1l.captures(value) {
-            let val: Val = Val::Lit(segment[2].parse::<isize>()?);
-            let ins = match &segment[1] {
-                "snd" => Inst::Snd(val),
-                "rcv" => Inst::Rcv(val),
-                _ => unreachable!(),
-            };
-            return Ok(ins);
-        }
-        if let Some(segment) = arg1r.captures(value) {
-            let reg: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
-            let ins = match &segment[1] {
-                "snd" => Inst::Snd(reg),
-                "rcv" => Inst::Rcv(reg),
-                _ => unreachable!(),
-            };
-            return Ok(ins);
-        }
-        if let Some(segment) = arg2ll.captures(value) {
-            let op1: Val = Val::Lit(segment[2].parse::<isize>()?);
-            let op2: Val = Val::Lit(segment[3].parse::<isize>()?);
-            let ins = match &segment[1] {
-                "set" => Inst::Set(op1, op2),
-                "add" => Inst::Add(op1, op2),
-                "mul" => Inst::Mul(op1, op2),
-                "mod" => Inst::Mod(op1, op2),
-                "jgz" => Inst::Jgz(op1, op2),
-                _ => unreachable!(),
-            };
-            return Ok(ins);
-        }
-        if let Some(segment) = arg2lr.captures(value) {
-            let op1: Val = Val::Lit(segment[2].parse::<isize>()?);
-            let op2: Val = Val::Reg(segment[3].chars().next().ok_or(ParseError)?);
-            let ins = match &segment[1] {
-                "set" => Inst::Set(op1, op2),
-                "add" => Inst::Add(op1, op2),
-                "mul" => Inst::Mul(op1, op2),
-                "mod" => Inst::Mod(op1, op2),
-                "jgz" => Inst::Jgz(op1, op2),
-                _ => unreachable!(),
-            };
-            return Ok(ins);
-        }
-        if let Some(segment) = arg2rl.captures(value) {
-            let op1: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
-            let op2: Val = Val::Lit(segment[3].parse::<isize>()?);
-            let ins = match &segment[1] {
-                "set" => Inst::Set(op1, op2),
-                "add" => Inst::Add(op1, op2),
-                "mul" => Inst::Mul(op1, op2),
-                "mod" => Inst::Mod(op1, op2),
-                "jgz" => Inst::Jgz(op1, op2),
-                _ => unreachable!(),
-            };
-            return Ok(ins);
-        }
-        if let Some(segment) = arg2rr.captures(value) {
-            let op1: Val = Val::Reg(segment[2].chars().next().ok_or(ParseError)?);
-            let op2: Val = Val::Reg(segment[3].chars().next().ok_or(ParseError)?);
-            let ins = match &segment[1] {
-                "set" => Inst::Set(op1, op2),
-                "add" => Inst::Add(op1, op2),
-                "mul" => Inst::Mul(op1, op2),
-                "mod" => Inst::Mod(op1, op2),
-                "jgz" => Inst::Jgz(op1, op2),
-                _ => unreachable!(),
-            };
-            return Ok(ins);
-        }
-        Err(ParseError)
-    }
 }
 
 #[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
@@ -274,12 +187,49 @@ pub struct Puzzle {
     line: Vec<Inst>,
 }
 
+mod parser {
+    use {
+        super::*,
+        crate::parser::parse_isize,
+        winnow::{
+            ascii::{newline, space1},
+            combinator::{alt, separated, seq},
+            token::one_of,
+            ModalResult, Parser,
+        },
+    };
+
+    fn parse_val(s: &mut &str) -> ModalResult<Val> {
+        alt((
+            parse_isize.map(Val::Lit),
+            one_of(|c: char| c.is_ascii_lowercase()).map(Val::Reg),
+        ))
+        .parse_next(s)
+    }
+
+    fn parse_inst(s: &mut &str) -> ModalResult<Inst> {
+        alt((
+            seq!(_: "snd ", parse_val).map(|(a,)| Inst::Snd(a)),
+            seq!(_: "set ", parse_val, _: space1, parse_val).map(|(a, b)| Inst::Set(a, b)),
+            seq!(_: "add ", parse_val, _: space1, parse_val).map(|(a, b)| Inst::Add(a, b)),
+            seq!(_: "mul ", parse_val, _: space1, parse_val).map(|(a, b)| Inst::Mul(a, b)),
+            seq!(_: "mod ", parse_val, _: space1, parse_val).map(|(a, b)| Inst::Mod(a, b)),
+            seq!(_: "rcv ", parse_val).map(|(a,)| Inst::Rcv(a)),
+            seq!(_: "jgz ", parse_val, _: space1, parse_val).map(|(a, b)| Inst::Jgz(a, b)),
+        ))
+        .parse_next(s)
+    }
+
+    pub fn parse(s: &mut &str) -> ModalResult<Vec<Inst>> {
+        separated(1.., parse_inst, newline).parse_next(s)
+    }
+}
+
 #[aoc(2017, 18)]
 impl AdventOfCode for Puzzle {
-    const DELIMITER: &'static str = "\n";
-    fn insert(&mut self, block: &str) -> Result<(), ParseError> {
-        self.line.push(Inst::try_from(block)?);
-        Ok(())
+    fn parse(&mut self, input: String) -> Result<String, ParseError> {
+        self.line = parser::parse(&mut input.as_str())?;
+        Self::parsed()
     }
     fn part1(&mut self) -> Self::Output1 {
         let mut processor: Runtime = Runtime::initialize(-1, &self.line);
