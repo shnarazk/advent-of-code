1ed0db120b4071643039347b21fd1fa76af95fdb
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-24 01:56:45 +0900

    (WIP) modified:   Julia/y2024day04.jl

--- Julia/y2024day04.jl
+++ Julia/y2024day04.jl
@@ -1,18 +1,21 @@
 #!/usr/bin/env julia
-# using Pkg
-# Pkg.add("ParserCombinator")
 using ParserCombinator
 include("AoCParser.jl")
 using .AoCParser
 
-function run()::NamedTuple{(:part1, :part2), Tuple{Int, Int}}
+function part1(m::Matrix{Char})::Int
+    size(m, 1)
+end
+
+function part2(m::Matrix{Char})::Int
+    size(m, 1)
+end
+
+function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
     open("../data/2024/input-day04.txt", "r") do file
-        # lines = String.(eachline(file)) |>
-        #     s -> filter(!isempty, s) |>
-        #     s -> map(t -> Int.(parse_one(t, 🔎ints)), s)
-        part1 = 0
-        part2 = 0
-        return (part1, part2)
+        lines = String.(eachline(file)) |> s -> filter(!isempty, s)
+        m = hcat(map(collect, lines)...) |> permutedims |> Matrix
+        return (part1(m), part2(m))
     end
 end
 
