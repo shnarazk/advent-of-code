1ecc375abaf205045d04443165996ca80d4d4529
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-23 11:35:35 +0900

    (Julia) Add pints parser for repeated integers

--- Julia/AoCParser.jl
+++ Julia/AoCParser.jl
@@ -1,9 +1,10 @@
 module AoCParser
 using ParserCombinator
-export pint, pfloat, pspaces, pnewline
+export pint, pfloat, pspaces, pnewline, pints
 
 pint = p"\d+" > s -> parse(Int, s)
 pfloat = p"\d+\.\d*" > s -> parse(Float64, s)
 pspaces = Drop(p"[ \t]*")
 pnewline = Drop(p"\n")
+pints = Repeat(pint + pspaces)
 end
