171656a754cd88c134a7a48b8ae1f284c248acb3
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-24 01:19:31 +0900

    new file:   Julia/y2024day04.jl

---
+++ Julia/y2024day04.jl
@@ -0,0 +1,21 @@
+#!/usr/bin/env julia
+# using Pkg
+# Pkg.add("ParserCombinator")
+using ParserCombinator
+include("AoCParser.jl")
+using .AoCParser
+
+function run()::NamedTuple{(:part1, :part2), Tuple{Int, Int}}
+    open("../data/2024/input-day04.txt", "r") do file
+        # lines = String.(eachline(file)) |>
+        #     s -> filter(!isempty, s) |>
+        #     s -> map(t -> Int.(parse_one(t, 🔎ints)), s)
+        part1 = 0
+        part2 = 0
+        return (part1, part2)
+    end
+end
+
+@time r = run()
+
+println(r)
