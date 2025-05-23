dd3b58f5b21e1915a44c4eb6fff7aee626ee418e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-23 09:41:25 +0900

    (Julia) Parse floats as Float64 in AoCParser

--- Julia/AoCParser.jl
+++ Julia/AoCParser.jl
@@ -3,7 +3,7 @@ using ParserCombinator
 export pint, pfloat, pspaces, pnewline
 
 pint = p"\d+" > s -> parse(Int, s)
-pfloat = p"\d+\.\d*" > s -> parse(Float, s)
+pfloat = p"\d+\.\d*" > s -> parse(Float64, s)
 pspaces = Drop(p"[ \t]*")
 pnewline = Drop(p"\n")
 end
