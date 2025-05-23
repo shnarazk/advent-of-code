a9f02beaae1bf01ed448aeb0319de611f54e06e1
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-23 13:55:17 +0900

    (Julia) Use PInt and PFloat64 for numeric parsing

--- Julia/AoCParser.jl
+++ Julia/AoCParser.jl
@@ -1,9 +1,10 @@
 module AoCParser
+
 using ParserCombinator
 export 🔎int, 🔎float, 🔎spaces, 🔎newline, 🔎ints
 
-🔎int = p"\d+" > s -> parse(Int, s)
-🔎float = p"\d+\.\d*" > s -> parse(Float64, s)
+🔎int = PInt()       # p"\d+" > s -> parse(Int, s)
+🔎float = PFloat64() # p"\d+\.\d*" > s -> parse(Float64, s)
 🔎spaces = Drop(p"[ \t]*")
 🔎newline = Drop(p"\n")
 🔎ints = Repeat(🔎int + 🔎spaces)
