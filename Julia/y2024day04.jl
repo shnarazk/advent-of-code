3f92d36a3fb5e7f14cfb6b56a2794ee855d235c9
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-24 11:39:29 +0900

    (Julia) y2024 day04 part1 done

--- Julia/y2024day04.jl
+++ Julia/y2024day04.jl
@@ -4,7 +4,18 @@ include("AoCParser.jl")
 using .AoCParser
 
 function part1(m::Matrix{Char})::Int
-    size(m, 1)
+    count(map(
+        ((i, j),) -> (s = String(m[i, j:j+3]); s == "XMAS" || s == "SAMX"),
+        [(i, j) for i in 1:size(m, 1) for j in 1:size(m, 2)-3])) +
+    count(map(
+        ((i, j),) -> (s = String(m[i:i+3, j]); s == "XMAS" || s == "SAMX"),
+        [(i, j) for i in 1:size(m, 1)-3 for j in 1:size(m, 2)])) +
+    count(map(
+        ((i, j),) -> (s = String([m[i, j], m[i+1, j+1], m[i+2, j+2], m[i+3, j+3]]); s == "XMAS" || s == "SAMX"),
+        [(i, j) for i in 1:size(m, 1)-3 for j in 1:size(m, 2)-3])) +
+    count(map(
+        ((i, j),) -> (s = String([m[i, j], m[i+1, j-1], m[i+2, j-2], m[i+3, j-3]]); s == "XMAS" || s == "SAMX"),
+        [(i, j) for i in 1:size(m, 1)-3 for j in 4:size(m, 2)]))
 end
 
 function part2(m::Matrix{Char})::Int
@@ -15,7 +26,7 @@ function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
     open("../data/2024/input-day04.txt", "r") do file
         lines = String.(eachline(file)) |> s -> filter(!isempty, s)
         m = hcat(map(collect, lines)...) |> permutedims |> Matrix
-        return (part1(m), part2(m))
+        (part1(m), part2(m))
     end
 end
 
