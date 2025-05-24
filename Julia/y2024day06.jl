32be6019d514992382e357897c56c8ed47f72916
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-25 07:56:33 +0900

    (Julia) y2024 day06 part1 done

--- Julia/y2024day06.jl
+++ Julia/y2024day06.jl
@@ -1,18 +1,48 @@
 #!/usr/bin/env julia
-#
-include("Dir.jl")
-using .Dirs
 
-println(pos(Up) + pos(Right))
+function turn(dir::Vector{Int})::Vector{Int}
+    if dir == [-1, 0]
+        [0, 1]
+    elseif dir == [0, 1]
+        [1, 0]
+    elseif dir == [1, 0]
+        [0, -1]
+    elseif dir == [0, -1]
+        [-1, 0]
+    else
+        error()
+    end
+end
+
+function included(m::Matrix{T}, pos::Vector{Int})::Bool where {T}
+    all([1, 1] .<= pos .<= size(m))
+end
+
+function part1(m::Matrix{Char})::Int
+    # println(collect(Tuple(findfirst(==('^'), m))))
+    cursor = (pos=collect(Tuple(findfirst(==('^'), m))), dir=[-1, 0])
+    passed = fill(false, size(m))
+    passed[cursor.pos...] = true
+    while included(m, cursor.pos + cursor.dir)
+        pos = cursor.pos + cursor.dir
+        if m[pos...] == '#'
+            dir = turn(cursor.dir)
+            pos = cursor.pos + dir
+            @assert m[pos...] != '#'
+            cursor = (pos=cursor.pos, dir=dir)
+        end
+        cursor = (pos=pos, dir=cursor.dir)
+        passed[pos...] = true
+    end
+    sum(passed)
+end
 
 function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
-    open("../data/2024/input-day06-0.txt", "r") do file
+    open("../data/2024/input-day06.txt", "r") do file
         lines = String.(eachline(file)) |> s -> filter(!isempty, s)
         m = hcat(map(collect, lines)...) |> permutedims |> Matrix
-        println(m)
-        part1 = 0
         part2 = 0
-        (part1, part2)
+        (part1(m), part2)
     end
 end
 
