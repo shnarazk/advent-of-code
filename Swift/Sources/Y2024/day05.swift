899772276d8a96a0dda1dd81cc26eedbbcf0d417
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-26 00:03:20 +0900

    modified:   Swift/Sources/Y2024/day05.swift

--- Swift/Sources/Y2024/day05.swift
+++ Swift/Sources/Y2024/day05.swift
@@ -1,6 +1,40 @@
+import Parsing
+
 public func day05(_ data: String) {
-  let sum1 = 0
-  let sum2 = 0
-  print("Part1: \(sum1)")
-  print("Part2: \(sum2)")
+  let rule_parser = Parse(input: Substring.self) {
+    Int.parser()
+    "|"
+    Int.parser()
+  }
+  // .map { ($0, $1) }
+  let rules_parser = Many { rule_parser } separator: { "\n" }
+
+  let update_parser: some Parser<Substring, [Int]> = Many {
+   Int.parser()
+  } separator: {
+    ","
+  }
+  let updates_parser: some Parser<Substring, [[Int]]> = Many {
+    update_parser
+  } separator: {
+    "\n"
+  }
+  let parser = Parse(input: Substring.self) {
+    rules_parser
+    "\n\n"
+    updates_parser
+  }
+  // .map { (a: [(Int, Int)], b: [Int]) in (a, b) }
+  do {
+    var input = try parser.parse(data[...])
+    input.1 = input.1.filter { !$0.isEmpty }
+    print("\(input)")
+    let sum1 = 0
+    let sum2 = 0
+    print("Part1: \(sum1)")
+    print("Part2: \(sum2)")
+  }
+  catch {
+    print("\(error)")
+  }
 }
