c5343aae13d77ece29612fe24f0a0e5e161d6af8
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-25 22:08:26 +0900

    modified:   Swift/Sources/parser1/parser1.swift

--- Swift/Sources/parser1/parser1.swift
+++ Swift/Sources/parser1/parser1.swift
@@ -13,7 +13,7 @@ struct Parser1 {
     public static func main() throws {
         let input = """
         	1,Blob,true
-        	2,Blob,Jr.,false
+        	2,Blob Jr.,false
         	3,Blob Sr.,true
         	"""
         let user = Parse(input: Substring.self) {
@@ -25,10 +25,16 @@ struct Parser1 {
         }
             .map { User(id: $0, name: $1, isAdmin: $2) }
         
+        let users = Many {
+            user
+        } separator: {
+            "\n"
+        }
+
         var inp = input[...]
         
         
-        let u = try user.parse(&inp)
-        print("done.\(inp) \(u)")
+        let us = try users.parse(&inp)
+        print("done.\(us) ~~ \(inp)")
     }
 }
