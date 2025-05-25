3a43f1c30eb03e2a6c194c129e4d33753dc7e6be
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-25 10:54:36 +0900

    (Julia) y2024 day06 done.

--- Julia/y2024day06.jl
+++ Julia/y2024day06.jl
@@ -25,24 +25,70 @@ function part1(m::Matrix{Char})::Int
     passed[cursor.pos...] = true
     while included(m, cursor.pos + cursor.dir)
         pos = cursor.pos + cursor.dir
+        dir = cursor.dir
         if m[pos...] == '#'
-            dir = turn(cursor.dir)
+            dir = turn(dir)
+            pos = cursor.pos + dir
+            @assert m[pos...] != '#'
+        end
+        cursor = (pos=pos, dir=dir)
+        passed[pos...] = true
+    end
+    sum(passed)
+end
+
+function is_loop(m::Matrix{Char}, cursor::NamedTuple, passed::Set)::Bool
+    m = copy(m)
+    m[(cursor.pos + cursor.dir)...] = '#'
+    passed = copy(passed)
+    while included(m, cursor.pos + cursor.dir)
+        pos = cursor.pos + cursor.dir
+        dir = cursor.dir
+        if m[pos...] == '#'
+            dir = turn(dir)
+            pos = cursor.pos + dir
+            if m[pos...] == '#'
+                dir = turn(dir)
+                pos = cursor.pos + dir
+            end
+        end
+        cursor = (pos=pos, dir=dir)
+        if cursor in passed
+            return true
+        else
+            push!(passed, cursor)
+        end
+    end
+    false
+end
+
+function part2(m::Matrix{Char})::Int
+    loops = Set()
+    cursor = (pos=collect(Tuple(findfirst(==('^'), m))), dir=[-1, 0])
+    passed = Set([cursor])
+    while included(m, cursor.pos + cursor.dir)
+        pos = cursor.pos + cursor.dir
+        dir = cursor.dir
+        if m[pos...] == '#'
+            dir = turn(dir)
             pos = cursor.pos + dir
             @assert m[pos...] != '#'
             cursor = (pos=cursor.pos, dir=dir)
         end
-        cursor = (pos=pos, dir=cursor.dir)
-        passed[pos...] = true
+        if all(c -> c.pos != pos, passed) && is_loop(m, cursor, passed)
+            push!(loops, pos)
+        end
+        cursor = (pos=pos, dir=dir)
+        push!(passed, cursor)
     end
-    sum(passed)
+    length(loops)
 end
 
 function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
     open("../data/2024/input-day06.txt", "r") do file
         lines = String.(eachline(file)) |> s -> filter(!isempty, s)
         m = hcat(map(collect, lines)...) |> permutedims |> Matrix
-        part2 = 0
-        (part1(m), part2)
+        (part1(m), part2(m))
     end
 end
 
