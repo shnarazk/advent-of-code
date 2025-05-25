17503f5264133cd668e8915274154d4f8f7656ad
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-25 17:53:59 +0900

    (Julia) y2024 day08 done.

--- Julia/y2024day08.jl
+++ Julia/y2024day08.jl
@@ -29,7 +29,27 @@ function part1(s::Tuple, antennas::Dict{Char,Vector{Tuple{Int,Int}}})::Int
 end
 
 function part2(s::Tuple, antennas::Dict{Char,Vector{Tuple{Int,Int}}})::Int
-    0
+    set = Set()
+    for (_, l) in antennas
+        for (i, p1) in enumerate(l)
+            for (j, p2) in enumerate(l)
+                if i < j
+                    diff = p1 .- p2
+                    d = (0, 0)
+                    while (p = within(s, p2 .- d)) !== nothing
+                        push!(set, p)
+                        d = d .+ diff
+                    end
+                    d = (0, 0)
+                    while (p = within(s, p1 .+ d)) !== nothing
+                        push!(set, p)
+                        d = d .+ diff
+                    end
+                end
+            end
+        end
+    end
+    length(set)
 end
 
 function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
