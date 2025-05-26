c3d5ad7a85cb41295480fbf473eb06dddd270063
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-26 10:29:15 +0900

    (Swift) Add explicit parser types and remove comments

--- Swift/Sources/Y2024/day05.swift
+++ Swift/Sources/Y2024/day05.swift
@@ -32,12 +32,12 @@ func part2(_ pages: [Int], by rules: [(Int, Int)]) -> Int {
 }
 
 public func day05(_ data: String) {
-    let rule_parser = Parse(input: Substring.self) {
+    let rule_parser: some Parser<Substring, (Int, Int)> = Parse(input: Substring.self) {
         Int.parser()
         "|"
         Int.parser()
     }
-    // .map { ($0, $1) }
+
     let rules_parser = Many {
         rule_parser
     } separator: {
@@ -49,17 +49,19 @@ public func day05(_ data: String) {
     } separator: {
         ","
     }
+
     let updates_parser: some Parser<Substring, [[Int]]> = Many {
         update_parser
     } separator: {
         "\n"
     }
-    let parser = Parse(input: Substring.self) {
+
+    let parser: some Parser<Substring, ([(Int, Int)], [[Int]])> = Parse(input: Substring.self) {
         rules_parser
         "\n\n"
         updates_parser
     }
-    // .map { (a: [(Int, Int)], b: [Int]) in (a, b) }
+
     do {
         let input = try parser.parse(data[...])
         let rules: [(Int, Int)] = input.0
