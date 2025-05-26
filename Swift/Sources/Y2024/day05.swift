26a6025e314b54f4ca7d7da7b4766b39e33885f4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-26 10:13:31 +0900

    (Swift) y2024 day05 done.

--- Swift/Sources/Y2024/day05.swift
+++ Swift/Sources/Y2024/day05.swift
@@ -1,5 +1,36 @@
 import Parsing
 
+func total_order(_ range: [Int], by rules: [(Int, Int)]) -> [Int] {
+    let unrestricted: [Int] = range.filter { node in rules.allSatisfy { rule in node != rule.0 } }
+    if unrestricted.isEmpty {
+        return range
+    } else {
+        let rs = rules.filter { !unrestricted.contains($0.1) }
+        return total_order(range.filter { !unrestricted.contains($0) }, by: rs) + unrestricted
+    }
+}
+func ordered(_ pages: [Int], by rules: [(Int, Int)]) -> [Int] {
+    let rs = rules.filter { pages.contains($0.0) && pages.contains($0.1) }
+    return total_order(pages, by: rs)
+}
+
+func part1(_ pages: [Int], by rules: [(Int, Int)]) -> Int {
+    if ordered(pages, by: rules) == pages {
+        return pages[pages.count / 2]
+    } else {
+        return 0
+    }
+}
+
+func part2(_ pages: [Int], by rules: [(Int, Int)]) -> Int {
+    let os = ordered(pages, by: rules)
+    if os != pages {
+        return os[os.count / 2]
+    } else {
+        return 0
+    }
+}
+
 public func day05(_ data: String) {
     let rule_parser = Parse(input: Substring.self) {
         Int.parser()
@@ -31,11 +62,10 @@ public func day05(_ data: String) {
     // .map { (a: [(Int, Int)], b: [Int]) in (a, b) }
     do {
         let input = try parser.parse(data[...])
-        let rules = input.0
-        let updates = input.1.filter { !$0.isEmpty }
-        print("\(rules.count) \(updates.count)")
-        let sum1 = 0
-        let sum2 = 0
+        let rules: [(Int, Int)] = input.0
+        let updates: [[Int]] = input.1.filter { !$0.isEmpty }
+        let sum1 = (updates.map { part1($0, by: rules) }).reduce(0, +)
+        let sum2 = (updates.map { part2($0, by: rules) }).reduce(0, +)
         print("Part1: \(sum1)")
         print("Part2: \(sum2)")
     } catch {
