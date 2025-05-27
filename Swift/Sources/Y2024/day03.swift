a3182cebe6f9828157dbb44abb09d9131ef0ce4d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-28 08:30:12 +0900

    (Swift) y2024 day03 done.

--- Swift/Sources/Y2024/day03.swift
+++ Swift/Sources/Y2024/day03.swift
@@ -1,14 +1,60 @@
-public func day03(_ data: String) {
-    let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
-    func part1(_ lines: [String.SubSequence]) {
-        // var lines = lines
-        print("Part1: \(1)")
-    }
+import Parsing
 
-    func part2(_ lines: [String.SubSequence]) {
-        // var lines = lines
-        print("Part2: \(2)")
+let mul: some Parser<Substring, (Int, Int)> = Parse {
+    "mul("
+    Int.parser()
+    ","
+    Int.parser()
+    ")"
+}
+
+let flip: some Parser<Substring, Bool> = Parse {
+    "do"
+    OneOf {
+        "()".map { true }
+        "n't()".map { false }
     }
-    part1(lines)
-    part2(lines)
+}
+
+func part1(_ data: String) -> Int {
+    var sum = 0
+    for i in 0..<data.count {
+        if data[data.index(data.startIndex, offsetBy: i)] != "m" {
+            continue
+        }
+        do {
+            var str = data.dropFirst(i)
+            let (a, b) = try mul.parse(&str)
+            sum += a * b
+        } catch {}
+    }
+    return sum
+}
+
+func part2(_ data: String) -> Int {
+    var sum = 0
+    var mode = true
+    for i in 0..<data.count {
+        do {
+            var str = data.dropFirst(i)
+            let (a, b) = try mul.parse(&str)
+            if mode {
+                sum += a * b
+            }
+            continue
+        } catch {}
+        do {
+            var str = data.dropFirst(i)
+            mode = try flip.parse(&str)
+            continue
+        } catch {}
+    }
+    return sum
+}
+
+public func day03(_ data: String) {
+    let sum1 = part1(data)
+    let sum2 = part2(data)
+    print("Part1: \(sum1)")
+    print("Part2: \(sum2)")
 }
