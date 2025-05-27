f31846e055f2b5adadc7ed66ccc15feaf9982413
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-27 20:46:36 +0900

    (Swift) y2024 day09 part2 done

--- Swift/Sources/Y2024/day09.swift
+++ Swift/Sources/Y2024/day09.swift
@@ -4,40 +4,73 @@
 //
 import Foundation
 
-typealias Record = (pos: Int, id: Int, lenght: Int)
+typealias Record = (pos: Int, id: Int, length: Int)
 
 func decode(_ line: [Int]) -> [Record] {
     var map = [Record]()
     func step(_ l: Int, _ status: (Int, Int, Bool)) -> (Int, Int, Bool) {
         if status.2 {
-            map.append( (status.0, status.1, l) )
+            map.append((status.0, status.1, l))
             return (state.0 + l, status.1 + 1, !state.2)
         }
         return (state.0 + l, status.1, !state.2)
     }
     var state = (0, 0, true)  // (start, id, file?)
     for l in line {
-        state = step(l,state)
+        state = step(l, state)
     }
-   return map
+    return map
+}
+
+/// Return the score
+func points(_ data: [Record]) -> Int {
+    var p: Int = 0
+    for r in data {
+        let s = r.pos
+        for i in 0..<r.length {
+            p += (s + i) * r.id
+        }
+    }
+    return p
+}
+
+func part1(_ data: [Record]) {
+    var mem = data
+    // var lines = lines
+    let p = points(mem)
+    print("Part1: \(p)")
+}
+
+func part2(_ data: [Record]) {
+    var mem = data
+    for r in data[1...].reversed() {
+        for i in 0..<mem.count - 1 {
+            let gap = mem[i + 1].pos - (mem[i].pos + mem[i].length)
+            if r.length <= gap {
+                let pos = mem[i].pos + mem[i].length
+                if r.pos < pos {
+                    continue
+                }
+                mem = mem.filter { $0.id != r.id }
+                mem.insert((pos: pos, id: r.id, length: r.length), at: i + 1)
+                break
+            }
+        }
+    }
+    let p = points(mem)
+    print("Part2: \(p)")
 }
 
 public func day09(_ data: String) {
-    let input: [Int] = data.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).map {
-        Int($0.asciiValue! - Character("0").asciiValue!) }
+    let input: [Int] = data.trimmingCharacters(
+        in: CharacterSet.whitespacesAndNewlines
+    ).map {
+        Int($0.asciiValue! - Character("0").asciiValue!)
+    }
 
     let initplaces = decode(input)
     print(initplaces)
 
-    func part1(_ data: [Record]) {
-        // var lines = lines
-        print("Part1: \(1)")
-    }
-
-    func part2(_ data: [Record]) {
-        // var lines = lines
-        print("Part2: \(2)")
-    }
-//    part1(data)
-//    part2(data)
+    part1(initplaces)
+    part2(initplaces)
 }
