40177d2ab2530a6426e559fb0559ecc15af798db
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-27 11:26:57 +0900

    (Swift) Enhance append_digits tests with edge cases

--- Swift/Tests/Test.swift
+++ Swift/Tests/Test.swift
@@ -8,8 +8,10 @@ import Testing
 
 struct Y2024Tests {
 
-    @Test func test1() async throws {
+    @Test("check append_digits (y2024 day07)") func test1() throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        #expect(Y2024.append_digits(0, 12) == 12)
         #expect(Y2024.append_digits(10, 12) == 1012)
+        #expect(Y2024.append_digits(120, 34) == 12034)
     }
 }
