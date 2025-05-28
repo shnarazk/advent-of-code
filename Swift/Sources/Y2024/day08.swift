7342ca8ec77de1748f97e7fb77b6148e9d9b9368
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-28 23:43:40 +0900

    (Swift) y2024 day08 done.

--- Swift/Sources/Y2024/day08.swift
+++ Swift/Sources/Y2024/day08.swift
@@ -1,25 +1,69 @@
-private func part1(_ size: (Int, Int), _ mirrors: Set<[Int]>, ) -> Int? {
-    return 0
+private func add(_ a: (Int, Int), _ b: (Int, Int)) -> (Int, Int) {
+    (a.0 + b.0, a.1 + b.1)
 }
 
-private func part2(_ size: (Int, Int), _ mirrors: Set<[Int]>, ) -> Int {
-    return 0
+private func sub(_ a: (Int, Int), _ b: (Int, Int)) -> (Int, Int) {
+    (a.0 - b.0, a.1 - b.1)
+}
+
+private func part1(_ size: (Int, Int), _ antennas: [Character: [(Int, Int)]]) -> Int {
+    var antis = Set<[Int]>()
+    for (_, ans) in antennas {
+        for (i, p1) in ans.enumerated() {
+            for (j, p2) in ans.enumerated() {
+                if i < j {
+                    let d = sub(p2, p1)
+                    if let p = within(add(p2, d), in: size) {
+                        antis.insert([p.0, p.1])
+                    }
+                    if let p = within(sub(p1, d), in: size) {
+                        antis.insert([p.0, p.1])
+                    }
+                }
+            }
+        }
+    }
+    return antis.count
+}
+
+private func part2(_ size: (Int, Int), _ antennas: [Character: [(Int, Int)]]) -> Int {
+    var antis = Set<[Int]>()
+    for (_, ans) in antennas {
+        for (i, p1) in ans.enumerated() {
+            for (j, p2) in ans.enumerated() {
+                if i < j {
+                    let d = sub(p2, p1)
+                    var x = (0, 0)
+                    while let p = within(add(p2, x), in: size) {
+                        antis.insert([p.0, p.1])
+                        x = add(x, d)
+                    }
+                    x = (0, 0)
+                    while let p = within(sub(p1, x), in: size) {
+                        antis.insert([p.0, p.1])
+                        x = add(x, d)
+                    }
+                }
+            }
+        }
+    }
+    return antis.count
 }
 
 public func day08(_ data: String) {
     let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
         .map { Array($0) }
     let size = (lines.count, lines[0].count)
-    var mirrors: Set<[Int]> = Set()
+    var antennas: [Character: [(Int, Int)]] = [:]
     for (i, l) in lines.enumerated() {
         for (j, c) in l.enumerated() {
-            if c == "#" {
-                mirrors.insert([i, j])
+            if c != "." {
+                antennas[c] = antennas[c, default: [(Int, Int)]()] + [(i, j)]
             }
         }
     }
-    let sum1 = 0  // part1(size, mirrors, now, passing, looping)!
-    let sum2 = 0  // part2(size, mirrors, now, passing, looping)
+    let sum1 = part1(size, antennas)
+    let sum2 = part2(size, antennas)
     print("Part1: \(sum1)")
     print("Part2: \(sum2)")
 }
