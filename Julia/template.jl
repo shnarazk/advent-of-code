addcf6475cd41191dac3afdb3f07fe6f503e08b4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-28 20:50:33 +0900

    (Julia) Add parallel_map and enable Julia threads

--- Julia/template.jl
+++ Julia/template.jl
@@ -5,7 +5,24 @@ using ParserCombinator
 include("AoCParser.jl")
 using .AoCParser
 
-function run()::NamedTuple{(:part1, :part2), Tuple{Int, Int}}
+# ─── Threaded parallel_map ─────────────────
+using Base.Threads            # brings @threads into scope
+
+"""
+    parallel_map(f, A::AbstractVector)
+
+Apply `f` to every element of `A` in parallel using Julia’s threads.
+Returns a new Vector of results in the same order as `A`.
+"""
+function parallel_map(f, A::AbstractVector)
+    R = Vector{Any}(undef, length(A))
+    @threads for i in eachindex(A)
+        R[i] = f(A[i])
+    end
+    return R
+end
+
+function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
     open("../data/2024/input-day00.txt", "r") do file
         # lines = String.(eachline(file)) |>
         #     s -> filter(!isempty, s) |>
