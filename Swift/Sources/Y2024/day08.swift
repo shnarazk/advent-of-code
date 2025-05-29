0db4f8691d229293cee6946e52e52660caee4b8a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-29 21:43:23 +0900

    (Swift) Refactor day08 to use Pos type for positions

--- Swift/Sources/Y2024/day08.swift
+++ Swift/Sources/Y2024/day08.swift
@@ -1,25 +1,18 @@
 import Utils
 
-private func add(_ a: (Int, Int), _ b: (Int, Int)) -> (Int, Int) {
-    (a.0 + b.0, a.1 + b.1)
-}
-
-private func sub(_ a: (Int, Int), _ b: (Int, Int)) -> (Int, Int) {
-    (a.0 - b.0, a.1 - b.1)
-}
-
-private func part1(_ size: (Int, Int), _ antennas: [Character: [(Int, Int)]]) -> Int {
-    var antis = Set<[Int]>()
+private func part1(_ size: (Int, Int), _ antennas: [Character: [Pos]]) -> Int {
+    let boundary = Pos.asBoundary(size)
+    var antis = Set<Pos>()
     for (_, ans) in antennas {
         for (i, p1) in ans.enumerated() {
             for (j, p2) in ans.enumerated() {
                 if i < j {
-                    let d = sub(p2, p1)
-                    if let p = within(add(p2, d), in: size) {
-                        antis.insert([p.0, p.1])
+                    let d = p2 - p1
+                    if let p = (p2 + d).within(boundary) {
+                        antis.insert(p)
                     }
-                    if let p = within(sub(p1, d), in: size) {
-                        antis.insert([p.0, p.1])
+                    if let p = (p1 - d).within(boundary) {
+                        antis.insert(p)
                     }
                 }
             }
@@ -28,22 +21,23 @@ private func part1(_ size: (Int, Int), _ antennas: [Character: [(Int, Int)]]) ->
     return antis.count
 }
 
-private func part2(_ size: (Int, Int), _ antennas: [Character: [(Int, Int)]]) -> Int {
-    var antis = Set<[Int]>()
+private func part2(_ size: (Int, Int), _ antennas: [Character: [Pos]]) -> Int {
+    let boundary = Pos.asBoundary(size)
+    var antis = Set<Pos>()
     for (_, ans) in antennas {
         for (i, p1) in ans.enumerated() {
             for (j, p2) in ans.enumerated() {
                 if i < j {
-                    let d = sub(p2, p1)
-                    var x = (0, 0)
-                    while let p = within(add(p2, x), in: size) {
-                        antis.insert([p.0, p.1])
-                        x = add(x, d)
+                    let d = p2 - p1
+                    var x = Pos.zero
+                    while let p = (p2 + x).within(boundary) {
+                        antis.insert(p)
+                        x = x + d
                     }
-                    x = (0, 0)
-                    while let p = within(sub(p1, x), in: size) {
-                        antis.insert([p.0, p.1])
-                        x = add(x, d)
+                    x = Pos.zero
+                    while let p = (p1 - x).within(boundary) {
+                        antis.insert(p)
+                        x = x + d
                     }
                 }
             }
@@ -56,11 +50,11 @@ public func day08(_ data: String) {
     let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
         .map { Array($0) }
     let size = (lines.count, lines[0].count)
-    var antennas: [Character: [(Int, Int)]] = [:]
+    var antennas: [Character: [Pos]] = [:]
     for (i, l) in lines.enumerated() {
         for (j, c) in l.enumerated() {
             if c != "." {
-                antennas[c] = antennas[c, default: [(Int, Int)]()] + [(i, j)]
+                antennas[c] = antennas[c, default: [Pos]()] + [Pos(y: i, x: j)]
             }
         }
     }
