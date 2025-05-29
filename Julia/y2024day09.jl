d68d0a40c580558b2cdec57fb17bf401f816b1ea
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-30 08:12:33 +0900

    new file:   Julia/y2024day09.jl

---
+++ Julia/y2024day09.jl
@@ -0,0 +1,44 @@
+#!/usr/bin/env julia
+#
+
+const Record = NamedTuple{(:id,:pos,:len),Tuple{Int,Int,Int}}
+
+function decode(v::Vector{Int})::Vector{Record}
+    at_disk = true
+    id = 0
+    pos = 0
+    memmap = []
+    for len in v
+        if at_disk
+            push!(memmap, (id, pos, len))
+            id += 1
+            pos += len
+        end
+        at_disk = !at_disk
+    end
+    memmap
+end
+
+function part1(init::Vector{Record})::Int
+    0
+end
+
+function part2(init::Vector{Record})::Int
+    0
+end
+
+function run()::NamedTuple{(:part1,:part2),Tuple{Int,Int}}
+    open("../data/2024/input-day09-0.txt", "r") do file
+        mem::Vector{Record} = read(file, String) |>
+            strip |>
+            collect |>
+            s -> map(c -> c - '0', s) |>
+            decode
+        println(mem)
+        (part1(mem), part2(mem))
+    end
+end
+
+@time r = run()
+
+println(r)
