802adb8a80a1fd9a4cca23edcbf2aa4edcaebe4b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-30 08:55:00 +0900

    (Julia) Refactor antenna parsing to use CartesianIndices

--- Julia/y2024day08.jl
+++ Julia/y2024day08.jl
@@ -1,5 +1,7 @@
 #!/usr/bin/env julia
 #
+include("AoCUtils.jl")
+
 const D2 = CartesianIndex{2}
 
 function within(s::D2, p::D2)::Union{D2,Nothing}
@@ -55,16 +57,16 @@ function part2(s::D2, antennas::Dict{Char,Vector{D2}})::Int
 end
 
 function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
-    open("../data/2024/input-day08.txt", "r") do file
+    open(datafile(2024, 8), "r") do file
         lines = String.(eachline(file)) |> s -> filter(!isempty, s)
         m = hcat(map(collect, lines)...) |> permutedims |> Matrix
         s = CartesianIndex(size(m))
         antennas = Dict{Char,Vector{D2}}()
-        for i in axes(m, 1), j in axes(m, 2)
-            c = m[i, j]
+        for ix in CartesianIndices(m)
+            c = m[ix]
             if c != '.'
                 l = get(antennas, c, D2[])
-                antennas[c] = push!(l, CartesianIndex(i, j))
+                antennas[c] = push!(l, ix)
             end
         end
         (part1(s, antennas), part2(s, antennas))
