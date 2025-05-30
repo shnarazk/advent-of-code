72d9913e18545171aaa60ee4742a6ec810cb7765
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-05-30 23:33:54 +0900

    (Julia) y2024 day10 done.

--- Julia/y2024day10.jl
+++ Julia/y2024day10.jl
@@ -16,12 +16,11 @@ function part1_aux(m::Matrix{Int}, start::Dim2)::Int
             push!(goals, pos)
             continue
         end
-        aa = neighbors4(pos, boundary)
         for p in neighbors4(pos, boundary)
             if h + 1 == m[p] && !(p in visited)
                 push!(to_visit, p)
             end
-       end
+        end
     end
     length(goals)
 end
@@ -30,17 +29,42 @@ function part1(m::Matrix{Int}, starts::Vector{Dim2})::Int
     map(s -> part1_aux(m, s), starts) |> sum
 end
 
-function run()::NamedTuple{(:part1,:part2),Tuple{Int,Int}}
+function part2_aux(m::Matrix{Int}, start::Dim2)::Int
+    boundary = Dim2(size(m))
+    to_visit::Vector{Dim2} = [start]
+    goals::Int = 0
+    while !isempty(to_visit)
+        pos = pop!(to_visit)
+        h = m[pos]
+        if h == 9
+            goals += 1
+            continue
+        end
+        for p in neighbors4(pos, boundary)
+            if h + 1 == m[p]
+                push!(to_visit, p)
+            end
+        end
+    end
+    goals
+end
+
+function part2(m::Matrix{Int}, starts::Vector{Dim2})::Int
+    map(s -> part2_aux(m, s), starts) |> sum
+end
+
+function run()::NamedTuple{(:part1, :part2),Tuple{Int,Int}}
     open(datafile(2024, 10), "r") do file
         lines = String.(eachline(file)) |> s -> filter(!isempty, s)
         m::Matrix{Int} = hcat(map(collect, lines)...) |>
-            permutedims |>
-            Matrix |>
-            m -> map(e -> e - '0', m)
+                         permutedims |>
+                         Matrix |>
+                         m -> map(e -> e - '0', m)
         starts = filter(p -> m[p] == 0, collect(CartesianIndices(m)))
         # println("starts: $(length(starts))")
         sum1 = part1(m, starts)
-        (sum1, 0)
+        sum2 = part2(m, starts)
+        (sum1, sum2)
     end
 end
 
