50494db1a934c7e02cd8be4738c618af04e62d82
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-06-18 08:00:49 +0900

    ditto

--- Swift/Sources/Utils/dataStore.swift
+++ Swift/Sources/Utils/dataStore.swift
@@ -10,15 +10,18 @@ public class AoCDescription {
     @Attribute(.unique) public var aocId: String
     public var year: Int
     public var day: Int
+    public var variant: String? = nil
     public var text: String
     public var desc: String = ""
     public var answer1: String?
     public var answer2: String?
-    public init(year: Int, day: Int, text: String, desc: String = "") {
+    public init(year: Int, day: Int, variant: String?, text: String, desc: String = "") {
         let d = day < 10 ? "0" : ""
-        self.aocId = "\(year)-\(d)\(day)"
+        let v = if let variant { "-\(variant)" } else { "" }
+        self.aocId = "\(year)-\(d)\(day)\(v)"
         self.year = year
         self.day = day
+        self.variant = variant
         self.text = text
         self.desc = desc
         self.answer1 = nil
@@ -28,14 +31,20 @@ public class AoCDescription {
 
 /// Save the text to the storage
 @MainActor
-public func saveDescription(year: Int, day: Int, text: String, force: Bool = false) {
+public func saveDescription(
+    year: Int,
+    day: Int,
+    variant: String?,
+    text: String,
+    force: Bool = false
+) {
     do {
-        let puzzle = AoCDescription(year: year, day: day, text: text)
+        let puzzle = AoCDescription(year: year, day: day, variant: variant, text: text)
         let config = getAoCModelConfiguration()
         let container = try! ModelContainer(for: AoCDescription.self, configurations: config)
         let context = container.mainContext
         var descriptor = FetchDescriptor<AoCDescription>(
-            predicate: #Predicate { $0.year == year && $0.day == day },
+            predicate: #Predicate { $0.year == year && $0.day == day && $0.variant == variant },
             sortBy: [.init(\.answer1)]
         )
         descriptor.fetchLimit = 10
@@ -57,7 +66,9 @@ public func saveDescription(year: Int, day: Int, text: String, force: Bool = fal
 
 public func getAoCModelConfiguration() -> ModelConfiguration {
     let appGroupID = "com.shnarazk.sharedgroup.aoc"
-    let containerURL = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupID)!
+    let containerURL = FileManager.default.containerURL(
+        forSecurityApplicationGroupIdentifier: appGroupID
+    )!
     let storeURL = containerURL.appendingPathComponent("AdventOfCode.sqlite")
     if #available(macOS 15, *) {
         return ModelConfiguration(url: storeURL)
@@ -66,4 +77,3 @@ public func getAoCModelConfiguration() -> ModelConfiguration {
         fatalError()
     }
 }
-
