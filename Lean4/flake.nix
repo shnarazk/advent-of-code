9664e562cb212c4d9f68ac29de15e6e5f45feb48
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-06-27 23:25:41 +0900

    Replace nixvim with home.nvim4lean in devShells

--- Lean4/flake.nix
+++ Lean4/flake.nix
@@ -2,71 +2,28 @@
   description = "A basic flake with a shell";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixvim.url = "github:nix-community/nixvim";
+  inputs.home.url = "github:shnarazk/flakes";
 
   outputs =
     {
       nixpkgs,
       flake-utils,
-      nixvim,
+      home,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-        nvim = nixvim.legacyPackages.${system};
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
             # pkgs.bashInteractive
             pkgs.elan
-            (nvim.makeNixvim {
-              colorschemes.gruvbox.enable = true;
-              globals = {
-                mapleader = " ";
-                maplocalleader = "  ";
-              };
-              lsp.inlayHints.enable = true;
-              lsp.keymaps = [
-                {
-                  key = "gd";
-                  lspBufAction = "definition";
-                }
-                {
-                  key = "gD";
-                  lspBufAction = "references";
-                }
-                {
-                  key = "gt";
-                  lspBufAction = "type_definition";
-                }
-                {
-                  key = "gi";
-                  lspBufAction = "implementation";
-                }
-                {
-                  key = "k";
-                  lspBufAction = "hover";
-                }
-              ];
-              plugins.cmp.enable = true;
-              plugins.cmp.autoEnableSources = true;
-              plugins.lean.enable = true;
-              plugins.lean.autoLoad = true;
-              plugins.lean.leanPackage = null;
-              plugins.lean.settings.mappings = true;
-              plugins.lean.settings.progress_bars.enable = false;
-              plugins.lualine.enable = true;
-              plugins.nvim-surround.enable = true;
-              plugins.which-key.enable = true;
-              # plugins.tree-sitter.enable = true;
-              # plugins.treesitter-textobjects.enable = true;
-            })
+            home.packages.${system}.nvim4lean
             # pkgs.nodejs_22
             # pkgs.tokei
-            # pkgs.vscodium
           ];
         };
       }
