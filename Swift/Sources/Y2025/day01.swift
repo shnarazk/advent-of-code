f76aaaa019edf91dcedcead1d16f3936eb503980
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-01 16:54:19 +0900

    (Swift) y2025 day1 done

--- Swift/Sources/Y2025/day01.swift
+++ Swift/Sources/Y2025/day01.swift
@@ -1,22 +1,44 @@
 import Parsing
 
-private func part1() -> Int {
-    return 0
+private func part1(_ lines: [Int]) -> Int {
+    var pos = 50
+    var zeros = 0
+    for step in lines {
+        pos = (pos + step) % 100
+        zeros += pos == 0 ? 1 : 0
+    }
+    return zeros
 }
 
-private func part2() -> Int {
-    return 0
+private func part2(_ lines: [Int]) -> Int {
+    var pos = 50
+    var zeros = 0
+    for step in lines {
+        let old = pos
+        pos += step
+        zeros += old > 0 && pos <= 0 ? 1 : 0
+        zeros += abs(pos) / 100
+        pos = ((pos % 100) + 100) % 100
+    }
+    return zeros
 }
 
 public func day01(_ data: String) {
-    let line_parser: some Parser<Substring, (Int, Int)> = Parse(input: Substring.self) {
-        Int.parser()
-        Prefix { !$0.isNumber }
-        Int.parser()
+    let line_parser: some Parser<Substring, Int> = Parse(input: Substring.self) {
+        OneOf {
+            Parse {
+                "L"
+                Int.parser()
+            }.map { -$0 }
+            Parse {
+                "R"
+                Int.parser()
+            }
+        }
     }
-    .map { ($0, $2) }
+    .map { $0 }
 
-    let parser: some Parser<Substring, [(Int, Int)]> = Many {
+    let parser: some Parser<Substring, [Int]> = Many {
         line_parser
     } separator: {
         "\n"
@@ -24,15 +46,9 @@ public func day01(_ data: String) {
         "\n"
     }
     do {
-        let lines: [(Int, Int)] = try parser.parse(data + "\n")
-        let (_, _) = lines.reduce(([Int](), [Int]())) { (acc, line) in
-            (
-                acc.0 + [line.0],
-                acc.1 + [line.1]
-            )
-        }
-        let sum1 = part1()
-        let sum2 = part2()
+        let lines: [Int] = try parser.parse(data)
+        let sum1 = part1(lines)
+        let sum2 = part2(lines)
         print("Part1: \(sum1)")
         print("Part2: \(sum2)")
     } catch {
