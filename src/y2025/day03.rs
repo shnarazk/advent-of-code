e27b52b6508ac30ce74b48ad2bdf3941973cba65
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-03 19:28:34 +0900

    Use par_iter and remove unused imports

--- src/y2025/day03.rs
+++ src/y2025/day03.rs
@@ -1,20 +1,7 @@
 //! <https://adventofcode.com/2025/day/3>
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
 use {
-    crate::{
-        framework::{AdventOfCode, ParseError, aoc},
-        geometric::neighbors,
-    },
-    // rayon::prelude::*,
-    rustc_data_structures::fx::{FxHashMap, FxHasher},
-    // serde::Serialize,
-    std::{
-        cmp::{Ordering, Reverse},
-        collections::{BinaryHeap, HashMap},
-        hash::BuildHasherDefault,
-    },
+    crate::framework::{AdventOfCode, ParseError, aoc},
+    rayon::prelude::*,
 };
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
@@ -27,8 +14,8 @@ mod parser {
         crate::parser::parse_dec,
         winnow::{
             ModalResult, Parser,
-            ascii::{alpha1, newline, space1},
-            combinator::{alt, repeat, separated, seq},
+            ascii::newline,
+            combinator::{repeat, separated},
         },
     };
 
@@ -49,7 +36,7 @@ impl AdventOfCode for Puzzle {
     }
     fn part1(&mut self) -> Self::Output1 {
         self.line
-            .iter()
+            .par_iter()
             .map(|line| {
                 let mut a = [line[line.len() - 2], line[line.len() - 1]];
                 for n in line.iter().rev().skip(2) {
@@ -64,7 +51,7 @@ impl AdventOfCode for Puzzle {
     }
     fn part2(&mut self) -> Self::Output2 {
         self.line
-            .iter()
+            .par_iter()
             .map(|line| {
                 let mut a = line[line.len() - 12..].to_vec();
                 for n in line.iter().rev().skip(12) {
