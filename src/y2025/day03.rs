c16be842d372d73f9546ec2a4b0c4b7a1e6d8b45
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-03 18:23:04 +0900

    Y2025 day3 part1 done

--- src/y2025/day03.rs
+++ src/y2025/day03.rs
@@ -19,24 +19,24 @@ use {
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<u8>>,
 }
 
 mod parser {
     use {
-        crate::parser::parse_usize,
+        crate::parser::parse_dec,
         winnow::{
             ModalResult, Parser,
             ascii::{alpha1, newline, space1},
-            combinator::{alt, separated, seq},
+            combinator::{alt, repeat, separated, seq},
         },
     };
 
-    fn parse_line(s: &mut &str) -> ModalResult<()> {
-        ().parse_next(s)
+    fn parse_line(s: &mut &str) -> ModalResult<Vec<u8>> {
+        repeat(1.., parse_dec.map(|x| x as u8)).parse_next(s)
     }
 
-    pub fn parse(s: &mut &str) -> ModalResult<Vec<()>> {
+    pub fn parse(s: &mut &str) -> ModalResult<Vec<Vec<u8>>> {
         separated(1.., parse_line, newline).parse_next(s)
     }
 }
@@ -48,8 +48,19 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
-        // let mut _: FxHashMap<_, _> = HashMap::<_, _, BuildHasherDefault<FxHasher>>::default();
-        1
+        self.line
+            .iter()
+            .map(|line| {
+                let mut a = [line[line.len() - 2], line[line.len() - 1]];
+                for n in line.iter().rev().skip(2) {
+                    if *n >= a[0] {
+                        a[1] = a[1].max(a[0]);
+                        a[0] = *n;
+                    }
+                }
+                (a[0] * 10 + a[1]) as usize
+            })
+            .sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
         2
