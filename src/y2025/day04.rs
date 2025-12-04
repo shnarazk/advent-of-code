e34ba8bb546609b309274f978d65b498ea565f0d
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-04 14:39:56 +0900

    Y2025 day04 part1 done

--- src/y2025/day04.rs
+++ src/y2025/day04.rs
@@ -5,7 +5,7 @@
 use {
     crate::{
         framework::{AdventOfCode, ParseError, aoc},
-        geometric::neighbors,
+        geometric::{neighbors, neighbors8},
     },
     // rayon::prelude::*,
     rustc_data_structures::fx::{FxHashMap, FxHasher},
@@ -19,7 +19,7 @@ use {
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Vec<bool>>,
 }
 
 mod parser {
@@ -28,15 +28,15 @@ mod parser {
         winnow::{
             ModalResult, Parser,
             ascii::{alpha1, newline, space1},
-            combinator::{alt, separated, seq},
+            combinator::{alt, repeat, repeat_till, separated, seq},
+            token::one_of,
         },
     };
 
-    fn parse_line(s: &mut &str) -> ModalResult<()> {
-        ().parse_next(s)
+    fn parse_line(s: &mut &str) -> ModalResult<Vec<bool>> {
+        repeat(1.., one_of(['.', '@']).map(|c: char| c == '@')).parse_next(s)
     }
-
-    pub fn parse(s: &mut &str) -> ModalResult<Vec<()>> {
+    pub fn parse(s: &mut &str) -> ModalResult<Vec<Vec<bool>>> {
         separated(1.., parse_line, newline).parse_next(s)
     }
 }
@@ -48,8 +48,23 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
-        // let mut _: FxHashMap<_, _> = HashMap::<_, _, BuildHasherDefault<FxHasher>>::default();
-        1
+        let mut found: usize = 0;
+        let height = self.line.len();
+        let width = self.line[0].len();
+        for (i, l) in self.line.iter().enumerate() {
+            for (j, b) in l.iter().enumerate() {
+                if *b
+                    && neighbors8(i, j, height, width)
+                        .iter()
+                        .filter(|(i, j)| self.line[*i][*j])
+                        .count()
+                        < 4
+                {
+                    found += 1;
+                }
+            }
+        }
+        found
     }
     fn part2(&mut self) -> Self::Output2 {
         2
