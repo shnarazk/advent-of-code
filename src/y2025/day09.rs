b0c9d99e31dee3f718df03bb3f787c3e0d033816
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-09 17:37:56 +0900

    Y2025 day9 part1 done

--- src/y2025/day09.rs
+++ src/y2025/day09.rs
@@ -5,7 +5,7 @@
 use {
     crate::{
         framework::{AdventOfCode, ParseError, aoc},
-        // geometric::{Dim2, NeighborIter},
+        geometric::{Dim2, NeighborIter},
     },
     // rayon::prelude::*,
     rustc_data_structures::fx::{FxHashMap, FxHasher},
@@ -19,12 +19,12 @@ use {
 
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Dim2<usize>>,
 }
 
 mod parser {
     use {
-        crate::parser::parse_usize,
+        crate::{geometric::Dim2, parser::parse_usize},
         winnow::{
             ModalResult, Parser,
             ascii::{alpha1, newline, space1},
@@ -32,11 +32,13 @@ mod parser {
         },
     };
 
-    fn parse_line(s: &mut &str) -> ModalResult<()> {
-        ().parse_next(s)
+    fn parse_line(s: &mut &str) -> ModalResult<Dim2<usize>> {
+        seq!(parse_usize, _: ",", parse_usize)
+            .map(|(x, y)| (y, x))
+            .parse_next(s)
     }
 
-    pub fn parse(s: &mut &str) -> ModalResult<Vec<()>> {
+    pub fn parse(s: &mut &str) -> ModalResult<Vec<Dim2<usize>>> {
         separated(1.., parse_line, newline).parse_next(s)
     }
 }
@@ -45,11 +47,24 @@ mod parser {
 impl AdventOfCode for Puzzle {
     fn prepare(&mut self, mut input: &str) -> Result<(), ParseError> {
         self.line = parser::parse(&mut input)?;
+        dbg!(&self.line);
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
         // let mut _: FxHashMap<_, _> = HashMap::<_, _, BuildHasherDefault<FxHasher>>::default();
-        1
+        self.line
+            .iter()
+            .enumerate()
+            .map(|(i, p)| {
+                self.line
+                    .iter()
+                    .skip(i)
+                    .map(|q| (p.0.abs_diff(q.0) + 1) * (p.1.abs_diff(q.1) + 1))
+                    .max()
+                    .expect("!")
+            })
+            .max()
+            .unwrap()
     }
     fn part2(&mut self) -> Self::Output2 {
         2
