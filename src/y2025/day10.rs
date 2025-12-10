45320ea662d1f6075288a1bc89dcf55a75f8926c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-10 17:05:57 +0900

    Y2025 day10 part1 done

--- src/y2025/day10.rs
+++ src/y2025/day10.rs
@@ -12,31 +12,53 @@ use {
     // serde::Serialize,
     std::{
         cmp::{Ordering, Reverse},
-        collections::{BinaryHeap, HashMap},
+        collections::{BinaryHeap, HashMap, HashSet},
         hash::BuildHasherDefault,
     },
 };
 
+type Spec = (Vec<bool>, Vec<Vec<usize>>, Vec<usize>);
+
 #[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
 pub struct Puzzle {
-    line: Vec<()>,
+    line: Vec<Spec>,
 }
 
 mod parser {
     use {
+        super::Spec,
         crate::parser::parse_usize,
         winnow::{
             ModalResult, Parser,
             ascii::{alpha1, newline, space1},
-            combinator::{alt, separated, seq},
+            combinator::{alt, repeat, separated, seq},
+            token::one_of,
         },
     };
 
-    fn parse_line(s: &mut &str) -> ModalResult<()> {
-        ().parse_next(s)
+    fn parse_indicator(s: &mut &str) -> ModalResult<Vec<bool>> {
+        seq!(_: "[", repeat(1.., one_of(['#', '.']).map(|s: char| s == '#')), _: "]")
+            .map(|(v,)| v)
+            .parse_next(s)
     }
-
-    pub fn parse(s: &mut &str) -> ModalResult<Vec<()>> {
+    fn parse_nums(s: &mut &str) -> ModalResult<Vec<usize>> {
+        separated(1.., parse_usize, ",").parse_next(s)
+    }
+    fn parse_buttons(s: &mut &str) -> ModalResult<Vec<Vec<usize>>> {
+        separated(1.., seq!(_: "(", parse_nums, _:")").map(|(v,)| v), " ").parse_next(s)
+    }
+    fn parse_requirement(s: &mut &str) -> ModalResult<Vec<usize>> {
+        seq!(_: "{", parse_nums, _:"}").map(|(v,)| v).parse_next(s)
+    }
+    fn parse_line(s: &mut &str) -> ModalResult<Spec> {
+        seq!(
+            parse_indicator, _: " ",
+            parse_buttons, _: " ",
+            parse_requirement,
+        )
+        .parse_next(s)
+    }
+    pub fn parse(s: &mut &str) -> ModalResult<Vec<Spec>> {
         separated(1.., parse_line, newline).parse_next(s)
     }
 }
@@ -48,10 +70,70 @@ impl AdventOfCode for Puzzle {
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
-        // let mut _: FxHashMap<_, _> = HashMap::<_, _, BuildHasherDefault<FxHasher>>::default();
-        1
+        self.line
+            .iter()
+            .map(|(goal, buttons, _)| {
+                let mut checked: HashSet<Vec<bool>> = HashSet::new();
+                let mut to_visit: HashSet<Vec<bool>> = HashSet::new();
+                let mut next: HashSet<Vec<bool>> = HashSet::new();
+                to_visit.insert(vec![false; goal.len()]);
+                for i in 1_usize.. {
+                    next.clear();
+                    for s in to_visit.iter() {
+                        if checked.contains(s) {
+                            continue;
+                        }
+                        checked.insert(s.clone());
+                        for button in buttons.iter() {
+                            let mut s1 = s.clone();
+                            for bi in button.iter() {
+                                s1[*bi] = !s1[*bi];
+                            }
+                            if s1 == *goal {
+                                return i;
+                            }
+                            if !checked.contains(&s1) {
+                                next.insert(s1);
+                            }
+                        }
+                    }
+                    std::mem::swap(&mut next, &mut to_visit);
+                }
+                unreachable!()
+            })
+            .sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
-        2
+        dbg!(self.line.len());
+        self.line
+            .iter()
+            .map(|(_, buttons, goal)| {
+                let mut to_visit: HashSet<Vec<usize>> = HashSet::new();
+                let mut next: HashSet<Vec<usize>> = HashSet::new();
+                to_visit.insert(vec![0; goal.len()]);
+                for i in 1_usize.. {
+                    next.clear();
+                    dbg!(to_visit.len());
+                    for s in to_visit.iter() {
+                        'next_button: for button in buttons.iter() {
+                            let mut s1 = s.clone();
+                            for bi in button.iter() {
+                                s1[*bi] += 1;
+                                if s1[*bi] > goal[*bi] {
+                                    continue 'next_button;
+                                }
+                            }
+                            if s1 == *goal {
+                                return dbg!(i);
+                            }
+                            next.insert(s1);
+                        }
+                    }
+                    std::mem::swap(&mut next, &mut to_visit);
+                    assert!(!to_visit.is_empty());
+                }
+                unreachable!()
+            })
+            .sum::<usize>()
     }
 }
