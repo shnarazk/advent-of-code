3a5cb8b39498fc2d781f89daa4c6217cbc3f2870
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-11 23:07:19 +0900

    (WIP) Y2025 day9: import microlp

--- src/y2025/day10.rs
+++ src/y2025/day10.rs
@@ -3,7 +3,11 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 use {
-    crate::framework::{self, AdventOfCode, ParseError, aoc},
+    crate::{
+        framework::{self, AdventOfCode, ParseError, aoc},
+        math::{gcd, lcm},
+    },
+    microlp::{ComparisonOp, OptimizationDirection, Problem, Variable},
     rayon::prelude::*,
     rustc_data_structures::fx::{FxHashMap, FxHasher},
     std::{
@@ -63,6 +67,13 @@ mod parser {
 impl AdventOfCode for Puzzle {
     fn prepare(&mut self, mut input: &str) -> Result<(), ParseError> {
         self.line = parser::parse(&mut input)?;
+        self.line.iter().take(10).for_each(|(_, buttons, goal)| {
+            let ws = buttons.iter().map(|l| l.len()).collect::<Vec<_>>();
+            let total = goal.iter().sum::<usize>();
+            let g = ws.iter().skip(1).fold(ws[0], |acc, n| gcd(acc, *n));
+            dbg!(g, total);
+        });
+        demo();
         Ok(())
     }
     fn part1(&mut self) -> Self::Output1 {
@@ -100,18 +111,24 @@ impl AdventOfCode for Puzzle {
             .sum::<usize>()
     }
     fn part2(&mut self) -> Self::Output2 {
-        dbg!(self.line.len());
         self.line
             .par_iter()
-            .take(1)
+            .take(10)
             .map(|(_, buttons, goal)| {
                 let mut sorted = buttons.clone();
                 sorted.sort_unstable_by_key(|l| l.len());
                 sorted.reverse();
-                let counts = vec![None; buttons.len()];
-                let levels = vec![0; goal.len()];
-                let mut memo: HashSet<Vec<usize>> = HashSet::new();
-                dbg!(traverse(&sorted, goal, counts, &levels, &mut memo).expect(""))
+
+                let b = sorted[0].len();
+                let s = sorted[sorted.len() - 1].len();
+                let memo: HashSet<Vec<usize>> = HashSet::new();
+                // let range = dbg!(b - s);
+
+                let counts: Vec<Option<usize>> = vec![None; buttons.len()];
+                let levels = vec![0_usize; goal.len()];
+
+                // dbg!(traverse(&sorted, goal, counts, &levels, &mut memo).expect(""))
+                0
                 /*
                 let size = goal.len();
                 let mut to_visit: HashSet<Vec<usize>> = HashSet::new();
@@ -134,10 +151,13 @@ impl AdventOfCode for Puzzle {
                             if s1 == *goal {
                                 return dbg!(i);
                             }
-                            for n in next.iter() {
-                                if (0..size).all(|i| s1[i] <= n[i]) {
-                                    continue 'next_button;
-                                }
+                            // for n in next.iter() {
+                            //     if (0..size).all(|i| s1[i] <= n[i]) {
+                            //         continue 'next_button;
+                            //     }
+                            // }
+                            if to_visit.contains(&s1) {
+                                dbg!();
                             }
                             next.insert(s1);
                         }
@@ -160,7 +180,7 @@ fn traverse(
     memo: &mut HashSet<Vec<usize>>,
 ) -> Option<usize> {
     if *levels == *goal {
-        return Some(counts.iter().flat_map(|n| n).sum::<usize>());
+        return Some(counts.iter().flatten().sum::<usize>());
     } else if let Some(cursor) = counts.iter().position(|n| n.is_none()) {
         let mut max_assign = buttons[cursor]
             .iter()
@@ -168,31 +188,87 @@ fn traverse(
             .min();
         let tmp = max_assign;
         while let Some(count) = max_assign {
-            if cursor == 0 {
-                dbg!(count, memo.len());
-            }
+            // if cursor == 0 {
+            //     dbg!(count, memo.len());
+            // }
             let mut new_counts = counts.clone();
             new_counts[cursor] = max_assign;
             let mut new_levels = levels.clone();
             for li in buttons[cursor].iter() {
                 new_levels[*li] += count;
             }
-            if memo.contains(&new_levels) {
-                return None;
-                dbg!("");
-            }
+            // if memo.contains(&new_levels) {
+            //     return None;
+            // }
             if let Some(ans) = traverse(buttons, goal, new_counts, &new_levels, memo) {
                 return Some(ans);
             }
             max_assign = count.checked_sub(1);
         }
-        if let Some(n) = tmp {
-            let mut l = levels.clone();
-            for li in buttons[cursor].iter() {
-                l[*li] += n;
-            }
-            memo.insert(l);
+        // if let Some(n) = tmp {
+        //     let mut l = levels.clone();
+        //     for li in buttons[cursor].iter() {
+        //         l[*li] += n;
+        //     }
+        //     memo.insert(l);
+        // }
+    }
+    None
+}
+
+fn f(v: &[usize], count: Vec<usize>, n: usize) -> Option<Vec<usize>> {
+    if n == 0 {
+        println!("{count:?}");
+        return None;
+    }
+    if v.is_empty() {
+        return None;
+    }
+    for i in (0..=n / v[0]).rev() {
+        let mut c = count.clone();
+        c.push(i);
+        if let Some(mut a) = f(&v[1..], c, n - v[0] * i) {
+            a.push(i);
+            return Some(a);
         }
     }
     None
+}
+
+// (3,4,5,7) (2,4,5,6,7) (1,4,7) (1,3,4,7) (1,2,3,4,5,7) (7) (1,2,3,6) (0,1,3,6,7)
+// {4,59,39,250,242,220,26,250}
+fn demo() {
+    let mut problem = Problem::new(OptimizationDirection::Minimize);
+    let mut buttons: Vec<Variable> = Vec::new();
+    for i in 0..8 {
+        let b = problem.add_integer_var(1.0, (0, i32::MAX));
+        buttons.push(b);
+    }
+    // (3,4,5,7) (2,4,5,6,7) (1,4,7) (1,3,4,7) (1,2,3,4,5,7) (7) (1,2,3,6) (0,1,3,6,7)
+    let config: Vec<Vec<usize>> = vec![
+        vec![3, 4, 5, 7],
+        vec![2, 4, 5, 6, 7],
+        vec![1, 4, 7],
+        vec![1, 3, 4, 7],
+        vec![1, 2, 3, 4, 5, 7],
+        vec![7],
+        vec![1, 2, 3, 6],
+        vec![0, 1, 3, 6, 7],
+    ];
+    let goals: Vec<usize> = vec![4, 59, 39, 250, 242, 220, 26, 250];
+    for (gi, g) in goals.iter().enumerate() {
+        let mut group: Vec<(Variable, f64)> = Vec::new();
+        for (bi, b) in config.iter().enumerate() {
+            if b.contains(&gi) {
+                group.push((buttons[bi], 1.0));
+            }
+        }
+        problem.add_constraint(&group, ComparisonOp::Eq, *g as f64);
+    }
+
+    let solution = problem.solve().unwrap();
+    for (vi, v) in buttons.iter().enumerate() {
+        println!("b{vi} = {:?}", solution[*v]);
+    }
+    println!("done");
 }
