a00b5549a42cdd01a747fe8f4bd10553388308fa
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-15 19:03:04 +0900

    (Lean4) Y2024 day6: Remove unused line field and simplify movement tracing

--- Lean4/Y2024/Day06.lean
+++ Lean4/Y2024/Day06.lean
@@ -10,7 +10,6 @@ namespace Y2024.Day06
 open Accumulation CiCL Dim2 Std
 
 structure Input where
-  line : Array (Array Char)
   obstruction : HashSet Idx₂
   guardPos : Idx₂
   guardDir : Dir
@@ -21,7 +20,7 @@ instance : ToString Input where toString self := s!"{self.obstruction.toList}"
 
 namespace Input
 
-def status (self : Input) : (Idx₂ × Dir) := (self.guardPos, self.guardDir)
+def status (self : Input) : Idx₂ × Dir := (self.guardPos, self.guardDir)
 
 def turn (self : Input) : Input := { self with guardDir := self.guardDir.turn }
 
@@ -30,9 +29,10 @@ def moveTo (self : Input) (pos : Idx₂) : Input := { self with guardPos := pos 
 def includes (self : Input) (pos : Vec₂) : Option Idx₂ :=
   if h : (0, 0) ≤ pos ∧ pos.1 < self.size.1 ∧ pos.2 < self.size.2 then some ⟨pos, h.left⟩ else none
 
-def nextPos (self : Input) : Option Idx₂ :=
-  self.includes <| self.guardPos + self.guardDir.asVec₂
+/-- 移動先が領域内でなければ `none` -/
+def nextPos (self : Input) : Option Idx₂ := self.includes <| self.guardPos + self.guardDir.asVec₂
 
+/-- 同じ場所を辿れば`true`。`trail`に記録 -/
 partial
 def loop (self : Input) (trail : HashSet (Idx₂ × Dir)) (pos : Option Idx₂) : Bool :=
   match pos with
@@ -43,20 +43,20 @@ def loop (self : Input) (trail : HashSet (Idx₂ × Dir)) (pos : Option Idx₂) 
         then true
         else
           let trail' := trail.insert self₀.status
-          if let some p' := self₀.nextPos
+          if let q@(some p') := self₀.nextPos
             then
               if self₀.obstruction.contains p'
                 then
                   let self₁ := self₀.turn
-                  if let some p'' := self₁.nextPos
+                  if let q'@(some p'') := self₁.nextPos
                     then
-                        if self₁.obstruction.contains p''
-                            then
-                              let self₂ := self₁.turn
-                              self₂.loop trail' self₂.nextPos
-                            else self₁.loop trail' self₁.nextPos
+                      if self₁.obstruction.contains p''
+                        then
+                          let self₂ := self₁.turn
+                          self₂.loop trail' self₂.nextPos
+                        else self₁.loop trail' q'
                     else false
-                else self₀.loop trail' self₀.nextPos
+                else self₀.loop trail' q
             else false
 
 def isLoop (self : Input) (pos : Idx₂) (pre: Idx₂ × Dir) : Bool :=
@@ -74,8 +74,7 @@ open AoCParser
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
-def parseLine := do
-  many1 (pchar '.' <|> pchar '#' <|> pchar '^')
+def parseLine := do many1 (pchar '.' <|> pchar '#' <|> pchar '^')
 -- #eval AoCParser.parse parseLine "^..#"
 
 def parse : String → Option Input := AoCParser.parse parser
@@ -91,14 +90,17 @@ def parse : String → Option Input := AoCParser.parse parser
           (fun (i, l) ↦ l.enum.flatMap (fun (j, c) ↦ if c == '^' then #[(i, j)] else #[]))
           |> (·[0]!)
       let size := (v.size, v[0]!.size)
-      return Input.mk v h (p.1, p.2) Dir.N size
+      return Input.mk h (p.1, p.2) Dir.N size
 
 end parser
 
+/-- 辿った場所をHashMapとして返す -/
 partial
-def traceMove (self : Input) (pre : Option (Idx₂ × Dir)) (hash : HashMap Idx₂ (Option (Idx₂ × Dir)))
-    : HashMap Idx₂ (Option (Idx₂ × Dir)) :=
-  let hash' := if hash.contains self.guardPos then hash else hash.insert self.guardPos pre
+def traceMove (self : Input) (pre : Option (Idx₂ × Dir)) (hash : HashMap Idx₂ (Idx₂ × Dir))
+    : HashMap Idx₂ (Idx₂ × Dir) :=
+  let hash' := if let some p := pre
+      then if !hash.contains self.guardPos then hash.insert self.guardPos p else hash
+      else hash
   match self.nextPos with
     | none   => hash'
     | some p =>
@@ -118,11 +120,11 @@ end Part1
 namespace Part2
 
 def solve (input : Input) : Nat :=
-  traceMove input none HashMap.emptyWithCapacity
-    |>.filter (fun pos pre ↦ if let some p := pre then (input.isLoop pos p) else false)
-    |>.size
+  traceMove input none HashMap.emptyWithCapacity |>.filter input.isLoop |>.size
 
 end Part2
+
+def return0 (_: Input) : Nat := 0
 
 public def solve := AocProblem.config 2024 06 parser.parse Part1.solve Part2.solve
 
