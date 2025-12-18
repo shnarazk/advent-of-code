b4df0a40398b8855365ca2b3695f40eda617ffec
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-18 18:40:40 +0900

    Lean4/Y2024/Day07.lean: use A* search

--- Lean4/Y2024/Day07.lean
+++ Lean4/Y2024/Day07.lean
@@ -2,6 +2,7 @@ module
 
 public import «AoC».Basic
 public import «AoC».Parser
+public import «AoC».Combinator
 
 namespace Y2024.Day07
 open Accumulation Std
@@ -43,9 +44,30 @@ def shift (a b : Nat) (b0 : Nat := b) : Nat :=
   if b0 < 10 then a * 10 + b else shift (a * 10) b (b0 / 10)
 -- #eval shift 3000000 1000000
 
-def solve₂ (input : Input) : Nat :=
-  input.map (fun ((val : Nat), v) ↦ exp #[(· + ·), (· * ·), shift] val v.toList) |> sum
+namespace Part2
 
-public def solve := AocProblem.config 2024 07 parser.parse solve₁ solve₂
+open Batteries CiCL
+
+def search (goal : Nat) (numbers : Array Nat) : Nat := Id.run do
+  let end_index := numbers.size
+  let mut to_visit : BinaryHeap (Nat × Nat) _ := BinaryHeap.empty (fun (a b : Nat × Nat) ↦ a.1 < b.1)
+  to_visit := to_visit.insert (numbers[0]!, 1)
+  repeat
+    let some state := to_visit.max | break
+    to_visit := to_visit.popMax
+    if state.2 == end_index
+    then
+      if state.1 == goal then return goal
+    else
+      let operand := numbers[state.2]!
+      for next in [state.1 + operand, state.1 * operand, shift state.1 operand] do
+        if next ≤ goal then to_visit := to_visit.insert (next, state.2 + 1)
+  return 0
+
+def solve (input : Input) : Nat := input.map (uncurry search) |> sum
+
+end Part2
+
+public def solve := AocProblem.config 2024 07 parser.parse solve₁ Part2.solve
 
 end Y2024.Day07
