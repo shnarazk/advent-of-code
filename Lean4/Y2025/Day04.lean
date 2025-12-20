f3162d7c57e8bd890081398d2c26c4aeb5d3df4b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-20 13:18:54 +0900

    (Lean4) Y2025 day4 WIP

--- Lean4/Y2025/Day04.lean
+++ Lean4/Y2025/Day04.lean
@@ -3,43 +3,53 @@ module
 public import «AoC».Basic
 public import «AoC».Combinator
 public import «AoC».Parser
--- public import «AoC».Rect64
+public import «AoC».Rect64
 
 namespace Y2025.Day04
-open Accumulation CiCL
+open Accumulation CiCL TwoDimensionalVector64 Rect
 
 structure Input where
-deriving BEq, Hashable, Repr
+  grid: Rect Bool
+deriving BEq
 
-instance : ToString Input where toString _ := s!""
+instance : ToString Input where toString s := s!"{s.grid}"
 
 namespace parser
 
 open AoCParser
-open Std.Internal.Parsec
 open Std.Internal.Parsec.String
+
+def parse_line := repeated ((· == '@') <$> (pchar '.' <|> pchar '@'))
 
 def parse : String → Option Input := AoCParser.parse parser
   where
-    parser : Parser Input := return Input.mk
+    parser : Parser Input := builder <$> separated parse_line eol
+    builder := fun v ↦ Input.mk (Rect.of2DMatrix v)
 
 end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := 0
+open Std
+
+def solve (input : Input) : Nat := Id.run do
+  let l := input.grid.enum.filter (fun ib ↦ ib.2) |>.map (·.fst)
+  let h := HashSet.ofArray l
+  h.size
 
 end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := 0
+open Std
+
+def solve (input : Input) : Nat := Id.run do
+  let l := input.grid.enum.filter (fun ib ↦ ib.2) |>.map (·.fst)
+  let h := HashSet.ofArray l
+  h.size
 
 end Part2
 
-public def solve := AocProblem.config 2025 04
-  ((dbg "parsed as ") ∘ parser.parse)
-  Part1.solve
-  Part2.solve
+public def solve := AocProblem.config 2025 04 parser.parse Part1.solve Part2.solve
 
 end Y2025.Day04
