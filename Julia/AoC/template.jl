dc023cca81a6d271205820304761d545a14510c9
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-26 14:00:32 +0900

    (Julia) update template

--- Julia/AoC/template.jl
+++ Julia/AoC/template.jl
@@ -1,13 +1,13 @@
-using AoC
+using AoC, AoC.Parser, ParserCombinator
 
 function run()::ANS
-    open(datafile(2024, 0), "r") do file
+    open(datafile(2025, 0), "r") do file
         # lines = String.(eachline(file)) |>
         #     s -> filter(!isempty, s) |>
         #     s -> map(t -> Int.(parse_one(t, 🔎ints)), s)
-        sum1 = 0
-        sum2 = 0
-        (part1 = sum1, part2 = sum2)
+        part1 = 0
+        part2 = 0
+        (part1, part2)
     end
 end
 
