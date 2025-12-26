a71d6ec96cb8e3f8376c29d61bda7a9ecfc74c96
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-26 11:56:39 +0900

    (Lean4) y2025 day7 part1 done

--- Lean4/Y2025/Day07.lean
+++ Lean4/Y2025/Day07.lean
@@ -3,16 +3,17 @@ module
 public import «AoC».Basic
 public import «AoC».Combinator
 public import «AoC».Parser
--- public import «AoC».Vec
 
 namespace Y2025.Day07
-open Accumulation CiCL
+
+open Std Accumulation
 
 structure Input where
-  input : Array (Array Char)
-deriving BEq, Hashable, Repr
+  splitters : List (List Nat)
+  start : Nat
+deriving BEq
 
-instance : ToString Input where toString s := s!"{s.input}"
+instance : ToString Input where toString s := s!"{s.splitters}, {s.start}"
 
 namespace parser
 
@@ -20,25 +21,50 @@ open AoCParser
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
-def parse_nmemonic := pchar '.' <|> pchar '|' <|> pchar 'S'
+def parse_nmemonic := pchar '.' <|> pchar '^' <|> pchar 'S'
 def parse_line := repeated parse_nmemonic
 def parse_input := separated parse_line eol
 
 def parse : String → Option Input := AoCParser.parse parser
   where
-    parser : Parser Input := Input.mk <$> parse_input
+    parser : Parser Input := do
+    let grid ← parse_input
+    let mut start := 0
+    let mut splitters : List (List Nat) := []
+    for l in grid do
+      let mut sps := []
+      for (j, ch) in l.enumerate do
+        if ch == 'S' then start := j
+        if ch == '^' then sps := sps.insert j
+      if !sps.isEmpty then splitters := splitters.concat sps
+    pure <| Input.mk splitters start
 
 end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := 0
+def solve (input : Input) : Nat := Id.run do
+  let mut numSplits := 0
+  let mut pos : HashSet Nat := HashSet.emptyWithCapacity 100
+  pos := pos.insert input.start
+  for line in input.splitters do
+    let mut next : HashSet Nat := HashSet.emptyWithCapacity 10
+    for p in pos do
+      if line.contains p then
+        numSplits := numSplits + 1
+        next := next.insert (p - 1)
+        next := next.insert (p + 1)
+      else
+        next := next.insert p
+    pos := next
+  numSplits
 
 end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := 0
+def solve (input : Input) : Nat := Id.run do
+  0
 
 end Part2
 
