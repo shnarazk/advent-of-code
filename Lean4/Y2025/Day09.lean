55567b9e69cba44501e16baf3baa265ecf31515e
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-28 12:42:54 +0900

    WIP

--- Lean4/Y2025/Day09.lean
+++ Lean4/Y2025/Day09.lean
@@ -1,6 +1,6 @@
 module
 
-public import Std.Data.Iterators.Combinators.Zip
+-- public import Std.Data.Iterators.Combinators.Zip
 public import «AoC».Basic
 public import «AoC».Combinator
 public import «AoC».Parser
@@ -42,19 +42,45 @@ namespace Part1
 
 def solve (input : Input) : Nat :=
   input.line.iter
-    |> (fun {α : Type} (a : Iter α) ↦ (0...a.count).iter.zip a)
+    |>.enumerate
     |>.map (fun (i, p) ↦
       input.line.iterFromIdx (i + 1)
         |>.map (fun (q : Idx₂) ↦ (dist₁ p.val.1 q.val.1) * (dist₁ p.val.2 q.val.2))
-        |>.fold (fun (a b : Nat) ↦ max a b) 0)
-    |>.fold (fun (a b : Nat) ↦ max a b) 0
+        |>.fold max 0)
+    |>.fold max 0
 
 end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := Id.run do
+def solve (input : Input) : Nat := Id.run do
+  let ys := input.line.iter |>.map (·.val.fst) |>.toList |> HashSet.ofList |>.toArray |>.push 0 |>.qsort
+  let encodeY := ys.iter.enumerate.map (fun (i, d) ↦ (d, i)) |>.toList |> HashMap.ofList
+  let xs := input.line.iter |>.map (·.val.snd) |>.toList |> HashSet.ofList |>.toArray |>.push 0 |>.qsort
+  let encodeX := xs.iter.enumerate.map (fun (i, d) ↦ (d, i)) |>.toList |> HashMap.ofList
+  let mut sliceY : HashMap Nat (Array Nat) := HashMap.emptyWithCapacity 100
+  let mut sliceX : HashMap Nat (Array Nat) := HashMap.emptyWithCapacity 100
+  for p in input.line.iter do
+    sliceY := sliceY.alter p.val.fst.toNat (fun o ↦ o)
+    sliceX := sliceX.alter p.val.fst.toNat (fun o ↦ o)
+  sliceY := sliceY.iter |>.map (fun (k, v) ↦ (k, v.qsort)) |>.toList |> HashMap.ofList
+  sliceX := sliceX.iter |>.map (fun (k, v) ↦ (k, v.qsort)) |>.toList |> HashMap.ofList
+  let gridSize := ys.size + 2
+  let mut grid : Rect Nat := Rect.ofDim2 gridSize gridSize 3
+  for (y, xs) in sliceY.iter do
+    grid := grid.set (encodeY.get! y, encodeX.get! xs[0]!) 1 
+    grid := grid.set (encodeY.get! y, encodeX.get! xs[1]!) 1 
+    for x in (encodeX.get! xs[0]! + 1) ... (encodeX.get! xs[1]!) do grid := grid.set (encodeY.get! y, x) 2 
+  for (x, ys) in sliceX.iter do
+    for y in (encodeX.get! ys[0]! + 1) ... (encodeX.get! ys[1]!) do grid := grid.set (y, encodeY.get! x) 2 
+  -- categorize the unvisited cells
+  let mut toVisit : HashSet Idx₂ := HashSet.emptyWithCapacity 100
+  toVisit := toVisit.insert (default : Idx₂)
+  while !toVisit.isEmpty do
+    break
   0
+
+-- #eval toIdx₂ ((0, 8) : Vec₂) --.toIdx₂
 
 end Part2
 
