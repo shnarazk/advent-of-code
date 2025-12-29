32ecbc4b0e2862e62792137e24c8e1bd6baf9fa1
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-29 14:04:35 +0900

    (Lean4): y2025 day10 part1 done

--- Lean4/Y2025/Day10.lean
+++ Lean4/Y2025/Day10.lean
@@ -3,16 +3,14 @@ module
 public import «AoC».Basic
 public import «AoC».Combinator
 public import «AoC».Parser
--- public import «AoC».Vec
 
 namespace Y2025.Day10
 
-open Accumulation CiCL
-
 structure Input where
+  line : Array (Array Bool × Array (Array Nat) × Array Nat)
 deriving BEq, Hashable, Repr
 
-instance : ToString Input where toString _ := s!""
+instance : ToString Input where toString s := s!"{s.line}"
 
 namespace parser
 
@@ -20,15 +18,62 @@ open AoCParser
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
+def parse_indicators := do
+  let v ← pchar '[' *> repeated (pchar '.' <|> pchar '#') <* pchar ']'
+  v.iter.map (· == '#') |>.toArray |> pure
+-- #eval AoCParser.parse parse_indicators "[..##.]"
+
+def parse_nums := separated number (pchar ',') 
+-- #eval AoCParser.parse parse_nums "42,31,8"
+
+def parse_buttons := separated (pchar '(' *> parse_nums <* pchar ')') (pchar ' ') 
+-- #eval AoCParser.parse parse_buttons "(42,31) (4,31)"
+
+def parse_requirement := pchar '{' *> parse_nums <* pchar '}'
+-- #eval AoCParser.parse parse_requirement "{42,31,4,31}"
+
+def parse_line := do
+  let i ← parse_indicators <* pchar ' '
+  let b ← parse_buttons <* pchar ' '
+  let r ← parse_requirement
+  return (i, b, r)
+
 def parse : String → Option Input := AoCParser.parse parser
   where
-    parser : Parser Input := return Input.mk
+    parser : Parser Input := do Input.mk <$> separated parse_line eol
+-- #eval parse "[.#] (1,3) (2,3,4) {42,31,4,31}"
 
 end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := 0
+open Std
+
+def toIdicator (buttons : Array (Array Nat)) (state : Array Bool) (len : Nat) : Array Bool :=
+  state.iter
+    |>.enumerate
+    |>.fold
+      (fun acc (n, b) ↦
+        if b then buttons[n]! |>.iter |>.fold (fun acc i ↦ acc.modify i (!·)) acc else acc)
+      (Array.replicate len false)
+ -- #eval toIdicator #[#[1], #[0,2]] #[false, true] 3
+
+def solve' (setting : Array Bool × Array (Array Nat) × Array Nat) : Nat := Id.run do
+  let (indicator, buttons,_ ) := setting
+  let len := indicator.size
+  let mut toVisit : Array (Array Bool) := #[Array.replicate buttons.size false]
+  while !toVisit.isEmpty do
+    let mut next : HashSet (Array Bool) := HashSet.emptyWithCapacity 1
+    for state in toVisit.iter do
+      if toIdicator buttons state len == indicator then return (state.iter.filter (·) |>.count)
+      for (i, b) in state.iter.enumerate do
+        if b then continue
+        let s := state.set! i true
+        next := next.insert s
+    toVisit := next.toArray
+  10000
+
+def solve (input : Input) : Nat := input.line.iter |>.map solve' |>.sum
 
 end Part1
 
@@ -38,9 +83,6 @@ def solve (_ : Input) : Nat := 0
 
 end Part2
 
-public def solve := AocProblem.config 2025 10
-  ((dbg "parsed as ") ∘ parser.parse)
-  Part1.solve
-  Part2.solve
+public def solve := AocProblem.config 2025 10 parser.parse Part1.solve Part2.solve
 
 end Y2025.Day10
