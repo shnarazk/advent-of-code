350254cefa5fae9effd69104b719f1890b5277f4
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-29 12:26:10 +0900

    (Lean4) prepare y2025 day10

--- Lean4/Y2025.lean
+++ Lean4/Y2025.lean
@@ -10,7 +10,7 @@ public import «Y2025».Day06
 public import «Y2025».Day07
 public import «Y2025».Day08
 public import «Y2025».Day09
--- public import «Y2025».Day10
+public import «Y2025».Day10
 -- public import «Y2025».Day11
 -- public import «Y2025».Day12
 
@@ -28,7 +28,7 @@ def solvers : List (Option String → IO AocProblem) := [
   Y2025.Day07.solve,
   Y2025.Day08.solve,
   Y2025.Day09.solve,
-  -- Y2025.Day10.solve,
+  Y2025.Day10.solve,
   -- Y2025.Day11.solve,
   -- Y2025.Day12.solve,
 ]
