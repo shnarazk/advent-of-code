37a3067aaae1aa550f698446b04e93967040c28f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-29 23:47:34 +0900

    (Lean4) prepare y2025 day{11,12}

--- Lean4/Y2025.lean
+++ Lean4/Y2025.lean
@@ -11,8 +11,8 @@ public import «Y2025».Day07
 public import «Y2025».Day08
 public import «Y2025».Day09
 public import «Y2025».Day10
--- public import «Y2025».Day11
--- public import «Y2025».Day12
+public import «Y2025».Day11
+public import «Y2025».Day12
 
 @[expose] public section
 
@@ -29,8 +29,8 @@ def solvers : List (Option String → IO AocProblem) := [
   Y2025.Day08.solve,
   Y2025.Day09.solve,
   Y2025.Day10.solve,
-  -- Y2025.Day11.solve,
-  -- Y2025.Day12.solve,
+  Y2025.Day11.solve,
+  Y2025.Day12.solve,
 ]
 
 protected def solvedDays : Nat := solvers.length
---
+++ Lean4/Y2025/Day12.lean
@@ -0,0 +1,46 @@
+module
+
+public import «AoC».Basic
+public import «AoC».Combinator
+public import «AoC».Parser
+-- public import «AoC».Vec
+
+namespace Y2025.Day12
+
+open Accumulation CiCL
+
+structure Input where
+deriving BEq, Hashable, Repr
+
+instance : ToString Input where toString _ := s!""
+
+namespace parser
+
+open AoCParser
+open Std.Internal.Parsec
+open Std.Internal.Parsec.String
+
+def parse : String → Option Input := AoCParser.parse parser
+  where
+    parser : Parser Input := return Input.mk
+
+end parser
+
+namespace Part1
+
+def solve (_ : Input) : Nat := Id.run do 0
+
+end Part1
+
+namespace Part2
+
+def solve (_ : Input) : Nat := Id.run do 0
+
+end Part2
+
+public def solve := AocProblem.config 2025 12
+  ((dbg "parsed as ") ∘ parser.parse)
+  Part1.solve
+  Part2.solve
+
+end Y2025.Day12
