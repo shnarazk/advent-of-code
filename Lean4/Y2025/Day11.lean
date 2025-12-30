138b4b30fef5bbac359ab8f8b7ba006346276cea
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-30 15:37:30 +0900

    (Lean4) y2025 day11 part1 done

--- Lean4/Y2025/Day11.lean
+++ Lean4/Y2025/Day11.lean
@@ -39,7 +39,29 @@ end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := Id.run do 0
+partial
+def traverse
+    (map : HashMap String (Array String))
+    (table' : HashMap (String × String) Nat)
+    (src dst : String)
+    : Nat × HashMap (String × String) Nat := Id.run do
+  match table'.get? (src, dst) with
+  | some n => return (n, table')
+  | none =>
+    let mut table := table'
+    let mut pathes := 0
+    for dest in map.get! src do
+      let (c, t) := traverse map table dest dst
+      pathes := pathes + c
+      table := t
+    (pathes, table.insert (src, dst) pathes)
+
+def solve (input : Input) : Nat := Id.run do
+  let mut count : HashMap (String × String) Nat := HashMap.emptyWithCapacity input.names.size
+  for (s, ds) in input.flow do
+    for d in ds do
+      count := count.insert (s, d) 1
+  traverse input.flow count "you" "out" |>.fst
 
 end Part1
 
