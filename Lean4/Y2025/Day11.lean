696ac2360dffb3771b919b9b3e29b78ed844e7b1
Narazaki, Shuji <shujinarazaki@protonmail.com>
2025-12-30 19:44:18 +0900

    (Lean4) y2025 day11 done.

--- Lean4/Y2025/Day11.lean
+++ Lean4/Y2025/Day11.lean
@@ -67,7 +67,46 @@ end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := Id.run do 0
+partial
+def traverse
+    (map : HashMap String (Array String))
+    (table' : HashMap (String × String) Nat)
+    (src dst : String)
+    : Nat × HashMap (String × String) Nat := Id.run do
+  match table'.get? (src, dst) with
+  | some n => return (n, table')
+  | none =>
+    let mut table := table'
+    let mut pathes := 0
+    let some dests := map.get? src | return (0, table)
+    for dest in dests do
+      let (c, t) := traverse map table dest dst
+      pathes := pathes + c
+      table := t
+    (pathes, table.insert (src, dst) pathes)
+
+def solve (input : Input) : Nat := Id.run do
+  let mut count : HashMap (String × String) Nat := HashMap.emptyWithCapacity input.names.size
+  for (s, ds) in input.flow do
+    for d in ds do
+      count := count.insert (s, d) 1
+  let (fft_dac, count') := traverse input.flow count "fft" "dac"
+  count := count'
+  let (dac_fft, count') := traverse input.flow count "dac" "fft"
+  count := count'
+  match fft_dac == 0, dac_fft == 0 with
+  | false, false => return (panic s!"unexpected: {fft_dac}, {dac_fft}")
+  | false, true =>
+      let (svr_fft, count') := traverse input.flow count "svr" "fft"
+      count := count'
+      let (dac_out, _) := traverse input.flow count "dac" "out"
+      return svr_fft * fft_dac * dac_out
+  | true, false =>
+      let (svr_dac, count') := traverse input.flow count "svr" "dac"
+      count := count'
+      let (fft_out, _) := traverse input.flow count "fft" "out"
+      return svr_dac * dac_fft * fft_out
+  | true, true => return (dbg s!"??" 0)
 
 end Part2
 
