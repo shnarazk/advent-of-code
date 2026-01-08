5f99375f12c76c2d633cb808f32ac2ba2967fbc2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-08 21:47:47 +0900

    (Lean4) y2024 day11 part1 done

--- Lean4/Y2024/Day11.lean
+++ Lean4/Y2024/Day11.lean
@@ -14,6 +14,51 @@ deriving BEq, Hashable, Repr
 
 instance : ToString Input where toString s := s!"{s.line}"
 
+def digits (n : Nat) : List Nat :=
+  if n == 0 then
+    []
+  else
+    digits (n / 10) ++ [n % 10]
+decreasing_by
+  expose_names
+  simp at h
+  refine Nat.div_lt_self ?_ ?_
+  · exact Nat.ne_zero_iff_zero_lt.mp h
+  · exact Nat.one_lt_succ_succ 8
+
+#guard digits 12 == [1, 2]
+#guard digits 2 == [2]
+#guard digits 0 == []
+#guard digits 3568 == [3, 5, 6, 8]
+
+def undigits (l : List Nat) : Nat := l.foldl (· * 10 + ·) 0
+
+#guard undigits [1, 2, 5, 0] == 1250
+#guard undigits [5] == 5
+
+def dividable (n : Nat) : Option (Nat × Nat) :=
+  let l := digits n
+  if l.length % 2 == 0 then
+    some (l.take (l.length / 2) |> undigits, l.drop (l.length / 2) |> undigits)
+  else
+    none
+
+#guard dividable 1234 == some (12, 34)
+#guard dividable 14 == some (1, 4)
+#guard dividable 148 == none
+
+partial
+def count_stones (max_depth depth n : Nat) : Nat :=
+  if max_depth == depth then
+    1
+  else
+    if n == 0 then
+      count_stones max_depth (depth + 1) 1
+    else
+      match dividable n with
+      | some (a, b) => (count_stones max_depth (depth + 1) a) + (count_stones max_depth (depth + 1) b)
+      | _ => count_stones max_depth (depth + 1) (2024 * n)
+
 namespace parser
 
 open WinnowParsers
@@ -28,7 +73,7 @@ end parser
 
 namespace Part1
 
-def solve (input : Input) : Nat := input.line.iter.map (fun _ ↦ 1) |>.sum
+def solve (input : Input) : Nat := input.line.iter.map (fun n ↦ count_stones 25 0 n) |>.sum
 
 end Part1
 
