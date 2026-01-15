b543dc5a3409a7fe090f3e24265fcf4f464a0aa2
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-15 22:56:32 +0900

    chore (Ocaml): dune fmt

--- Ocaml5/hello/bin/main.ml
+++ Ocaml5/hello/bin/main.ml
@@ -4,12 +4,14 @@ let () =
   Eio_main.run @@ fun env ->
   let stdout = Stdenv.stdout env
   and fs = Stdenv.fs env
-  and aoc_dir = match Sys.getenv_opt "AOC_DIR" with
-  | Some dir -> dir
-  | None -> failwith "AOC_DIR is not defined" in
+  and aoc_dir =
+    match Sys.getenv_opt "AOC_DIR" with
+    | Some dir -> dir
+    | None -> failwith "AOC_DIR is not defined"
+  in
   let data_file = Path.(fs / aoc_dir / "data/2025/input-day11.txt") in
   Flow.copy_string "\nHello, World!\n" stdout;
   Flow.copy_string ("AOC_DIR: " ^ aoc_dir ^ "\n") stdout;
   Flow.copy_string (Path.native data_file |> Option.get) stdout;
   let data : string = Path.load data_file in
-  Flow.copy_string data stdout;
+  Flow.copy_string data stdout
