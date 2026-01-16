ab485e4b6d581d8f77b02c881320d3867c879872
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-16 15:08:12 +0900

    feat (Ocaml5): add parsers for y2025 day11

--- Ocaml5-AoC/bin/main.ml
+++ Ocaml5-AoC/bin/main.ml
@@ -1,5 +1,14 @@
+open Angstrom
 open Eio
 
+let token = take_while (function 'A' .. 'z' -> true | _ -> false)
+
+let parse_line =
+  lift2 (fun h t -> (h, t)) (token <* string ": ") (Aoc.Parsers.separated token (char ' '))
+
+let parser = Aoc.Parsers.separated parse_line (char '\n')
+
+(* for Advent of Code 2025 *)
 let () =
   Eio_main.run @@ fun env ->
   let stdout = Stdenv.stdout env
@@ -13,5 +22,12 @@ let () =
   Flow.copy_string "\nHello, World!\n" stdout;
   Flow.copy_string ("AOC_DIR: " ^ aoc_dir ^ "\n") stdout;
   Flow.copy_string (Path.native data_file |> Option.get) stdout;
+  Flow.copy_string "\n" stdout;
   let data : string = Path.load data_file in
-  Flow.copy_string data stdout
+  (* Flow.copy_string data stdout; *)
+  let parsed =
+    match Angstrom.(parse_string ~consume:Prefix parser data) with
+    | Ok v -> v
+    | Error msg -> failwith msg
+  in
+  Flow.copy_string (Format.sprintf "%d" (snd parsed.(1) |> Array.length) ^ "\n") stdout
