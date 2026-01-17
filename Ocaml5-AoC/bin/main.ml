cd1c4f735075482e8fd0658b56d9e771bbfa8f92
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-17 09:45:27 +0900

    feat (Ocaml5): implement year/day dispatcher

--- Ocaml5-AoC/bin/main.ml
+++ Ocaml5-AoC/bin/main.ml
@@ -1,15 +1,23 @@
 open Eio
+open Format
+
+let year = ref 2025
+let day = ref 1
+let help_msg = "aoc [-y YEAR] day"
+let spec_lst = [ ("-y", Arg.Set_int year, sprintf "year (default: %d)" !year) ]
+let () = Arg.parse spec_lst (fun d -> day := int_of_string d) help_msg
 
 let aoc_dir =
   match Sys.getenv_opt "AOC_DIR" with Some dir -> dir | None -> failwith "AOC_DIR is not defined"
 
-(* Flow.copy_string ("AOC_DIR: " ^ aoc_dir ^ "\n") stdout; *)
-
-(* for Advent of Code 2025 day11 *)
 let () =
   Eio_main.run @@ fun env ->
   let stdout = Stdenv.stdout env and fs = Stdenv.fs env in
-  let data_file = Path.(fs / aoc_dir / "data/2025/input-day11.txt") in
-  (* Flow.copy_string (Path.native data_file |> Option.get) stdout; *)
-  (* Flow.copy_string "\n" stdout; *)
-  Y2025.Day11.solve data_file stdout
+  let data_file = Path.(fs / aoc_dir / sprintf "data/%d/input-day%2d.txt" !year !day) in
+  if Path.is_file data_file then
+    Flow.copy_string ((Path.native data_file |> Option.get) ^ "\n") stdout
+  else failwith @@ sprintf "%s does not exist." (Path.native data_file |> Option.get);
+  match !year with
+  | 2025 -> (
+      match !day with 11 -> Y2025.Day11.solve data_file stdout | _ -> failwith "invalid day")
+  | _ -> failwith "invalid year"
