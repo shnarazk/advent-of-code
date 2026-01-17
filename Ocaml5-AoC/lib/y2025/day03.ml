36fbcbbc665c5b3e8efd6a8b567eb411333e06c5
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-17 12:31:01 +0900

    feat (Ocaml5): y2025 day3 done.

--- Ocaml5-AoC/lib/y2025/day03.ml
+++ Ocaml5-AoC/lib/y2025/day03.ml
@@ -2,8 +2,26 @@ open Angstrom
 open Eio
 open Format
 
-let digits = take_while (function '0' .. '9' -> true | _ -> false)
-let parser = Aoc.Parsers.separated digits end_of_line <* end_of_input
+let digits =
+  take_while1 (function '0' .. '9' -> true | _ -> false) >>| fun (v : string) ->
+  String.to_seq v |> Seq.map (fun ch -> Char.code ch - Char.code '0') |> Array.of_seq
+
+let parser = Aoc.Parsers.separated digits end_of_line <* end_of_line <* end_of_input
+
+(** solve the unit problem *)
+let evaluate (len : int) (a : int array) : int =
+  let buffer = Array.(sub a (length a - len) len) in
+  for i = Array.length a - len - 1 downto 0 do
+    let target = ref a.(i) in
+    let j = ref 0 in
+    while !j < Array.length buffer && buffer.(!j) <= !target do
+      let tmp = buffer.(!j) in
+      buffer.(!j) <- !target;
+      target := tmp;
+      j := !j + 1
+    done
+  done;
+  Array.fold_left (fun acc n -> (acc * 10) + n) 0 buffer
 
 let solve data_file stdout =
   let data =
@@ -11,6 +29,7 @@ let solve data_file stdout =
     | Ok v -> v
     | Error msg -> failwith msg
   in
-  Flow.copy_string ([%show: string array] data) stdout;
-  Flow.copy_string (sprintf "Part1: %d\n" 0) stdout;
-  Flow.copy_string (sprintf "Part2: %d\n" 0) stdout
+  let part1 = Array.map (evaluate 2) data |> Array.fold_left ( + ) 0
+  and part2 = Array.map (evaluate 12) data |> Array.fold_left ( + ) 0 in
+  Flow.copy_string (sprintf "Part1: %d\n" part1) stdout;
+  Flow.copy_string (sprintf "Part2: %d\n" part2) stdout
