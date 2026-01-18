2633212cbc04edf6e28fc4e747c7e060d46a7d2c
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-18 19:53:00 +0900

    feat (Ocaml5): y2025 day1 done.

--- Ocaml5-AoC/lib/y2025/day01.ml
+++ Ocaml5-AoC/lib/y2025/day01.ml
@@ -5,8 +5,27 @@ open Format
 let sign = char 'L' <|> char 'R'
 let parse_line = lift2 (fun s n -> if s = 'L' then -n else n) sign Aoc.Parsers.integer
 let parser = Aoc.Parsers.separated parse_line end_of_line <* end_of_line <* end_of_input
-let solve1 (_turns : int array) : int = 0
-let solve2 (_turns : int array) : int = 0
+
+let solve1 (turns : int array) : int =
+  Array.to_seq turns
+  |> Seq.fold_left
+       (fun (value, count) turn ->
+         let next = (((value + turn) mod 100) + 100) mod 100 in
+         let inc = if next = 0 then 1 else 0 in
+         (next, count + inc))
+       (50, 0)
+  |> snd
+
+let solve2 (turns : int array) : int =
+  Array.to_seq turns
+  |> Seq.fold_left
+       (fun (value, count) turn ->
+         let n = value + turn in
+         let inc = (abs n / 100) + if value > 0 && n <= 0 then 1 else 0 in
+         let next = ((n mod 100) + 100) mod 100 in
+         (next, count + inc))
+       (50, 0)
+  |> snd
 
 let solve data_file stdout =
   let data =
@@ -14,7 +33,6 @@ let solve data_file stdout =
     | Ok v -> v
     | Error msg -> failwith msg
   in
-  print_endline @@ [%show: int array] data;
   let part1 = solve1 data and part2 = solve2 data in
   Flow.copy_string (sprintf "Part1: %d\n" part1) stdout;
   Flow.copy_string (sprintf "Part2: %d\n" part2) stdout
