ccff20306352ffd128bf8242ae015c6a2c471a0a
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-18 19:30:56 +0900

    chore (Ocaml5): prepare y2025 day1

--- Ocaml5-AoC/bin/main.ml
+++ Ocaml5-AoC/bin/main.ml
@@ -32,6 +32,7 @@ let () =
     match !year with
     | 2025 -> (
         match !day with
+        | 01 -> Y2025.Day01.solve data_file
         | 03 -> Y2025.Day03.solve data_file
         | 04 -> Y2025.Day04.solve data_file
         | 07 -> Y2025.Day07.solve data_file
--- Ocaml5-AoC/lib/y2025/day01.ml
+++ Ocaml5-AoC/lib/y2025/day01.ml
@@ -1,27 +1,20 @@
-(* open Angstrom
+open Angstrom
 open Eio
 open Format
 
-let token = take_while1 (function 'A' .. 'z' -> true | _ -> false)
-let parse_line = Aoc.Parsers.separated token (char ' ')
+let sign = char 'L' <|> char 'R'
+let parse_line = lift2 (fun s n -> if s = 'L' then -n else n) sign Aoc.Parsers.integer
 let parser = Aoc.Parsers.separated parse_line end_of_line <* end_of_line <* end_of_input
-
-(* module Dist = Hashtbl.Make (struct
-  type t = 
-
-  let equal a b = 
-  let hash = Hashtbl.hash
-end)
-*)
+let solve1 (_turns : int array) : int = 0
+let solve2 (_turns : int array) : int = 0
 
 let solve data_file stdout =
-  let _data =
+  let data =
     match Angstrom.(parse_string ~consume:All parser @@ Path.load data_file) with
     | Ok v -> v
     | Error msg -> failwith msg
   in
   print_endline @@ [%show: int array] data;
-  let part1 = 0 and part2 = 0 in
+  let part1 = solve1 data and part2 = solve2 data in
   Flow.copy_string (sprintf "Part1: %d\n" part1) stdout;
   Flow.copy_string (sprintf "Part2: %d\n" part2) stdout
-*)
