f19bb2a051ba0e5e80380cd37493fa4f41275168
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-20 12:50:50 +0900

    wip

--- Ocaml5-AoC/lib/y2025/day05.ml
+++ Ocaml5-AoC/lib/y2025/day05.ml
@@ -10,20 +10,29 @@ let parse_block2 = Aoc.Parsers.separated Parsers.integer end_of_line
 let parser =
   lift4 (fun a _ b _ -> (a, b)) parse_block1 (count 2 end_of_line) parse_block2 end_of_line
 
-(* module Dist = Hashtbl.Make (struct
-  type t = 
+module Level = Hashtbl.Make (struct
+  type t = int
 
-  let equal a b = 
+  let equal = ( = )
   let hash = Hashtbl.hash
 end)
-*)
 
 let solve1 (ranges : (int * int) array) (n : int) : int =
   Array.to_seq ranges
   |> Seq.find (fun (b, e) -> b <= n && n <= e)
   |> Option.fold ~none:0 ~some:(fun _ -> 1)
 
-let solve2 (ranges : (int * int) array) : int = Array.length ranges
+let solve2 (ranges : (int * int) array) : int =
+  let level = Level.create 64 in
+  Array.to_seq ranges
+  |> Seq.iter (fun (b, e) ->
+      Level.add level b @@ Option.fold ~none:1 ~some:(fun l -> l + 1) @@ Level.find_opt level b;
+      Level.add level e @@ Option.fold ~none:(-1) ~some:(fun l -> l - 1) @@ Level.find_opt level e);
+  let level =
+    List.fast_sort (fun (a, _) (b, _) -> compare a b) @@ List.of_seq @@ Level.to_seq level
+  in
+  print_endline @@ [%show: (int * int) list] level;
+  List.fold_left (fun (n, c) _ -> (n + 1, c + 1)) (0, 0) level |> snd
 
 let solve data_file stdout =
   let ranges, data =
