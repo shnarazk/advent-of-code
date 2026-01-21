b7c238b9af1b7a7cf8737bde8a6baf14b5905b04
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-22 00:12:16 +0900

    wip

--- Ocaml5-AoC/lib/y2025/day08.ml
+++ Ocaml5-AoC/lib/y2025/day08.ml
@@ -17,6 +17,36 @@ let dist3 ((x1 : int), (y1 : int), (z1 : int)) ((x2 : int), (y2 : int), (z2 : in
   let x = x2 - x1 and y = y2 - y1 and z = z2 - z1 in
   (x * x) + (y * y) + (z * z)
 
+let groups1 (dists : (int * int) array) (n : int) (upto : int) : int =
+  let num_groups = ref n
+  and _next_group_id = ref 1
+  and groups = ref [| 0 |]
+  and to_group : int array = Array.make n 0 in
+  let belongsTo (id : int) : int =
+    let rec loop (gid : int) : int =
+      match !groups.(gid) with 0 -> gid | _ -> loop !groups.(gid)
+    in
+    loop to_group.(id)
+  in
+  for i = 0 to upto do
+    let p1, p2 = dists.(i) in
+    let p1_group = belongsTo p1 and p2_group = belongsTo p2 in
+    match (p1_group, p2_group) with
+    | 0, 0 ->
+        num_groups := !num_groups + 1;
+        ()
+    | _g, 0 -> ()
+    | 0, _g -> ()
+    | g1, g2 ->
+        if g1 = g2 then ()
+        else (
+          num_groups := !num_groups - 1;
+          ())
+  done;
+  0
+
+let groups2 (_dists : (int * int) array) (_n : int) : int = 0
+
 let solve data_file stdout =
   let data =
     match Angstrom.(parse_string ~consume:All parser @@ Path.load data_file) with
@@ -32,6 +62,9 @@ let solve data_file stdout =
   let dists = Array.of_seq @@ Dist.to_seq dists in
   Array.fast_sort (fun (a, _) (b, _) -> compare a b) dists;
   print_endline @@ [%show: (int * (int * int)) array] dists;
-  let part1 = 0 and part2 = 0 in
+  let dists = Array.map (fun (_, p) -> p) dists in
+  let num_nodes = Array.length data in
+  let part1_limit = if num_nodes == 20 then 10 else 100 in
+  let part1 = groups1 dists num_nodes part1_limit and part2 = groups2 dists num_nodes in
   Flow.copy_string (sprintf "Part1: %d\n" part1) stdout;
   Flow.copy_string (sprintf "Part2: %d\n" part2) stdout
