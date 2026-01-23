9b22d7cfd770e8897926b40a2b690a137411cd08
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-23 09:26:44 +0900

    feat (Ocaml5): add more functions to dim2

--- Ocaml5-AoC/lib/dim2.ml
+++ Ocaml5-AoC/lib/dim2.ml
@@ -3,7 +3,12 @@
 type dim2 = int * int
 
 let add ((y1, x1) : dim2) ((y2, x2) : dim2) : dim2 = (y1 + y2, x1 + x2)
+let eq ((y1, x1) : dim2) ((y2, x2) : dim2) : bool = y1 = y2 && x1 = x2
 let le ((y1, x1) : dim2) ((y2, x2) : dim2) : bool = y1 <= y2 && x1 <= x2
+let lt ((y1, x1) : dim2) ((y2, x2) : dim2) : bool = y1 < y2 && x1 < x2
+
+let cmp ((y1, x1) : dim2) ((y2, x2) : dim2) : int =
+  match compare y1 y2 with 0 -> compare x1 x2 | c -> c
 
 (** 4 neighbors *)
 let dir4 : dim2 array = [| (-1, 0); (0, 1); (1, 0); (0, -1) |]
@@ -17,3 +22,14 @@ let neightbor4 (pos : dim2) (upto : dim2) : dim2 array =
   |> Seq.map (fun d -> add pos d)
   |> Seq.filter (fun p -> le (0, 0) p && le p upto)
   |> Array.of_seq
+
+module Dim2Set = Hashtbl.Make (struct
+  type t = dim2
+
+  let equal = eq
+  let hash = Hashtbl.hash
+end)
+
+let makeArray (h : int) (w : int) (default : 'a) : 'a array array =
+  let makeVector (n : int) : 'a array = Array.make n default in
+  Seq.init h (fun n -> n) |> Seq.map (fun _ -> makeVector w) |> Array.of_seq
