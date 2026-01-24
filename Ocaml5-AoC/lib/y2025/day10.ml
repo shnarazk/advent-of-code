79004bf3d6bc4c862f2477ab5b751307f8382879
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-01-25 00:45:39 +0900

    feat (Ocaml): y2025 day10 part1 done

--- Ocaml5-AoC/lib/y2025/day10.ml
+++ Ocaml5-AoC/lib/y2025/day10.ml
@@ -15,13 +15,47 @@ let parse_goal = char '{' *> Parsers.separated Parsers.integer (char ',') <* cha
 let parse_line = lift3 (fun a b c -> (a, b, c)) parse_indicator parse_buttons parse_goal
 let parser = Parsers.separated parse_line end_of_line <* end_of_line <* end_of_input
 
-(* module Dist = Hashtbl.Make (struct
-  type t = 
+module Map = Hashtbl.Make (struct
+  type t = bool array
 
-  let equal a b = 
+  let equal v1 v2 = v1 = v2
+  (* Array.length v1 = Array.length v2 && Array.to_seqi v1 |> Seq.for_all (fun (i, b) -> b = v2.(i)) *)
+
   let hash = Hashtbl.hash
 end)
-*)
+
+let solve1 (indicator : bool array) (buttons : int array list) : int =
+  let checked = Map.create 64
+  and to_visit = ref @@ Map.create 64
+  and next = Map.create 64
+  and steps = ref 0
+  and continue = ref true in
+  Map.add !to_visit Array.(make (length indicator) false) ();
+  while !continue do
+    Map.clear next;
+    let cands = Map.to_seq_keys !to_visit |> Array.of_seq in
+    for i = 0 to Array.length cands - 1 do
+      if !continue then begin
+        let state = cands.(i) in
+        if state = indicator then continue := false
+        else if not (Map.mem checked state) then begin
+          Map.add checked state ();
+          List.to_seq buttons
+          |> Seq.iter (fun btn_spec ->
+              let s = Array.copy state in
+              Array.to_seq btn_spec |> Seq.iter (fun i -> s.(i) <- not s.(i));
+              Map.replace next s ();
+              ())
+        end
+      end
+    done;
+    if !continue then begin
+      to_visit := Map.copy next;
+      assert (0 < Map.length !to_visit);
+      steps := !steps + 1
+    end
+  done;
+  !steps
 
 let solve data_file stdout =
   let data =
@@ -29,7 +63,8 @@ let solve data_file stdout =
     | Ok v -> v
     | Error msg -> failwith msg
   in
-  print_endline @@ [%show: (bool array * int array list * int array) array] data;
-  let part1 = 0 and part2 = 0 in
+  (* print_endline @@ [%show: (bool array * int array list * int array) array] data; *)
+  let part1 = Array.to_seq data |> Seq.map (fun (i, b, _) -> solve1 i b) |> Seq.fold_left ( + ) 0
+  and part2 = 0 in
   Flow.copy_string (sprintf "Part1: %d\n" part1) stdout;
   Flow.copy_string (sprintf "Part2: %d\n" part2) stdout
