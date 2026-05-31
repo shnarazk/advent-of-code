e5bfde0b1448bc59fb72b8349f6a1590b6e3470b
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-05-31 23:32:45 +0900

    feat(Swift): y2025 day5 part1 done

--- Swift/Sources/Y2025/day05.swift
+++ Swift/Sources/Y2025/day05.swift
@@ -1,26 +1,42 @@
 import Parsing
 
-private func part1() -> Int {
-    0
+private func part1(_ ranges: [(Int, Int)], _ availables: [Int]) -> Int {
+    availables.filter { id in
+        ranges.contains(where: { (b, e) in b <= id && id <= e })
+    }
+    .count
 }
 
-private func part2() -> Int {
+private func part2(_ _ranges: [(Int, Int)], _ _availables: [Int]) -> Int {
     0
 }
 
 public func day05(_ data: String) {
-    let parser: some Parser<Substring, [Int]> = Many {
+    let range_parser: some Parser<Substring, (Int, Int)> = Parse {
         Int.parser()
+        "-"
+        Int.parser()
+    }
+    let parser_part1: some Parser<Substring, [(Int, Int)]> = Many {
+        range_parser
     } separator: {
-        " "
-    } terminator: {
         "\n"
     }
+    let parser_part2: some Parser<Substring, [Int]> = Many {
+        Parse {
+            Int.parser()
+            "\n"
+        }
+    }
+    let parser: some Parser<Substring, ([(Int, Int)], [Int])> = Parse {
+        parser_part1
+        "\n\n"
+        parser_part2
+    }
     do {
-        // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
-        let input = try parser.parse(data)
-        let sum1 = part1()
-        let sum2 = part2()
+        let (ranges, avalables) = try parser.parse(data)
+        let sum1 = part1(ranges, avalables)
+        let sum2 = part2(ranges, avalables)
         print("Part 1: \(sum1)")
         print("Part 2: \(sum2)")
     } catch {
