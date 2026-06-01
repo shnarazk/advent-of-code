57d3e40d3c2493135a22b1711a1ee039817b5921
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-06-02 01:28:15 +0900

    feat(Swift): complete y2025 day7

--- Swift/Sources/Y2025/day07.swift
+++ Swift/Sources/Y2025/day07.swift
@@ -1,31 +1,72 @@
 import Parsing
+import Utils
 
-private func part1() -> Int {
-    0
+private func evaluate(_ branches: [[Int]], _ start: Int) -> (Int, Int) {
+    var count = 0
+    var line: [Int: Int] = [start: 1]
+    for branch in branches {
+        var next: [Int: Int] = [:]
+        for (x, n) in line {
+            if branch.contains(x) {
+                count += 1
+                next[x - 1, default: 0] += n
+                next[x + 1, default: 0] += n
+            } else {
+                next[x, default: 0] += n
+            }
+        }
+        line = next
+    }
+    return (count, line.values.reduce(0, +))
 }
 
-private func part2() -> Int {
-    0
+private func part2(_ branches: [[Int]], _ start: Int) -> Int {
+    var line: [Int: Int] = [start: 1]
+    for branch in branches {
+        var next: [Int: Int] = [:]
+        for (x, n) in line {
+            if branch.contains(x) {
+                next[x - 1, default: 0] += n
+                next[x + 1, default: 0] += n
+            } else {
+                next[x, default: 0] += n
+            }
+        }
+        line = next
+    }
+    return line.values.reduce(0, +)
 }
 
-public func day07(_ data: String) {
-    let parser: some Parser<Substring, [Int]> = Many {
-        Int.parser()
-    } separator: {
-        " "
+public func day07(_ data: String) throws {
+    let line_parser: some Parser<Substring, [Character]> = Many {
+        First<Substring>()
+            .filter { $0 == "." || $0 == "^" || $0 == "S" }
     } terminator: {
         "\n"
     }
-    do {
-        // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
-        let input = try parser.parse(data)
-        print(input)
-        let sum1 = part1()
-        let sum2 = part2()
-        print("Part 1: \(sum1)")
-        print("Part 2: \(sum2)")
-    } catch {
-        print(error)
-        fatalError()
+    let parser: some Parser<Substring, [[Character]]> = Many {
+        line_parser
     }
+
+    // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
+    let grid = try parser.parse(data)
+    var start: Pos = Pos(y: 0, x: 0)
+    var branches: [[Int]] = []
+    outer: for i in grid.indices {
+        var l: [Int] = []
+        for j in grid[i].indices {
+            switch grid[i][j] {
+            case "S":
+                start = Pos(y: i, x: j)
+            case "^":
+                l.append(j)
+            default:
+                break
+            }
+        }
+        branches.append(l)
+    }
+    let (sum1, sum2) = evaluate(branches, start.x)
+    print("Part 1: \(sum1)")
+    print("Part 2: \(sum2)")
 }
