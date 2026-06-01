4a85c640cf617bea0adadb8156b62f3e8a3787db
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-06-02 01:29:22 +0900

    refactor(Swift): move error handlers to aoc.swift

--- Swift/Sources/Y2025/day10.swift
+++ Swift/Sources/Y2025/day10.swift
@@ -8,7 +8,7 @@ private func part2() -> Int {
     0
 }
 
-public func day10(_ data: String) {
+public func day10(_ data: String) throws {
     let parser: some Parser<Substring, [Int]> = Many {
         Int.parser()
     } separator: {
@@ -16,16 +16,11 @@ public func day10(_ data: String) {
     } terminator: {
         "\n"
     }
-    do {
-        // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
-        let input = try parser.parse(data)
-        print(input)
-        let sum1 = part1()
-        let sum2 = part2()
-        print("Part 1: \(sum1)")
-        print("Part 2: \(sum2)")
-    } catch {
-        print(error)
-        fatalError()
-    }
+    // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
+    let input = try parser.parse(data)
+    print(input)
+    let sum1 = part1()
+    let sum2 = part2()
+    print("Part 1: \(sum1)")
+    print("Part 2: \(sum2)")
 }
--- Swift/Sources/aoc/aoc.swift
+++ Swift/Sources/aoc/aoc.swift
@@ -46,86 +46,91 @@ struct Aoc: @preconcurrency ParsableCommand, Decodable {
             saveDescription(year: year, day: day, variant: nil, text: "", force: false)
         }
         let beg = Date()
-        switch year {
-        case 2025:
-            switch day {
-            case 1: Y2025.day01(data)
-            case 2: Y2025.day02(data)
-            case 3: Y2025.day03(data)
-            case 4: Y2025.day04(data)
-            case 5: Y2025.day05(data)
-            case 6: Y2025.day06(data)
-            case 7: Y2025.day07(data)
-            case 8: Y2025.day08(data)
-            case 9: Y2025.day09(data)
-            case 10: Y2025.day10(data)
-            case 11: Y2025.day11(data)
-            case 12: Y2025.day12(data)
+        do {
+            switch year {
+            case 2025:
+                switch day {
+                case 1: Y2025.day01(data)
+                case 2: Y2025.day02(data)
+                case 3: Y2025.day03(data)
+                case 4: Y2025.day04(data)
+                case 5: Y2025.day05(data)
+                case 6: Y2025.day06(data)
+                case 7: try Y2025.day07(data)
+                case 8: try Y2025.day08(data)
+                case 9: try Y2025.day09(data)
+                case 10: try Y2025.day10(data)
+                case 11: try Y2025.day11(data)
+                case 12: try Y2025.day12(data)
+                default: fatalError()
+                }
+            case 2024:
+                switch day {
+                case 1: Y2024.day01(data)
+                case 2: Y2024.day02(data)
+                case 3: Y2024.day03(data)
+                case 4: Y2024.day04(data)
+                case 5: Y2024.day05(data)
+                case 6: Y2024.day06(data)
+                case 7: Y2024.day07(data)
+                case 8: Y2024.day08(data)
+                case 9: Y2024.day09(data)
+                case 10: Y2024.day10(data)
+                case 11: Y2024.day11(data)
+                case 12: Y2024.day12(data)
+                case 13: Y2024.day13(data)
+                case 14: Y2024.day14(data, save)
+                case 15: Y2024.day15(data)
+                case 16: Y2024.day16(data)
+                default: fatalError()
+                }
+            case 2023:
+                switch day {
+                case 1: Y2023.day01(data)
+                case 2: Y2023.day02(data)
+                case 3: Y2023.day03(data)
+                case 4: Y2023.day04(data)
+                case 5: Y2023.day05(data)
+                case 6: Y2023.day06(data)
+                case 7: Y2023.day07(data)
+                case 8: Y2023.day08(data)
+                case 9: Y2023.day09(data)
+                case 10: Y2023.day10(data)
+                case 11: Y2023.day11(data)
+                case 12: Y2023.day12(data)
+                default: fatalError()
+                }
+            case 2022:
+                switch day {
+                case 1: Y2022.day01(data)
+                case 2: Y2022.day02(data)
+                case 3: Y2022.day03(data)
+                case 4: Y2022.day04(data)
+                case 5: Y2022.day05(data)
+                case 6: Y2022.day06(data)
+                case 7: Y2022.day07(data)
+                case 8: Y2022.day08(data)
+                case 9: Y2022.day09(data)
+                case 10: Y2022.day10(data)
+                case 11: Y2022.day11(data)
+                case 12: Y2022.day12(data)
+                default: fatalError()
+                }
+            case 2016:
+                switch day {
+                case 18: Y2016.day18(data)
+                case 19: Y2016.day19(data)
+                case 22: Y2016.day22(data)
+                default: fatalError()
+                }
             default: fatalError()
             }
-        case 2024:
-            switch day {
-            case 1: Y2024.day01(data)
-            case 2: Y2024.day02(data)
-            case 3: Y2024.day03(data)
-            case 4: Y2024.day04(data)
-            case 5: Y2024.day05(data)
-            case 6: Y2024.day06(data)
-            case 7: Y2024.day07(data)
-            case 8: Y2024.day08(data)
-            case 9: Y2024.day09(data)
-            case 10: Y2024.day10(data)
-            case 11: Y2024.day11(data)
-            case 12: Y2024.day12(data)
-            case 13: Y2024.day13(data)
-            case 14: Y2024.day14(data, save)
-            case 15: Y2024.day15(data)
-            case 16: Y2024.day16(data)
-            default: fatalError()
-            }
-        case 2023:
-            switch day {
-            case 1: Y2023.day01(data)
-            case 2: Y2023.day02(data)
-            case 3: Y2023.day03(data)
-            case 4: Y2023.day04(data)
-            case 5: Y2023.day05(data)
-            case 6: Y2023.day06(data)
-            case 7: Y2023.day07(data)
-            case 8: Y2023.day08(data)
-            case 9: Y2023.day09(data)
-            case 10: Y2023.day10(data)
-            case 11: Y2023.day11(data)
-            case 12: Y2023.day12(data)
-            default: fatalError()
-            }
-        case 2022:
-            switch day {
-            case 1: Y2022.day01(data)
-            case 2: Y2022.day02(data)
-            case 3: Y2022.day03(data)
-            case 4: Y2022.day04(data)
-            case 5: Y2022.day05(data)
-            case 6: Y2022.day06(data)
-            case 7: Y2022.day07(data)
-            case 8: Y2022.day08(data)
-            case 9: Y2022.day09(data)
-            case 10: Y2022.day10(data)
-            case 11: Y2022.day11(data)
-            case 12: Y2022.day12(data)
-            default: fatalError()
-            }
-        case 2016:
-            switch day {
-            case 18: Y2016.day18(data)
-            case 19: Y2016.day19(data)
-            case 22: Y2016.day22(data)
-            default: fatalError()
-            }
-        default: fatalError()
+            let end = Date()
+            let t = String(format: "%.2f", end.timeIntervalSince(beg) * 1000)
+            print("\u{001B}[34m# Elapsed time: \(t) msec\u{001B}[0m")
+        } catch {
+            print(error)
+            fatalError()
         }
-        let end = Date()
-        let t = String(format: "%.2f", end.timeIntervalSince(beg) * 1000)
-        print("\u{001B}[34m# Elapsed time: \(t) msec\u{001B}[0m")
     }
 }
