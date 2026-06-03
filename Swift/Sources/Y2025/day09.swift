324889b9c7d0a465ddd8fbcdd14bec1e66572b5f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-06-03 19:24:09 +0900

    feat(Swift): complete y2025 day9 part1

--- Swift/Sources/Y2025/day09.swift
+++ Swift/Sources/Y2025/day09.swift
@@ -1,26 +1,30 @@
 import Parsing
 
-private func part1() -> Int {
-    0
+private func part1(_ input: [(Int, Int)]) -> Int {
+    input.indices.map { i in
+        input.indices.dropFirst(i + 1).map { j in
+            abs(input[i].0 - input[j].0 + 1) * abs(input[i].1 - input[j].1 + 1)
+        }.reduce(0, max)
+    }.reduce(0, max)
 }
 
-private func part2() -> Int {
+private func part2(_ input: [(Int, Int)]) -> Int {
     0
 }
 
 public func day09(_ data: String) throws {
-    let parser: some Parser<Substring, [Int]> = Many {
+    let line_parser: some Parser<Substring, (Int, Int)> = Parse {
         Int.parser()
-    } separator: {
-        " "
-    } terminator: {
+        ","
+        Int.parser()
         "\n"
     }
-    // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
+    let parser: some Parser<Substring, [(Int, Int)]> = Many {
+        line_parser
+    }
     let input = try parser.parse(data)
-    print(input)
-    let sum1 = part1()
-    let sum2 = part2()
+    let sum1 = part1(input)
+    let sum2 = part2(input)
     print("Part 1: \(sum1)")
     print("Part 2: \(sum2)")
 }
