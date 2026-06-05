5155edf35d47b11a85c6e50ba7e840f15131a7ce
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-06-05 09:49:38 +0900

    feat(Swift): complete y2025 day11

--- Swift/Sources/Y2025/day11.swift
+++ Swift/Sources/Y2025/day11.swift
@@ -1,26 +1,63 @@
 import Parsing
 
-private func part1() -> Int {
-    0
+private struct Path: Hashable {
+    let from: String
+    let to: String
+    init(_ f: String, _ t: String) {
+        from = f
+        to = t
+    }
 }
 
-private func part2() -> Int {
-    0
+private func countPath(_ dict: [String: [String]], from: String, to: String) -> Int {
+    var table: [Path: Int] = [:]
+    for p in dict {
+        for dist in p.1 {
+            table[Path(p.0, dist)] = 1
+        }
+    }
+    func get(from: String, to: String) -> Int {
+        if let value = table[Path(from, to)] {
+            return value
+        }
+        let count = dict[from, default: []].map({ get(from: $0, to: to) }).reduce(0, +)
+        table[Path(from, to)] = count
+        return count
+    }
+    return get(from: from, to: to)
+}
+
+private func part1(_ dict: [String: [String]]) -> Int {
+    return countPath(dict, from: "you", to: "out")
+}
+
+private func part2(_ dict: [String: [String]]) -> Int {
+    return
+        (countPath(dict, from: "svr", to: "fft")
+        * countPath(dict, from: "fft", to: "dac")
+        * countPath(dict, from: "dac", to: "out"))
+        + (countPath(dict, from: "svr", to: "dac")
+            * countPath(dict, from: "dac", to: "fft")
+            * countPath(dict, from: "fft", to: "out"))
 }
 
 public func day11(_ data: String) throws {
-    let parser: some Parser<Substring, [Int]> = Many {
-        Int.parser()
-    } separator: {
-        " "
-    } terminator: {
-        "\n"
+    let name: some Parser<Substring, String> = Prefix(3) { $0.isLowercase }.map(String.init)
+    let line: some Parser<Substring, (String, [String])> = Parse {
+        name
+        ": "
+        Many {
+            name
+        } separator: {
+            " "
+        } terminator: {
+            "\n"
+        }
     }
-    // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
-    let input = try parser.parse(data)
-    print(input)
-    let sum1 = part1()
-    let sum2 = part2()
+    let parser: some Parser<Substring, [(String, [String])]> = Many { line }
+    let dict: [String: [String]] = Dictionary(try parser.parse(data), uniquingKeysWith: +)
+    let sum1 = part1(dict)
+    let sum2 = part2(dict)
     print("Part 1: \(sum1)")
     print("Part 2: \(sum2)")
 }
