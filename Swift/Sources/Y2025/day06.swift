85f48f959b930630d7ce5d84d771dabda8c09c98
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-06-06 12:58:25 +0900

    feat(Swift): complete y2025 day6

--- Swift/Sources/Y2025/day06.swift
+++ Swift/Sources/Y2025/day06.swift
@@ -1,26 +1,45 @@
 import Parsing
 
-private func part1() -> Int {
-    0
-}
-
-private func part2() -> Int {
-    0
+private func rotateCCW<T>(_ m: [[T]]) -> [[T]] {
+    var t: [[T]] = Array(repeating: [], count: m[0].count)
+    for i in m.indices {
+        for j in m[i].indices {
+            t[j].append(m[i][j])
+        }
+    }
+    return t
 }
 
 public func day06(_ data: String) throws {
     let parser: some Parser<Substring, [Int]> = Many {
-        Int.parser()
-    } separator: {
-        " "
+        Parse {
+            Many { " " }
+            Int.parser()
+        }.map { $0.1 }
     } terminator: {
-        "\n"
+        Many { " " }
     }
-    // let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
-    let input = try parser.parse(data)
-    print(input)
-    let sum1 = part1()
-    let sum2 = part2()
+    let lines = Array(data.split(separator: "\n", omittingEmptySubsequences: true))
+    let ops = zip(Array(lines.last!), 0...).filter({ ["+", "*"].contains($0.0) })
+    let h = rotateCCW(try lines.dropLast().map({ try parser.parse($0) }))
+    let tmp1 = zip(ops.map({ $0.0 == "*" }), h)
+    let sum1 = tmp1.map({ mul, l in mul ? l.reduce(1, *) : l.reduce(0, +) }).reduce(0, +)
+    let vString: [String] = rotateCCW(lines.dropLast().map({ Array($0) })).map({
+        String($0).trimmingCharacters(in: .whitespaces)
+    })
+    var v: [[Int]] = []
+    var tmp: [Int] = []
+    for n in vString {
+        if n == "" {
+            v.append(tmp)
+            tmp = []
+        } else {
+            tmp.append(Int(String(n))!)
+        }
+    }
+    v.append(tmp)
+    let tmp2 = zip(ops.map({ $0.0 == "*" }), v)
+    let sum2 = tmp2.map({ mul, l in mul ? l.reduce(1, *) : l.reduce(0, +) }).reduce(0, +)
     print("Part 1: \(sum1)")
     print("Part 2: \(sum2)")
 }
