8335a4638c1ef9a83106ad7911db3cf21d8a2980
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-08-14 14:20:29 +0900

    feat(Lean4): add parser for Y2024/Day13

--- Lean4/Y2024/Day13.lean
+++ Lean4/Y2024/Day13.lean
@@ -13,21 +13,65 @@ namespace Y2024.Day13
 
 open Std
 
-/-- The input data. -/
+/-- The input data.
+  - buttonA : Nat × Nat
+  - buttonB : Nat × Nat
+  - prize : Nat × Nat
+-/
 structure Input where
+  buttonA : Nat × Nat
+  buttonB : Nat × Nat
+  prize : Nat × Nat
+
 deriving BEq, Hashable, Repr
 
-instance : ToString Input where toString _ := s!""
+instance : ToString Input where toString s := s!"{s.buttonA}, {s.buttonB}, {s.prize}\n"
 
+/- The input data format:
+
+```
+Button A: X+57, Y+16
+Button B: X+20, Y+74
+Prize: X=3288, Y=1772
+
+Button A: X+51, Y+75
+Button B: X+67, Y+15
+Prize: X=2803, Y=2535
+```
+-/
 namespace parser
 
 open WinnowParsers
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
-def parse : String → Option Input := AoCParser.parse parser
+/-- Parse "Button A: X+57, Y+16\n" -/
+def button (label : Char) : Parser (Nat × Nat) := do
+ let _ ← pstring "Button " *> pchar label *> pstring ": X+"
+ let x ← number <* pstring ", Y+"
+ let y ← number <* eol
+ pure (x, y)
+
+#guard AoCParser.parse (button 'A') "Button A: X+57, Y+16\n" == some (57, 16)
+
+/-- Parse "Prize: X=2803, Y=2535" -/
+def prize : Parser (Nat × Nat) := do
+ let x ← pstring "Prize: X=" *> number <* pstring ", Y="
+ let y ← number <* eol
+ pure (x, y)
+
+def block : Parser Input := do
+  let a ← button 'A'
+  let b ← button 'B'
+  let p ← prize
+  pure <| Input.mk a b p
+
+#guard AoCParser.parse prize "Prize: X=2803, Y=2535\n" == some (2803, 2535)
+
+def parse : String → Option (Array Input) := AoCParser.parse parser
   where
-    parser : Parser Input := return Input.mk
+    parser : Parser (Array Input) := do
+    separated block eol
 
 #guard AoCParser.parse number "123" == some 123
 
@@ -35,18 +79,18 @@ end parser
 
 namespace Part1
 
-def solve (_ : Input) : Nat := Id.run do 0
+def solve (_ : Array Input) : Nat := Id.run do 0
 
 end Part1
 
 namespace Part2
 
-def solve (_ : Input) : Nat := Id.run do 0
+def solve (_ : Array Input) : Nat := Id.run do 0
 
 end Part2
 
 public def solve := AocProblem.config 2024 13
-  ((CiCL.T dbg (fun data ↦ s!"parsed as {data}")) ∘ parser.parse)
+  ((CiCL.T dbg (fun data ↦ s!"got {data.unwrapOr #[] |>.size} items")) ∘ parser.parse)
   Part1.solve
   Part2.solve
 
