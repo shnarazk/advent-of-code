2ff0b9af05391dae0c1a7c9916577b84f322e904
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-08-16 11:40:46 +0900

    feat(Lean4): Y2024/Day15 part1

--- Lean4/Y2024/Day15.lean
+++ Lean4/Y2024/Day15.lean
@@ -28,29 +28,39 @@ instance : ToString Kind where
     | .empty => " "
     | .wall => "#"
     | .robot => "@"
-    | .box => "o"
-    | .boxH => "O"
+    | .box => "O"
+    | .boxH => "|"
 
 #guard s!"{Kind.robot}" == "@"
 
-/-- The status. -/
+/-- The status.
+- mapping : `Rect Kind`
+- moves : `Array Dir`
+-- - dir : `Dir`
+- pos : `Idx₂`
+- posHalf : `Bool`
+-/
 structure State where
   mapping : Rect Kind
   moves   : Array Dir
+  -- dir     : Dir
   pos     : Idx₂
   posHalf : Bool
 deriving BEq, Hashable
 
-namespace Input
-
-def new (ma : Array (Array Kind)) (mv : Array Dir) : State :=
-    State.mk (Rect.of2DMatrix ma) mv (default : Idx₂) false
-
-end Input
-
-
 instance : ToString State where
   toString s := s!"{s.mapping} {s.moves}"
+
+namespace State
+
+def new (ma : Array (Array Kind)) (mv : Array Dir) : State :=
+  let mapping : Rect Kind := Rect.of2DMatrix ma
+  let pos := mapping.findPosition? (· == Kind.robot) |>.unwrapOr (dbg "error" (default : Idx₂))
+  State.mk (mapping.set pos Kind.empty) mv pos false
+
+def dump (state : State) : Rect Kind := state.mapping.set state.pos Kind.robot
+
+end State
 
 namespace parser
 
@@ -73,6 +83,7 @@ open WinnowParsers
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
+/-- Convert a character to an inductive type. -/
 @[inline]
 public def parseAs {α : Type} (ch : Char) (a : α) : Parser α := pchar ch *> pure a
 
@@ -82,8 +93,7 @@ def parseKind : Parser Kind := do
   ('.' ?> Kind.empty) <|>
   ('#' ?> Kind.wall) <|>
   ('@' ?> Kind.robot) <|>
-  ('o' ?> Kind.box) <|>
-  ('O' ?> Kind.boxH)
+  ('O' ?> Kind.box)
 
 #guard AoCParser.parse parseKind "#" == some Kind.wall
 
@@ -99,17 +109,42 @@ def parseDir : Parser Dir := do
   ('v' ?> Dir.S) <|>
   ('<' ?> Dir.W)
 
-def parseMoves : Parser (Array Dir) := many1 parseDir <* eol
+def parseMoves : Parser (Array Dir) := Array.flatten <$> (many1 (many1 parseDir <* eol))
 
-#guard AoCParser.parse parseMoves "^>v\n" == some #[Dir.N, Dir.E, Dir.S]
+#guard AoCParser.parse parseMoves "^>v<\n" == some #[Dir.N, .E, .S, .W]
 
-def parse : String → Option State := AoCParser.parse (Input.new <$> parseGrid <*> parseMoves)
+def parse : String → Option State := AoCParser.parse (State.new <$> parseGrid <*> parseMoves)
 
 end parser
 
 namespace Part1
 
-def solve (_ : State) : Nat := Id.run do 0
+def press (state : State) : State := Id.run do
+  let some dir :=  state.moves[0]? | return state
+  let moves := state.moves.drop 1
+  let some next := toIdx₂ ((↑ state.pos : Vec₂) + dir.asVec₂) | return dbg "error" state
+  let mut p := next
+  while state.mapping.get? p == some Kind.box do
+    let some q := toIdx₂ ((↑ p : Vec₂) + dir.asVec₂) | return dbg "error" state
+    p := q
+  match state.mapping.get? p with
+    | some .empty =>
+      return { state with
+        mapping := state.mapping.set p Kind.box |>.set next Kind.empty
+        moves := moves
+        pos := next }
+    | some .wall => return { state with moves := moves }
+    | _ => return { state with moves := moves }
+
+def evaluate (state : State) : Nat :=
+  state.mapping.enum
+    |>.map (fun e ↦ let (p, k) := e ; if k == Kind.box then p.fst * 100 + p.snd else 0)
+    |>.sum
+
+def solve (state : State) : Nat := Id.run do
+  let mut s := state
+  while ! s.moves.isEmpty do s := press s
+  return (evaluate s)
 
 end Part1
 
