03d1107afff2cba3ff5f0d00c5f8be27d2a3e4ea
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-08-21 11:28:30 +0900

    exp: a bad snapshot

--- Lean4/Y2024/Day12.lean
+++ Lean4/Y2024/Day12.lean
@@ -11,6 +11,35 @@ namespace Y2024.Day12
 
 open Std Dim2 CiCL
 
+structure RectHash (t : Type) where
+  hashmap :Std.HashMap Int t
+  height: Int
+  width: Int
+deriving BEq, Repr
+
+def RectHash.new (t : Type) (h w : Int) : RectHash t :=
+  RectHash.mk Std.HashMap.emptyWithCapacity h w
+
+instance RectHash.isGetElem (t : Type) :
+    GetElem? (RectHash t) Vec₂ t (fun h i ↦ i.fst * h.width + i.snd ∈ h.hashmap) where
+  getElem? self i := self.hashmap[i.fst * self.width + i.snd]?
+  getElem self i p := self.hashmap.get (i.fst * self.width + i.snd) p
+
+@[inline]
+def RectHash.set {t : Type} (self : @&RectHash t) (i : Vec₂) (k : t) : RectHash t :=
+  { self with hashmap := self.hashmap.insert (i.fst * self.width + i.snd) k }
+
+@[inline]
+def RectHash.erase {t : Type} (self : @&RectHash t) (i : Vec₂) : RectHash t :=
+  { self with hashmap := self.hashmap.erase (i.fst * self.width + i.snd) }
+
+def RectHash.of2DArray {t : Type} (a : Array (Array t)) : RectHash t := Id.run do
+  let mut h := RectHash.new t a.size (a.iter.map (·.size) |>.sum)
+  for (i, l) in a.iter.enumerate do
+    for (j, v) in l.iter.enumerate do
+      h := h.set (i, j) v
+  h
+
 structure Input where
   grid : Rect Char
 deriving BEq
@@ -23,49 +52,49 @@ open WinnowParsers
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
-def parse : String → Option Input := AoCParser.parse parser
+def parse : String → Option (RectHash Char) := AoCParser.parse parser
   where
-    parser : Parser Input := do
+    parser : Parser (RectHash Char) := do
       let grid ← separated alphabets eol
-      return Input.mk (Rect.of2DMatrix (grid.map (·.toList.toArray)))
+      return RectHash.of2DArray (grid.map (·.toList.toArray))
 
 end parser
 
 namespace Part1
 
-def evaluate_at (accum' : Rect Bool) (grid : Rect Char) (i j : Nat) : Rect Bool × Nat := Id.run do
-  if accum'.get (i, j) true then return (accum', 0)
-  let some ch := grid.get? (i, j) | return (accum', 0)
+def evaluate_at (accum' : RectHash Bool) (grid : RectHash Char) (i j : Int) : RectHash Bool × Nat := Id.run do
+  if accum'[(i, j)]? == some true then return (accum', 0)
+  let some ch := grid[(i, j)]? | return (accum', 0)
   let mut accum := accum'
   let mut count := 0
-  let mut checked : Rect Bool := accum.map (K false)
-  if checked.geometory != grid.geometory then return (accum, dbg "!!!" 0)
+  let mut checked : RectHash Bool := RectHash.mk (accum.hashmap.map (fun _ _ ↦ false)) accum.height accum.width
+  -- if checked.geometory != grid.geometory then return (accum, dbg "!!!" 0)
   let mut to_visit := [(i, j)]
   let mut seg_h : HashSet Vec₂ := HashSet.emptyWithCapacity 100
   let mut seg_v : HashSet Vec₂ := HashSet.emptyWithCapacity 100
   while !to_visit.isEmpty do
     let p :: to_visit' := to_visit | continue
     to_visit := to_visit'
-    if checked.get p true then continue
+    if checked[p]? == some true then continue
     checked := checked.set p true
-    if grid.get? p == some ch then
+    if grid[p]? == some ch then
       count := count + 1
       accum := accum.set p true
-      if p.fst == 0 || grid.get? (p.fst - 1, p.snd) != some ch then seg_h := seg_h.insert p
-      if               grid.get? (p.fst + 1, p.snd) != some ch then seg_h := seg_h.insert (p.fst + 1, p.snd)
-      if p.snd == 0 || grid.get? (p.fst, p.snd - 1) != some ch then seg_v := seg_v.insert p
-      if               grid.get? (p.fst, p.snd + 1) != some ch then seg_v := seg_v.insert (p.fst, p.snd + 1)
+      if p.fst == 0 || grid[(p.fst - 1, p.snd)]? != some ch then seg_h := seg_h.insert p
+      if               grid[(p.fst + 1, p.snd)]? != some ch then seg_h := seg_h.insert (p.fst + 1, p.snd)
+      if p.snd == 0 || grid[(p.fst, p.snd - 1)]? != some ch then seg_v := seg_v.insert p
+      if               grid[(p.fst, p.snd + 1)]? != some ch then seg_v := seg_v.insert (p.fst, p.snd + 1)
       for dir in [Dir.N, Dir.E, Dir.S, Dir.W] do
-        let some q := p + dir | continue
-        if grid.validIndex? q then to_visit := to_visit.concat q
+        let q := p + dir
+        if grid[q]?.isSome then to_visit := to_visit.concat q
   (accum, count * (seg_v.size + seg_h.size))
 
-def solve (input : Input) : Nat := Id.run do
-  let mut accum := input.grid.map (K false)
+def solve (input : RectHash Char) : Nat := Id.run do
+  let mut accum := RectHash.mk (input.hashmap.map (fun _ _ ↦ false)) input.height input.width
   let mut sum := 0
-  for h in 0 ... input.grid.height do
-    for w in 0 ... input.grid.width do
-      let (accum', c) := evaluate_at accum input.grid h w
+  for h in 0 ... input.height do
+    for w in 0 ... input.width do
+      let (accum', c) := evaluate_at accum input w h
       accum := accum'
       sum := sum + c
   return sum
@@ -74,12 +103,12 @@ end Part1
 
 namespace Part2
 
-instance : Ord (Nat × Bool) where
+instance : Ord (Int × Bool) where
   compare a b := match compare a.fst b.fst with
   | Ordering.eq => compare a.snd b.snd
   | cmp => cmp
 
-def count_sides (hash : HashMap Nat (Array (Nat × Bool))) : Nat :=
+def count_sides (hash : HashMap Int (Array (Int × Bool))) : Nat :=
   hash.values.iter
     |>.map (fun v' ↦ Id.run do
       let v := v'.qsortOrd
@@ -93,52 +122,52 @@ def count_sides (hash : HashMap Nat (Array (Nat × Bool))) : Nat :=
       count)
     |>.sum
 
-def evaluate_at (accum' : Rect Bool) (grid : Rect Char) (i j : Nat) : Rect Bool × Nat := Id.run do
-  if accum'.get (i, j) true then return (accum', 0)
-  let some ch := grid.get? (i, j) | return (accum', 0)
+def evaluate_at (accum' : @&RectHash Bool) (grid : @&RectHash Char) (i j : Int) : RectHash Bool × Nat := Id.run do
+  if accum'[(i, j)]? == some true then return (accum', 0)
+  let some ch := grid[(i, j)]? | return (accum', 0)
   let mut accum := accum'
   let mut count := 0
-  let mut checked : Rect Bool := accum.map (K false)
-  if checked.geometory != grid.geometory then return (accum, dbg "!!!" 0)
+  let mut checked : RectHash Bool := RectHash.mk (accum.hashmap.map (fun _ _ ↦ false)) accum.height accum.width
+  -- if checked.geometory != grid.geometory then return (accum, dbg "!!!" 0)
   let mut to_visit := [(i, j)]
-  let mut seg_h : HashSet ((Nat × Nat) × Bool) := HashSet.emptyWithCapacity 100
-  let mut seg_v : HashSet ((Nat × Nat) × Bool) := HashSet.emptyWithCapacity 100
+  let mut seg_h : HashMap (Vec₂ × Bool) Unit := HashMap.emptyWithCapacity 100
+  let mut seg_v : HashMap (Vec₂ × Bool) Unit := HashMap.emptyWithCapacity 100
   while !to_visit.isEmpty do
     let p :: to_visit' := to_visit | continue
     to_visit := to_visit'
-    if checked.get p true then continue
+    if checked[p]? == some true then continue
     checked := checked.set p true
-    if grid.get? p == some ch then
+    if grid[p]? == some ch then
       count := count + 1
       accum := accum.set p true
-      if p.fst == 0 || grid.get? (p.fst - 1, p.snd) != some ch then seg_h := seg_h.insert (p, false)
-      if grid.get? (p.fst + 1, p.snd) != some ch then seg_h := seg_h.insert ((p.fst + 1, p.snd), true)
-      if p.snd == 0 || grid.get? (p.fst, p.snd - 1) != some ch then seg_v := seg_v.insert (p, false)
-      if grid.get? (p.fst, p.snd + 1) != some ch then seg_v := seg_v.insert ((p.fst, p.snd + 1), true)
+      if p.fst == 0 || grid[(p.fst - 1, p.snd)]? != some ch then seg_h := seg_h.insert (p, false) ()
+      if grid[(p.fst + 1, p.snd)]? != some ch then seg_h := seg_h.insert ((p.fst + 1, p.snd), true) ()
+      if p.snd == 0 || grid[(p.fst, p.snd - 1)]? != some ch then seg_v := seg_v.insert (p, false) ()
+      if grid[(p.fst, p.snd + 1)]? != some ch then seg_v := seg_v.insert ((p.fst, p.snd + 1), true) ()
       for dir in [Dir.N, Dir.E, Dir.S, Dir.W] do
-        let some q := p + dir | continue
-        if grid.validIndex? q then to_visit := to_visit.concat q
+        let q := p + dir
+        if grid[q]?.isSome then to_visit := to_visit.concat q
   -- build longer segments
-  let hss : HashMap Nat (Array (Nat × Bool)) := seg_h.iter
+  let hss : HashMap Int (Array (Int × Bool)) := seg_h.keysIter
     |>.fold
-      (fun (acc : HashMap Nat (Array (Nat × Bool))) ps ↦
+      (fun (acc : HashMap Int (Array (Int × Bool))) ps ↦
         let (pos, spin) := ps
         acc.alter pos.fst (fun ol ↦ some <| (ol.unwrapOr #[]).push (pos.snd, spin)))
       (HashMap.emptyWithCapacity 100)
-  let vss : HashMap Nat (Array (Nat × Bool)) := seg_v.iter
+  let vss : HashMap Int (Array (Int × Bool)) := seg_v.keysIter
     |>.fold
-      (fun (acc : HashMap Nat (Array (Nat × Bool))) ps ↦
+      (fun (acc : HashMap Int (Array (Int × Bool))) ps ↦
         let (pos, spin) := ps
         acc.alter pos.snd (fun ol ↦ some <| (ol.unwrapOr #[]).push (pos.fst, spin)))
       (HashMap.emptyWithCapacity 100)
   (accum, count * (count_sides hss + count_sides vss))
 
-def solve (input : Input) : Nat := Id.run do
-  let mut accum := input.grid.map (K false)
+def solve (input : RectHash Char) : Nat := Id.run do
+  let mut accum := RectHash.mk (input.hashmap.map (fun _ _ ↦ false)) input.height input.width
   let mut sum := 0
-  for h in 0 ... input.grid.height do
-    for w in 0 ... input.grid.width do
-      let (accum', c) := evaluate_at accum input.grid h w
+  for h in 0 ... input.height do
+    for w in 0 ... input.width do
+      let (accum', c) := evaluate_at accum input h w
       accum := accum'
       sum := sum + c
   return sum
