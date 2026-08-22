f29484506b81be570f1bcfcfbbabea851c09fda0
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-08-22 23:29:54 +0900

    chore(Lean4:Y2025/Day09): not use Idx_2

--- Lean4/Y2025/Day09.lean
+++ Lean4/Y2025/Day09.lean
@@ -18,35 +18,29 @@ def dist (a b : Int) : Nat := (if a ≤ b then b - a else a - b).toNat
 /-- return one plus the distance between a and b -/
 def dist₁ (a b : Int) : Nat := (1 + if a ≤ b then b - a else a - b).toNat
 
-structure Input where
-  line : Array Idx₂
-deriving BEq, Hashable, Repr
-
-instance : ToString Input where toString s := s!"{s.line.size}"
-
 namespace parser
 
 open WinnowParsers
 open Std.Internal.Parsec
 open Std.Internal.Parsec.String
 
-def parse_line := (fun (a b : Nat) ↦ (↑(b, a) : Idx₂)) <$> number <* pchar ',' <*> number
+def parse_line := (fun (a b : Nat) ↦ (b, a)) <$> number <* pchar ',' <*> number
 def parse_input := separated parse_line eol
 
-def parse : String → Option Input := AoCParser.parse parser
+def parse : String → Option (Array (Nat × Nat)) := AoCParser.parse parser
   where
-    parser : Parser Input := Input.mk <$> parse_input
+    parser : Parser (Array (Nat × Nat)) := parse_input
 
 end parser
 
 namespace Part1
 
-def solve (input : Input) : Nat :=
-  input.line.iter
+def solve (input : Array (Nat × Nat)) : Nat :=
+  input.iter
     |>.enumerate
     |>.map (fun (i, p) ↦
-      input.line.iterFromIdx (i + 1)
-        |>.map (fun (q : Idx₂) ↦ (dist₁ p.val.1 q.val.1) * (dist₁ p.val.2 q.val.2))
+      input.iterFromIdx (i + 1)
+        |>.map (fun (q : Nat × Nat) ↦ (dist₁ p.1 q.1) * (dist₁ p.2 q.2))
         |>.fold max 0)
     |>.fold max 0
 
@@ -60,18 +54,18 @@ namespace Part2
   - 1 : corner
   - 2 : filled
   - 3 : unvisited -/
-def solve (input : Input) : Nat := Id.run do
-  let ys := input.line.iter |>.map (·.val.fst) |>.toList |> HashSet.ofList |>.toArray |>.push 0 |>.qsort
+def solve (input : Array (Nat × Nat)) : Nat := Id.run do
+  let ys := input.iter |>.map (·.fst) |>.toList |> HashSet.ofList |>.toArray |>.push 0 |>.qsort
   let encodeY := ys.iter.enumerate.map (fun (i, d) ↦ (d, i)) |>.toList |> HashMap.ofList
   if encodeY.size == 0 then return 0
-  let xs := input.line.iter |>.map (·.val.snd) |>.toList |> HashSet.ofList |>.toArray |>.push 0 |>.qsort
+  let xs := input.iter |>.map (·.snd) |>.toList |> HashSet.ofList |>.toArray |>.push 0 |>.qsort
   let encodeX := xs.iter.enumerate.map (fun (i, d) ↦ (d, i)) |>.toList |> HashMap.ofList
   if encodeX.size == 0 then return 0
   let mut sliceY : HashMap Nat (Array Nat) := HashMap.emptyWithCapacity 100
   let mut sliceX : HashMap Nat (Array Nat) := HashMap.emptyWithCapacity 100
-  for p in input.line.iter do
-    sliceY := sliceY.alter p.fst.toNat (fun o ↦ o.mapOr (·.push p.snd.toNat) #[p.snd.toNat])
-    sliceX := sliceX.alter p.snd.toNat (fun o ↦ o.mapOr (·.push p.fst.toNat) #[p.fst.toNat])
+  for p in input.iter do
+    sliceY := sliceY.alter p.fst (fun o ↦ o.mapOr (·.push p.snd) #[p.snd])
+    sliceX := sliceX.alter p.snd (fun o ↦ o.mapOr (·.push p.fst) #[p.fst])
   sliceY := sliceY.iter |>.map (fun (k, v) ↦ (k, v.qsort)) |>.toList |> HashMap.ofList
   if sliceY.size == 0 then return 0
   sliceX := sliceX.iter |>.map (fun (k, v) ↦ (k, v.qsort)) |>.toList |> HashMap.ofList
