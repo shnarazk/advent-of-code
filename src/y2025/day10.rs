48070d5e7402f570733ead67b8b473d9d2abcebb
Narazaki, Shuji <shujinarazaki@protonmail.com>
2026-08-27 08:36:39 +0900

    chore: rebase on the latest main

--- src/y2025/day10.rs
+++ src/y2025/day10.rs
@@ -3,7 +3,12 @@ use {
     crate::framework::{AdventOfCode, ParseError, aoc},
     microlp::{ComparisonOp, OptimizationDirection, Problem, Variable},
     rayon::prelude::*,
-    std::collections::HashSet,
+    rustc_data_structures::fx::{FxHashSet, FxHasher},
+    std::{
+        cmp::{Ordering, Reverse},
+        collections::{BinaryHeap, HashSet},
+        hash::BuildHasherDefault,
+    },
 };
 
 type Spec = (Vec<bool>, Vec<Vec<usize>>, Vec<usize>);
@@ -95,7 +100,13 @@ impl AdventOfCode for Puzzle {
     fn part2(&mut self) -> Self::Output2 {
         self.line
             .par_iter()
-            .map(|(_, buttons, goal)| solve(buttons, goal))
+            .map(|(_, buttons, goal)| {
+                if true {
+                    solve2(buttons, goal)
+                } else {
+                    solve(buttons, goal)
+                }
+            })
             .sum::<usize>()
     }
 }
@@ -125,4 +136,114 @@ fn solve(buttons: &[Vec<usize>], goals: &[usize]) -> usize {
         .map(|b| solution[*b])
         .map(|f| f.round() as usize)
         .sum::<usize>()
+}
+
+#[derive(Clone, Debug, Default, Eq, Hash, PartialEq)]
+struct State {
+    remain: usize,
+    counts: Vec<usize>,
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.remain.partial_cmp(&other.remain)
+    }
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.remain.cmp(&other.remain)
+    }
+}
+
+impl State {
+    fn sum(&self, buttons: &[Vec<usize>], n: usize) -> Vec<usize> {
+        let mut result = vec![0; n];
+        for (bi, n) in self.counts.iter().enumerate() {
+            for i in buttons[bi].iter() {
+                result[*i] += *n as usize;
+            }
+        }
+        result
+    }
+}
+
+fn exceeds(values: &[usize], dist: &[usize], goal: &[usize]) -> bool {
+    assert_eq!(values.len(), goal.len());
+    values
+        .iter()
+        .enumerate()
+        .any(|(i, n)| dist.contains(&(i as usize)) as usize + n > goal[i])
+}
+
+fn solve2(buttons: &[Vec<usize>], goals: &[usize]) -> usize {
+    let goal = goals.iter().map(|n| *n as usize).collect::<Vec<_>>();
+    // let scale: usize = 10000;
+    let mut counter = 0;
+    let num_buttons = buttons.len();
+    let goal_len = goal.len();
+    // let button_weight: Vec<usize> =
+    //     buttons.iter().map(|l| l.len()).collect::<Vec<usize>>();
+    // let mut distribution = buttons.clone();
+    // distribution.sort_unstable_by_key(|l| l.len());
+    // distribution.reverse();
+    let mut visited: FxHashSet<Vec<usize>> = HashSet::<_, BuildHasherDefault<FxHasher>>::default();
+    let mut to_visit: BinaryHeap<Reverse<State>> = BinaryHeap::new();
+    to_visit.push(Reverse(State {
+        remain: goal.iter().map(|d| *d).sum::<usize>(),
+        counts: vec![0; num_buttons],
+    }));
+    while let Some(Reverse(state)) = to_visit.pop() {
+        let values = state.sum(buttons, goal_len);
+        if values == *goal {
+            return dbg!(state.counts.into_iter().sum::<usize>());
+        }
+        if state.remain == 0 || visited.contains(&values) {
+            continue;
+        }
+        let sum = state.counts.iter().sum::<usize>();
+        if counter < sum {
+            counter = sum;
+            dbg!(&state);
+        }
+        // assert_eq!(values.len(), goal.len());
+        for (bi, distribution) in buttons.iter().enumerate() {
+            if exceeds(&values, &distribution, &goal) {
+                continue;
+            }
+            let mut s = state.clone();
+            s.counts[bi] += 1;
+            // if s.remain < distribution.len() {
+            //     panic!(
+            //         "{:?} < {:?} :: {:?}, {}: {:?}",
+            //         s, goal, buttons, bi, distribution
+            //     );
+            // }
+            let v = s.sum(buttons, goal_len);
+            s.remain = goal
+                            .iter()
+                            .zip(v.iter())
+                            .map(|(a, b)| (*a - *b).pow(1) + 1)
+                            .product::<usize>()
+                        // * goal
+                        //     .iter()
+                        //     .zip(v.iter())
+                        //     .filter(|(a, b)| *a != *b)
+                        //     .count()
+                            // / s.counts
+                            //     .iter()
+                            //     .enumerate()
+                            //     .map(|(i, c)| *c * button_weight[i])
+                            //     .sum::<usize>();
+                            // * s.counts
+                            //     .iter()
+                            //     .sum::<usize>().isqrt();
+                            ;
+            if !visited.contains(&v) {
+                to_visit.push(Reverse(s));
+            }
+        }
+        visited.insert(values);
+    }
+    unreachable!()
 }
